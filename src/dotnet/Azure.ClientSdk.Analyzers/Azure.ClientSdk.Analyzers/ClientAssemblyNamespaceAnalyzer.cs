dcb70e686092038c142afce9b2cf8564fd47203a
Pavel Krymets <pavel@krymets.com>
2019-05-03 13:03:30 -0700

    Update namespace list

--- src/dotnet/Azure.ClientSdk.Analyzers/Azure.ClientSdk.Analyzers/ClientAssemblyNamespaceAnalyzer.cs
+++ src/dotnet/Azure.ClientSdk.Analyzers/Azure.ClientSdk.Analyzers/ClientAssemblyNamespaceAnalyzer.cs
@@ -12,14 +12,14 @@ namespace Azure.ClientSdk.Analyzers
     {
         internal static readonly string[] AllowedNamespacePrefix = new[]
         {
-            "Azure.Diagnostics",
-            "Azure.Cognitive",
+            "Azure.Analytics",
+            "Azure.Data",
             "Azure.Iot",
-            "Azure.Networking",
-            "Azure.Runtime",
+            "Azure.Media",
+            "Azure.Messaging",
+            "Azure.ML",
             "Azure.Security",
-            "Azure.Storage",
-            "Azure.ApplicationModel"
+            "Azure.Storage"
         };
 
         public ClientAssemblyNamespaceAnalyzer()

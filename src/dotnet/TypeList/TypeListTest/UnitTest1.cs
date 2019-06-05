edffa9abd0859c14d36c86c4fb75a4c589cf706c
McCoy Patino Middaugh <t-mcpat@microsoft.com>
2019-06-04 17:11:47 -0700

    Start establishing testing strategy

--- src/dotnet/TypeList/TypeListTest/UnitTest1.cs
+++ src/dotnet/TypeList/TypeListTest/UnitTest1.cs
@@ -5,6 +5,14 @@ namespace TypeListTest
 {
     public class UnitTest1
     {
+        /**
+         * Testing strategy for each type variant in TypeList:
+         * 
+         * 
+         * 
+         * Covering each part of each input/output partition.
+         */
+
         [Fact]
         public void Test1()
         {

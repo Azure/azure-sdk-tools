b3c69796fe1355359f0e81d2e0cffec1a0206682
Arpan Laha <arpanlaha99@gmail.com>
2019-06-06 16:07:57 -0700

    add header to processTests.js

--- tests/lib/utils/processTests.js
+++ tests/lib/utils/processTests.js
@@ -1,3 +1,10 @@
+/**
+ * @fileoverview Helper to feed in processor information to RuleTester.
+ * @author Arpan Laha
+ */
+
+"use strict";
+
 var processJSON = require("../../../lib/index").processors[".json"];
 
 module.exports = function(fileName) {

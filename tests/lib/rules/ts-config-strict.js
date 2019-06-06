82d82910c3d7adc9aa3f4249ce7e04c100ac79e9
Arpan Laha <arpanlaha99@gmail.com>
2019-06-06 16:23:50 -0700

    more comprehensive tests (still need to implement robust structural tests)

--- tests/lib/rules/ts-config-strict.js
+++ tests/lib/rules/ts-config-strict.js
@@ -7,7 +7,95 @@
 
 var rule = require("../../../lib/rules/ts-config-strict");
 var RuleTester = require("eslint").RuleTester;
-var processJSON = require("../../../lib/index").processors[".json"];
+var processJSONFile = require("../utils/processTests");
+
+//------------------------------------------------------------------------------
+// Example files
+//------------------------------------------------------------------------------
+
+const example_tsconfig_good = `{
+  "compilerOptions": {
+    /* Basic Options */
+    "target": "es6" /* Specify ECMAScript target version: 'ES3' (default), 'ES5', 'ES2015', 'ES2016', 'ES2017','ES2018' or 'ESNEXT'. */,
+    "module": "es6" /* Specify module code generation: 'none', 'commonjs', 'amd', 'system', 'umd', 'es2015', or 'ESNext'. */,
+
+    "declaration": true /* Generates corresponding '.d.ts' file. */,
+    "declarationMap": true /* Generates a sourcemap for each corresponding '.d.ts' file. */,
+    "sourceMap": true /* Generates corresponding '.map' file. */,
+
+    "outDir": "./dist-esm" /* Redirect output structure to the directory. */,
+    "declarationDir": "./typings" /* Output directory for generated declaration files.*/,
+
+    "importHelpers": true /* Import emit helpers from 'tslib'. */,
+
+    /* Strict Type-Checking Options */
+    "strict": true /* Enable all strict type-checking options. */,
+    "noImplicitReturns": true /* Report error when not all code paths in function return a value. */,
+
+    /* Additional Checks */
+    "noUnusedLocals": true /* Report errors on unused locals. */,
+
+    /* Module Resolution Options */
+    "moduleResolution": "node" /* Specify module resolution strategy: 'node' (Node.js) or 'classic' (TypeScript pre-1.6). */,
+    "allowSyntheticDefaultImports": true /* Allow default imports from modules with no default export. This does not affect code emit, just typechecking. */,
+    "esModuleInterop": true /* Enables emit interoperability between CommonJS and ES Modules via creation of namespace objects for all imports. Implies 'allowSyntheticDefaultImports'. */,
+
+    /* Experimental Options */
+    "forceConsistentCasingInFileNames": true,
+
+    /* Other options */
+    "newLine": "LF" /*	Use the specified end of line sequence to be used when emitting files: "crlf" (windows) or "lf" (unix).”*/,
+    "allowJs": false /* Don't allow JavaScript files to be compiled.*/,
+    "resolveJsonModule": true
+  },
+  "compileOnSave": true,
+  "exclude": ["node_modules", "typings/**", "./samples/**/*.ts"],
+  "include": ["./src/**/*.ts", "./test/**/*.ts"]
+}`;
+
+const example_tsconfig_bad = `{
+  "compilerOptions": {
+    /* Basic Options */
+    "target": "es6" /* Specify ECMAScript target version: 'ES3' (default), 'ES5', 'ES2015', 'ES2016', 'ES2017','ES2018' or 'ESNEXT'. */,
+    "module": "es6" /* Specify module code generation: 'none', 'commonjs', 'amd', 'system', 'umd', 'es2015', or 'ESNext'. */,
+
+    "declaration": true /* Generates corresponding '.d.ts' file. */,
+    "declarationMap": true /* Generates a sourcemap for each corresponding '.d.ts' file. */,
+    "sourceMap": true /* Generates corresponding '.map' file. */,
+
+    "outDir": "./dist-esm" /* Redirect output structure to the directory. */,
+    "declarationDir": "./typings" /* Output directory for generated declaration files.*/,
+
+    "importHelpers": true /* Import emit helpers from 'tslib'. */,
+
+    /* Strict Type-Checking Options */
+    "strict": false /* Enable all strict type-checking options. */,
+    "noImplicitReturns": true /* Report error when not all code paths in function return a value. */,
+
+    /* Additional Checks */
+    "noUnusedLocals": true /* Report errors on unused locals. */,
+
+    /* Module Resolution Options */
+    "moduleResolution": "node" /* Specify module resolution strategy: 'node' (Node.js) or 'classic' (TypeScript pre-1.6). */,
+    "allowSyntheticDefaultImports": true /* Allow default imports from modules with no default export. This does not affect code emit, just typechecking. */,
+    "esModuleInterop": true /* Enables emit interoperability between CommonJS and ES Modules via creation of namespace objects for all imports. Implies 'allowSyntheticDefaultImports'. */,
+
+    /* Experimental Options */
+    "forceConsistentCasingInFileNames": true,
+
+    /* Other options */
+    "newLine": "LF" /*	Use the specified end of line sequence to be used when emitting files: "crlf" (windows) or "lf" (unix).”*/,
+    "allowJs": false /* Don't allow JavaScript files to be compiled.*/,
+    "resolveJsonModule": true
+  },
+  "compileOnSave": true,
+  "exclude": ["node_modules", "typings/**", "./samples/**/*.ts"],
+  "include": ["./src/**/*.ts", "./test/**/*.ts"]
+}`;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
 
 var ruleTester = new RuleTester({
   parser: "@typescript-eslint/parser",
@@ -17,14 +105,43 @@ var ruleTester = new RuleTester({
 ruleTester.run("ts-config-strict", rule, {
   valid: [
     {
+      // only the fields we care about
       code: '{"compilerOptions": { "strict": true }}',
-      filename: Object.assign(processJSON, { filename: "tsconfig.json" }) // this is stupid but it works
+      filename: processJSONFile("tsconfig.json") // this is stupid but it works
+    },
+    {
+      // a full example tsconfig.json (taken from https://github.com/Azure/azure-sdk-for-js/blob/master/sdk/eventhub/event-hubs/tsconfig.json)
+      code: example_tsconfig_good,
+      filename: processJSONFile("tsconfig.json")
+    },
+    {
+      // incorrect format but in a file we don't care about
+      code: '{"compilerOptions": { "strict": false }}',
+      filename: processJSONFile("not_tsconfig.json")
     }
   ],
   invalid: [
-    // {
-    //   code: '{"compilerOptions": { "strict": false }}',
-    //   filename: "tsconfig.json"
-    // }
+    {
+      // only the fields we care about
+      code: '{"compilerOptions": { "strict": false }}',
+      filename: processJSONFile("tsconfig.json"),
+      errors: [
+        {
+          message:
+            "tsconfig.json: compilerOptions.strict is set to false when it should be set to true"
+        }
+      ]
+    },
+    {
+      // example file with compilerOptions.strict set to false
+      code: example_tsconfig_bad,
+      filename: processJSONFile("tsconfig.json"),
+      errors: [
+        {
+          message:
+            "tsconfig.json: compilerOptions.strict is set to false when it should be set to true"
+        }
+      ]
+    }
   ]
 });

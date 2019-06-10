9e6782c444c8a05ded547141a43f8bd2a75868da
Arpan Laha <arpanlaha99@gmail.com>
2019-06-10 16:51:37 -0700

    finish testing of current processor functionality

--- tests/processors.ts
+++ tests/processors.ts
@@ -18,5 +18,38 @@ describe("plugin", function() {
         "processors is not a member of the plugin"
       );
     });
+    it(".json should be a member of processors", function() {
+      assert.property(
+        plugin.processors,
+        ".json",
+        ".json is not a member of processors"
+      );
+    });
+    it("preprocess should be a member of .json", function() {
+      assert.property(
+        plugin.processors[".json"],
+        "preprocess",
+        "preprocess is not a member of .json"
+      );
+    });
+    it("postprocess should be a member of .json", function() {
+      assert.property(
+        plugin.processors[".json"],
+        "postprocess",
+        "postprocess is not a member of .json"
+      );
+    });
   });
+  describe("preprocess", function() {
+    const preprocess = plugin.processors[".json"].preprocess;
+    it("preprocess should prepend 'const json = ' to the input and return its singleton array", function() {
+      const input = "input";
+      const output = preprocess(input);
+      assert.isArray(output, "preprocess should always return an array");
+      assert.strictEqual(output[0], "const json = input");
+    });
+  });
+  /**
+   * TODO: Implement postprocess tests after functionality complete
+   */
 });

28ff40c8fed0b3c840517dcc8dde1e01a0dc67fc
Arpan Laha <arpanlaha99@gmail.com>
2019-06-11 14:06:32 -0700

    add processJSON.ts (forgot to add in previous commit)

---
+++ tests/utils/processJSON.ts
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview Helper to feed in processor information to RuleTester.
+ * @author Arpan Laha
+ */
+
+import plugin from "../../src";
+import { Linter } from "eslint";
+
+interface Info {
+  preprocess: (text: string) => string[];
+  postprocess: (messages: Linter.LintMessage[][]) => Linter.LintMessage[];
+  filename: string;
+}
+
+export = function(fileName: string): Info {
+  return { ...plugin.processors[".json"], filename: fileName };
+};

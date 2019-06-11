8dfc928c97e184a3e7392a053deb613e44c17695
Arpan Laha <arpanlaha99@gmail.com>
2019-06-11 15:41:42 -0700

    fix casing

--- src/rules/ts-config-forceconsistentcasinginfilenames.ts
+++ src/rules/ts-config-forceconsistentcasinginfilenames.ts
@@ -20,7 +20,7 @@ export = {
       category: "Best Practices",
       recommended: true,
       url:
-        "https://azuresdkspecs.z5.web.core.windows.net/TypeScriptSpec.html#ts-config-forceConsistentCasingInFileNames"
+        "https://azuresdkspecs.z5.web.core.windows.net/TypeScriptSpec.html#ts-config-forceconsistentcasinginfilenames"
     },
     schema: [] // no options
   },

6527f17aa2e206a6755ef3ae78e4118b392becc6
Arpan Laha <arpanlaha99@gmail.com>
2019-06-11 14:55:28 -0700

    add rules to recommended config

--- src/configs.ts
+++ src/configs.ts
@@ -13,8 +13,11 @@ export = {
       "azure/ts-config-module": "error",
       "azure/ts-config-no-experimentaldecorators": "error",
       "azure/ts-config-strict": "error",
+      "azure/ts-package-json-author": "error",
+      "azure/ts-package-json-bugs": "error",
+      "azure/ts-package-json-license": "error",
       "azure/ts-package-json-repo": "error",
-      "azure/ts-package-json-bugs": "error"
+      "azure/ts-package-json-sideeffects": "error"
     }
   }
 };

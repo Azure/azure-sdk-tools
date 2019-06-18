5b1defbdf5d2a8d248453f2f68c97a54368791bf
Arpan Laha <arpanlaha99@gmail.com>
2019-06-18 15:59:51 -0700

    add ts-modules-no-default

--- src/ts/eslint-plugin-azure-sdk/src/configs/index.ts
+++ src/ts/eslint-plugin-azure-sdk/src/configs/index.ts
@@ -24,6 +24,7 @@ export = {
       "@ts-common/azure-sdk/ts-config-no-experimentaldecorators": "error",
       "@ts-common/azure-sdk/ts-config-sourcemap": "error",
       "@ts-common/azure-sdk/ts-config-strict": "error",
+      "@ts-common/azure-sdk/ts-modules-no-default": "error",
       "@ts-common/azure-sdk/ts-package-json-author": "error",
       "@ts-common/azure-sdk/ts-package-json-bugs": "error",
       "@ts-common/azure-sdk/ts-package-json-homepage": "error",

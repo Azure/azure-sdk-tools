d1ed5ca824355879724a3fc08dc10a514dcf9111
Arpan Laha <arpanlaha99@gmail.com>
2019-06-25 15:53:24 -0700

    add github-source-headers tests

---
+++ tools/eslint-plugin-azure-sdk/tests/rules/github-source-headers.ts
@@ -0,0 +1,84 @@
+/**
+ * @fileoverview Testing the github-source-headers rule.
+ * @author Arpan Laha
+ */
+
+import rule from "../../src/rules/github-source-headers";
+import { RuleTester } from "eslint";
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json"
+  }
+});
+
+const valid = `
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+console.log("hello")`;
+
+const invalid1 = `
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+console.log("hello")`;
+
+const invalid2 = `
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the Apache 2.0 License.
+console.log("hello")`;
+
+const configError = `copyright header not properly configured - expected value:
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.
+`;
+
+ruleTester.run("github-source-headers", rule, {
+  valid: [
+    {
+      // only the fields we care about
+      code: valid,
+      filename: "test.ts"
+    },
+    {
+      // incorrect format but in a file we don't care about
+      code: 'console.log("hello")',
+      filename: "test.js"
+    }
+  ],
+  invalid: [
+    {
+      // no comments
+      code: 'console.log("hello")',
+      filename: "test.ts",
+      errors: [
+        {
+          message: "no copyright header found"
+        }
+      ]
+    },
+    // wrong headers
+    {
+      code: invalid1,
+      filename: "test.ts",
+      errors: [
+        {
+          message: configError
+        }
+      ]
+    },
+    {
+      code: invalid2,
+      filename: "test.ts",
+      errors: [
+        {
+          message: configError
+        }
+      ]
+    }
+  ]
+});

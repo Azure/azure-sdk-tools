3ecf7376c5ed799a3bfbd4a41aa03f1850c2fb2f
Arpan Laha <arpanlaha99@gmail.com>
2019-06-25 10:03:47 -0700

    add ts-package-json-main-is-cjs tests

--- tools/eslint-plugin-azure-sdk/src/configs/index.ts
+++ tools/eslint-plugin-azure-sdk/src/configs/index.ts
@@ -35,7 +35,7 @@ export = {
       "@ts-common/azure-sdk/ts-package-json-homepage": "error",
       "@ts-common/azure-sdk/ts-package-json-keywords": "error",
       "@ts-common/azure-sdk/ts-package-json-license": "error",
-      "@ts-common/azure/sdk/ts-package-json-main-is-cjs": "error",
+      "@ts-common/azure-sdk/ts-package-json-main-is-cjs": "error",
       "@ts-common/azure-sdk/ts-package-json-name": "error",
       "@ts-common/azure-sdk/ts-package-json-repo": "error",
       "@ts-common/azure-sdk/ts-package-json-required-scripts": "error",

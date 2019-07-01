0c58b9aeedee5f8182f7251ff2801a54c19ab2f0
Arpan Laha <arpanlaha99@gmail.com>
2019-07-01 09:56:23 -0700

    switch off ts-error-handling

--- tools/eslint-plugin-azure-sdk/src/configs/index.ts
+++ tools/eslint-plugin-azure-sdk/src/configs/index.ts
@@ -27,7 +27,7 @@ export = {
       "@ts-common/azure-sdk/ts-config-sourcemap": "error",
       "@ts-common/azure-sdk/ts-config-strict": "error",
       "@ts-common/azure-sdk/ts-config-target": "error",
-      "@ts-common/azure-sdk/ts-error-handling": "error",
+      "@ts-common/azure-sdk/ts-error-handling": "off",
       "@ts-common/azure-sdk/ts-modules-only-named": "error",
       "@ts-common/azure-sdk/ts-package-json-author": "error",
       "@ts-common/azure-sdk/ts-package-json-bugs": "error",

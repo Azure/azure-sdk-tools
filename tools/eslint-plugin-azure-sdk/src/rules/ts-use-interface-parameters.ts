e9aec51531fdb629f3b8b35a9085ba58e6c3f6d2
Arpan Laha <arpanlaha99@gmail.com>
2019-07-02 09:19:57 -0700

    add overload functionality to ts-use-interface-parameters

--- tools/eslint-plugin-azure-sdk/src/rules/ts-use-interface-parameters.ts
+++ tools/eslint-plugin-azure-sdk/src/rules/ts-use-interface-parameters.ts
@@ -7,16 +7,28 @@ import { Rule } from "eslint";
 import {
   ArrowFunctionExpression,
   AssignmentPattern,
+  BlockStatement,
+  ClassBody,
   FunctionDeclaration,
   FunctionExpression,
   Identifier,
-  Pattern
+  Node,
+  MethodDefinition,
+  Pattern,
+  Program,
+  VariableDeclarator
 } from "estree";
 import { SymbolFlags, TypeChecker } from "typescript";
 
 //------------------------------------------------------------------------------
 // Rule Definition
 //------------------------------------------------------------------------------
+
+type FunctionType =
+  | FunctionExpression
+  | FunctionDeclaration
+  | ArrowFunctionExpression;
+type BodyNodeType = BlockStatement | ClassBody | Program;
 
 export = {
   meta: {
@@ -33,10 +45,45 @@ export = {
     schema: [] // no options
   },
   create: (context: Rule.RuleContext): Rule.RuleListener => {
+    const verified: string[] = [];
+    let name = "";
     return {
-      ":function": (
-        node: FunctionExpression | FunctionDeclaration | ArrowFunctionExpression
-      ): void => {
+      ":function": (node: FunctionType): void => {
+        const ancestors = context.getAncestors().reverse();
+        switch (node.type) {
+          case "FunctionExpression": {
+            const parent:
+              | MethodDefinition
+              | VariableDeclarator = ancestors[0] as
+              | MethodDefinition
+              | VariableDeclarator;
+            if (parent.type === "MethodDefinition") {
+              const key: Identifier = parent.key as Identifier;
+              name = key.name;
+            } else {
+              // VariableDeclarator
+              const id: Identifier = parent.id as Identifier;
+              name = id.name;
+            }
+            break;
+          }
+          case "FunctionDeclaration": {
+            const id: Identifier = node.id as Identifier;
+            name = id.name;
+            break;
+          }
+          case "ArrowFunctionExpression": {
+            const parent: VariableDeclarator = ancestors[0] as VariableDeclarator;
+            const id: Identifier = parent.id as Identifier;
+            name = id.name;
+            break;
+          }
+        }
+
+        if (verified.includes(name)) {
+          return;
+        }
+
         node.params.forEach((param: Pattern): void => {
           let identifier = param;
           if (param.type === "AssignmentPattern") {
@@ -49,17 +96,88 @@ export = {
           const TSNode = parserServices.esTreeNodeToTSNodeMap.get(identifier);
           const type = typeChecker.getTypeAtLocation(TSNode);
           const symbol = type.getSymbol();
-          symbol &&
-            symbol.getFlags() === SymbolFlags.Class &&
-            context.report({
-              node: identifier,
-              message:
-                "type {{ type }} of parameter {{ param }} is a class, not an interface",
-              data: {
-                type: typeChecker.typeToString(type),
-                param: identifier.name
+
+          if (symbol === undefined || symbol.getFlags() !== SymbolFlags.Class) {
+            return;
+          }
+
+          let bodyNode: BodyNodeType = ancestors.find(
+            (ancestor: Node): boolean => {
+              return ["BlockStatement", "ClassBody", "Program"].includes(
+                ancestor.type
+              );
+            }
+          ) as BodyNodeType;
+
+          let overloads: FunctionType[] = [];
+          if (node.type === "FunctionExpression") {
+            const parent: Node = ancestors[0];
+            if (parent.type === "MethodDefinition") {
+              bodyNode = bodyNode as ClassBody;
+              overloads = bodyNode.body
+                .filter((methodDefinition: MethodDefinition): boolean => {
+                  const functionExpression = methodDefinition.value;
+                  return functionExpression.params !== node.params;
+                })
+                .map(
+                  (methodDefinition: MethodDefinition): FunctionExpression => {
+                    return methodDefinition.value;
+                  }
+                );
+            }
+          } else if (node.type === "FunctionDeclaration") {
+            bodyNode = bodyNode as BlockStatement | Program;
+            overloads = bodyNode.body.filter((element: Node): boolean => {
+              if (element.type !== "FunctionDeclaration") {
+                return false;
               }
-            });
+              const functionDeclaration = element as FunctionDeclaration;
+              return functionDeclaration.params !== node.params;
+            }) as FunctionDeclaration[];
+          }
+
+          const interfacesOnly = overloads.find(
+            (overload: FunctionType): boolean => {
+              return (
+                overload.params.find((overloadParam: Pattern): boolean => {
+                  let overloadIdentifier = overloadParam;
+                  if (overloadParam.type === "AssignmentPattern") {
+                    const assignmentPattern: AssignmentPattern = overloadParam as AssignmentPattern;
+                    overloadIdentifier = assignmentPattern.left;
+                  }
+                  overloadIdentifier = overloadIdentifier as Identifier;
+                  const overloadTSNode = parserServices.esTreeNodeToTSNodeMap.get(
+                    overloadIdentifier
+                  );
+                  const overloadType = typeChecker.getTypeAtLocation(
+                    overloadTSNode
+                  );
+
+                  const overloadSymbol = overloadType.getSymbol();
+
+                  return (
+                    overloadSymbol !== undefined &&
+                    overloadSymbol.getFlags() === SymbolFlags.Class
+                  );
+                }) === undefined
+              );
+            }
+          );
+
+          if (overloads.length !== 0 && interfacesOnly) {
+            verified.push(name);
+            return;
+          }
+
+          context.report({
+            node: identifier,
+            message:
+              "type {{ type }} of parameter {{ param }} is a class, not an interface",
+            data: {
+              type: typeChecker.typeToString(type),
+              param: identifier.name
+            }
+          });
         });
       }
     } as Rule.RuleListener;
--- tools/eslint-plugin-azure-sdk/tests/rules/ts-use-interface-parameters.ts
+++ tools/eslint-plugin-azure-sdk/tests/rules/ts-use-interface-parameters.ts
@@ -14,6 +14,10 @@ const example = `class A {
   message: string
 }
 
+class A2 {
+  message: string
+}
+
 interface B {
   message: string
 }
@@ -49,37 +53,60 @@ ruleTester.run("ts-use-interface-parameters", rule, {
     // single parameter
     {
       // arrow function expression
-      code: example + "const func = (b: B): void => { console.log(b); }"
+      code: example + "const func1 = (b: B): void => { console.log(b); }"
     },
     {
       // function expression
-      code: example + "const func = function(b: B): void { console.log(b); }"
+      code: example + "const func2 = function(b: B): void { console.log(b); }"
     },
     {
-      // function expression
-      code: example + "function func(b: B): void { console.log(b); }"
+      // function declaration
+      code: example + "function func3(b: B): void { console.log(b); }"
+    },
+    {
+      // class method
+      code: example + "class C { method1(b: B): void { console.log(b); } }"
     },
     // multiple parameters
     {
       // arrow function expression
-      code: example + "const func = (b1: B, b2: B): void => { console.log(b); }"
+      code:
+        example + "const func4 = (b1: B, b2: B): void => { console.log(b); }"
     },
     {
       // function expression
       code:
         example +
-        "const func = function(b1: B, b2: B): void { console.log(b); }"
+        "const func5 = function(b1: B, b2: B): void { console.log(b); }"
     },
     {
-      // function expression
-      code: example + "function func(b1: B, b2: B): void { console.log(b); }"
+      // function declaration
+      code: example + "function func6(b1: B, b2: B): void { console.log(b); }"
+    },
+    {
+      // class method
+      code:
+        example + "class C { method2(b1: B, b2: B): void { console.log(b); } }"
+    },
+    // overloads
+    {
+      // class methods
+      code:
+        example +
+        "class C { overloadMethod(a: A): void { console.log(a); }; overloadMethod(b: B): void { console.log(b); }; }"
+    },
+    {
+      // function declaration
+      code:
+        example +
+        "function overloadDeclaration(a: A): void { console.log(a); }; function overloadDeclaration(b: B): void { console.log(b); }"
     }
   ],
   invalid: [
     // single parameter
     {
       // arrow function expression
-      code: example + "const func = (a: A): void => { console.log(a); }",
+      code: example + "const func7 = (a: A): void => { console.log(a); }",
       errors: [
         {
           message: "type A of parameter a is a class, not an interface"
@@ -88,7 +115,7 @@ ruleTester.run("ts-use-interface-parameters", rule, {
     },
     {
       // function expression
-      code: example + "const func = function(a: A): void { console.log(a); }",
+      code: example + "const func8 = function(a: A): void { console.log(a); }",
       errors: [
         {
           message: "type A of parameter a is a class, not an interface"
@@ -97,7 +124,16 @@ ruleTester.run("ts-use-interface-parameters", rule, {
     },
     {
       // function declaration
-      code: example + "function func(a: A): void { console.log(a); }",
+      code: example + "function func9(a: A): void { console.log(a); }",
+      errors: [
+        {
+          message: "type A of parameter a is a class, not an interface"
+        }
+      ]
+    },
+    {
+      // class method
+      code: example + "class { method3(a: A): void { console.log(a); } }",
       errors: [
         {
           message: "type A of parameter a is a class, not an interface"
@@ -108,7 +144,7 @@ ruleTester.run("ts-use-interface-parameters", rule, {
     {
       // arrow function expression
       code:
-        example + "const func = (a: A, b: B): void => { console.log(a, b); }",
+        example + "const func10 = (a: A, b: B): void => { console.log(a, b); }",
       errors: [
         {
           message: "type A of parameter a is a class, not an interface"
@@ -119,7 +155,7 @@ ruleTester.run("ts-use-interface-parameters", rule, {
       // function expression
       code:
         example +
-        "const func = function(a: A, b: B): void { console.log(a, b); }",
+        "const func11 = function(a: A, b: B): void { console.log(a, b); }",
       errors: [
         {
           message: "type A of parameter a is a class, not an interface"
@@ -128,7 +164,18 @@ ruleTester.run("ts-use-interface-parameters", rule, {
     },
     {
       // function declaration
-      code: example + "function func(a: A, b: B): void { console.log(a, b); }",
+      code:
+        example + "function func12(a: A, b: B): void { console.log(a, b); }",
+      errors: [
+        {
+          message: "type A of parameter a is a class, not an interface"
+        }
+      ]
+    },
+    {
+      // class method
+      code:
+        example + "class { method4(a: A, b: B): void { console.log(a, b); } }",
       errors: [
         {
           message: "type A of parameter a is a class, not an interface"
@@ -140,7 +187,7 @@ ruleTester.run("ts-use-interface-parameters", rule, {
       // arrow function expression
       code:
         example +
-        "const func = (a1: A, a2: A): void => { console.log(a1, a2); }",
+        "const func13 = (a1: A, a2: A): void => { console.log(a1, a2); }",
       errors: [
         {
           message: "type A of parameter a1 is a class, not an interface"
@@ -154,7 +201,7 @@ ruleTester.run("ts-use-interface-parameters", rule, {
       // function expression
       code:
         example +
-        "const func = function(a1: A, a2: A): void { console.log(a1, a2); }",
+        "const func14 = function(a1: A, a2: A): void { console.log(a1, a2); }",
       errors: [
         {
           message: "type A of parameter a1 is a class, not an interface"
@@ -167,8 +214,58 @@ ruleTester.run("ts-use-interface-parameters", rule, {
     {
       // function declaration
       code:
-        example + "function func(a1: A, a2: A): void { console.log(a1, a2); }",
+        example +
+        "function func15(a1: A, a2: A): void { console.log(a1, a2); }",
       errors: [
+        {
+          message: "type A of parameter a1 is a class, not an interface"
+        },
+        {
+          message: "type A of parameter a2 is a class, not an interface"
+        }
+      ]
+    },
+    {
+      // class method
+      code:
+        example +
+        "class { method3(a1: A, a2: A): void { console.log(a1, a2); } }",
+      errors: [
+        {
+          message: "type A of parameter a1 is a class, not an interface"
+        },
+        {
+          message: "type A of parameter a2 is a class, not an interface"
+        }
+      ]
+    },
+    // bad overloads
+    {
+      // class methods
+      code:
+        example +
+        "class C { overloadMethodBad(a: A): void { console.log(a); } overloadMethodBad(a1: A, a2: A): void { console.log(a1, a2); }; }",
+      errors: [
+        {
+          message: "type A of parameter a is a class, not an interface"
+        },
+        {
+          message: "type A of parameter a1 is a class, not an interface"
+        },
+        {
+          message: "type A of parameter a2 is a class, not an interface"
+        }
+      ]
+    },
+    {
+      // function declaration
+      code:
+        example +
+        "function overloadDeclarationBad(a: A): void { console.log(a); } function overloadDeclarationBad(a1: A, a2: A): void { console.log(a1, a2); }",
+      errors: [
+        {
+          message: "type A of parameter a is a class, not an interface"
+        },
         {
           message: "type A of parameter a1 is a class, not an interface"
         },

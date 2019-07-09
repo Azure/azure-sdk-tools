46a51a824b24b6ea71a5af2c59a02194a4099695
git@github.com <annelo@microsoft.com>
2019-07-09 09:48:58 -0700

    Update test with PR comments.

--- src/dotnet/Azure.ClientSdk.Analyzers/Azure.ClientSdk.Analyzers.Tests/AZC0008Tests.cs
+++ src/dotnet/Azure.ClientSdk.Analyzers/Azure.ClientSdk.Analyzers.Tests/AZC0008Tests.cs
@@ -45,13 +45,11 @@ namespace RandomNamespace
 }
 ");
             var diagnostics = await _runner.GetDiagnosticsAsync(testSource.Source);
-
             if (diagnostics == null)
             {
                 return;
             }
-
-            Assert.True(diagnostics.Where(d => d.Id == "AZC0008").FirstOrDefault() == null);
+            Assert.Empty(diagnostics.Where(d => d.Id == "AZC0008"));
         }
     }
 }

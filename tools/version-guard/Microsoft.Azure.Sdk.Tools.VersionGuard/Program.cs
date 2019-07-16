4c3136deddefd8a04b989b9f87123ce7bd1b623d
Mitch Denny <midenn@microsoft.com>
2019-07-16 14:02:23 -0700

    Address warnings causing build failure.

--- tools/version-guard/Microsoft.Azure.Sdk.Tools.VersionGuard/Program.cs
+++ tools/version-guard/Microsoft.Azure.Sdk.Tools.VersionGuard/Program.cs
@@ -4,11 +4,16 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Sdk.Tools.VersionGuard
 {
+    /// <summary>Program entry point for Version Guard.</summary>
     public class Program
     {
+        /// <summary>Version Guard</summary>
+        /// <param name="packageName">Name of the package to scan for.</param>
+        /// <param name="packageVersion">Version of the package being proposed.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
         public static async Task<int> Main(string packageName, string packageVersion, CancellationToken cancellationToken)
         {
-            return 0;
+            return await Task.FromResult(0);
         }
     }
 }

9cb5d0825e14ab110d161516d5b17cae73a5534f
Mitch Denny <midenn@microsoft.com>
2019-07-16 17:15:07 -0700

    WIP: Still building things out.

---
+++ tools/version-guard/Azure.Sdk.Tools.VersionGuard/PackageRegistry.cs
@@ -0,0 +1,15 @@
+namespace Azure.Sdk.Tools.VersionGuard
+{
+    /// <summary>Package registry that version guard supports.</summary>
+    public enum PackageRegistry
+    {
+        /// <summary>Maven</summary>
+        Maven,
+        /// <summary>NPM</summary>
+        Npm,
+        /// <summary>NuGet</summary>
+        NuGet,
+        /// <summary>PiPy</summary>
+        PiPy
+    }
+}
--- tools/version-guard/Azure.Sdk.Tools.VersionGuard/Program.cs
+++ tools/version-guard/Azure.Sdk.Tools.VersionGuard/Program.cs
@@ -8,12 +8,16 @@ namespace Azure.Sdk.Tools.VersionGuard
     public class Program
     {
         /// <summary>Version Guard</summary>
+        /// <param name="packageRegistry">Name of the package to scan for.</param>
         /// <param name="packageName">Name of the package to scan for.</param>
         /// <param name="packageVersion">Version of the package being proposed.</param>
         /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
-        public static async Task<int> Main(string packageName, string packageVersion, CancellationToken cancellationToken)
+        public static async Task<int> Main(PackageRegistry packageRegistry, string packageName, string packageVersion, CancellationToken cancellationToken)
         {
-            return await Task.FromResult(0);
+            if (string.IsNullOrEmpty(packageName)) throw new ArgumentNullException(nameof(packageName));
+            if (string.IsNullOrEmpty(packageVersion)) throw new ArgumentNullException(nameof(packageName));
+
+            return await Task.FromResult(1);
         }
     }
 }

6ab2d31d4daf1b3701e7aa7b5d16ec83fcb82787
Arpan Laha <arpanlaha99@gmail.com>
2019-07-22 15:06:45 -0700

    fix errors

--- tools/eslint-plugin-azure-sdk/src/rules/ts-no-const-enums.ts
+++ tools/eslint-plugin-azure-sdk/src/rules/ts-no-const-enums.ts
@@ -22,7 +22,7 @@ export = {
 
           // check Enum to make sure it doesn't have a const keyword
           TSEnumDeclaration: (node: any): void => {
-            if (node.const) {
+            if (node.const !== undefined) {
               context.report({
                 node: node,
                 message: "const enums should not be used"

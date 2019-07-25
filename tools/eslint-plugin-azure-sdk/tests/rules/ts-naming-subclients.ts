f20fbd06cfdfb2c7a2d07ac8fd70ebb6ea315978
Arpan Laha <arpanlaha99@gmail.com>
2019-07-25 10:14:03 -0700

    add ts-naming-subclients tests

---
+++ tools/eslint-plugin-azure-sdk/tests/rules/ts-naming-subclients.ts
@@ -0,0 +1,62 @@
+/**
+ * @fileoverview Testing the ts-naming-subclients rule.
+ * @author Arpan Laha
+ */
+
+import rule from "../../src/rules/ts-naming-subclients";
+import { RuleTester } from "eslint";
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json"
+  }
+});
+
+ruleTester.run("ts-naming-subclients", rule, {
+  valid: [
+    // normal valid example
+    {
+      code: "class ExampleClient { getSubClient(): SubClient {}; };"
+    },
+    // bad name but returning the same class
+    {
+      code: "class ExampleClient { getExampleClient(): ExampleClient {}; };"
+    },
+    // bad name in a non-client class
+    {
+      code: "class Example { get(): SubClient {}; };"
+    }
+  ],
+  invalid: [
+    // single
+    {
+      code: "class ExampleClient { get(): SubClient {}; };",
+      errors: [
+        {
+          message:
+            "ExampleClient's method get returns a subclient of type SubClient and should be called getSubClient"
+        }
+      ]
+    },
+    // multiple
+    {
+      code:
+        "class ExampleClient { get(): SubClient {}; get2(): SubSubClient {}; };",
+      errors: [
+        {
+          message:
+            "ExampleClient's method get returns a subclient of type SubClient and should be called getSubClient"
+        },
+        {
+          message:
+            "ExampleClient's method get2 returns a subclient of type SubSubClient and should be called getSubSubClient"
+        }
+      ]
+    }
+  ]
+});

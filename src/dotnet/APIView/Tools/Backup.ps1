a8e075c34aa5bf61a9ccec04d17739929fa395d5
McCoy Patiño <t-mcpat@microsoft.com>
2019-08-16 14:51:45 -0700

    Organize files, add PowerShell scripts

---
+++ src/dotnet/APIView/Tools/Backup.ps1
@@ -0,0 +1,12 @@
+﻿$acct = "apiview"
+$assemblies_container = "assemblies"
+$comments_container = "comments"
+$sas_token = Read-Host -Prompt "SAS token for $($acct)"
+
+if (-Not (Test-Path .\azcopy.exe)) {
+    Invoke-WebRequest https://azcopyvnext.azureedge.net/release20190517/azcopy_windows_amd64_10.1.2.zip -OutFile dl.zip
+    Expand-Archive dl.zip
+    cp .\dl\*\azcopy.exe .
+}
+./azcopy copy "https://$($acct).blob.core.windows.net/$($assemblies_container)$($sas_token)" "." --recursive
+./azcopy copy "https://$($acct).blob.core.windows.net/$($comments_container)$($sas_token)" "." --recursive
---
+++ src/dotnet/APIView/Tools/Restore.ps1
@@ -0,0 +1,16 @@
+﻿$acct = "apiview"
+$assemblies_container = "assemblies"
+$comments_container = "comments"
+$sas_token = Read-Host -Prompt "SAS token for $($acct)"
+
+if (-Not (Test-Path .\azcopy.exe)) {
+    Invoke-WebRequest https://azcopyvnext.azureedge.net/release20190517/azcopy_windows_amd64_10.1.2.zip -OutFile dl.zip
+    Expand-Archive dl.zip
+    cp .\dl\*\azcopy.exe .
+}
+
+./azcopy make "https://$($acct).blob.core.windows.net/$($assemblies_container)$($sas_token)"
+./azcopy make "https://$($acct).blob.core.windows.net/$($comments_container)$($sas_token)"
+
+./azcopy copy "./$($assemblies_container)/*" "https://$($acct).blob.core.windows.net/$($assemblies_container)$($sas_token)" --recursive
+./azcopy copy "./$($comments_container)/*" "https://$($acct).blob.core.windows.net/$($comments_container)$($sas_token)" --recursive

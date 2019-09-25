01ce77f635f5c3569e956f5c139496eb11e46c61
Jonathan Giles <jonathan@jonathangiles.net>
2019-09-25 14:13:35 +1200

    Initial import of the existing APIView java processor code

---
+++ src/java/apiview-java-processor/src/main/java/com/azure/tools/apiview/processor/model/TokenKind.java
@@ -0,0 +1,29 @@
+package com.azure.tools.apiview.processor.model;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum TokenKind {
+    TEXT(0),
+    NEW_LINE(1),
+    WHITESPACE(2),
+    PUNCTUATION(3),
+    KEYWORD(4),
+
+    // use this if there are no visible tokens with ID on the line but you still want to be able to leave a comment for it
+    LINE_ID_MARKER(5),
+
+    TYPE_NAME(6),
+    MEMBER_NAME(7),
+    STRING_LITERAL(8);
+
+    private final int id;
+
+    TokenKind(int id) {
+        this.id = id;
+    }
+
+    @JsonValue
+    public int getId() {
+        return id;
+    }
+}

e37f48396c18d39af6ca5c077a50293c17cd49d3
Alexander Sher <alsher@microsoft.com>
2019-11-01 15:05:01 -0500

    Add ConfigureAwait(false) static analyzer

---
+++ src/dotnet/Azure.ClientSdk.Analyzers/Azure.ClientSdk.Analyzers.Tests/AZC0011Tests.cs
@@ -0,0 +1,225 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Azure.ClientSdk.Analyzers.Tests 
+{
+    public class AZC0011Tests 
+    {
+        private readonly DiagnosticAnalyzerRunner _runner = new DiagnosticAnalyzerRunner(new AddConfigureAwaitAnalyzer());
+
+        [Fact]
+        public async Task AZC0011WarningOnTask()
+        {
+            var testSource = TestSource.Read(@"
+namespace RandomNamespace
+{
+    public class MyClass
+    {
+        public static async System.Threading.Tasks.Task Foo()
+        {
+            /*MM*/await System.Threading.Tasks.Task.Run(() => {});
+        }
+    }
+}
+");
+            var diagnostics = await _runner.GetDiagnosticsAsync(testSource.Source);
+
+            var diagnostic = Assert.Single(diagnostics);
+
+            Assert.Equal("AZC0011", diagnostic.Id);
+            AnalyzerAssert.DiagnosticLocation(testSource.DefaultMarkerLocation, diagnostics[0].Location);
+        }
+
+        [Fact]
+        public async Task AZC0011WarningOnTaskOfT() 
+        {
+            var testSource = TestSource.Read(@"
+namespace RandomNamespace
+{
+    public class MyClass
+    {
+        public static async System.Threading.Tasks.Task Foo()
+        {
+            var i = /*MM*/await System.Threading.Tasks.Task.Run(() => 42);
+        }
+    }
+}
+");
+            var diagnostics = await _runner.GetDiagnosticsAsync(testSource.Source);
+
+            var diagnostic = Assert.Single(diagnostics);
+
+            Assert.Equal("AZC0011", diagnostic.Id);
+            AnalyzerAssert.DiagnosticLocation(testSource.DefaultMarkerLocation, diagnostics[0].Location);
+        }
+
+        [Fact]
+        public async Task AZC0011WarningOnValueTask() 
+        {
+            var testSource = TestSource.Read(@"
+namespace RandomNamespace
+{
+    public class MyClass
+    {
+        private static int _x;
+
+        public static async System.Threading.Tasks.ValueTask Foo()
+        {
+            /*MM*/await RunAsync();
+        }
+
+        private static async System.Threading.Tasks.ValueTask RunAsync()
+        {
+            _x++;
+        }
+    }
+}
+");
+            var diagnostics = await _runner.GetDiagnosticsAsync(testSource.Source);
+
+            var diagnostic = Assert.Single(diagnostics);
+
+            Assert.Equal("AZC0011", diagnostic.Id);
+            AnalyzerAssert.DiagnosticLocation(testSource.DefaultMarkerLocation, diagnostics[0].Location);
+        }
+
+        [Fact]
+        public async Task AZC0011WarningOnValueTaskOfT() 
+        {
+            var testSource = TestSource.Read(@"
+namespace RandomNamespace
+{
+    public class MyClass
+    {
+        public static async System.Threading.Tasks.ValueTask Foo()
+        {
+            var i = /*MM*/await GetValueAsync();
+        }
+
+        private static async System.Threading.Tasks.ValueTask<int> GetValueAsync()
+        {
+            return 0;
+        }
+    }
+}
+");
+            var diagnostics = await _runner.GetDiagnosticsAsync(testSource.Source);
+
+            var diagnostic = Assert.Single(diagnostics);
+
+            Assert.Equal("AZC0011", diagnostic.Id);
+            AnalyzerAssert.DiagnosticLocation(testSource.DefaultMarkerLocation, diagnostics[0].Location);
+        }
+
+        [Fact(Skip = "It isn't clear if ConfigureAwait(true) should be treated as error")]
+        public async Task AZC0011WarningOnExistingConfigureAwaitTrue()
+        {
+            var testSource = TestSource.Read(@"
+namespace RandomNamespace
+{
+    public class MyClass
+    {
+        public static async System.Threading.Tasks.Task Foo()
+        {
+           /*MM*/await System.Threading.Tasks.Task.Run(() => {}).ConfigureAwait(true);
+        }
+    }
+}
+");
+            var diagnostics = await _runner.GetDiagnosticsAsync(testSource.Source);
+            var diagnostic = Assert.Single(diagnostics);
+
+            Assert.Equal("AZC0011", diagnostic.Id);
+            AnalyzerAssert.DiagnosticLocation(testSource.DefaultMarkerLocation, diagnostics[0].Location);
+        }
+
+        [Fact]
+        public async Task AZC0011NoWarningOnExistingConfigureAwaitFalse()
+        {
+            var testSource = TestSource.Read(@"
+namespace RandomNamespace
+{
+    public class MyClass
+    {
+        public static async System.Threading.Tasks.Task Foo()
+        {
+           await System.Threading.Tasks.Task.Run(() => {}).ConfigureAwait(false);
+        }
+    }
+}
+");
+            var diagnostics = await _runner.GetDiagnosticsAsync(testSource.Source);
+            Assert.Empty(diagnostics);
+        }
+
+        [Fact]
+        public async Task AZC0011WarningOnTaskDelay()
+        {
+            var testSource = TestSource.Read(@"
+namespace RandomNamespace
+{
+    public class MyClass
+    {
+        public static async System.Threading.Tasks.Task Foo()
+        {
+            /*MM*/await System.Threading.Tasks.Task.Delay(42);
+        }
+    }
+}
+");
+            var diagnostics = await _runner.GetDiagnosticsAsync(testSource.Source);
+
+            var diagnostic = Assert.Single(diagnostics);
+
+            Assert.Equal("AZC0011", diagnostic.Id);
+            AnalyzerAssert.DiagnosticLocation(testSource.DefaultMarkerLocation, diagnostics[0].Location);
+        }
+
+        [Fact]
+        public async Task AZC0011NoWarningOnTaskYield()
+        {
+            var testSource = TestSource.Read(@"
+namespace RandomNamespace
+{
+    public class MyClass
+    {
+        public static async System.Threading.Tasks.Task Foo()
+        {
+           await System.Threading.Tasks.Task.Yield();
+        }
+    }
+}
+");
+            var diagnostics = await _runner.GetDiagnosticsAsync(testSource.Source);
+            Assert.Empty(diagnostics);
+        }
+
+        [Fact]
+        public async Task AZC0011WarningOnVariable()
+        {
+            var testSource = TestSource.Read(@"
+namespace RandomNamespace
+{
+    public class MyClass
+    {
+        public static async System.Threading.Tasks.Task Foo()
+        {
+            var task = System.Threading.Tasks.Task.Run(() => {});
+            /*MM*/await task;
+        }
+    }
+}
+");
+            var diagnostics = await _runner.GetDiagnosticsAsync(testSource.Source);
+
+            var diagnostic = Assert.Single(diagnostics);
+
+            Assert.Equal("AZC0011", diagnostic.Id);
+            AnalyzerAssert.DiagnosticLocation(testSource.DefaultMarkerLocation, diagnostics[0].Location);
+        }
+    }
+}
---
+++ src/dotnet/Azure.ClientSdk.Analyzers/Azure.ClientSdk.Analyzers/AddConfigureAwaitAnalyzer.cs
@@ -0,0 +1,96 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Azure.ClientSdk.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class AddConfigureAwaitAnalyzer : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
+            ImmutableArray.Create(Descriptors.AZC0011);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AnalyzeAwaitExpression, SyntaxKind.AwaitExpression);
+        }
+
+        private void AnalyzeAwaitExpression(SyntaxNodeAnalysisContext context)
+        {
+            var invocation = (AwaitExpressionSyntax)context.Node;
+            var operation = context.SemanticModel.GetOperation(invocation, context.CancellationToken);
+            if (!(operation is IAwaitOperation awaitOperation)) {
+                return;
+            }
+
+            if (awaitOperation.Operation is IInvocationOperation configureAwaitOperation)
+            {
+                if (IsConfigureAwait(configureAwaitOperation.TargetMethod, context.Compilation))
+                {
+                    return;
+                }
+
+                if (!IsTaskType(configureAwaitOperation.Type, context.Compilation))
+                {
+                    return;
+                }
+            }
+
+            var location = awaitOperation.Syntax.GetLocation();
+            var diagnostic = Diagnostic.Create(Descriptors.AZC0011, location);
+            context.ReportDiagnostic(diagnostic);
+        }
+
+        private static bool IsConfigureAwait(IMethodSymbol method, Compilation compilation) 
+            => method.Name == "ConfigureAwait" && IsTaskType(method.ReceiverType, compilation);
+
+        private static bool IsTaskType(ITypeSymbol type, Compilation compilation) 
+        {
+            if (type == null) 
+            {
+                return false;
+            }
+
+            var (task, taskOfT, valueTask, valueTaskOfT) = GetTaskTypes(compilation);
+
+            if (task == null || taskOfT == null)
+            {
+                return false;
+            }
+
+            if (type.Equals(task) || type.Equals(valueTask)) 
+            {
+                return true;
+            }
+
+            var originalDefinition = type.OriginalDefinition;
+            if (Equals(originalDefinition, taskOfT) || Equals(originalDefinition, valueTaskOfT)) 
+            {
+                return true;
+            }
+
+            if (type.TypeKind == TypeKind.Error) 
+            {
+                return type.Name.Equals("Task") || type.Name.Equals("ValueTask");
+            }
+
+            return false;
+        }
+
+        private static (INamedTypeSymbol task, INamedTypeSymbol taskOfT, INamedTypeSymbol valueTask, INamedTypeSymbol valueTaskOfT) GetTaskTypes(Compilation compilation) =>
+        (
+            compilation.GetTypeByMetadataName(typeof(Task).FullName),
+            compilation.GetTypeByMetadataName(typeof(Task<>).FullName),
+            compilation.GetTypeByMetadataName("System.Threading.Tasks.ValueTask"),
+            compilation.GetTypeByMetadataName("System.Threading.Tasks.ValueTask`1")
+        );
+    }
+}

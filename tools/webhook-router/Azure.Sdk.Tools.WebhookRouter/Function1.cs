6e5a28a7e83c3e81195de6bf61013dc0507561cf
Mitch Denny <midenn@microsoft.com>
2020-04-10 19:51:16 +1000

    Test end to end deployment.

--- tools/webhook-router/Azure.Sdk.Tools.WebhookRouter/Function1.cs
+++ tools/webhook-router/Azure.Sdk.Tools.WebhookRouter/Function1.cs
@@ -27,7 +27,7 @@ namespace Azure.Sdk.Tools.WebhookRouter
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"G'day, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }

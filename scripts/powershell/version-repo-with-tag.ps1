0f934ea823124cd320c1c7eaeeaf6b643d1f423f
Chidozie Ononiwu <chononiw@microsoft.com>
2020-04-22 15:48:37 -0700

    Switch to using git

--- scripts/powershell/version-repo-with-tag.ps1
+++ scripts/powershell/version-repo-with-tag.ps1
@@ -21,119 +21,26 @@ param (
     [string] $RepoName,
 
     [Parameter(Mandatory = $true)]
-    [string] $RepoOrg,
+    [string] $SourceDir,
+
+    [string] $CommitSHA="HEAD",
+    [string] $PATForAPI,
 
     [Parameter(Mandatory = $true)]
-    [ValidateSet('github', 'git')]
-    [string] $RepoType,
-
-    [string] $CommitSHA="master",
-
-    [Parameter(Mandatory = $true)]
-    [string] $RepoProjectID,
-
-    [Parameter(Mandatory = $true)]
-    [string] $GitHubPAT,
-    [Parameter(Mandatory = $true)]
-    [string] $AzureGitPAT
+    [string] $BuildNumber
 )
 
 Write-Host $MyInvocation.Line
 
-$HttpHeaders = @{}
+$VersionTag = $RepoName + "_" + $BuildNumber
 
-if ($RepoType -eq 'github')
-{
-    $RepoApiURL = "https://api.github.com/repos/$RepoOrg/$RepoName"
-    $CommitsURL = "$RepoApiURL/commits/$CommitSHA"
-    $CreateTagRefURL = "$RepoApiURL/git/refs"
-    $ListTagsURL = "$RepoApiURL/git/refs/tags"
-}
-else {
-    # It's an Azure git Repo in DevOps
-    $APIVersion = "api-version=5.1"
-    $RepoApiURL = "https://dev.azure.com/$RepoOrg/$RepoProjectID/_apis/git/repositories/$RepoName"
+pushd "$SourceDir/$RepoName"
 
-    $ItemVersionType = "branch"
-    if ($CommitSHA -ne "master") { $ItemVersionType = "commit" }
+Write-Host "Get actual commit SHA"
+$CommitSHA = git rev-parse $CommitSHA
 
-    $CommitsURL = "$RepoApiURL/commits?searchCriteria." + '$top' + "=1&searchCriteria.itemVersion.versionType=$ItemVersionType&searchCriteria.itemVersion.version=$CommitSHA&$APIVersion"
-    Write-Host $CommitsURL
-    $CreateTagRefURL = "$RepoApiURL/refs?$APIVersion"
-    $ListTagsURL = "$RepoApiURL/refs?filter=tags/&$APIVersion"
+Write-Host "Tagging $RepoName with $VersionTag"
+git -c user.name="azure-sdk" -c user.email="azuresdk@microsoft.com" tag -a $VersionTag $CommitSHA -m "Append version tag: $VersionTag to $RepoName"
 
-    # Configure Authorization Header
-    $Encoder = [System.Text.ASCIIEncoding]::new()
-    $Credentials = "Chidozie Ononiwu" + ":" + $AzureGitPAT
-    $NoofBytesRequired = $Encoder.GetByteCount($Credentials)
-    $CredentialsEncoded = [System.Byte[]]::new($NoofBytesRequired)
-    $NoOfBytesWriten = $Encoder.GetBytes($Credentials, 0, $CredentialsEncoded.Length, $CredentialsEncoded, 0)
-    $CredentialsBase64 = [System.Convert]::ToBase64String($CredentialsEncoded);
-    $HttpHeaders.Add("Authorization", "Basic $CredentialsBase64")
-}
+git push https://$PATForAPI@github.com/chidozieononiwu/azure-sdk-tools.git origin/master $VersionTag -v
 
-$CurrentDate = Get-Date -Format "yyyyMMdd"
-$TagHead = $RepoName + "_" + $CurrentDate + "."
-
-Import-Module "$PSScriptRoot/../../eng/common/scripts/modules/git-api-calls.psm1"
-
-try {
-    $CurrentTagsInRepo = GetExistingTags -apiUrl $ListTagsURL -repoType $RepoType -headers $HttpHeaders
-}
-catch {
-    Write-Error "$_"
-    Write-Error "Ensure that the RepoName and RepoOrg are correct."
-    Exit 1
-}
-
-$TagsLikeHead = $CurrentTagsInRepo | where { $_ -like "$TagHead*" }
-$TagVersionNo = 1
-
-foreach ($version in $TagsLikeHead)
-{
-    $versionNo = $version.Replace($TagHead, '') -as [int]
-    if ($versionNo -ge $TagVersionNo)
-    {
-        $TagVersionNo = $versionNo + 1
-    }
-}
-
-$FullTag = $TagHead + $TagVersionNo
-
-try {
-    $CommitToTag = FireAPIRequest -url $CommitsURL -method "Get" -headers $HttpHeaders -repoType $RepoType
-    $CommitToTagSHA = $null
-    if ($RepoType -eq 'github')
-    {
-        $CommitToTagSHA = $CommitToTag.sha
-    }
-    else {
-        $CommitToTagSHA = $CommitToTag.value.commitId
-    }
-}
-catch {
-    Write-Error "$_"
-    Write-Error "Ensure that the specified commit SHA exist in the specified repository"
-    Exit 1
-}
-
-if ($RepoType -eq 'github')
-{
-    $TagBody = @{}
-    $TagBody.Add("ref", "refs/tags/$FullTag")
-    $TagBody.Add("sha", $CommitToTagSHA)
-    $HttpHeaders.Add("Content-Type", "application/json")
-    $HttpHeaders.Add("Authorization", "token $GitHubPAT")
-}
-else {
-    $TagObj = @{}
-    $TagObj.Add("name", "refs/tags/$FullTag")
-    $TagObj.Add("oldObjectId", "0000000000000000000000000000000000000000")
-    $TagObj.Add("newObjectId", $CommitToTagSHA)
-    $TagBody = @($TagObj)
-    $HttpHeaders.Add("Content-Type", "application/json")
-}
-
-$TagBodyJson = ConvertTo-Json $TagBody
-
-$RequestResponse = FireAPIRequest -url $CreateTagRefURL -body $TagBodyJson -headers $HttpHeaders -method "Post" -repoType $RepoType

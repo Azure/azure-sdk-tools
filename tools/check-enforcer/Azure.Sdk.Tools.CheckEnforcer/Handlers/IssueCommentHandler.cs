1111d238c3ccaf547d39fcae6350ac24e691e767
Mitch Denny <midenn@microsoft.com>
2020-04-28 10:04:30 +1000

    Removed debugging commands, updated docs.

--- tools/check-enforcer/Azure.Sdk.Tools.CheckEnforcer/Handlers/IssueCommentHandler.cs
+++ tools/check-enforcer/Azure.Sdk.Tools.CheckEnforcer/Handlers/IssueCommentHandler.cs
@@ -35,18 +35,6 @@ namespace Azure.Sdk.Tools.CheckEnforcer.Handlers
 
             switch (comment)
             {
-                case "/check-enforcer queued":
-                    await SetQueuedAsync(context.Client, repositoryId, sha, cancellationToken);
-                    break;
-
-                case "/check-enforcer inprogress":
-                    await SetInProgressAsync(context.Client, repositoryId, sha, cancellationToken);
-                    break;
-
-                case "/check-enforcer success":
-                    await SetSuccessAsync(context.Client, repositoryId, sha, cancellationToken);
-                    break;
-
                 case "/check-enforcer reset":
                     await CreateCheckAsync(context.Client, installationId, repositoryId, sha, true, cancellationToken);
                     break;

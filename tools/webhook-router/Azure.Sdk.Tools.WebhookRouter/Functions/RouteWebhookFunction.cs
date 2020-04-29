8409dbf0c74ed506e72668ef070a8b7fedac6029
Mitch Denny <midenn@microsoft.com>
2020-04-29 14:58:50 +1000

    Added calls to app config.

--- tools/webhook-router/Azure.Sdk.Tools.WebhookRouter/Functions/RouteWebhookFunction.cs
+++ tools/webhook-router/Azure.Sdk.Tools.WebhookRouter/Functions/RouteWebhookFunction.cs
@@ -7,6 +7,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Azure.Identity;
+using Azure.Data.AppConfiguration;
 
 namespace Azure.Sdk.Tools.WebhookRouter.Functions
 {
@@ -18,6 +20,21 @@ namespace Azure.Sdk.Tools.WebhookRouter.Functions
             ILogger log,
             string route)
         {
+            var credential = new DefaultAzureCredential();
+            var client = new ConfigurationClient(new Uri("https://webhookrouterstaging.azconfig.io/"), credential);
+
+            var selector = new SettingSelector()
+            {
+                KeyFilter = "webhookrouter/routes/test/*"
+            };
+
+            var settings = client.GetConfigurationSettingsAsync(selector);
+
+            await foreach (var setting in settings)
+            {
+                log.LogInformation("Setting {key} has value {value}.", setting.Key, setting.Value);
+            }
+
             return new OkResult();
         }
     }

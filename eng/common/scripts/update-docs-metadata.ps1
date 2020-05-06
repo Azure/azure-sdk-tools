685a8a30f103b3ef8c004f779e7828477257a463
Scott Beddall <scbedd@microsoft.com>
2020-05-06 11:44:08 -0700

    output docs metadata

--- eng/common/scripts/update-docs-metadata.ps1
+++ eng/common/scripts/update-docs-metadata.ps1
@@ -14,6 +14,9 @@ param (
   $DocRepoContentLocation = "docs-ref-services/" # within the doc repo, where does our readme go?
 )
 
+Write-Host "> $PSCommandPath $args"
+
+
 # import artifact parsing and semver handling
 . (Join-Path $PSScriptRoot artifact-metadata-parsing.ps1)
 . (Join-Path $PSScriptRoot SemVer.ps1)

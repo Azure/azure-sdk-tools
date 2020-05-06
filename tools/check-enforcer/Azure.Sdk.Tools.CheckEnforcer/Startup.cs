eb8cb02270cc81462bb151e3bae53cf0264b0759
Mitch Denny <midenn@microsoft.com>
2020-05-06 21:39:30 +1000

    Setup DI in preparation for Event Hubs trigger function.

---
+++ tools/check-enforcer/Azure.Sdk.Tools.CheckEnforcer/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Sdk.Tools.CheckEnforcer;
+using Azure.Sdk.Tools.CheckEnforcer.Configuration;
+using Azure.Sdk.Tools.CheckEnforcer.Integrations.GitHub;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace Azure.Sdk.Tools.CheckEnforcer
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<IGlobalConfigurationProvider, GlobalConfigurationProvider>();
+            builder.Services.AddSingleton<IGitHubClientProvider, GitHubClientProvider>();
+            builder.Services.AddSingleton<IRepositoryConfigurationProvider, RepositoryConfigurationProvider>();
+            builder.Services.AddSingleton<GitHubWebhookProcessor>();
+        }
+    }
+}

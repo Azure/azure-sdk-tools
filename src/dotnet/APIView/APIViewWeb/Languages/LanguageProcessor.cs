97f2dbfecc406e57da85a6c30c80af5b31368903
praveenkuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2020-05-06 11:06:53 -0700

    Restructured Python and Java language service to use base class

---
+++ src/dotnet/APIView/APIViewWeb/Languages/LanguageProcessor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using ApiView;
+
+namespace APIViewWeb
+{
+    public abstract class LanguageProcessor: ILanguageService
+    {
+        public abstract string Name { get; }
+        public abstract bool IsSupportedExtension(string extension);
+        public abstract string GetProccessorArguments(string originalName, string tempDirectory, string jsonPath);
+        public abstract string GetLanguage();
+        public abstract string GetProcessName();
+        public abstract string GetVersionString();
+
+        public bool CanUpdate(string versionString)
+        {
+            return versionString != GetVersionString();
+        }
+
+        public async Task<CodeFile> GetCodeFileAsync(string originalName, Stream stream, bool runAnalysis)
+        {
+            var tempPath = Path.GetTempPath();
+            var randomSegment = Guid.NewGuid().ToString("N");
+            var tempDirectory = Path.Combine(tempPath, "ApiView", randomSegment);
+            Directory.CreateDirectory(tempDirectory);
+            var originalFilePath = Path.Combine(tempDirectory, originalName);
+
+            var jsonFilePath = Path.ChangeExtension(originalFilePath, ".json");
+
+            using (var file = File.Create(originalFilePath))
+            {
+                await stream.CopyToAsync(file);
+            }
+
+            try
+            {
+                var arguments = GetProccessorArguments(originalName, tempDirectory, jsonFilePath);
+                var processStartInfo = new ProcessStartInfo(GetProcessName(), arguments);
+                processStartInfo.WorkingDirectory = tempDirectory;
+                processStartInfo.RedirectStandardError = true;
+                processStartInfo.RedirectStandardOutput = true;
+
+                using (var process = Process.Start(processStartInfo))
+                {
+                    process.WaitForExit();
+                    if (process.ExitCode != 0)
+                    {
+                        throw new InvalidOperationException(
+                            "Processor failed: " + Environment.NewLine +
+                            "stdout: " + Environment.NewLine +
+                            process.StandardOutput.ReadToEnd() + Environment.NewLine +
+                            "stderr: " + Environment.NewLine +
+                            process.StandardError.ReadToEnd() + Environment.NewLine);
+                    }
+                }
+
+                using (var codeFileStream = File.OpenRead(jsonFilePath))
+                {
+                    var codeFile = await CodeFile.DeserializeAsync(codeFileStream);
+                    codeFile.VersionString = GetVersionString();
+                    codeFile.Language = GetLanguage();
+                    return codeFile;
+                }
+            }
+            finally
+            {
+                Directory.Delete(tempDirectory, true);
+            }
+        }
+    }
+}
--- src/dotnet/APIView/APIViewWeb/Languages/PythonLanguageService.cs
+++ src/dotnet/APIView/APIViewWeb/Languages/PythonLanguageService.cs
@@ -9,77 +9,42 @@ using ApiView;
 
 namespace APIViewWeb
 {
-    public class PythonLanguageService : ILanguageService
+    public class PythonLanguageService : LanguageProcessor
     {
-        public string Name { get; } = "Python";
-
         public string pythonParserVersion = "0.1.1";
 
-        public bool IsSupportedExtension(string extension)
+        public override string Name { get; } = "Python";
+
+        public override bool IsSupportedExtension(string extension)
         {
             return string.Equals(extension, ".whl", comparisonType: StringComparison.OrdinalIgnoreCase);
         }
-
-        public bool CanUpdate(string versionString)
+        
+        public override string GetProccessorArguments(string originalName, string tempDirectory, string jsonPath)
         {
-            return versionString != pythonParserVersion;
-        }
-
-        public async Task<CodeFile> GetCodeFileAsync(string originalName, Stream stream, bool runAnalysis)
-        {
-            var tempPath = Path.GetTempPath();
-            var randomSegment = Guid.NewGuid().ToString("N");
-            var tempDirectory = Path.Combine(tempPath, "ApiView", randomSegment);
-            Directory.CreateDirectory(tempDirectory);
-            var originalFilePath = Path.Combine(tempDirectory, originalName);
-            var jsonFilePath = Path.ChangeExtension(originalFilePath, ".json");
-
-            using (var file = File.Create(originalFilePath))
-            {
-                await stream.CopyToAsync(file);
-            }
-
-            try
-            {
-                var pythonScriptPath = Path.Combine(
+            var pythonScriptPath = Path.Combine(
                     Path.GetDirectoryName(typeof(PythonLanguageService).Assembly.Location),
                     "api-stub-generator",
                     "apistubgen.py"
                     );
-                var arguments = $"{pythonScriptPath} --pkg-path {originalFilePath} --temp-path {tempDirectory}" +
-                    $" --out-path {jsonFilePath} --hide-report";
-                var processStartInfo = new ProcessStartInfo("python", arguments);
-                processStartInfo.WorkingDirectory = tempDirectory;
-                processStartInfo.RedirectStandardError = true;
-                processStartInfo.RedirectStandardOutput = true;
-                processStartInfo.UseShellExecute = false;
-
-                using (var process = Process.Start(processStartInfo))
-                {
-                    process.WaitForExit();
-                    if (process.ExitCode != 0)
-                    {
-                        throw new InvalidOperationException(
-                            "Python stub generator failed: " + Environment.NewLine +
-                            "stdout: " + Environment.NewLine +
-                            process.StandardOutput.ReadToEnd() + Environment.NewLine +
-                            "stderr: " + Environment.NewLine +
-                            process.StandardError.ReadToEnd() + Environment.NewLine);
-                    }
-                }
-                using (var codeFileStream = File.OpenRead(jsonFilePath))
-                {
-                    var codeFile = await CodeFile.DeserializeAsync(codeFileStream);
-                    codeFile.VersionString = pythonParserVersion;
-                    codeFile.Language = "Python";
-                    return codeFile;
-                }
-            }
-            finally
-            {
-                Directory.Delete(tempDirectory, true);
-            }
+            return $"{pythonScriptPath} --pkg-path {originalName} --temp-path {tempDirectory}" +
+                $" --out-path {jsonPath} --hide-report";
         }
-        
+
+        public override string GetLanguage()
+        {
+            return "Python";
+        }
+
+        public override string GetProcessName()
+        {
+            return "python";
+        }
+
+        public override string GetVersionString()
+        {
+            return pythonParserVersion;
+        }
+
     }
 }

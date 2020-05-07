74097d6f8ae50b2edd4b317b848af816bfde721c
Mitch Denny <midenn@microsoft.com>
2020-05-07 13:37:18 +1000

    Route incoming event hub messages into the processor. Yay!

--- tools/check-enforcer/Azure.Sdk.Tools.CheckEnforcer/Functions/GitHubWebhookOverEventHubsFunction.cs
+++ tools/check-enforcer/Azure.Sdk.Tools.CheckEnforcer/Functions/GitHubWebhookOverEventHubsFunction.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
@@ -21,15 +22,34 @@ namespace Azure.Sdk.Tools.CheckEnforcer.Functions
         private GitHubWebhookProcessor processor;
 
         [FunctionName("webhook-eventhubs")]
-        public async Task Run([EventHubTrigger("github-webhooks", Connection = "CheckEnforcerEventHubConnectionString")] EventData eventData, ILogger log)
+        public async Task Run([EventHubTrigger("github-webhooks", Connection = "CheckEnforcerEventHubConnectionString")] EventData eventData, ILogger log, CancellationToken cancellationToken)
         {
-            string messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
-            var message = JsonDocument.Parse(messageBody);
+            var message = GetMessage(eventData);
+            var eventName = GetEventName(message);
+
             var encodedContent = message.RootElement.GetProperty("content").ToString();
             var contentBytes = Convert.FromBase64String(encodedContent);
             var json = Encoding.UTF8.GetString(contentBytes);
 
-            log.LogInformation("EventHubs Payload: {json}", json);
+            await processor.ProcessWebhookAsync(eventName, json, log, cancellationToken);
+        }
+
+        private string GetEventName(JsonDocument message)
+        {
+            return message
+                .RootElement
+                .GetProperty("headers")
+                .GetProperty("X-GitHub-Event")
+                .EnumerateArray()
+                .Single()
+                .ToString();
+        }
+
+        private JsonDocument GetMessage(EventData eventData)
+        {
+            string messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
+            var message = JsonDocument.Parse(messageBody);
+            return message;
         }
     }
 }

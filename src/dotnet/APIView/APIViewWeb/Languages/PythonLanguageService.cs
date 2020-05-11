0ce2c04febbfccd72a231cc3d2ef300b75babc59
praveenkuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2020-05-11 10:37:49 -0700

    Use configuration to fetch env variable APIVIEW_PYTHONHOME

--- src/dotnet/APIView/APIViewWeb/Languages/PythonLanguageService.cs
+++ src/dotnet/APIView/APIViewWeb/Languages/PythonLanguageService.cs
@@ -3,17 +3,27 @@
 
 using System;
 using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace APIViewWeb
 {
     public class PythonLanguageService : LanguageProcessor
     {
-        private static readonly string PythonHome = Environment.GetEnvironmentVariable("APIVIEW_PYTHONHOME") ?? string.Empty;
-
+        private readonly string ApiViewPythonProcessor;
         public override string Name { get; } = "Python";
         public override string Extension { get; } = ".whl";
-        public override string ProcessName { get; } = Path.Combine(PythonHome, "Scripts", "apistubgen");
+        public override string ProcessName
+        {
+            get{ return ApiViewPythonProcessor; }
+        }
         public override string VersionString { get; } = "0.1.1";
+
+        public PythonLanguageService(IConfiguration configuration)
+        {
+            // apistubgen is located in python's scripts path e.g. <Pythonhome>/Scripts
+            var PythonHome = configuration["APIVIEW_PYTHONHOME"] ?? string.Empty;
+            ApiViewPythonProcessor = Path.Combine(PythonHome, "Scripts", "apistubgen");
+        }
 
         public override string GetProccessorArguments(string originalName, string tempDirectory, string jsonPath)
         {

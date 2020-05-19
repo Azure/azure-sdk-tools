115b4ebbea7a335b27be4a00a64f9e9d61689c14
Mitch Denny <midenn@microsoft.com>
2020-05-19 15:32:40 +1000

    Add timeout policy on HTTP client.

--- tools/pipeline-witness/Azure.Sdk.Tools.PipelineWitness/Startup.cs
+++ tools/pipeline-witness/Azure.Sdk.Tools.PipelineWitness/Startup.cs
@@ -3,6 +3,7 @@ using Azure.Sdk.Tools.PipelineWitness;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
 using System;
 using System.Collections.Generic;
 using System.Net.NetworkInformation;
@@ -19,7 +20,14 @@ namespace Azure.Sdk.Tools.PipelineWitness
             builder.Services.AddLogging();
             builder.Services.AddMemoryCache();
             builder.Services.AddSingleton<RunProcessor>();
-            builder.Services.AddHttpClient();
+
+            // POSSIBLE WORKAROUND: The Azure Functions host environment has a health check
+            //                      which pulls down the host if it exceeds 300 active outbound
+            //                      connections within a 1 second period.
+            //
+            //                      By more aggressively timing out handlers we might be able to
+            //                      work around this but at this point it is just a hypothesis.
+            builder.Services.AddHttpClient<RunProcessor>().SetHandlerLifetime(TimeSpan.FromSeconds(60));
         }
     }
 }

0ce3350c3acaa90ced261b6f97fd8d29436f6d6c
Mitch Denny <midenn@microsoft.com>
2020-05-27 16:34:38 +1000

    Using Azure DevOps client libraries to extract data.

---
+++ tools/pipeline-witness/Azure.Sdk.Tools.PipelineWitness/Entities/AzurePipelines/RunStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.Sdk.Tools.PipelineWitness.Entities.AzurePipelines
+{
+    public enum RunStatus
+    {
+        None,
+        InProgress,
+        Completed,
+        Cancelling,
+        Postponed,
+        NotStarted
+    }
+}
--- tools/pipeline-witness/Azure.Sdk.Tools.PipelineWitness/RunProcessor.cs
+++ tools/pipeline-witness/Azure.Sdk.Tools.PipelineWitness/RunProcessor.cs
@@ -1,12 +1,19 @@
 ﻿using Azure.Cosmos;
 using Azure.Identity;
+using Azure.Sdk.Tools.PipelineWitness.Entities.AzurePipelines;
 using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
+using Microsoft.TeamFoundation.Build.WebApi;
+using Microsoft.TeamFoundation.Core.WebApi;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.Organization.Client;
+using Microsoft.VisualStudio.Services.WebApi;
 using Microsoft.WindowsAzure.Storage.Blob.Protocol;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -37,20 +44,115 @@ namespace Azure.Sdk.Tools.PipelineWitness
             var header = new AuthenticationHeaderValue("Basic", encodedUsernameAndPassword);
             return header;
         }
+        private bool IsValidAzureDevOpsUri(Uri uri)
+        {
+            // We want to throw if we start getting messages from other Azure DevOps
+            // organizations since currently Pipeline Witness is designed to only work
+            // against our instance.
+            return uri.Host == "dev.azure.com" && uri.PathAndQuery.StartsWith("/azure-sdk/");
+        }
+
+        private Guid GetCollectionIdFromProject(TeamProject project)
+        {
+            var url = ((ReferenceLink)project.Links.Links["web"]).Href;
+            var collectionIdAsString = url.Split("/").Last();
+            var collectionId = Guid.Parse(collectionIdAsString);
+            return collectionId;
+        }
 
         public async Task ProcessRunAsync(Uri runUri)
         {
             try
             {
-                logger.LogInformation("Processing run: {runUri}", runUri);
-                var content = await GetContentFromAzureDevOpsAsync(runUri);
-                var document = JsonDocument.Parse(content);
-                var record = new AzurePipelinesRun(document);
+                if (!IsValidAzureDevOpsUri(runUri))
+                {
+                    throw new ArgumentOutOfRangeException("Run URI does not point to the Azure SDK instance of Azure DevOps");
+                }
 
-                var container = await GetItemContainerAsync("scratch");
+                var runUriPath = runUri.AbsolutePath;
+                var runUriPathSegments = runUri.PathAndQuery.Split("/");
+                var organization = runUriPathSegments[1];
+                var projectGuid = Guid.Parse(runUriPathSegments[2]);
+                var pipelineId = int.Parse(runUriPathSegments[5]);
+                var runId = int.Parse(runUriPathSegments[7]);
 
-                logger.LogInformation("Upserting run: {runUri}", runUri);
-                await container.UpsertItemAsync(record);
+                var azureDevOpsPersonalAccessToken = await GetAzureDevOpsPersonalAccessTokenAsync();
+                var credentials = new VssBasicCredential("nobody", azureDevOpsPersonalAccessToken);
+
+                var baseUri = new Uri($"https://dev.azure.com/{organization}");
+                var connection = new VssConnection(baseUri, credentials);
+
+                var buildClient = connection.GetClient<BuildHttpClient>();
+                var projectClient = connection.GetClient<ProjectHttpClient>();
+                var organizationClient = connection.GetClient<OrganizationHttpClient>();
+
+                var build = await buildClient.GetBuildAsync(projectGuid, runId);
+                var project = await projectClient.GetProject(projectGuid.ToString());
+                var pipeline = await buildClient.GetDefinitionAsync(project.Id, build.Definition.Id);
+                var timeline = await buildClient.GetBuildTimelineAsync(projectGuid, runId);
+
+                double agentDurationInSeconds = 0;
+                double queueDurationInSeconds = 0;
+
+                if (timeline != null)
+                {
+                    agentDurationInSeconds = (from record in timeline.Records
+                                              where record.RecordType == "Task"
+                                              where (record.Name == "Initialize job" || record.Name == "Finalize Job") // Love consistency in capitalization!
+                                              group record by record.ParentId into job
+                                              let jobStartTime = job.Min(jobRecord => jobRecord.StartTime)
+                                              let jobFinishTime = job.Max(jobRecord => jobRecord.FinishTime)
+                                              let agentDuration = jobFinishTime - jobStartTime
+                                              select agentDuration.Value.TotalSeconds).Sum();
+
+                    queueDurationInSeconds = (from taskRecord in timeline.Records
+                                              where taskRecord.RecordType == "Task"
+                                              where taskRecord.Name == "Initialize job"
+                                              join jobRecord in timeline.Records on taskRecord.ParentId equals jobRecord.Id
+                                              let queueDuration = taskRecord.StartTime - jobRecord.StartTime
+                                              select queueDuration.Value.TotalSeconds).Sum();
+                }
+
+                var run = new Run()
+                {
+                    RunId = build.Id,
+                    RunName = build.BuildNumber,
+                    RunUrl = new Uri(((ReferenceLink)build.Links.Links["web"]).Href),
+                    PipelineId = pipeline.Id,
+                    PipelineName = pipeline.Name,
+                    PipelineUrl = new Uri(((ReferenceLink)pipeline.Links.Links["web"]).Href),
+                    ProjectId = build.Project.Id,
+                    ProjectName = build.Project.Name,
+                    ProjectUrl = new Uri(((ReferenceLink)project.Links.Links["web"]).Href),
+                    State = build.Status switch
+                    {
+                        BuildStatus.None => RunStatus.None,
+                        BuildStatus.Completed => RunStatus.Completed,
+                        BuildStatus.Cancelling => RunStatus.Cancelling,
+                        BuildStatus.InProgress => RunStatus.InProgress,
+                        BuildStatus.NotStarted => RunStatus.NotStarted,
+                        BuildStatus.Postponed => RunStatus.Postponed,
+                        _ => RunStatus.None
+                    },
+                    Result = build.Result switch
+                    {
+                        BuildResult.Canceled => RunResult.Cancelled,
+                        BuildResult.Failed => RunResult.Failed,
+                        BuildResult.None => RunResult.None,
+                        BuildResult.PartiallySucceeded => RunResult.PartiallySucceded,
+                        BuildResult.Succeeded => RunResult.Succeeded,
+                        _ => RunResult.None
+                    },
+                    GitReference = build.SourceBranch,
+                    GitCommitSha = build.SourceVersion,
+                    StartTime = build.StartTime.Value,
+                    FinishTime = build.FinishTime.Value,
+                    AgentDurationInSeconds = agentDurationInSeconds,
+                    QueueDurationInSeconds = queueDurationInSeconds
+                };
+
+                var container = await GetItemContainerAsync("azure-pipelines-runs");
+                await container.UpsertItemAsync(run);
             }
             catch (ContentNotFoundException ex)
             {
@@ -82,36 +184,6 @@ namespace Azure.Sdk.Tools.PipelineWitness
             return cosmosDbPrimaryAuthorizationKey;
         }
 
-        private bool IsContentUrlValid(Uri contentUri)
-        {
-            return contentUri.Host == "dev.azure.com";
-        }
-
-        private async Task<string> GetContentFromAzureDevOpsAsync(Uri contentUri)
-        {
-            if (!IsContentUrlValid(contentUri)) throw new InvalidOperationException($"Invalid contentUrl: {contentUri}");
-
-            // TODO: Add content here to cache and archive these payloads.
-            var request = new HttpRequestMessage(HttpMethod.Get, contentUri);
-            var azureDevOpsPersonalAccessToken = await GetAzureDevOpsPersonalAccessTokenAsync();
-            request.Headers.Authorization = GetAuthenticationHeader(azureDevOpsPersonalAccessToken);
-
-            logger.LogInformation("Downloading content from: {contentUri}", contentUri);
-            var response = await httpClient.SendAsync(request);
-            if (response.StatusCode == HttpStatusCode.NotFound)
-            {
-                throw new ContentNotFoundException(contentUri);
-            }
-            else if (response.StatusCode != HttpStatusCode.OK)
-            {
-                throw new ContentDownloadException($"Failed to download content (status {response.StatusCode}): {contentUri}");
-            }
-            else
-            {
-                var content = await response.Content.ReadAsStringAsync();
-                return content;
-            }
-        }
 
         private string GetWebsiteResourceGroupEnvironmentVariable()
         {

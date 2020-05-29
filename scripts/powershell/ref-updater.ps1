2206524c8f1ef3abdf4cdd56b03758c9b2495af9
Chidozie Ononiwu <chononiw@microsoft.com>
2020-05-29 12:05:11 -0700

    Begin Implementing Ref Updater

---
+++ scripts/powershell/ref-updater.ps1
@@ -0,0 +1,40 @@
+# Updates the Ref in all Yml files in the Repository
+param (
+    [String]$RepoRoot,
+    [String]$Tag,
+    [String]$ToolRepo
+)
+
+Install-Module -Name powershell-yaml -RequiredVersion 0.4.2 -Force -Scope CurrentUser
+
+$ymlFiles = Get-ChildItem -Path $RepoRoot -File -Include *.yml -Recurse
+
+foreach ($file in $ymlFiles)
+{
+    $ymlContent = Get-Content $file.FullName -Raw
+    $ymlObj = ConvertFrom-Yaml $ymlContent -Ordered
+    if ($ymlObj.Contains("resources"))
+    {
+        $resources = $ymlObj["resources"]
+        if ($resources.Contains("repositories"))
+        {
+
+            $repositories = $resources["repositories"]
+            foreach ($repository in $repositories)
+            {
+                if ($repository["repository"] -eq $ToolRepo)
+                {
+                    $repository["ref"] = "refs/tags/$Tag"
+                }
+            }
+
+            $resourcesYml = "resources:`n  " + (ConvertTo-Yaml $resources)
+
+            Write-Host $resourcesYml.GetType()
+            Write-Host $resourcesYml
+
+            $ymlContent = ($ymlContent -replace "(?ms)resources:.*repositories:.*repository.*?name:.*?\n|ref:.*?\n|endpoint:.*?\n|type:.*?\n", $resourcesYml)
+            Set-Content -Path $file.FullName -Value $ymlContent -Force
+        }
+    }
+}

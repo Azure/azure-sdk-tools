14c53612d37f6ce14ef8337e03e417016fadabbd
Chidozie Ononiwu <chononiw@microsoft.com>
2020-05-29 12:05:11 -0700

    Begin Implementing Ref Updater

---
+++ scripts/powershell/ref-updater.ps1
@@ -0,0 +1,33 @@
+# Updates the Ref in all Yml files in the Repository
+param (
+    [String]$RepoRoot,
+    [String]$Tag,
+    [String]$ToolRepo
+)
+
+Install-Module -Name powershell-yaml -RequiredVersion 0.4.2 -Force -Scope CurrentUser
+
+$ymlFiles = Get-ChildItem -Path $RepoRoot -File -Include *.yml -Recurse
+
+foreach ($file in $ymlFiles)
+{
+    $ymlObj = ConvertFrom-Yaml (Get-Content $file.FullName -Raw) -Ordered
+    if ($ymlObj.Contains("resources"))
+    {
+        $resources = $ymlObj["resources"]
+        if ($resources.Contains("repositories"))
+        {
+
+            $repositories = $resources["repositories"]
+            foreach ($repository in $repositories)
+            {
+                if ($repository["repository"] -eq $ToolRepo)
+                {
+                    $repository["ref"] = "refs/tags/$Tag"
+                }
+            }
+
+            ConvertTo-Yaml $ymlObj -OutFile $file.FullName -Force
+        }
+    }
+}

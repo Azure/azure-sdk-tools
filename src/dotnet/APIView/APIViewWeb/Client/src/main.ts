50e270a754ad7127bf234fe2091d97f6e7fc15dc
Heath Stewart <heaths@microsoft.com>
2020-06-01 16:06:01 -0700

    Resolve PR feedback

--- src/dotnet/APIView/APIViewWeb/Client/src/main.ts
+++ src/dotnet/APIView/APIViewWeb/Client/src/main.ts
@@ -1,15 +1,3 @@
 import "./comments.ts";
 import "./file-input.ts";
 import "./navbar.ts";
-import Split from "split.js";
-
-// Split left and right review panes using split.js.
-const rl = $('#review-left');
-const rr = $('#review-right');
-
-if (rl.length && rr.length) {
-  Split(['#review-left', '#review-right'], {
-    direction: 'horizontal',
-    sizes: [17, 83],
-  });
-}

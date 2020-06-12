67e20b503dd25bf5afe4d5eb651462ff4ac173d8
Mitch Denny <midenn@microsoft.com>
2020-06-12 21:39:11 +1000

    Added classifier for Cosmos DB start-up failures.

---
+++ tools/pipeline-witness/Azure.Sdk.Tools.PipelineWitness/Services/FailureAnalysis/CosmosDbEmulatorStartFailureClassifier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Build.WebApi;
+
+namespace Azure.Sdk.Tools.PipelineWitness.Services.FailureAnalysis
+{
+    public class CosmosDbEmulatorStartFailureClassifier : IFailureClassifier
+    {
+        public async Task ClassifyAsync(FailureAnalyzerContext context)
+        {
+            var failedTasks = from r in context.Timeline.Records
+                              where r.RecordType == "Task"
+                              where r.Name == "Start Cosmos DB Emulator"
+                              where r.Result == TaskResult.Failed
+                              select r;
+
+            if (failedTasks.Count() > 0)
+            {
+                foreach (var failedTask in failedTasks)
+                {
+                    context.AddFailure(failedTask, "Cosmos DB Emulator Failure");
+                }
+            }
+        }
+    }
+}

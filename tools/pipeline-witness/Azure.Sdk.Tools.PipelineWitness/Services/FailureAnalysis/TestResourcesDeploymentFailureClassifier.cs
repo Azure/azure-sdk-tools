6d0b14363790be27d8b184feae275c1669e25706
Mitch Denny <midenn@microsoft.com>
2020-06-15 09:22:09 +1000

    Add test resource deployment failure classifier.

---
+++ tools/pipeline-witness/Azure.Sdk.Tools.PipelineWitness/Services/FailureAnalysis/TestResourcesDeploymentFailureClassifier.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.TeamFoundation.Build.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.Sdk.Tools.PipelineWitness.Services.FailureAnalysis
+{
+   public class TestResourcesDeploymentFailureClassifier : IFailureClassifier
+    {
+        public async Task ClassifyAsync(FailureAnalyzerContext context)
+        {
+            var failedTasks = from r in context.Timeline.Records
+                              where r.Name.StartsWith("Deploy test resources")
+                              where r.Result == TaskResult.Failed
+                              select r;
+
+            if (failedTasks.Count() > 0)
+            {
+                foreach (var failedTask in failedTasks)
+                {
+                    context.AddFailure(failedTask, "Test Resource Failure");
+                }
+            }
+        }
+    }
+}

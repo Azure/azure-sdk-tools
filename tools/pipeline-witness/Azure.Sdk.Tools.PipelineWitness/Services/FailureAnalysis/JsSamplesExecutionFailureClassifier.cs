f1b2827e822f2fd52a0a870510a05e429bc644de
Mitch Denny <midenn@microsoft.com>
2020-06-19 13:07:37 +1000

    Classifiers for two new kinds of JS failures. (#714)

---
+++ tools/pipeline-witness/Azure.Sdk.Tools.PipelineWitness/Services/FailureAnalysis/JsSamplesExecutionFailureClassifier.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.TeamFoundation.Build.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.Sdk.Tools.PipelineWitness.Services.FailureAnalysis
+{
+    public class JsSamplesExecutionFailureClassifier : IFailureClassifier
+    {
+        public async Task ClassifyAsync(FailureAnalyzerContext context)
+        {
+            if (context.Build.Definition.Name.StartsWith("js -"))
+            {
+                var failedTasks = from r in context.Timeline.Records
+                                  where r.Name == "Execute Samples"
+                                  where r.Result == TaskResult.Failed
+                                  select r;
+
+                if (failedTasks.Count() > 0)
+                {
+                    foreach (var failedTask in failedTasks)
+                    {
+                        context.AddFailure(failedTask, "Sample Execution");
+                    }
+                }
+            }
+        }
+    }
+}

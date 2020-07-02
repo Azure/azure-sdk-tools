8e5e6d79375b37f45267d56a8ab54daa4cb596a9
Scott Beddall <scbedd@microsoft.com>
2020-07-01 19:26:18 -0700

    cleanup some namespace usage and extraneous variables

--- tools/notification-configuration/identity-resolver/Program.cs
+++ tools/notification-configuration/identity-resolver/Program.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using NotificationConfiguration.Helpers;
+using System;
+using System.Threading.Tasks;
 
 namespace identity_resolver
 {
     class Program
     {
-        static void Main(string[] args)
+
+        /// <summary>
+        /// Synchronizes CODEOWNERS contacts to appropriate DevOps groups
+        /// </summary>
+        /// <param name="organization">Azure DevOps organization name</param>
+        /// <param name="project">Azure DevOps project name</param>
+        /// <param name="devOpsTokenVar">Personal Access Token environment variable name</param>
+        /// <param name="aadAppIdVar">AAD App ID environment variable name (Kusto access)</param>
+        /// <param name="aadAppSecretVar">AAD App Secret environment variable name (Kusto access)</param>
+        /// <param name="aadTenantVar">AAD Tenant environment variable name (Kusto access)</param>
+        /// <param name="kustoUrlVar">Kusto URL environment variable name</param>
+        /// <param name="kustoDatabaseVar">Kusto DB environment variable name</param>
+        /// <param name="kustoTableVar">Kusto Table environment variable name</param>
+        /// <param name="identity">The full name of the employee</param>
+        /// <returns></returns>
+        public static async Task Main(
+            string organization,
+            string project,
+            string devOpsTokenVar,
+            string aadAppIdVar,
+            string aadAppSecretVar,
+            string aadTenantVar,
+            string kustoUrlVar,
+            string kustoDatabaseVar,
+            string kustoTableVar,
+            string identity
+            )
         {
-            Console.WriteLine("Hello World!");
+
+#pragma warning disable CS0618 // Type or member is obsolete
+            using (var loggerFactory = new LoggerFactory().AddConsole(includeScopes: true))
+#pragma warning restore CS0618 // Type or member is obsolete
+            {
+                var githubNameResolver = new GitHubNameResolver(
+                    Environment.GetEnvironmentVariable(aadAppIdVar),
+                    Environment.GetEnvironmentVariable(aadAppSecretVar),
+                    Environment.GetEnvironmentVariable(aadTenantVar),
+                    Environment.GetEnvironmentVariable(kustoUrlVar),
+                    Environment.GetEnvironmentVariable(kustoDatabaseVar),
+                    Environment.GetEnvironmentVariable(kustoTableVar),
+                    loggerFactory.CreateLogger<GitHubNameResolver>()
+                );
+
+                var result = await githubNameResolver.GetMappingInformationFromAADName(identity);
+
+                Console.Write(JsonConvert.SerializeObject(result));
+            }
         }
     }
 }

8b4878023055073bed93383be0c952cbd640dd5f
Scott Beddall <scbedd@microsoft.com>
2020-07-01 20:22:26 -0700

    update tag-reviewer.ps1

--- eng/common/scripts/tag-reviewer.ps1
+++ eng/common/scripts/tag-reviewer.ps1
@@ -12,13 +12,45 @@ param(
     $PRNumber,
   
     [Parameter(Mandatory = $true)]
-    $AuthToken,
+    $AuthToken
 )
 
 $headers = @{
   Authorization = "bearer $AuthToken"
 }
+$uri = "https://api.github.com/repos/$RepoOwner/$RepoName/pulls/$PRNumber/requested_reviewers"
 
+try {
+  $resp = Invoke-RestMethod -Headers $headers $uri -MaximumRetryCount 3
+}
+catch {
+  Write-Error "Invoke-RestMethod [$uri] failed with exception:`n$_"
+  exit 1
+}
 
-# sample
-# https://api.github.com/repos/MicrosoftDocs/azure-docs-sdk-python/pulls/677/requested_reviewers
+# the response object takes this form: https://developer.github.com/v3/pulls/review_requests/#response-1
+# before we can push a new reviewer, we need to pull the simple Ids out of the complex objects that came back in the response
+$userReviewers = @($resp.users | % { return $_.login })
+$teamReviewers = @($resp.teams | % { return $_.slug })
+
+if (-not $userReviewers.Contains($GitHubUser)){
+  $userReviewers += $GitHubUser
+
+  $postResp = @{
+    reviewers = $userReviewers
+    team_reviewers = $teamReviewers
+  } | ConvertTo-Json
+
+  try {
+    $resp = Invoke-RestMethod -Method Post -Headers $headers -Body $postResp -Uri $uri -MaximumRetryCount 3
+    $resp | Write-Verbose
+  }
+  catch {
+    Write-Error "Unable to add reviewer."
+    Write-Error $_
+  }
+}
+else {
+  Write-Host "Reviewer $GitHubUser already added. Exiting."
+  exit(0)
+}

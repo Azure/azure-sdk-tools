4e166050451eb21810d4ea02f7d1b52adc2da3e2
Scott Beddall <scbedd@microsoft.com>
2020-07-10 11:26:37 -0700

    clean up the results

--- eng/common/scripts/get-codeowners.ps1
+++ eng/common/scripts/get-codeowners.ps1
@@ -3,19 +3,12 @@ param (
   $RootDirectory # ideally $(Build.SourcesDirectory)
 )
 
-$codeOwnersLocation = @(Get-ChildItem -R -Force -Path $RootDirectory -Filter "CODEOWNERS")
+$codeOwnersLocation = Join-Path $RootDirectory -ChildPath ".github/CODEOWNERS"
 
-if ($codeOwnersLocation.Length -eq 0) {
+if (!(Test-Path $codeOwnersLocation)) {
   Write-Host "Unable to find CODEOWNERS file in target directory $RootDirectory"
   exit 1
 }
-
-if ($codeOwnersLocation.Length -gt 1) {
-  Write-Host "Multiple CODEOWNERS files detected in  $RootDirectory."
-  Write-Host "$codeOwnersLocation"
-  exit 1
-}
-
 
 $codeOwnersContent = Get-Content ($codeOwnersLocation | Select-Object -Last 1)
 
@@ -25,9 +18,11 @@ foreach ($contentLine in $codeOwnersContent) {
   if (-not $contentLine.StartsWith("#") -and $contentLine){
     $splitLine = $contentLine -split "\s+"
     
+    # CODEOWNERS file can also have labels present after the owner aliases
+    # gh aliases start with @ in codeowners. don't pass on to API calls
     $ownedFolders[$splitLine[0].ToLower()] = ($splitLine[1..$($splitLine.Length)] `
-      | ? { $_.StartsWith("@") } ` # CODEOWNERS file can also have labels present after the owner aliases
-      | % { return $_.substring(1) }) -join ","     # gh aliases start with @ in codeowners. don't pass on to API calls
+      | ? { $_.StartsWith("@") } `
+      | % { return $_.substring(1) }) -join "," 
   }
 }
 

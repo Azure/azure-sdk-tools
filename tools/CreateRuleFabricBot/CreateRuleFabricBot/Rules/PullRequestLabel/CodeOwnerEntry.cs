2abebac7c6d7d3136f14cf18a318e9ef4f436836
Alex Ghiondea <alex.ghiondea@gmail.com>
2020-07-22 12:10:19 -0700

    Change the format for how the PRLabels are specified.
    We will specify the labels on the line directly above the line that contains the Owners.

--- tools/CreateRuleFabricBot/CreateRuleFabricBot/Rules/PullRequestLabel/CodeOwnerEntry.cs
+++ tools/CreateRuleFabricBot/CreateRuleFabricBot/Rules/PullRequestLabel/CodeOwnerEntry.cs
@@ -1,92 +1,170 @@
-﻿using System.Collections.Generic;
+﻿using CreateRuleFabricBot.Rules.IssueRouting;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CreateRuleFabricBot.Rules.PullRequestLabel
 {
-    internal class CodeOwnerEntry
-    {
-        public string PathExpression { get; set; }
+	internal class CodeOwnerEntry
+	{
+		public string PathExpression { get; set; }
 
-        public bool ContainsWildcard { get; set; }
+		public bool ContainsWildcard { get; set; }
 
-        public List<string> Owners { get; set; } = new List<string>();
+		public List<string> Owners { get; set; } = new List<string>();
 
-        public List<string> Labels { get; set; } = new List<string>();
+		public List<string> Labels { get; set; } = new List<string>();
+		public bool IsValid
+		{
+			get
+			{
+				return !string.IsNullOrEmpty(PathExpression);
+			}
+		}
 
-        public static CodeOwnerEntry Parse(string line)
-        {
-            // The format for the Codeowners File will be:
-            // <path> @<owner> ... @<owner> %<label> ... %<label>
+		//public static CodeOwnerEntry Parse(CodeOwnerEntry entry, string line)
+		//{
+		//	// The format for the Codeowners File will be:
+		//	// # PRLabel: %label, %label
+		//	// <path> @<owner> ... @<owner>
 
-            // get rid of tabs
-            line = line.Replace('\t', ' ');
-            // trim the line to get rid of whitespace before and after.
-            line = line.Trim();
+		//	// get rid of tabs
+		//	line = line.Replace('\t', ' ');
+		//	// trim the line to get rid of whitespace before and after.
+		//	line = line.Trim();
 
-            if (string.IsNullOrEmpty(line) || line.StartsWith('#'))
-            {
-                return null;
-            }
+		//	if (string.IsNullOrEmpty(line) || line.StartsWith('#'))
+		//	{
+		//		return null;
+		//	}
 
-            List<string> entries = SplitLine(line).ToList();
+		//	// At this point, we know the line does not start with '#'
+		//	List<string> entries = SplitLine(line).ToList();
 
-            if (!entries.Any())
-            {
-                return null;
-            }
+		//	if (!entries.Any())
+		//	{
+		//		return null;
+		//	}
 
-            CodeOwnerEntry coe = new CodeOwnerEntry
-            {
-                // the first entry is the path/regex
-                PathExpression = entries[0],
-                ContainsWildcard = entries[0].Contains('*')
-            };
+		//	CodeOwnerEntry coe = new CodeOwnerEntry
+		//	{
+		//		// the first entry is the path/regex
+		//		PathExpression = entries[0],
+		//		ContainsWildcard = entries[0].Contains('*')
+		//	};
 
-            // remove the '/' from the path
-            if (coe.PathExpression.StartsWith("/"))
-            {
-                coe.PathExpression = coe.PathExpression.Substring(1);
-            }
+		//	// remove the '/' from the path
+		//	if (coe.PathExpression.StartsWith("/"))
+		//	{
+		//		coe.PathExpression = coe.PathExpression.Substring(1);
+		//	}
 
-            for (int i = 1; i < entries.Count; i++)
-            {
-                string entry = entries[i].Trim();
+		//	for (int i = 1; i < entries.Count; i++)
+		//	{
+		//		string entry = entries[i].Trim();
 
-                if (entry[0] == '@')
-                {
-                    coe.Owners.Add(entry.Substring(1));
-                }
+		//		if (entry[0] == '@')
+		//		{
+		//			coe.Owners.Add(entry.Substring(1));
+		//		}
 
-                if (entry[0] == '%')
-                {
-                    coe.Labels.Add(entry.Substring(1));
-                }
-            }
+		//		if (entry[0] == '%')
+		//		{
+		//			coe.Labels.Add(entry.Substring(1));
+		//		}
+		//	}
 
-            return coe;
-        }
+		//	return coe;
+		//}
 
-        private static IEnumerable<string> SplitLine(string line)
-        {
-            // Split the line into segments that are delimited by '@', '%' and the end of the string.
+		private static IEnumerable<string> SplitLine(string line)
+		{
+			// Split the line into segments that are delimited by '@', '%' and the end of the string.
 
-            int previousSplit = 0;
-            for (int i = 0; i < line.Length; i++)
-            {
-                if (line[i] == '@' || line[i] == '%')
-                {
-                    yield return line.Substring(previousSplit, i - previousSplit).Trim();
-                    previousSplit = i;
-                }
-            }
+			int previousSplit = -1;
+			for (int i = 0; i < line.Length; i++)
+			{
+				if (line[i] == '@' || line[i] == '%')
+				{
 
-            // add the last entry
-            yield return line.Substring(previousSplit, line.Length - previousSplit).Trim();
-        }
+					if (previousSplit != -1)
+					{
+						yield return line.Substring(previousSplit, i - previousSplit).Trim();
+					}
+					previousSplit = i;
+				}
+			}
 
-        public override string ToString()
-        {
-            return $"RegEx:{ContainsWildcard} Expression:{PathExpression} Owners:{string.Join(',', Owners)}  Labels:{string.Join(',', Labels)}";
-        }
-    }
+			// add the last entry
+			yield return line.Substring(previousSplit, line.Length - previousSplit).Trim();
+		}
+
+		public override string ToString()
+		{
+			return $"RegEx:{ContainsWildcard} Expression:{PathExpression} Owners:{string.Join(',', Owners)}  Labels:{string.Join(',', Labels)}";
+		}
+
+		internal static void ParseLabels(CodeOwnerEntry entry, string line)
+		{
+			// Parse a line that looks like # PRLabel: %Label, %Label
+
+			// If we don't have a ':', nothing to do
+			int colonPosition = line.IndexOf(':');
+			if (colonPosition == -1)
+			{
+				return;
+			}
+
+			line = line.Substring(colonPosition + 1).Trim();
+			foreach (string label in SplitLine(line).ToList())
+			{
+				if (label[0] == '%')
+				{
+					entry.Labels.Add(label.Substring(1));
+				}
+
+			}
+		}
+
+		internal static void ParseOwnersAndPath(CodeOwnerEntry entry, string line)
+		{
+			if (string.IsNullOrEmpty(line) || line.StartsWith('#'))
+			{
+				return;
+			}
+
+			// Get the start of the owner in the string
+			int ownerStartPosition = line.IndexOf('@');
+			if (ownerStartPosition == -1)
+			{
+				return;
+			}
+
+			string path = line.Substring(0, ownerStartPosition).Trim();
+			// the first entry is the path/regex
+			entry.PathExpression = path;
+			entry.ContainsWildcard = path.Contains('*');
+			// remove the '/' from the path
+			if (entry.PathExpression.StartsWith("/"))
+			{
+				entry.PathExpression = entry.PathExpression.Substring(1);
+			}
+
+			// remove the path from the string.
+			line = line.Substring(ownerStartPosition);
+
+			// At this point, we know the line does not start with '#'
+			List<string> entries = SplitLine(line).ToList();
+
+			for (int i = 0; i < entries.Count; i++)
+			{
+				string author = entries[i].Trim();
+
+				if (author[0] == '@')
+				{
+					entry.Owners.Add(author.Substring(1));
+				}
+			}
+		}
+	}
 }

02f73fb57808470c1eebdedce378b614de17bbdd
praveenkuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2020-08-11 12:52:36 -0700

    Revert help text and set python executable path to python3 (#864)

--- src/dotnet/APIView/APIViewWeb/Languages/PythonLanguageService.cs
+++ src/dotnet/APIView/APIViewWeb/Languages/PythonLanguageService.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace APIViewWeb
 {
@@ -10,10 +11,18 @@ namespace APIViewWeb
     {
         public override string Name { get; } = "Python";
         public override string Extension { get; } = ".whl";
-        public override string ProcessName { get; } = "python";
         public override string VersionString { get; } = "0.1.3";
 
+        private readonly string _python3ProcessName;
+        public override string ProcessName => _python3ProcessName;
 
+        public PythonLanguageService(IConfiguration configuration)
+        {
+            // Default python version in azure web app is py 2.7 and python3 is not used by default
+            // when running script using python process.
+            // We need to run the script using full path to python3
+            _python3ProcessName = configuration["PYTHON3HOME"] ?? "python";
+        }
         public override string GetProcessorArguments(string originalName, string tempDirectory, string jsonPath)
         {
             var pythonScriptPath = Path.Combine(

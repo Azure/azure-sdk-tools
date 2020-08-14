4642a52bbaf984cea41df09814dca657b05fe407
Sima Zhu <sizhu@microsoft.com>
2020-08-14 13:03:28 -0700

    Address comments

--- eng/common/scripts/Verify-Links.ps1
+++ eng/common/scripts/Verify-Links.ps1
@@ -16,9 +16,9 @@ param (
   # flag to allow resolving relative paths or not
   [bool] $resolveRelativeLinks = $true,
   # development repo owner from pr
-  [string] $sourceCommit = "",
+  [string] $branchReplaceRegex = "",
   # development repo branch from pr
-  [string] $sourceRepoUrl = ""
+  [string] $branchReplacementName = ""
 )
 
 $ProgressPreference = "SilentlyContinue"; # Disable invoke-webrequest progress dialog
@@ -171,10 +171,12 @@ function CheckLink ([System.Uri]$linkUri)
   }
   $checkedLinks[$linkUri] = $true;
 }
-$GithubRegex = "($sourceRepoUrl/blob/)([a-zA-Z0-9/-/_]+)(/.*)"
 $ReplacementPattern = "`${1}$sourceCommit`$3"
 function ReplaceGithubLink([string]$originLink) {
-  return $originLink -replace $GithubRegex, $ReplacementPattern
+  if ($sourceCommit -eq "") {
+    return $originLink
+  }
+  return $originLink -replace $branchReplaceRegex, $ReplacementPattern
 }
 
 function GetLinks([System.Uri]$pageUri)

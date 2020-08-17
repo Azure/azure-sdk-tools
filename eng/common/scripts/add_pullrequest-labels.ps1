a922b3b5152cb43e1e4d524425a0ec9f1dc3b3b6
Sima Zhu <sizhu@microsoft.com>
2020-08-17 09:51:37 -0700

    exit when label is empty or null

--- eng/common/scripts/add_pullrequest-labels.ps1
+++ eng/common/scripts/add_pullrequest-labels.ps1
@@ -15,28 +15,32 @@ param(
     $PRLabel
 )
 
+# at least one of label needs to be populated
+if (-not $PRLabel ) {
+    Write-Host "No labels provided for addition, exiting."
+    exit 0
+  }
+
 # Add labels to the pull request
-if ($null -ne $PRLabel -and  "" -ne $PRLabel) {
-    $uri = "https://api.github.com/repos/$RepoOwner/$RepoName/issues/$PRNumber"
-    $data = @{
-        maintainer_can_modify = $true
-        labels                = $PRLabel
-    }
-    $headers = @{
-        Authorization = "bearer $AuthToken"
-    }
-      
-    try {
-        $resp = Invoke-RestMethod -Method PATCH -Headers $headers $uri -Body ($data | ConvertTo-Json)
-    }
-    catch {
-        Write-Error "Invoke-RestMethod $uri failed with exception:`n$_"
-        exit 1
-    }
-
-    $resp | Write-Verbose
-    Write-Host -f green "Label added to pull request: https://github.com/$RepoOwner/$RepoName/pull/($PRNumber)"
-
-    # setting variable to reference the pull request by number
-    Write-Host "##vso[task.setvariable variable=Submitted.PullRequest.Number]$PRNumber"                                                                       
+$uri = "https://api.github.com/repos/$RepoOwner/$RepoName/issues/$PRNumber"
+$data = @{
+    maintainer_can_modify = $true
+    labels                = $PRLabel
 }
+$headers = @{
+    Authorization = "bearer $AuthToken"
+}
+    
+try {
+    $resp = Invoke-RestMethod -Method PATCH -Headers $headers $uri -Body ($data | ConvertTo-Json)
+}
+catch {
+    Write-Error "Invoke-RestMethod $uri failed with exception:`n$_"
+    exit 1
+}
+
+$resp | Write-Verbose
+Write-Host -f green "Label added to pull request: https://github.com/$RepoOwner/$RepoName/pull/($PRNumber)"
+
+# setting variable to reference the pull request by number
+Write-Host "##vso[task.setvariable variable=Submitted.PullRequest.Number]$PRNumber"                                                                       

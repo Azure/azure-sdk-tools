f23675108c86600c51be2d91b9c465c8c762eaab
Catalina Peralta <caperal@microsoft.com>
2020-08-19 14:08:03 -0700

    Add license header, remove output folder

--- src/go/testdata/test_funcDecl/test.go
+++ src/go/testdata/test_funcDecl/test.go
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package testfuncdecl
 
 func DoSomething(s string, i int, b bool, f1, f2 float32) (string, error) {

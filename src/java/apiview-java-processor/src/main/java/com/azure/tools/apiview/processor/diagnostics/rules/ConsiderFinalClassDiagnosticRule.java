e9cbf80c033416efb6bfc4a902601c3fc7d2d534
Jonathan Giles <jonathan.giles@microsoft.com>
2020-08-20 14:05:07 +1200

    [APIView] Java improvements (#889)
    1) Remove the version number from the generated review name.
    2) Add support for specifying the 'Language' on all generated reviews. I hope that this might, in the future, lead to a way of filtering the view to only see APIViews for certain languages only. From now on, all Java reviews will have a 'Language: Java' entry. This is not currently being exported to the generated JSON file until there is cross-language support.
    3) New diagnostics:
        1) Added a new diagnostic rule to validate that fluent model types setters return the same type as the fluent type itself.
        2) Added new diagnostic to encourage users to make classes final whenever possible.
    4) Add support for more annotations, as well as linking between references in the annotation properties.

---
+++ src/java/apiview-java-processor/src/main/java/com/azure/tools/apiview/processor/diagnostics/rules/ConsiderFinalClassDiagnosticRule.java
@@ -0,0 +1,22 @@
+package com.azure.tools.apiview.processor.diagnostics.rules;
+
+import com.azure.tools.apiview.processor.diagnostics.DiagnosticRule;
+import com.azure.tools.apiview.processor.model.APIListing;
+import com.azure.tools.apiview.processor.model.Diagnostic;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Modifier;
+
+import static com.azure.tools.apiview.processor.analysers.util.ASTUtils.makeId;
+
+public class ConsiderFinalClassDiagnosticRule implements DiagnosticRule {
+
+    @Override
+    public void scan(final CompilationUnit cu, final APIListing listing) {
+        cu.getTypes().forEach(type -> {
+            if (!type.hasModifier(Modifier.Keyword.FINAL)) {
+                listing.addDiagnostic(new Diagnostic(makeId(type),
+                        "Consider making all classes final by default - only make non-final if subclassing is supported."));
+            }
+        });
+    }
+}

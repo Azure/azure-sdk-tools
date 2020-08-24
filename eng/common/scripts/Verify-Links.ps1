4f83b80c85274759ff2336ca04af988f9cb0e2f1
Sima Zhu <sizhu@microsoft.com>
2020-08-24 10:42:10 -0700

    remove extra logging

--- eng/common/scripts/Verify-Links.ps1
+++ eng/common/scripts/Verify-Links.ps1
@@ -286,10 +286,7 @@ while ($pageUrisToCheck.Count -ne 0)
   Write-Host "Found $($linkUris.Count) links on page $pageUri";
   $badLinksPerPage = @();
   foreach ($linkUri in $linkUris) {
-    Write-Host "before the replace $linkUri"
-    Write-Host "the commit $branchReplacementName"
-    $linkUri = ReplaceGithubLink $linkUri
-    Write-Host "after the replace $linkUri"
+    $linkUri = ReplaceGithubLink $linkUri\
     $isLinkValid = CheckLink $linkUri
     if (!$isLinkValid -and !$badLinksPerPage.Contains($linkUri)) {
       $badLinksPerPage += $linkUri

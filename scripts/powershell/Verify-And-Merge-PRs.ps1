ee8f63505586b566be50e4d15283e8250669ac86
Wes Haggard <Wes.Haggard@microsoft.com>
2020-08-26 20:43:27 -0700

    Clean-up verify script and fix devopslogging parameter

--- scripts/powershell/Verify-And-Merge-PRs.ps1
+++ scripts/powershell/Verify-And-Merge-PRs.ps1
@@ -1,176 +1,108 @@
 param(
-    [Parameter(Mandatory = $true)]
-    $AuthToken,
-
-    [Parameter(Mandatory = $true)]
-    $PRDataArtifactPath,
-
-    [switch]$devOpsLogging = $false
+  $AuthToken,
+  $PRDataArtifactPath,
+  $ShouldMerge,
+  [switch]$devOpsLogging = $false
 )
 
 function LogWarning
 {
-  if ($devOpsLogging)
-  {
+  if ($devOpsLogging) {
     Write-Host "##vso[task.LogIssue type=warning;]$args"
   }
-  else
-  {
+  else {
     Write-Warning "$args"
   }
 }
 
 function LogError
 {
-  if ($devOpsLogging)
-  {
+  if ($devOpsLogging) {
     Write-Host "##vso[task.logissue type=error]$args"
   }
-  else
-  {
+  else {
     Write-Error "$args"
   }
 }
 
-function LogInfo
-{
-  if ($devOpsLogging)
-  {
-    Write-Host "##vso[task.logissue type=debug]$args"
-  }
-  else
-  {
-    Write-Error "$args"
-  }
+$ReadyForMerge = $true
+$mergablePRs = @()
+$headers = @{ }
+
+if ($null -eq $ShouldMerge) {
+  $ShouldMerge = $null -ne $AuthToken;
 }
 
-
-$PRObjs = [System.Collections.ArrayList]::new()
-$ClosedPRs = [System.Collections.ArrayList]::new()
-$UnexpectedState = [System.Collections.ArrayList]::new()
-$MergablePRs = [System.Collections.ArrayList]::new()
-$BlockedPRs = [System.Collections.ArrayList]::new()
-
-
-$GitHubAPIUrlBase = "https://api.github.com/repos/"
-$Headers = @{
+if ($AuthToken) {
+  $headers = @{
     Authorization = "bearer $AuthToken"
+  }
 }
 
-# Get PRData from File
 $PRData = Get-Content $PRDataArtifactPath
-foreach ($line in $PRData)
-{
-    $PRDetails = $line.Split(';')
-    $PRDataObj = [PSCustomObject]@{
-        RepoOwner = $PRDetails[0]
-        RepoName = $PRDetails[1]
-        PRNumber = $PRDetails[2]
-    }
-    [void]$PRObjs.Add($PRDataObj)
-}
-
 # Confirm Mergability
-foreach ($obj in $PRObjs)
+foreach ($prDataLine in $PRData)
 {
-    $APIUrl = "${GitHubAPIUrlBase}$($obj.RepoOwner)/$($obj.RepoName)/pulls/$($obj.PRNumber)"
-    try
-    {
-        $response = Invoke-RestMethod -Headers $Headers $APIUrl
-        if ($response.merged)
-        {
-            LogInfo "PR $($obj.PRNumber) in Repo $($obj.RepoName) is Merged"
-            continue
-        }
-        elseif ($response.state -eq "closed")
-        {
-            LogInfo "PR $($obj.PRNumber) in Repo $($obj.RepoName) is Closed"
-            [void]$ClosedPRs.Add($obj)
-        }
-        else 
-        {
-            if ($response.mergeable_state -ne "clean")
-            {
-                LogInfo "PR $($obj.PRNumber) in Repo $($obj.RepoName) is Blocked"
-                [void]$BlockedPRs.Add($obj)
-            }
-            elseif ($response.mergeable -and ($response.mergeable_state -eq "clean"))
-            {
-                $obj | Add-Member -MemberType NoteProperty -Name "HeadSHA" -Value $response.head.sha
-                $obj | Add-Member -MemberType NoteProperty -Name "HeadLabel" -Value $response.head.label
-                LogInfo "PR $($obj.PRNumber) in Repo $($obj.RepoName) is MergablePR"
-                [void]$MergablePRs.Add($obj)
-            }
-            else 
-            {
-                $obj | Add-Member -MemberType NoteProperty -Name "Response" -Value $response
-                LogInfo "PR $($obj.PRNumber) in Repo $($obj.RepoName) is in an UnexpectedState"
-                [void]$UnexpectedState.Add($obj)
-            }
-        }
-    }
-    catch
-    {
-        LogError "Invoke-RestMethod ${APIUrl} failed with exception:`n$_"
-        exit 1
-    }
-}
+  $repoOwner, $repoName, $prNumber = $prDataLine.Split(";")
 
-# Verify and Report all Unmergable PRS
-if (($BlockedPRs.Count -gt 0) -or ($ClosedPRs.Count -gt 0) -or ($UnexpectedState.Count -gt 0))
-{
-    if ($BlockedPRs.Count -gt 0)
-    {
-        LogWarning "The following PRs are Blocked, Please ensure all checks are green and PR is approved"
-        foreach ($obj in $BlockedPRs)
-        {
-            LogWarning "`thttps://github.com/$($obj.RepoOwner)/$($obj.RepoName)/pull/$($obj.PRNumber): State: Blocked"
-        }
+  $prApiUrl = "https://api.github.com/repos/${repoOwner}/${repoName}/pulls/${prNumber}"
+  try
+  {
+    $response = Invoke-RestMethod -Headers $headers $prApiUrl
+    if ($response.merged) {
+      Write-Host "${prApiUrl} is merged."
     }
-
-    if ($ClosedPRs.Count -gt 0)
-    {
-        LogWarning "The following PRs are Closed, Please investigate why a Sync PR was closed prematurely without being merged."
-        foreach ($obj in $ClosedPRs)
-        {
-            LogWarning "`thttps://github.com/$($obj.RepoOwner)/$($obj.RepoName)/pull/$($obj.PRNumber): State: Closed"
-        }
+    elseif ($response.state -eq "closed") {
+      LogWarning "${prApiUrl} is closed. Please investigate why was not merged."
+      $ReadyForMerge = $false
     }
+    elseif ($response.mergeable -and $response.mergeable_state -eq "clean") {
+      Write-Host "${prApiUrl} is ready to merge."
 
-    if ($UnexpectedState.Count -gt 0)
-    {
-        LogWarning "The following PRs are Unexpected state, Please contact EngSys team"
-        foreach ($obj in $UnexpectedState)
-        {
-            LogWarning "`thttps://github.com/$($obj.RepoOwner)/$($obj.RepoName)/pull/$($obj.PRNumber): State: Unexpected"
-            LogWarning $obj.Response
-        }
+      $prApiUrl | Add-Member -MemberType NoteProperty -Name "HeadSHA" -Value $response.head.sha
+      $mergablePRs += $prApiUrl
     }
-
-    LogError "At least one sync PR is not able to be merged please investigate and then retry running this job to auto-merge them again"
+    elseif ($response.mergeable_state -ne "clean") {
+      LogWarning "${prApiUrl} is blocked ($($response.mergeable_state)). Please ensure all checks are green and reviewers have approved."
+      $ReadyForMerge = $false
+    }
+    else {
+      LogWarning "${prApiUrl} is in an unknown state please contact engineering system team to understand the state."
+      LogWarning $response
+      $ReadyForMerge = $false
+    }
+  }
+  catch {
+    LogError "Invoke-RestMethod ${prApiUrl} failed with exception:`n$_"
     exit 1
+  }
 }
 
-# Merge Pull Requests
-foreach ($obj in $MergablePRs)
+if (!$ReadyForMerge) {
+  LogError "At least one sync PR is not able to be merged please investigate and then retry running this job to auto-merge them again"
+  exit 1
+}
+
+if ($ReadyForMerge -and $ShouldMerge)
 {
-    $MergeAPIUrl = "${GitHubAPIUrlBase}$($obj.RepoOwner)/$($obj.RepoName)/pulls/$($obj.PRNumber)/merge"
+  # Merge Pull Requests
+  foreach ($mergablePR in $mergablePRs)
+  {
+    $mergeApiUrl = $mergablePr + "/merge"
 
     $data = @{
-        sha = $obj.HeadSHA
-        merge_method = "squash"
+      sha = $mergablePR.HeadSHA
+      merge_method = "squash"
     }
 
     # Merge Pull Request
-    try 
-    {
-        LogInfo "Merging $($obj.PRNumber) in Repo $($obj.RepoName)"
-        $response = Invoke-RestMethod -Method Put -Headers $Headers $MergeAPIUrl -Body ($data | ConvertTo-Json)
+    try {
+      Write-Host "Merging $mergablePR"
+      $response = Invoke-RestMethod -Method Put -Headers $headers $mergeApiUrl -Body ($data | ConvertTo-Json)
     }
-    catch 
-    {
-        LogWarning "Could not merge https://github.com/$($obj.RepoOwner)/$($obj.RepoName)/pull/$($obj.PRNumber)"
-        LogError "Invoke-RestMethod [$MergeAPIUrl] failed with exception:`n$_"
-        exit 1
+    catch {
+      LogError "Invoke-RestMethod [$mergeApiUrl] failed with exception:`n$_"
+      exit 1
     }
+  }
 }

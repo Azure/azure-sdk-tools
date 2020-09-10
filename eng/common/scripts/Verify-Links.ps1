b48ad151ddd7ce880b045516c1ecb7ca5016a2cc
Sima Zhu <48036328+sima-zhu@users.noreply.github.com>
2020-09-10 13:33:37 -0700

    Update eng/common/scripts/Verify-Links.ps1
    Co-authored-by: Wes Haggard <weshaggard@users.noreply.github.com>

--- eng/common/scripts/Verify-Links.ps1
+++ eng/common/scripts/Verify-Links.ps1
@@ -198,7 +198,7 @@ function CheckLink ([System.Uri]$linkUri)
   if ($checkLinkGuidance) {
     # Check if the url is relative links
     if (!$linkUri.IsAbsoluteUri) {
-      LogWarning "DO NOT use relative link $linkUri. Please use absolute link instead. Check here for more infomation: https://aka.ms/azsdk/guideline/links"
+      LogWarning "DO NOT use relative link $linkUri. Please use absolute link instead. Check here for more information: https://aka.ms/azsdk/guideline/links"
       $linkValid = $false
     }
      # Check if link uri includes locale info.

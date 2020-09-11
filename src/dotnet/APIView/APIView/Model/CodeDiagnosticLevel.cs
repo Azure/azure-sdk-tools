49a24fcac705a19a0a7f0d6818c07236bdd7c9d4
praveenkuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2020-09-11 10:58:38 -0700

    Diagnostic level chagnes

---
+++ src/dotnet/APIView/APIView/Model/CodeDiagnosticLevel.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+namespace APIView
+{
+    public enum CodeDiagnosticLevel
+    {
+        Info = 1,
+        Warning = 2,
+        Error = 3
+    }
+}

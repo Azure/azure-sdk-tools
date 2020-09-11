96d707a07641c161d32f3a9c0a3f64b5297ea409
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-09-10 18:41:45 -0700

    Search perf (#988)
    * Make header appear clickable
    
    * Add a timeout to improve perf when filtering

--- src/dotnet/APIView/APIViewWeb/Client/src/reviews.ts
+++ src/dotnet/APIView/APIViewWeb/Client/src/reviews.ts
@@ -5,17 +5,20 @@
   searchBox.focus();
 
   const context = $(".review-name") as any;
-  searchBox.keyup(function () {
-
-    // highlight matching text using mark.js framework and hide rows that don't match
-    const searchText = (searchBox.val() as string).toUpperCase();
-    context.closest("tr").show().unmark();
-    if (searchText) {
-      context.mark(searchText, {
-        done: function () {
-          context.not(":has(mark)").closest("tr").hide();
-        }
-      });
-    }
+  searchBox.on("input", function () {
+    setTimeout(function () {
+      // highlight matching text using mark.js framework and hide rows that don't match
+      const searchText = (searchBox.val() as string).toUpperCase();
+      context.closest("tr").show().unmark();
+      if (searchText) {
+        context.mark(searchText, {
+          done: function () {
+            context.not(":has(mark)").closest("tr").hide();
+          }
+        });
+      }
+    },
+      // use a timeout to prevent bad perf for holding down backspace or really fast typing
+      200);
   });
 });

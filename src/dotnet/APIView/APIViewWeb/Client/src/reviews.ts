aa55ba42d0d8e5a43c158594ae7ac9856bd03bca
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-09-10 17:24:31 -0700

    PR fb

--- src/dotnet/APIView/APIViewWeb/Client/src/reviews.ts
+++ src/dotnet/APIView/APIViewWeb/Client/src/reviews.ts
@@ -4,23 +4,18 @@
   // make the search box the initial focused element so users can just start typing once page loads
   searchBox.focus();
 
-  const context = $(".review-name");
+  const context = $(".review-name") as any;
   searchBox.keyup(function () {
 
-    // highlight matching text using mark.js framework
+    // highlight matching text using mark.js framework and hide rows that don't match
     const searchText = (searchBox.val() as string).toUpperCase();
-    (context as any).unmark();
+    context.closest("tr").show().unmark();
     if (searchText) {
-      (context as any).mark(searchText, {
+      context.mark(searchText, {
         done: function () {
-          $(this).not(":has(mark)").hide();
+          context.not(":has(mark)").closest("tr").hide();
         }
       });
     }
-
-    // hide rows that do not match
-    context.each(function () {
-      $(this).closest("tr").toggle($(this)[0].innerText.toUpperCase().indexOf(searchText) > -1);
-    });
   });
 });

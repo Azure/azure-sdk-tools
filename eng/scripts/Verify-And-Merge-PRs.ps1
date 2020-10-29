6b0f68481687bb2a3a634bd2c59a505a8129ed50
Chidozie Ononiwu <chononiw@microsoft.com>
2020-10-28 19:56:37 -0700

    Use different commit SHA instead

--- eng/scripts/Verify-And-Merge-PRs.ps1
+++ eng/scripts/Verify-And-Merge-PRs.ps1
@@ -9,6 +9,7 @@ param(
 $ReadyForMerge = $true
 $mergablePRs = @()
 $headers = @{ }
+$RetryCount = 5
 
 if ($null -eq $ShouldMerge) {
   $ShouldMerge = $null -ne $AuthToken;
@@ -22,43 +23,60 @@ if ($AuthToken) {
 
 $PRData = Get-Content $PRDataArtifactPath
 # Confirm Mergability
-foreach ($prDataLine in $PRData)
+
+do 
 {
-  $repoOwner, $repoName, $prNumber = $prDataLine.Split(";")
-
-  $prApiUrl = "https://api.github.com/repos/${repoOwner}/${repoName}/pulls/${prNumber}"
-  $prUrl = "https://github.com/${repoOwner}/${repoName}/pull/${prNumber}"
-  
-  try
+  $unMergablePRs = @()
+  foreach ($prDataLine in $PRData)
   {
-    $response = Invoke-RestMethod -Headers $headers $prApiUrl
-    if ($response.merged) {
-      Write-Host "${prUrl} is merged."
-    }
-    elseif ($response.state -eq "closed") {
-      LogWarning "${prUrl} is closed. Please investigate why was not merged."
-      $ReadyForMerge = $false
-    }
-    elseif ($response.mergeable -and $response.mergeable_state -eq "clean") {
-      Write-Host "${prUrl} is ready to merge."
+    $repoOwner, $repoName, $prNumber = $prDataLine.Split(";")
 
-      $mergablePRs += @{ Url = $prApiUrl; HeadSHA = $response.head.sha }
+    $prApiUrl = "https://api.github.com/repos/${repoOwner}/${repoName}/pulls/${prNumber}"
+    $prUrl = "https://github.com/${repoOwner}/${repoName}/pull/${prNumber}"
+    
+    try
+    {
+      $response = Invoke-RestMethod -Headers $headers $prApiUrl
+      if ($response.merged) {
+        Write-Host "${prUrl} is merged."
+      }
+      elseif ($response.state -eq "closed") {
+        LogWarning "${prUrl} is closed. Please investigate why was not merged."
+        $ReadyForMerge = $false
+      }
+      elseif ($response.mergeable -and $response.mergeable_state -eq "clean") {
+        Write-Host "${prUrl} is ready to merge."
+
+        $mergablePRs += @{ Url = $prApiUrl; HeadSHA = $response.head.sha }
+      }
+      elseif ($response.mergeable_state -ne "clean") {
+        LogWarning "${prUrl} is blocked ($($response.mergeable_state)). Please ensure all checks are green and reviewers have approved."
+        $ReadyForMerge = $false
+        $unMergablePRs += $prDataLine
+      }
+      else {
+        LogWarning "${prUrl} is in an unknown state please contact engineering system team to understand the state."
+        LogWarning $response
+        $ReadyForMerge = $false
+        $unMergablePRs += $prDataLine
+      }
     }
-    elseif ($response.mergeable_state -ne "clean") {
-      LogWarning "${prUrl} is blocked ($($response.mergeable_state)). Please ensure all checks are green and reviewers have approved."
-      $ReadyForMerge = $false
-    }
-    else {
-      LogWarning "${prUrl} is in an unknown state please contact engineering system team to understand the state."
-      LogWarning $response
-      $ReadyForMerge = $false
+    catch {
+      LogError "Invoke-RestMethod ${prApiUrl} failed with exception:`n$_"
+      exit 1
     }
   }
-  catch {
-    LogError "Invoke-RestMethod ${prApiUrl} failed with exception:`n$_"
-    exit 1
+  if ($unMergablePRs.Count -gt 0)
+  {
+    $PRData = $unMergablePRs
+    Start-Sleep -Seconds 30
+    $RetryCount--
+    LogDebug "Retrying merging for unmergable Prs"
+    $ReadyForMerge = $true
   }
 }
+while (($unMergablePRs.Count -gt 0) -and ($RetryCount -gt 0) -and $ShouldMerge)
+
 
 if (!$ReadyForMerge) {
   LogError "At least one sync PR is not able to be merged please investigate and then retry running this job to auto-merge them again"

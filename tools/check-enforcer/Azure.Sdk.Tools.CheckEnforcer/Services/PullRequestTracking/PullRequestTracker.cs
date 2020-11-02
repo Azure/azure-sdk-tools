cf0a1cf9971144e85bc219660c6b0e1ae1f84968
Mitch Denny <midenn@microsoft.com>
2020-11-03 10:38:21 +1100

    Add state tracking in Azure Storage (tables). (#1162)

--- tools/check-enforcer/Azure.Sdk.Tools.CheckEnforcer/Services/PullRequestTracking/PullRequestTracker.cs
+++ tools/check-enforcer/Azure.Sdk.Tools.CheckEnforcer/Services/PullRequestTracking/PullRequestTracker.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Azure.Data.Tables;
+using Azure.Security.KeyVault.Secrets;
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Text;
@@ -8,31 +10,61 @@ namespace Azure.Sdk.Tools.CheckEnforcer.Services.PullRequestTracking
 {
     public class PullRequestTracker : IPullRequestTracker
     {
-        private List<PullRequestTrackingTicket> pullRequestTrackingTickets = new List<PullRequestTrackingTicket>();
+        private SecretClient secretClient;
 
-        public PullRequestTracker()
+        public PullRequestTracker(SecretClient secretClient)
         {
+            this.secretClient = secretClient;
+        }
+
+        private const string ConnectionStringSecretName = "table-storage-connection-string";
+        private const string PullRequestStateTableName = "pullrequests";
+        private object tableClientLock = new object();
+        private TableClient tableClient;
+
+        private TableClient GetTableClient()
+        {
+            if (tableClient == null)
+            {
+                lock (tableClientLock)
+                {
+                    if (tableClient == null)
+                    {
+                        KeyVaultSecret secret = secretClient.GetSecret(ConnectionStringSecretName);
+                        var connectionString = secret.Value;
+
+                        tableClient = new TableClient(connectionString, PullRequestStateTableName);
+                    }
+                }
+            }
+
+            return tableClient;
         }
 
         public async Task StartTrackingPullRequestAsync(PullRequestTrackingTicket pullRequestTrackingTicket)
         {
-            pullRequestTrackingTickets.Add(pullRequestTrackingTicket);
+            var tableClient = GetTableClient();
+            await tableClient.UpsertEntityAsync<PullRequestTrackingTicket>(pullRequestTrackingTicket, TableUpdateMode.Replace);
         }
 
         public async Task StopTrackingPullRequestAsync(PullRequestTrackingTicket pullRequestTrackingTicket)
         {
-            pullRequestTrackingTickets.RemoveAll((ticket) =>
-            {
-                return
-                    ticket.InstallationId == pullRequestTrackingTicket.InstallationId &&
-                    ticket.RepositoryId == pullRequestTrackingTicket.RepositoryId &&
-                    ticket.PullRequestNumber == pullRequestTrackingTicket.PullRequestNumber;
-            });
+            var tableClient = GetTableClient();
+            await tableClient.DeleteEntityAsync(pullRequestTrackingTicket.PartitionKey, pullRequestTrackingTicket.RowKey);
         }
 
         public async Task<IEnumerable<PullRequestTrackingTicket>> GetTrackedPullRequestsAsync()
         {
-            return pullRequestTrackingTickets.ToImmutableList();
+            var tableClient = GetTableClient();
+            var pagable = tableClient.QueryAsync<PullRequestTrackingTicket>();
+            var tickets = new List<PullRequestTrackingTicket>();
+
+            await foreach (var ticket in pagable)
+            {
+                tickets.Add(ticket);
+            }
+
+            return tickets;
         }
     }
 }

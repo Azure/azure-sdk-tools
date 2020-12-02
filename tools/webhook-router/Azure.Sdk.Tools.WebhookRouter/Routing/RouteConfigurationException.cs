e41e7f4b93f7accbe535dd794bce84e91750ab31
Mitch Denny <midenn@microsoft.com>
2020-12-02 12:35:44 +1100

    Output payload for router config errors.

---
+++ tools/webhook-router/Azure.Sdk.Tools.WebhookRouter/Routing/RouteConfigurationException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.Sdk.Tools.WebhookRouter.Routing
+{
+    [Serializable]
+    public class RouterConfigurationException : RouterException
+    {
+        public RouterConfigurationException() { }
+        public RouterConfigurationException(string message) : base(message) { }
+        public RouterConfigurationException(string message, Exception inner) : base(message, inner) { }
+        protected RouterConfigurationException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}

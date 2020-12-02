54934467e4dd0646bca790e68e5d565afad328f6
Scott Beddall <45376673+scbedd@users.noreply.github.com>
2020-12-02 12:59:51 -0800

    Docs.MS SyncToC Update (#1232)
    * update sync_tocs.py to use a PathResolver class that abstracts the href updates
    
    * update sync_tocs.py to handle moniker-specific folders when updating a target ToC

--- scripts/python/docs-automation/sync_tocs.py
+++ scripts/python/docs-automation/sync_tocs.py
@@ -15,6 +15,79 @@ import json
 # The drop-in replacement oyaml is a handy solution for us.
 import oyaml as yaml
 
+MONIKER_REPLACEMENTS = ['{moniker}','<moniker>']
+
+class PathResolver:
+    def __init__(self, doc_repo_location = None, readme_suffix = "", moniker = ""):
+        self.readme_suffix = readme_suffix
+        self.excluded_href_paths = []
+        self.target_moniker = moniker
+
+        self.doc_repo_location = doc_repo_location
+
+        if self.doc_repo_location:
+            self.excluded_href_paths = self.get_non_standard_hrefs(self.doc_repo_location)
+
+    # the doc builds have the capability to reference readmes from external repos (they resolve during publishing)
+    # this means that we can't simply check the href values for existence. If they are an href that STARTS with one of the
+    # "dependent repositories" than we should leave them exactly as is.
+    # amend_href is the core of the logic for handling referenced files and ensures that we cannot refer to the same readme twice
+    # from two different reference ymls
+    def amend_href(self, toc_dict):
+        if not self.doc_repo_location:
+            return toc_dict
+
+        # We want the readme suffix only if we're dealing with a case that does NOT have a target_moniker
+        # this will maintain backwards compatibility with the first method of invoking this script. We don't
+        # want to entirely remove the "suffix" concept though, as the trailing `.pre` is used to update the uid 
+        # on the preview ToC.ymls.
+        # After a moniker folder is introduced, we NO LONGER want the suffix on the readme to change.
+        suffix = "-" + self.readme_suffix + ".md" if self.readme_suffix and not self.target_moniker else  ".md"
+        input_string = toc_dict["href"]
+
+        # if this is an external readme, we should not attempt to resolve the file to a different one, just return with no changes
+        if any([input_string.startswith(href) for href in self.excluded_href_paths]):
+            return toc_dict 
+
+        # create a resolvable path to the readme on disk, without any of the docs ms specificity
+        resolvable_path = os.path.normpath(os.path.join(self.doc_repo_location, input_string.replace("~/", "")))
+
+        # apply moniker folder adjustments if necessary
+        if self.target_moniker is not None:
+            for replacement in MONIKER_REPLACEMENTS:
+                # input string maintains leading ~/ necessary for docs. update the moniker folder if it exists
+                input_string.replace(replacement, self.target_moniker)
+
+                # the resolvable path is different from the input_string in that it is actually a resolvable path.
+                # update it with the moniker folder so we can test for existence of the file
+                resolvable_path.replace(replacement, self.target_moniker)
+            
+        # finally apply suffix
+        possible_target_readme = os.path.splitext(resolvable_path)[0] + suffix
+
+        if os.path.exists(possible_target_readme):
+            toc_dict["href"] = input_string.replace(".md", suffix)
+        else:
+            toc_dict.pop("href")
+            toc_dict["landingPageType"] = "Service"
+
+        return toc_dict
+
+    # the doc builds have the capability to reference readmes from external repos (they resolve during publishing)
+    # this means that we can't simply check the href values for existence. If they are an href that STARTS with one of the
+    # "dependent repositories" than we should leave them exactly as is. This function returns the start paths
+    def get_non_standard_hrefs(self, doc_repo_location):
+        excluded_href_paths = []
+
+        target = os.path.join(doc_repo_location, ".openpublishing.publish.config.json")
+        with open(target, "r") as f:
+            data = json.load(f)
+
+        for dependent_repo in data["dependent_repositories"]:
+            excluded_href_paths.append("~/{}".format(dependent_repo["path_to_root"]))
+
+        return excluded_href_paths
+
 
 def filter_children(targeted_ns_list, known_namespaces):
     amended_list = []
@@ -32,33 +105,9 @@ def filter_children(targeted_ns_list, known_namespaces):
 
     return amended_list
 
-# the doc builds have the capability to reference readmes from external repos (they resolve during publishing)
-# this means that we can't simply check the href values for existence. If they are an href that STARTS with one of the
-# "dependent repositories" than we should leave them exactly as is.
-# amend_href is the core of the logic for handling referenced files and ensures that we cannot refer to the same readme twice
-# from two different reference ymls
-def amend_href(toc_dict, repo_location, readme_suffix, excluded_href_paths):
-    suffix = "-" + readme_suffix + ".md" if readme_suffix else  ".md"
-    input_string = toc_dict["href"]
-
-    # if this is an external readme, we should not attempt to resolve the file to a different one, just return with no changes
-    if any([input_string.startswith(href) for href in excluded_href_paths]):
-        return toc_dict 
-
-    resolvable_path = os.path.join(repo_location, input_string.replace("~/", ""))
-    possible_target_readme = os.path.splitext(resolvable_path)[0] + suffix
-
-    if os.path.exists(possible_target_readme):
-        toc_dict["href"] = input_string.replace(".md", suffix)
-    else:
-        toc_dict.pop("href")
-        toc_dict["landingPageType"] = "Service"
-
-    return toc_dict
-
 # a post-order recursive function that returns a modified reference.yml
 # based on the set of namespaces that we've grabbed from autogenerated ToC.yml
-def filter_toc(toc_dict, namespaces, repo_location, readme_suffix="", excluded_href_paths = []):
+def filter_toc(toc_dict, namespaces, path_resolver):
     if toc_dict is None:
         return None
 
@@ -67,7 +116,7 @@ def filter_toc(toc_dict, namespaces, repo_location, readme_suffix="", excluded_h
         # recurse as mant times as necessary
         item_list = []
         for item in toc_dict["items"]:
-            result_n = filter_toc(item, namespaces, repo_location, readme_suffix, excluded_href_paths)
+            result_n = filter_toc(item, namespaces, path_resolver)
             # only append the result if we know it exists
             if result_n:
                 item_list.append(result_n)
@@ -77,8 +126,8 @@ def filter_toc(toc_dict, namespaces, repo_location, readme_suffix="", excluded_h
             return None
 
     # handle href
-    if "href" in toc_dict and repo_location:
-        toc_dict = amend_href(toc_dict, repo_location, readme_suffix, excluded_href_paths)
+    if "href" in toc_dict:
+        toc_dict = path_resolver.amend_href(toc_dict)
 
     # leaf node
     if "children" in toc_dict:
@@ -88,8 +137,8 @@ def filter_toc(toc_dict, namespaces, repo_location, readme_suffix="", excluded_h
             return None
 
     # always amend the uid to include the suffix if one is present.
-    if "uid" in toc_dict and readme_suffix:
-        toc_dict["uid"] = toc_dict["uid"] + "." + readme_suffix
+    if "uid" in toc_dict and path_resolver.readme_suffix:
+        toc_dict["uid"] = toc_dict["uid"] + "." + path_resolver.readme_suffix
 
     return toc_dict
 
@@ -98,21 +147,6 @@ def grep_children_namespaces(autogenerated_toc_yml):
         top_level_namespace["name"] for top_level_namespace in autogenerated_toc_yml
     ] + ["*"]
 
-
-# the doc builds have the capability to reference readmes from external repos (they resolve during publishing)
-# this means that we can't simply check the href values for existence. If they are an href that STARTS with one of the
-# "dependent repositories" than we should leave them exactly as is. This function returns the start paths
-def get_non_standard_hrefs(doc_repo_location):
-    excluded_href_paths = []
-
-    target = os.path.join(doc_repo_location, ".openpublishing.publish.config.json")
-    with open(target, "r") as f:
-        data = json.load(f)
-
-    for dependent_repo in data["dependent_repositories"]:
-        excluded_href_paths.append("~/{}".format(dependent_repo["path_to_root"]))
-
-    return excluded_href_paths
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -147,6 +181,14 @@ if __name__ == "__main__":
         default="",
         required=False,
     )
+    
+    parser.add_argument(
+        "-m",
+        "--moniker",
+        help="Selected moniker. Used when filling in moniker-folder path updates.",
+        default="",
+        required=False,
+    )
 
     args = parser.parse_args()
 
@@ -169,14 +211,10 @@ if __name__ == "__main__":
     for ns in sorted(present_in_target):
         print(" |__ " + ns)
 
-    if args.docrepo:
-        non_standard_paths = get_non_standard_hrefs(args.docrepo)
-    else:
-        non_standard_paths = []
+    path_resolver = PathResolver(doc_repo_location=args.docrepo, readme_suffix=args.suffix, moniker=args.moniker)
 
-    base_reference_toc[0] = filter_toc(
-        base_reference_toc[0], present_in_target, args.docrepo, args.suffix, non_standard_paths
-    )
+    base_reference_toc[0] = filter_toc(base_reference_toc[0], present_in_target, path_resolver)
+
     updated_content = yaml.dump(base_reference_toc, default_flow_style=False)
 
     with open(args.target, "w") as f:

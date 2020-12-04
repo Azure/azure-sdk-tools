1dbf1289d2174b1631fb6048300550cbacfdb1f6
praveenkuttappan <prmarott@microsoft.com>
2020-12-03 16:43:51 -0800

    Added support for bot user in API View

---
+++ src/dotnet/APIView/APIViewWeb/Controllers/AutoReviewController.cs
@@ -0,0 +1,43 @@
+﻿using APIViewWeb.Filters;
+using APIViewWeb.Respositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIViewWeb.Controllers
+{
+    [TypeFilter(typeof(ApiKeyAuthorizeAsyncFilter))]
+    public class AutoReviewController : Controller
+    {
+        private readonly ReviewManager _reviewManager;
+
+        public AutoReviewController(ReviewManager reviewManager)
+        {
+            _reviewManager = reviewManager;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UploadAutoReview([FromForm] IFormFile file)
+        {
+            if (file != null)
+            {
+                using (var openReadStream = file.OpenReadStream())
+                {
+                    var review = await _reviewManager.CreateMasterReviewAsync(User, file.FileName, "Auto Review", openReadStream, false);
+                    if(review != null)
+                    {
+                        //Return 200 OK if last revision is approved and 201 if revision is not yet approved.
+                        var result = review.Revisions.Last().Approvers.Count > 0 ? Ok() : StatusCode(statusCode: StatusCodes.Status201Created);
+                        return result;
+                    }
+                }
+            }
+            // Return internal server error for any unknown error
+            return StatusCode(statusCode: StatusCodes.Status500InternalServerError);
+        }
+    }
+}

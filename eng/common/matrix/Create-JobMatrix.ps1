cf89cfd5602b9eeb843cf807232c5e25ad3b0313
Ben Broderick Phillips <bebroder@microsoft.com>
2020-12-04 07:57:30 -0500

    Add cross product matrix tooling

---
+++ eng/common/matrix/Create-JobMatrix.ps1
@@ -0,0 +1,23 @@
+<#
+    .SYNOPSIS
+        Generates a JSON object representing an Azure Pipelines Job Matrix.
+        See https://docs.microsoft.com/en-us/azure/devops/pipelines/process/phases?view=azure-devops&tabs=yaml#parallelexec
+
+    .EXAMPLE
+    .\eng\scripts\Create-JobMatrix $context
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$True)][object] $ConfigPath
+)
+
+. $PSScriptRoot/functions.ps1
+
+$config = Get-Content $ConfigPath | ConvertFrom-Json -AsHashtable
+
+[Array]$matrix = GenerateMatrix $config "all"
+$serialized = SerializePipelineMatrix $matrix
+
+Write-Output $serialized.pretty
+Write-Output "##vso[task.setVariable variable=matrix;isOutput=true]$($serialized.compressed)"

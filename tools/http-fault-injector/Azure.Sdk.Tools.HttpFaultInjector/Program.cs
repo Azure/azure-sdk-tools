87b351e8e5308c2ec59abbe3f51cd10cbc4ef1e8
Mike Harder <mharder@microsoft.com>
2021-01-14 15:34:34 -0800

    Add tool http-fault-injector (#1320)
    - Increase default .NET Core SDK version to 3.1
    - Update .NET Core Installer task to V2

---
+++ tools/http-fault-injector/Azure.Sdk.Tools.HttpFaultInjector/Program.cs
@@ -0,0 +1,334 @@
+using CommandLine;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Connections.Features;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpFaultInjector
+{
+    public static class Program
+    {
+        private static HttpClient _httpClient;
+
+        private static readonly List<(string Option, string Description)> _selectionDescriptions = new List<(string Option, string Description)>()
+        {
+            ("f", "Full response"),
+            ("p", "Partial Response (full headers, 50% of body), then wait indefinitely"),
+            ("pc", "Partial Response (full headers, 50% of body), then close (TCP FIN)"),
+            ("pa", "Partial Response (full headers, 50% of body), then abort (TCP RST)"),
+            ("pn", "Partial Response (full headers, 50% of body), then finish normally"),
+            ("n", "No response, then wait indefinitely"),
+            ("nc", "No response, then close (TCP FIN)"),
+            ("na", "No response, then abort (TCP RST)")
+        };
+
+        private static readonly string[] _excludedRequestHeaders = new string[] {
+            // Only applies to request between client and proxy
+            "Proxy-Connection",
+            _responseSelectionHeader
+        };
+
+        // Headers which must be set on HttpContent instead of HttpRequestMessage
+        private static readonly string[] _contentRequestHeaders = new string[] {
+            "Content-Length",
+            "Content-Type",
+        };
+
+        private const string _responseSelectionHeader = "x-ms-faultinjector-response-option";
+
+        private class Options
+        {
+            [Option('i', "insecure", Default = false, HelpText = "Allow insecure upstream SSL certs")]
+            public bool Insecure { get; set; }
+
+            [Option('t', "keep-alive-timeout", Default = 120, HelpText = "Keep-alive timeout (in seconds)")]
+            public int KeepAliveTimeout { get; set; }
+        }
+
+        public static void Main(string[] args)
+        {
+            var parser = new Parser(settings =>
+            {
+                settings.CaseSensitive = false;
+                settings.HelpWriter = Console.Error;
+            });
+
+            parser.ParseArguments<Options>(args).WithParsed(options => Run(options));
+        }
+
+        private static void Run(Options options)
+        {
+            if (options.Insecure)
+            {
+                _httpClient = new HttpClient(new HttpClientHandler()
+                {
+                    // Allow insecure SSL certs
+                    ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+                });
+            }
+            else
+            {
+                _httpClient = new HttpClient();
+            }
+
+            new WebHostBuilder()
+                .UseKestrel(kestrelOptions =>
+                {
+                    kestrelOptions.Listen(IPAddress.Any, 7777);
+                    kestrelOptions.Listen(IPAddress.Any, 7778, listenOptions =>
+                    {
+                        listenOptions.UseHttps("testCert.pfx", "testPassword");
+                    });
+                    kestrelOptions.Limits.KeepAliveTimeout = TimeSpan.FromSeconds(options.KeepAliveTimeout);
+                })
+                .Configure(app => app.Run(async context =>
+                {
+                    try
+                    {
+                        var upstreamResponse = await SendUpstreamRequest(context.Request);
+
+                        // Attempt to remove the response selection header and use its value to handle the response selection.
+                        if (context.Request.Headers.Remove(_responseSelectionHeader, out var selection))
+                        {
+                            string optionDescription = _selectionDescriptions.FirstOrDefault(kvp => kvp.Option.Equals(selection)).Description;
+                            if (String.IsNullOrEmpty(optionDescription))
+                            {
+                                Console.WriteLine($"Invalid {_responseSelectionHeader} value {selection}.");
+                            }
+                            else if (await TryHandleResponseOption(selection, context, upstreamResponse))
+                            {
+                                Console.WriteLine($"Using response option {optionDescription} from header value.");
+                                return;
+                            }
+                        }
+
+                        // If we were passed an invalid response selection header, or none, continue prompting the user for input and attempt to handle the response.
+                        while (true)
+                        {
+                            Console.WriteLine();
+
+                            Console.WriteLine("Select a response then press ENTER:");
+                            foreach (var selectionDescription in _selectionDescriptions)
+                            {
+                                Console.WriteLine($"{selectionDescription.Option}: {selectionDescription.Description}");
+                            }
+
+                            Console.WriteLine();
+
+                            selection = Console.ReadLine();
+
+                            if (await TryHandleResponseOption(selection, context, upstreamResponse))
+                            {
+                                return;
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }))
+                .Build()
+                .Run();
+        }
+
+        private static async Task<UpstreamResponse> SendUpstreamRequest(HttpRequest request)
+        {
+            var upstreamUriBuilder = new UriBuilder()
+            {
+                Scheme = request.Scheme,
+                Host = request.Host.Host,
+                Path = request.Path.Value,
+                Query = request.QueryString.Value,
+            };
+
+            if (request.Host.Port.HasValue)
+            {
+                upstreamUriBuilder.Port = request.Host.Port.Value;
+            }
+
+            var upstreamUri = upstreamUriBuilder.Uri;
+
+            Console.WriteLine();
+            Log("Upstream Request");
+            Log($"URL: {upstreamUri}");
+
+            using (var upstreamRequest = new HttpRequestMessage(new HttpMethod(request.Method), upstreamUri))
+            {
+                Log("Headers:");
+
+                if (request.ContentLength > 0)
+                {
+                    upstreamRequest.Content = new StreamContent(request.Body);
+
+                    foreach (var header in request.Headers.Where(h => _contentRequestHeaders.Contains(h.Key)))
+                    {
+                        Log($"  {header.Key}:{header.Value.First()}");
+                        upstreamRequest.Content.Headers.Add(header.Key, values: header.Value);
+                    }
+                }
+
+                foreach (var header in request.Headers.Where(h => !_excludedRequestHeaders.Contains(h.Key) && !_contentRequestHeaders.Contains(h.Key)))
+                {
+                    Log($"  {header.Key}:{header.Value.First()}");
+                    if (!upstreamRequest.Headers.TryAddWithoutValidation(header.Key, values: header.Value))
+                    {
+                        throw new InvalidOperationException($"Could not add header {header.Key} with value {header.Value}");
+                    }
+                }
+
+                Log("Sending request to upstream server...");
+                using (var upstreamResponseMessage = await _httpClient.SendAsync(upstreamRequest))
+                {
+                    Console.WriteLine();
+                    Log("Upstream Response");
+                    var headers = new List<KeyValuePair<string, StringValues>>();
+
+                    Log($"StatusCode: {upstreamResponseMessage.StatusCode}");
+
+                    Log("Headers:");
+                    foreach (var header in upstreamResponseMessage.Headers)
+                    {
+                        Log($"  {header.Key}:{header.Value.First()}");
+
+                        // Must skip "Transfer-Encoding" header, since if it's set manually Kestrel requires you to implement
+                        // your own chunking.
+                        if (string.Equals(header.Key, "Transfer-Encoding", StringComparison.OrdinalIgnoreCase))
+                        {
+                            continue;
+                        }
+
+                        headers.Add(new KeyValuePair<string, StringValues>(header.Key, header.Value.ToArray()));
+                    }
+
+                    foreach (var header in upstreamResponseMessage.Content.Headers)
+                    {
+                        Log($"  {header.Key}:{header.Value.First()}");
+                        headers.Add(new KeyValuePair<string, StringValues>(header.Key, header.Value.ToArray()));
+                    }
+
+                    Log("Reading upstream response body...");
+
+                    var upstreamResponse = new UpstreamResponse()
+                    {
+                        StatusCode = (int)upstreamResponseMessage.StatusCode,
+                        Headers = headers.ToArray(),
+                        Content = await upstreamResponseMessage.Content.ReadAsByteArrayAsync()
+                    };
+
+                    Log($"ContentLength: {upstreamResponse.Content.Length}");
+
+                    return upstreamResponse;
+                }
+            }
+        }
+
+        private static async Task<bool> TryHandleResponseOption(string selection, HttpContext context, UpstreamResponse upstreamResponse)
+        {
+            switch (selection)
+            {
+                case "f":
+                    // Full response
+                    await SendDownstreamResponse(upstreamResponse, context.Response);
+                    return true;
+                case "p":
+                    // Partial Response (full headers, 50% of body), then wait indefinitely
+                    await SendDownstreamResponse(upstreamResponse, context.Response, upstreamResponse.Content.Length / 2);
+                    await Task.Delay(Timeout.InfiniteTimeSpan);
+                    return true;
+                case "pc":
+                    // Partial Response (full headers, 50% of body), then close (TCP FIN)
+                    await SendDownstreamResponse(upstreamResponse, context.Response, upstreamResponse.Content.Length / 2);
+                    Close(context);
+                    return true;
+                case "pa":
+                    // Partial Response (full headers, 50% of body), then abort (TCP RST)
+                    await SendDownstreamResponse(upstreamResponse, context.Response, upstreamResponse.Content.Length / 2);
+                    Abort(context);
+                    return true;
+                case "pn":
+                    // Partial Response (full headers, 50% of body), then finish normally
+                    await SendDownstreamResponse(upstreamResponse, context.Response, upstreamResponse.Content.Length / 2);
+                    return true;
+                case "n":
+                    // No response, then wait indefinitely
+                    await Task.Delay(Timeout.InfiniteTimeSpan);
+                    return true;
+                case "nc":
+                    // No response, then close (TCP FIN)
+                    Close(context);
+                    return true;
+                case "na":
+                    // No response, then abort (TCP RST)
+                    Abort(context);
+                    return true;
+                default:
+                    Console.WriteLine($"Invalid selection: {selection}");
+                    return false;
+            }
+        }
+
+        private static async Task SendDownstreamResponse(UpstreamResponse upstreamResponse, HttpResponse response, int? contentBytes = null)
+        {
+            Console.WriteLine();
+
+            Log("Sending downstream response...");
+
+            response.StatusCode = upstreamResponse.StatusCode;
+
+            Log($"StatusCode: {upstreamResponse.StatusCode}");
+
+            Log("Headers:");
+            foreach (var header in upstreamResponse.Headers)
+            {
+                Log($"  {header.Key}:{header.Value}");
+                response.Headers.Add(header.Key, header.Value);
+            }
+
+            var count = contentBytes ?? upstreamResponse.Content.Length;
+            Log($"Writing response body of {count} bytes...");
+            await response.Body.WriteAsync(upstreamResponse.Content, 0, count);
+            Log($"Finished writing response body");
+        }
+
+        // Close the TCP connection by sending FIN
+        private static void Close(HttpContext context)
+        {
+            context.Abort();
+        }
+
+        // Abort the TCP connection by sending RST
+        private static void Abort(HttpContext context)
+        {
+            // SocketConnection registered "this" as the IConnectionIdFeature among other things.
+            var socketConnection = context.Features.Get<IConnectionIdFeature>();
+            var socket = (Socket)socketConnection.GetType().GetField("_socket", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(socketConnection);
+            socket.LingerState = new LingerOption(true, 0);
+            socket.Dispose();
+        }
+
+        private static void Log(object value)
+        {
+            Console.WriteLine($"[{DateTime.Now:hh:mm:ss.fff}] {value}");
+        }
+
+        private class UpstreamResponse
+        {
+            public int StatusCode { get; set; }
+            public KeyValuePair<string, StringValues>[] Headers { get; set; }
+            public byte[] Content { get; set; }
+        }
+    }
+}
---
+++ tools/http-fault-injector/README.md
@@ -0,0 +1,86 @@
+# http-fault-injector
+
+1. [Install .Net](https://dotnet.microsoft.com/download)
+
+2. Install http-fault-injector
+```
+> dotnet tool install azure.sdk.tools.httpfaultinjector --global --add-source https://pkgs.dev.azure.com/azure-sdk/public/_packaging/azure-sdk/nuget/v3/index.json
+
+You can invoke the tool using the following command: http-fault-injector
+Tool 'azure.sdk.tools.httpfaultinjector' (version '0.1.0') was successfully installed.
+```
+
+3. Run http-fault-injector
+```
+> http-fault-injector
+
+Now listening on: http://0.0.0.0:7777
+Now listening on: https://0.0.0.0:7778
+Application started. Press Ctrl+C to shut down.
+```
+
+4. Clone and run .NET sample client
+```
+> git clone https://github.com/Azure/azure-sdk-tools
+
+> cd azure-sdk-tools\tools\http-fault-injector\sample-clients\net\
+
+> dotnet run
+
+Sending request...
+```
+
+5. View request in http-fault-injector
+```
+[10:47:20.089] Upstream Request
+[10:47:20.092] URL: https://www.example.org/
+[10:47:20.097] Sending request to upstream server...
+
+[10:47:20.190] Upstream Response
+[10:47:20.191] StatusCode: OK
+[10:47:20.191] Headers:
+[10:47:20.195]   Content-Length:1256
+[10:47:20.195] Reading upstream response body...
+[10:47:20.196] ContentLength: 1256
+
+Select a response then press ENTER:
+f: Full response
+```
+
+6. Choose response `f`.  Server sends full response, client prints OK.
+```
+> Server
+f
+
+[10:48:04.431] Sending downstream response...
+[10:48:04.432] StatusCode: 200
+[10:48:04.432] Headers:
+[10:48:04.436]   Content-Length:1256
+[10:48:04.436] Writing response body of 1256 bytes...
+[10:48:04.439] Finished writing response body
+
+> Client
+OK
+```
+
+7. Run client again, choose response `pc`.  Server sends partial response then closes, client shows error.
+
+```
+> Server
+Select a response then press ENTER:
+pc: Partial Response (full headers, 50% of body), then close (TCP FIN)
+
+pc
+
+[10:51:11.231] Sending downstream response...
+[10:51:11.232] StatusCode: 200
+[10:51:11.232] Headers:
+[10:51:11.233]   Content-Length:1256
+[10:51:11.233] Writing response body of 628 bytes...
+[10:51:11.234] Finished writing response body
+
+> Client
+Sending request...
+Unhandled exception. System.Net.Http.HttpRequestException: Error while copying content to a stream.
+ ---> System.IO.IOException: The response ended prematurely.
+```

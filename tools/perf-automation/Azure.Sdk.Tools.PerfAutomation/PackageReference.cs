ad019a6ced0048e9b3e5f871c93293b799a3244a
Mike Harder <mharder@microsoft.com>
2021-02-01 16:26:00 -0800

    Add package references to CLI

---
+++ tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/PackageReference.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.Sdk.Tools.PerfAutomation
+{
+    public class PackageReference
+    {
+        public PackageReference(string nameAtVersion)
+        {
+            var parts = nameAtVersion.Split('@');
+            if (parts.Length == 2)
+            {
+                Name = parts[0];
+                Version = new Version(parts[1]);
+            }
+            else
+            {
+                throw new ArgumentException(nameof(nameAtVersion));
+            }
+        }
+
+        public string Name { get; private set;}
+        public Version Version { get; private set; }
+    }
+}
--- tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/Program.cs
+++ tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/Program.cs
@@ -1,6 +1,8 @@
 using CommandLine;
 using CommandLine.Text;
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Azure.Sdk.Tools.PerfAutomation
 {
@@ -8,8 +10,14 @@ namespace Azure.Sdk.Tools.PerfAutomation
     {
         private class Options
         {
+            [Option('d', "directory", Required = true)]
+            public DirectoryInfo Directory { get; set; }
+
             [Option('l', "language", Required = true)]
             public Language Language { get; set; }
+
+            [Option('p', "packageReferences", HelpText = "List of package references to override.\nFormat: <name>@<version> ...\nExample: foo@1.0.0 bar@2.0.0")]
+            public IEnumerable<PackageReference> PackageReferences { get; set; }
         }
 
         public static void Main(string[] args)
@@ -41,7 +49,13 @@ namespace Azure.Sdk.Tools.PerfAutomation
 
         private static void Run(Options options)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine($"Directory: {options.Directory.FullName}");
+            Console.WriteLine($"Language: {options.Language}");
+
+            foreach (var p in options.PackageReferences)
+            {
+                Console.WriteLine($"PackageReference: {p.Name}@{p.Version}");
+            }
         }
     }
 }

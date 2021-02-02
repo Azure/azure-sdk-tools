fe9078a22b69f27e5417f688683ca530b2cb7540
Mike Harder <mharder@microsoft.com>
2021-02-02 00:51:43 -0800

    Accept input as YML

---
+++ tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/Models/LanguageName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.Sdk.Tools.PerfAutomation.Models
+{
+    public enum LanguageName
+    {
+        Net,
+        Java,
+        JS,
+        Python
+    }
+}

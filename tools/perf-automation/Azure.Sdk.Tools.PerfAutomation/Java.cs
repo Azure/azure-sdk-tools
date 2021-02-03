f569da23eb00e383949968ea76f81a2fec33d7a6
Mike Harder <mharder@microsoft.com>
2021-02-03 11:58:30 -0800

    Java test names must be lower-case

---
+++ tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/Java.cs
@@ -0,0 +1,106 @@
+﻿using Azure.Sdk.Tools.PerfAutomation.Models;
+using Microsoft.Crank.Agent;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Azure.Sdk.Tools.PerfAutomation
+{
+    static class Java
+    {
+        private static void SetPackageVersions(string projectFile, IDictionary<string, string> packageVersions)
+        {
+        }
+
+        private static void UnsetPackageVersions(string projectFile)
+        {
+        }
+
+        public static async Task<Result> RunAsync(
+            LanguageSettings languageSettings, string arguments, IDictionary<string, string> packageVersions)
+        {
+            var workingDirectory = Program.Config.WorkingDirectories[Language.Java];
+            var projectFile = Path.Combine(workingDirectory, languageSettings.Project);
+
+            try
+            {
+                SetPackageVersions(projectFile, packageVersions);
+
+                string buildFilename;
+                var buildArguments = $"package -T1C -am -Dmaven.test.skip=true -Dmaven.javadoc.skip=true --pl {languageSettings.Project}";
+                
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    buildFilename = "cmd";
+                    buildArguments = $"/c mvn {buildArguments}";
+                }
+                else
+                {
+                    buildFilename = "mvn";
+                }
+
+                var buildResult = await ProcessUtil.RunAsync(
+                    buildFilename,
+                    buildArguments,
+                    workingDirectory: workingDirectory,
+                    log: true,
+                    captureOutput: true,
+                    captureError: true
+                );
+
+                /*
+                [11:27:11.796] [INFO] Building jar: C:\Git\java\sdk\storage\azure-storage-perf\target\azure-storage-perf-1.0.0-beta.1-jar-with-dependencies.jar
+                */
+
+                var buildMatch = Regex.Match(buildResult.StandardOutput, @"Building jar: (.*\.jar)", RegexOptions.IgnoreCase | RegexOptions.RightToLeft);
+                var jar = buildMatch.Groups[1].Value;
+
+                var processArguments = $"-jar {jar} -- {languageSettings.TestName} {arguments}";
+
+                var result = await ProcessUtil.RunAsync(
+                    "java",
+                    processArguments,
+                    workingDirectory: workingDirectory,
+                    log: true,
+                    captureOutput: true,
+                    captureError: true
+                );
+
+                var match = Regex.Match(result.StandardOutput, @"\((.*) ops/s", RegexOptions.IgnoreCase | RegexOptions.RightToLeft);
+                var opsPerSecond = double.Parse(match.Groups[1].Value);
+
+                return new Result
+                {
+                    OperationsPerSecond = opsPerSecond,
+                    StandardError = buildResult.StandardError + Environment.NewLine + result.StandardError,
+                    StandardOutput = buildResult.StandardOutput + Environment.NewLine + result.StandardOutput,
+                };
+            }
+            finally
+            {
+                UnsetPackageVersions(projectFile);
+            }
+        }
+
+        /*
+        === Warmup ===
+        Current         Total           Average
+        124293          124293          127608.18
+        1879            126172          127618.75
+
+        === Results ===
+        Completed 126,172 operations in a weighted-average of 0.99s (127,618.75 ops/s, 0.000 s/op)
+
+        === Test ===
+        Current         Total           Average
+        157630          157630          156225.73
+        0               157630          156225.73
+
+        === Results ===
+        Completed 157,630 operations in a weighted-average of 1.01s (156,225.73 ops/s, 0.000 s/op)
+        */
+    }
+}

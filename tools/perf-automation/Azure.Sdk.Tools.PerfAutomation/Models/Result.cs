e3bda9026b3f837651b7bb8dde789ba26d3823f0
Mike Harder <mharder@microsoft.com>
2021-02-02 19:55:18 -0800

    Write results to JSON file

---
+++ tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/Models/Result.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Azure.Sdk.Tools.PerfAutomation.Models
+{
+    public class Result
+    {
+        public string TestName { get; set; }
+
+        public Language Language { get; set; }
+        public string Project { get; set; }
+        public string LanguageTestName { get; set; }
+        public string Arguments { get; set; }
+        public IDictionary<string, string> PackageVersions { get; set; }
+
+        public double OperationsPerSecond { get; set; }
+        public string StandardError { get; set; }
+        public string StandardOutput { get; set; }
+    }
+}

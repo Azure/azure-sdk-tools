0713b88c12d35791167bae59608a7bbf24728585
Mike Harder <mharder@microsoft.com>
2021-02-05 17:47:44 -0800

    Add median

--- tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/Models/Result.cs
+++ tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/Models/Result.cs
@@ -17,6 +17,20 @@ namespace Azure.Sdk.Tools.PerfAutomation.Models
 
         public ICollection<IterationResult> Iterations { get; } = new List<IterationResult>();
 
-        public double OperationsPerSecondAverage => Iterations.Any() ? Iterations.Average(i => i.OperationsPerSecond) : -1;
+        public double OperationsPerSecondMean => Iterations.Any() ? Iterations.Average(i => i.OperationsPerSecond) : -1;
+        public double OperationsPerSecondMedian => Iterations.Any() ? Median(Iterations.Select(i => i.OperationsPerSecond)) : -1;
+
+        private double Median(IEnumerable<double> values)
+        {
+            var count = values.Count();
+            if (count % 2 == 1)
+            {
+                return values.Skip((count - 1) / 2).First();
+            }
+            else
+            {
+                return values.Skip((count / 2) - 1).Take(2).Average();
+            }
+        }
     }
 }

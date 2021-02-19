4b60bbb3387905e6552cd49d516a11196c7539a1
Mike Harder <mharder@microsoft.com>
2021-02-18 18:01:06 -0800

    WIP: JavaScript automation

--- tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/JavaScript.cs
+++ tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/JavaScript.cs
@@ -1,43 +1,47 @@
 ﻿using Azure.Sdk.Tools.PerfAutomation.Models;
+using Microsoft.Crank.Agent;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Azure.Sdk.Tools.PerfAutomation
 {
-    public class JavaScript : ILanguage
+    public class JavaScript : LanguageBase
     {
-        public async Task<(string output, string error, string context)> SetupAsync(
+        protected override Language Language => Language.JS;
+
+        public override async Task<(string output, string error, string context)> SetupAsync(
             string project, string languageVersion, IDictionary<string, string> packageVersions)
         {
-            var workingDirectory = Program.Config.WorkingDirectories[Language.JS];
+            var outputBuilder = new StringBuilder();
+            var errorBuilder = new StringBuilder();
 
-            var commonVersionsFile = Path.Combine(workingDirectory, "common", "config", "rush", "common-versions.json");
+            var commonVersionsFile = Path.Combine(WorkingDirectory, "common", "config", "rush", "common-versions.json");
             var commonVersionsJson = JObject.Parse(File.ReadAllText(commonVersionsFile));
 
-            var projectDirectory = Path.Combine(workingDirectory, project);
+            var projectDirectory = Path.Combine(WorkingDirectory, project);
             var projectFile = Path.Combine(projectDirectory, "package.json");
             var projectJson = JObject.Parse(File.ReadAllText(projectFile));
 
             var track1 = projectDirectory.EndsWith("track-1", StringComparison.OrdinalIgnoreCase);
             var track2 = !track1;
 
-            // - Track 1
-            //   1. If not source, modify package.json
-            //   2. rush update (OR install)
-            //   3. cd project
-            //   4. npm run setup
+            // Track 1
+            // 1. If not source, modify package.json
+            // 2. rush update (OR install)
+            // 3. [projectDir] npm run setup
             //
-            // - Track 2
-            //   1. If not source, modify package.json and common-versions.json
-            //   2. rush update
-            //   3. Extract project name from package.json in project folder
-            //   4. rush build -t projectName
+            // Track 2
+            // 1. If not source, modify package.json and common-versions.json
+            // 2. rush update
+            // 3. Extract project name from package.json in project folder
+            // 4. rush build -t projectName
 
             foreach (var v in packageVersions)
             {
@@ -74,13 +78,32 @@ namespace Azure.Sdk.Tools.PerfAutomation
             File.Copy(projectFile, projectFile + ".bak", overwrite: true);
             File.WriteAllText(projectFile, projectJson.ToString() + Environment.NewLine);
 
-            await Task.CompletedTask;
-            return (null, null, null);
+            await Util.RunAsync("rush", "update", WorkingDirectory, outputBuilder: outputBuilder, errorBuilder: errorBuilder);
+
+            if (track1)
+            {
+                await Util.RunAsync("npm", "run setup", projectDirectory,
+                    outputBuilder: outputBuilder, errorBuilder: errorBuilder);
+            }
+            else
+            {
+                var projectName = projectJson["name"];
+                await Util.RunAsync("rush", $"build --to {projectName}", WorkingDirectory,
+                    outputBuilder: outputBuilder, errorBuilder: errorBuilder);
+            }
+
+            return (outputBuilder.ToString(), errorBuilder.ToString(), null);
         }
 
-        public async Task<IterationResult> RunAsync(string project, string languageVersion, string testName, string arguments, string context)
+        public override async Task<IterationResult> RunAsync(string project, string languageVersion, string testName, string arguments, string context)
         {
+            var outputBuilder = new StringBuilder();
+            var errorBuilder = new StringBuilder();
+
+            var projectDirectory = Path.Combine(WorkingDirectory, project);
+
             // Dump "npm list 2>nul | findstr @azure" to stdout
+            await Util.RunAsync("npm", "list", projectDirectory, outputBuilder, errorBuilder);
 
             // 1. cd project
             // 2. npm run perf-test:node -- testName arguments
@@ -89,15 +112,16 @@ namespace Azure.Sdk.Tools.PerfAutomation
 
             return new IterationResult
             {
+                StandardOutput = outputBuilder.ToString(),
+                StandardError = errorBuilder.ToString(),
             };
         }
 
-        public Task CleanupAsync(string project)
+        public override Task CleanupAsync(string project)
         {
-            var workingDirectory = Program.Config.WorkingDirectories[Language.JS];
-            var projectDirectory = Path.Combine(workingDirectory, project);
+            var projectDirectory = Path.Combine(WorkingDirectory, project);
 
-            var commonVersionsFile = Path.Combine(workingDirectory, "common", "config", "rush", "common-versions.json");
+            var commonVersionsFile = Path.Combine(WorkingDirectory, "common", "config", "rush", "common-versions.json");
             var projectFile = Path.Combine(projectDirectory, "package.json");
 
             // Restore backups
@@ -106,6 +130,5 @@ namespace Azure.Sdk.Tools.PerfAutomation
 
             return Task.CompletedTask;
         }
-
     }
 }

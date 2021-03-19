8b93dbee392c242c4479d1d0974c78c990e41506
Pavel Krymets <pavel@krymets.com>
2021-03-19 09:48:18 -0700

    Add Operation ctor analyzer

---
+++ src/dotnet/Azure.ClientSdk.Analyzers/Azure.ClientSdk.Analyzers/OperationConstructorAnalyzer.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Azure.ClientSdk.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class OperationConstructorAnalyzer : OperationAnalyzerBase
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(new[]
+        {
+            Descriptors.AZC0005,
+        });
+
+
+        public override void AnalyzeCore(ISymbolAnalysisContext context)
+        {
+            var type = (INamedTypeSymbol)context.Symbol;
+            if (!type.Constructors.Any(c => (c.DeclaredAccessibility == Accessibility.Protected || c.DeclaredAccessibility == Accessibility.Public) && c.Parameters.Length == 0))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Descriptors.AZC0005, type.Locations.First()), type);
+            }
+        }
+    }
+}

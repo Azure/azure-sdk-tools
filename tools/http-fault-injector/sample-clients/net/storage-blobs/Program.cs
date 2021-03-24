a37270764e275da6c4b6635f322f3b66c7e45ae1
Mike Harder <mharder@microsoft.com>
2021-03-24 12:52:21 -0700

    Rename sample to StorageBlobs

--- tools/http-fault-injector/sample-clients/net/storage-blobs/Program.cs
+++ tools/http-fault-injector/sample-clients/net/storage-blobs/Program.cs
@@ -7,7 +7,7 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Azure.Sdk.Tools.HttpFaultInjector.StorageBlobSample
+namespace Azure.Sdk.Tools.HttpFaultInjector.StorageBlobsSample
 {
     class Program
     {

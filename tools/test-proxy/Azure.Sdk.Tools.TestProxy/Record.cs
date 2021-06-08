0f5e56403e6fcdac80626681ef463d0f08a43065
scbedd <45376673+scbedd@users.noreply.github.com>
2021-06-08 14:00:45 -0700

    ensure we allow null for x-recording-file

--- tools/test-proxy/Azure.Sdk.Tools.TestProxy/Record.cs
+++ tools/test-proxy/Azure.Sdk.Tools.TestProxy/Record.cs
@@ -30,7 +30,7 @@ namespace Azure.Sdk.Tools.TestProxy
         [HttpPost]
         public void Start()
         {
-            string file = RecordingHandler.GetHeader(Request, "x-recording-file");
+            string file = RecordingHandler.GetHeader(Request, "x-recording-file", true);
 
             _recordingHandler.StartRecording(file, Response);
         }

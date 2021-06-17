09cc3585703d92585598d8e6c8afa732637cb044
scbedd <45376673+scbedd@users.noreply.github.com>
2021-06-17 11:42:59 -0700

    more progress on the powershell version. shelving for a second to publish an updated docker host

--- tools/test-proxy/startup-scripts/start-server.ps1
+++ tools/test-proxy/startup-scripts/start-server.ps1
@@ -1,16 +1,44 @@
-# assuming mode is startup to begin with
-$visibleContainers = (docker container ls -a --format "{{ json .}}") | ConvertFrom-Json
+# Currently assumes that:
+#   az login
+#   az acr login --name azsdkengsys
+# has been executed. a public container registry will resolve this going forward.
+param(
+    [ValidateSet("start", "stop")]    
+    [String]
+    $mode
+)
+
+try {
+    docker info
+}
+catch {
+    Write-Host $_
+    Write-Error "A invocation of docker info failed. This indicates that docker is not properly installed or running."
+    Write-Error "Please check your docker invocation and try again"
+}
 
 # need relative path to repo root. as we need to mount it
 $repoRoot = "<TODO>"
 
-# docker container ls -a --format -filter "label=blah" -filter "name=blah"
+$DOCKER_RUN = "run -v $repoRoot:/etc/testproxy -p 5001:5001 -p 5000:5000 azsdkengsys.azurecr.io/engsys/ubuntu_testproxy_server:90164"
+$DOCKER_CONTAINER_QUERY = "container ls -a --format -filter `"{{ json . }}`" `"label=blah`" -filter `"name=blah`""
+$DOCKER_CONTAINER_CREATE = ""
+$DOCKER_CONTAINER_RUN = ""
+$DOCKER_CONTAINER_STOP = ""
 
-# check set of visible containers for one. Take the first (there should only be one)
+if ($mode -eq "start"){
+    # check set of visible containers filtered by the expression
+    $proxyContainers = docker $DOCKER_CONTAINER_QUERY | ConvertFrom-Json
 
-# check status of container. if it's running, we're g2g
-# if not running, start it via 
+    # if there is no container, create it
+    # check status of container. if it's running, we are done
+    # if it's not running, start it.
+}
 
+if ($mode -eq "stop"){
+    $proxyContainers = docker $DOCKER_CONTAINER_QUERY | ConvertFrom-Json
+    # if there is no container, we're done
+    # if there is a container, and it's stopped, we are done
+    # if it's running. stop it.
 
-
-
+}

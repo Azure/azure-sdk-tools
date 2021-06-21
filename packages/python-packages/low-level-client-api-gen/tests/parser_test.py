563c433ccd6b0778b8bfcce99b45b6a2e0ff41bf
Libba Lawrence <t-llawrence@microsoft.com>
2021-06-21 16:47:25 -0400

    add test

--- packages/python-packages/low-level-client-api-gen/tests/parser_test.py
+++ packages/python-packages/low-level-client-api-gen/tests/parser_test.py
@@ -1,23 +1,35 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
+from llcapi.llc_view_models import LLCClientView
 import yaml
-from llcapi import LLCClientView 
 
 
 class TestParser:
     def _test_client_name(self):
-        self.path = "C:\\Users\\t-llawrence\\Desktop\\yaml\\translator_test.yaml"
-        with open(self.path) as f:
-            self.data = yaml.safe_load(f)
-        client = LLCClientView(self.data)
-        assert client.name == "Batch Document Translation Client"
+        path = "C:\\Users\\t-llawrence\\Desktop\\yaml\\translator_test.yaml"
+        with open(path) as f:
+            data = yaml.safe_load(f)
+        client = LLCClientView(data)
+        print(client.Name)
+        assert client.Name == "Batch Document Translation Client"
     
     def _test_client_operationGroups(self):
-        self.path = "C:\\Users\\t-llawrence\\Desktop\\yaml\\translator_test.yaml"
-        with open(self.path) as f:
-            self.data = yaml.safe_load(f)
-        client = LLCClientView(self.data)
+        path = "C:\\Users\\t-llawrence\\Desktop\\yaml\\translator_test.yaml"
+        with open(path) as f:
+            data = yaml.safe_load(f)
+        client = LLCClientView(data)
         groups = client.Operation_Groups 
         for g in groups:
             assert g.name == "DocumentTranslation"
+    
+    def _test_client_operation(self):
+        path = "C:\\Users\\t-llawrence\\Desktop\\yaml\\translator_test.yaml"
+        with open(path) as f:
+            data = yaml.safe_load(f)
+        client = LLCClientView(data)
+        operations = client.Operations
+        for o in operations:
+            print(o)

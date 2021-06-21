ec8049f6e0c973f259be73027c7b85280d19315b
Libba Lawrence <t-llawrence@microsoft.com>
2021-06-21 16:14:32 -0400

    testing - woring?

--- packages/python-packages/low-level-client-api-gen/tests/parser_test.py
+++ packages/python-packages/low-level-client-api-gen/tests/parser_test.py
@@ -1,15 +1,23 @@
-from .llc_view_models.llc_view_models import LLCClientView
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 import yaml
+from llcapi import LLCClientView 
 
 
 class TestParser:
-    def __init__(self) -> None:
-        self.path = "c:\\Users\\t-llawrence\\Desktop\\yaml\\translator_test.yaml"
+    def _test_client_name(self):
+        self.path = "C:\\Users\\t-llawrence\\Desktop\\yaml\\translator_test.yaml"
         with open(self.path) as f:
             self.data = yaml.safe_load(f)
-        print(self.data)
-
-
-    def _test_return_type(self):
         client = LLCClientView(self.data)
         assert client.name == "Batch Document Translation Client"
+    
+    def _test_client_operationGroups(self):
+        self.path = "C:\\Users\\t-llawrence\\Desktop\\yaml\\translator_test.yaml"
+        with open(self.path) as f:
+            self.data = yaml.safe_load(f)
+        client = LLCClientView(self.data)
+        groups = client.Operation_Groups 
+        for g in groups:
+            assert g.name == "DocumentTranslation"

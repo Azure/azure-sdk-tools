011b29c06ad3d19114554fb8778454b7f3c86910
Libba Lawrence <t-llawrence@microsoft.com>
2021-06-22 10:12:07 -0400

    rid of extra init

--- packages/python-packages/low-level-client-api-gen/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-import os, sys
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
-

72b12655aea531ef32a1783c853203ddd706a1fd
Srikanta <srnagar@microsoft.com>
2021-06-28 01:09:19 -0700

    limit service version check to http services only

--- src/java/apiview-java-processor/src/main/java/com/azure/tools/apiview/processor/diagnostics/rules/ServiceVersionDiagnosticRule.java
+++ src/java/apiview-java-processor/src/main/java/com/azure/tools/apiview/processor/diagnostics/rules/ServiceVersionDiagnosticRule.java
@@ -5,6 +5,10 @@ import com.azure.tools.apiview.processor.model.APIListing;
 import com.azure.tools.apiview.processor.model.Diagnostic;
 import com.azure.tools.apiview.processor.model.DiagnosticKind;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.expr.AnnotationExpr;
+import com.github.javaparser.ast.expr.MemberValuePair;
+
+import java.util.Optional;
 
 import static com.azure.tools.apiview.processor.analysers.util.ASTUtils.makeId;
 import static com.azure.tools.apiview.processor.model.DiagnosticKind.WARNING;
@@ -15,11 +19,26 @@ import static com.azure.tools.apiview.processor.model.DiagnosticKind.WARNING;
 public class ServiceVersionDiagnosticRule implements DiagnosticRule {
     private boolean foundServiceVersion;
     private String serviceClientPackage;
+    private boolean isHttpService;
 
     @Override
     public void scanIndividual(CompilationUnit cu, APIListing listing) {
-        if (cu.getPrimaryTypeName().get().endsWith("Client")) {
+        if (cu.getPrimaryTypeName().get().endsWith("ClientBuilder")) {
             serviceClientPackage = cu.getPackageDeclaration().get().getNameAsString();
+            cu.getTypes().forEach(typeDeclaration -> {
+                Optional<AnnotationExpr> clientBuilderAnnotation = typeDeclaration
+                        .getAnnotationByName("ServiceClientBuilder");
+                if (clientBuilderAnnotation.isPresent()) {
+                    Optional<MemberValuePair> protocol = clientBuilderAnnotation.get()
+                            .asAnnotationExpr()
+                            .toNormalAnnotationExpr().get()
+                            .getPairs()
+                            .stream()
+                            .filter(pair -> pair.getNameAsString().equals("protocol"))
+                            .findFirst();
+                    this.isHttpService = !protocol.isPresent() || protocol.get().getNameAsString().contains("HTTP");
+                }
+            });
         }
 
         if (cu.getPrimaryTypeName().get().endsWith("ServiceVersion")) {
@@ -45,7 +64,7 @@ public class ServiceVersionDiagnosticRule implements DiagnosticRule {
     public void scanFinal(APIListing listing) {
         // If ServiceVersion type is not found and the module contains a client
         // show a diagnostic warning about missing service version type.
-        if (!foundServiceVersion && serviceClientPackage != null) {
+        if (!foundServiceVersion && serviceClientPackage != null && isHttpService) {
             listing.addDiagnostic(new Diagnostic(DiagnosticKind.WARNING, makeId(serviceClientPackage),
                     "ServiceVersion type is missing."));
         }

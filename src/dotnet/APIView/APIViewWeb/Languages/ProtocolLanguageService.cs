07c8cf02afcf36a14986001b766ceeb27ce13b11
praveenkuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2021-06-28 14:13:59 -0400

    LLC support in APIView (#1656)

---
+++ src/dotnet/APIView/APIViewWeb/Languages/ProtocolLanguageService.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace APIViewWeb
+{
+    public class ProtocolLanguageService : JsonLanguageService
+    {
+        public override string Name { get; } = "Protocol";
+
+        public override bool IsSupportedFile(string name)
+        {
+            // Skip initial processing so this service won't be selected for LLC when json is uploaded
+            // This is only a temporary solution for POC and will be remvoed once autorest yaml is uploaded instead of json for LLC
+            return false;
+        }
+    }
+}

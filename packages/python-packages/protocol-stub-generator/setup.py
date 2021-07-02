7143aef711b6de4465251fd9e778588816964c48
Libba Lawrence <t-llawrence@microsoft.com>
2021-07-02 10:01:38 -0400

    python setup

--- packages/python-packages/protocol-stub-generator/setup.py
+++ packages/python-packages/protocol-stub-generator/setup.py
@@ -39,10 +39,11 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
     ],
 )

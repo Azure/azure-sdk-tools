1d97ca6848ec722bd17085d4a631ca6fb7d93f95
Scott Beddall <45376673+scbedd@users.noreply.github.com>
2021-07-22 12:58:23 -0700

    need to expose all headers, not just allow them (#1839)

--- tools/test-proxy/Azure.Sdk.Tools.TestProxy/Startup.cs
+++ tools/test-proxy/Azure.Sdk.Tools.TestProxy/Startup.cs
@@ -66,7 +66,8 @@ namespace Azure.Sdk.Tools.TestProxy
                     {
                         builder.AllowAnyHeader()
                                .AllowAnyMethod()
-                               .AllowAnyOrigin();
+                               .AllowAnyOrigin()
+                               .WithExposedHeaders("*");
                     });
             });
 

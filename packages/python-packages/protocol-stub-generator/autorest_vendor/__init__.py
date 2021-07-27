479f0119263745236705bd9cc6765b143ba40868
Libba Lawrence <t-llawrence@microsoft.com>
2021-07-27 12:35:47 -0400

    fixing code autorest

--- packages/python-packages/protocol-stub-generator/autorest_vendor/__init__.py
+++ packages/python-packages/protocol-stub-generator/autorest_vendor/__init__.py
@@ -3,8 +3,3 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-
-class ConstantGlobalParameter:
-    def __init__(self, name: str, value: str):
-        self.name = name
-        self.value = value

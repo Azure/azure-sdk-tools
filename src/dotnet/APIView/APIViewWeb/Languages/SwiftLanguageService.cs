54a503d2d507ab0343c6b52e8f9073428afe6bde
praveenkuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2021-08-02 17:09:16 -0400

    Swift filter button and create review help text for swift reviews (#1863)
    * Swift filter button and create review help text for swift reviews

---
+++ src/dotnet/APIView/APIViewWeb/Languages/SwiftLanguageService.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.Configuration;
+
+namespace APIViewWeb
+{
+    public class SwiftLanguageService : JsonLanguageService
+    {
+        public override string Name { get; } = "Swift";
+
+        //Swift doesn't have any parser for now
+        //It will upload a json file with name Swift so Swift reviews are listed under that filter type
+        public SwiftLanguageService(IConfiguration configuration)
+        {
+
+        }
+    }
+}

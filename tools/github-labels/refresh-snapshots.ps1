e4380f34e0488c756af6b45b086e90b1d66c10b5
Jesse Squire <jsquire@microsoft.com>
2021-08-09 17:20:14 -0400

    [GitHub Labels] Diff-vs-Common (#1885)
    The focus of these changes is to introduce a new script that will diff
    the labels in a given repository against the common set.   Also included
    are refreshes to the current snapshots for each repository and some minor
    formatting tweaks to existing scripts.

--- tools/github-labels/refresh-snapshots.ps1
+++ tools/github-labels/refresh-snapshots.ps1
@@ -13,22 +13,23 @@
     the set of repositories to query, with each repository appearing on a new line.
 
   .PARAMETER DelayMinutes
-    [optional] The number of minutes to delay between querying each individual repository; 
+    [optional] The number of minutes to delay between querying each individual repository;
     intended to help guard against throttling.
 #>
 
 [CmdletBinding()]
 param
-( 
+(
   [Parameter(Mandatory=$true, HelpMessage="Please provide your GitHub access token.", Position=0)]
-  [string] $GitHubAccessToken,
+  [ValidateNotNullOrEmpty()]
+  [string]$GitHubAccessToken,
 
   [Parameter(Mandatory=$false, HelpMessage="Please provide the directory to write snapshots to.")]
-  [ValidateScript({Test-Path $_ -PathType 'Container'})] 
+  [ValidateScript({Test-Path $_ -PathType 'Container'})]
   [string]$SnapshotDirectory = "./repository-snapshots",
 
   [Parameter(Mandatory=$false, HelpMessage="Please provide the path to the set of repositories.")]
-  [ValidateScript({Test-Path $_ -PathType 'Leaf'})] 
+  [ValidateScript({Test-Path $_ -PathType 'Leaf'})]
   [string]$RepositoryFilePath = "./repositories.txt",
 
   [Parameter(Mandatory=$false, HelpMessage="Please provide the time to delay between repositories (in minutes), between 0 and 100.")]
@@ -41,9 +42,9 @@ $repositories = Get-Content $RepositoryFilePath
 foreach ($currentRepository in $repositories)
 {
     $name = $currentRepository.Replace("Azure\", "")
-    
+
     Write-Host " ==================================================== " -ForegroundColor Green
-    Write-Host " Starting $($currentRepository)"                        -ForegroundColor Green    
+    Write-Host " Starting $($currentRepository)"                        -ForegroundColor Green
 
     (dotnet ./ghcreator/GHCreator.dll List Label $currentRepository -token $GitHubAccessToken) | Out-File -FilePath (Join-Path $SnapshotDirectory "$($name).csv")
 

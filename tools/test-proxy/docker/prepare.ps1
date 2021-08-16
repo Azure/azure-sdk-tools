38167191d839ba0a19ed6dd85c812be27e6fa445
scbedd <45376673+scbedd@users.noreply.github.com>
2021-08-16 13:20:42 -0700

    properly fix the prepare script

--- tools/test-proxy/docker/prepare.ps1
+++ tools/test-proxy/docker/prepare.ps1
@@ -1,9 +1,10 @@
 $targetDirectory = Resolve-Path (Join-Path -Path $PSScriptRoot -ChildPath "../../../eng/common/testproxy/")
 
-if (-not (Test-Path $PsScriptRoot/dev_certificate))
+if (-not (Test-Path $PsScriptRoot/dev_certificate/))
 {
-    mkdir dev_certificate
+    mkdir $PsScriptRoot/dev_certificate/
 }
 
 # copy all files other than .yml from eng/common/scripts/testproxy into local directory dev_certificate
-Get-ChildItem $targetDirectory -Exclude "*.yml" | % { Copy-Item -Path $_ -Destination $PsScriptRoot/dev_certificate }
+Get-ChildItem $targetDirectory -Exclude "*.yml" | % { Copy-Item -Path $_ -Destination "$PsScriptRoot/dev_certificate${$_.Name}" }
+

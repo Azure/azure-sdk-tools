e43f21f908071d3f10d5c4de494476259308b099
scbedd <45376673+scbedd@users.noreply.github.com>
2021-08-16 13:12:49 -0700

    ensure output directory doesn't need local context

--- tools/test-proxy/docker/prepare.ps1
+++ tools/test-proxy/docker/prepare.ps1
@@ -1,9 +1,9 @@
 $targetDirectory = Resolve-Path (Join-Path -Path $PSScriptRoot -ChildPath "../../../eng/common/testproxy/")
 
-if (-not (Test-Path "dev_certificate/"))
+if (-not (Test-Path $PsScriptRoot/dev_certificate))
 {
     mkdir dev_certificate
 }
 
 # copy all files other than .yml from eng/common/scripts/testproxy into local directory dev_certificate
-Get-ChildItem $targetDirectory -Exclude "*.yml" | % { Copy-Item -Path $_ -Destination dev_certificate }
+Get-ChildItem $targetDirectory -Exclude "*.yml" | % { Copy-Item -Path $_ -Destination $PsScriptRoot/dev_certificate }

57352ae9f2371242091ffdc026f9908c014e5d90
praveenkuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2021-08-20 16:17:29 -0400

    Swift parser should not process uploaded json file (#1929)
    * Skip processing json by swift parser

--- src/dotnet/APIView/APIViewWeb/Languages/SwiftLanguageService.cs
+++ src/dotnet/APIView/APIViewWeb/Languages/SwiftLanguageService.cs
@@ -15,5 +15,12 @@ namespace APIViewWeb
         {
 
         }
+
+        public override bool IsSupportedFile(string name)
+        {
+            // Skip initial processing so this service won't be selected for LLC when json is uploaded
+            // This is only a temporary solution for POC and will be remvoed once autorest yaml is uploaded instead of json for LLC
+            return false;
+        }
     }
 }

b9aded9a75dd26c1ed5e60680ed70363fd24cd6c
Praveen Kuttappan <praveen.kuttappan@gmail.com>
2021-10-05 14:34:10 -0400

    Create virtual env and run package parser inside virtual env

--- src/dotnet/APIView/APIViewWeb/Languages/PythonLanguageService.cs
+++ src/dotnet/APIView/APIViewWeb/Languages/PythonLanguageService.cs
@@ -2,7 +2,10 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
+using ApiView;
 using Microsoft.Extensions.Configuration;
 
 namespace APIViewWeb
@@ -13,19 +16,93 @@ namespace APIViewWeb
         public override string Extension { get; } = ".whl";
         public override string VersionString { get; } = "0.2.4";
 
-        private readonly string _apiViewPythonProcessor;
-        public override string ProcessName => _apiViewPythonProcessor;
+        private readonly string _pythonExecutablePath;
+        public override string ProcessName => _pythonExecutablePath;
+        private readonly string _apiStubWheelPath;
 
         public PythonLanguageService(IConfiguration configuration)
         {
-            // apistubgen is located in python's scripts path e.g. <Pythonhome>/Scripts/apistubgen
-            // Env variable PYTHONPROCESSORPATH is set to <pythonhome>/Scripts/apistubgen where parser is located
-            _apiViewPythonProcessor = configuration["PYTHONPROCESSORPATH"] ?? string.Empty;
+            _pythonExecutablePath = configuration["PYTHONEXECUTABLEPATH"] ?? "python";
+            _apiStubWheelPath = Path.Combine(Path.GetDirectoryName(typeof(PythonLanguageService).Assembly.Location), "api-stub-generator");
         }
         public override string GetProcessorArguments(string originalName, string tempDirectory, string jsonPath)
         {
             return $"--pkg-path {originalName} --temp-path {tempDirectory}" +
                 $" --out-path {jsonPath} --hide-report";
+        }
+
+        private string GetPythonVirtualEnv(string tempDirectory)
+        {
+            // Create virtual instance
+            RunProcess(tempDirectory, ProcessName, $" -m virtualenv {tempDirectory} --system-site-packages");
+            string venvPath = Path.Combine(tempDirectory, "Scripts", "python.exe");
+            if (File.Exists(venvPath))
+            {
+                RunProcess(tempDirectory, venvPath, $" -m pip install {_apiStubWheelPath}");
+            }
+            else
+            {
+                throw new InvalidOperationException("Failed to create virtualenv for python package parser");
+            }
+
+            return Path.Combine(tempDirectory, "Scripts", "apistubgen");
+        }
+
+        public override async Task<CodeFile> GetCodeFileAsync(string originalName, Stream stream, bool runAnalysis)
+        {
+            var tempPath = Path.GetTempPath();
+            var randomSegment = Guid.NewGuid().ToString("N");
+            var tempDirectory = Path.Combine(tempPath, "ApiView", randomSegment);
+            Directory.CreateDirectory(tempDirectory);
+            var originalFilePath = Path.Combine(tempDirectory, originalName);
+
+            var jsonFilePath = Path.ChangeExtension(originalFilePath, ".json");
+
+            using (var file = File.Create(originalFilePath))
+            {
+                await stream.CopyToAsync(file);
+            }
+
+            try
+            {
+                var apiStubGenPath = GetPythonVirtualEnv(tempDirectory);
+                var arguments = GetProcessorArguments(originalName, tempDirectory, jsonFilePath);
+                RunProcess(tempDirectory, apiStubGenPath, arguments);
+                using (var codeFileStream = File.OpenRead(jsonFilePath))
+                {
+                    var codeFile = await CodeFile.DeserializeAsync(codeFileStream);
+                    codeFile.VersionString = VersionString;
+                    codeFile.Language = Name;
+                    return codeFile;
+                }
+            }
+            finally
+            {
+                Directory.Delete(tempDirectory, true);
+            }
+        }
+
+        private void RunProcess(string workingDirectory, string processName, string args)
+        {
+            var processStartInfo = new ProcessStartInfo(processName, args)
+            {
+                WorkingDirectory = workingDirectory,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true
+            };
+            using (var process = Process.Start(processStartInfo))
+            {
+                process.WaitForExit();
+                if (process.ExitCode != 0)
+                {
+                    throw new InvalidOperationException(
+                        "Processor failed: " + Environment.NewLine +
+                        "stdout: " + Environment.NewLine +
+                        process.StandardOutput.ReadToEnd() + Environment.NewLine +
+                        "stderr: " + Environment.NewLine +
+                        process.StandardError.ReadToEnd() + Environment.NewLine);
+                }
+            }
         }
     }
 }

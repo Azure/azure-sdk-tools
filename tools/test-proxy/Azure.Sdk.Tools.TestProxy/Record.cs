a5922a12246aaf0ce83340a285b7103e4007cdf1
scbedd <45376673+scbedd@users.noreply.github.com>
2021-10-06 17:08:05 -0700

    clean up using

--- tools/test-proxy/Azure.Sdk.Tools.TestProxy/Record.cs
+++ tools/test-proxy/Azure.Sdk.Tools.TestProxy/Record.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 

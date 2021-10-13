7d205395d53cf39c6d81cad92ccbef581a1d3d10
Changlong Liu <changlong.liu@microsoft.com>
2021-10-13 17:59:03 +0800

    renew-bat

--- tools/mock-service-host/renew-ssh.bat
+++ tools/mock-service-host/renew-ssh.bat
@@ -1,4 +1,3 @@
-#!/bin/sh
 
 openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/CN=127.0.0.1/ST=Shanghai/L=Shanghai/O=ame" -keyout .ssh\127-0-0-1-ca.pem -out .ssh\127-0-0-1-ca.cer
 openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/CN=localhost/ST=Shanghai/L=Shanghai/O=ame" -keyout .ssh\localhost-ca.pem -out .ssh\localhost-ca.crt

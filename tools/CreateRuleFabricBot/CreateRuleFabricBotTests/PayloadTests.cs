defeb757912ba95729b0a14a791ea45b0b19fbe4
Alex Ghiondea <alexghi@microsoft.com>
2021-10-18 14:58:45 -0700

    Update tests to match the correct payload data

--- tools/CreateRuleFabricBot/CreateRuleFabricBotTests/PayloadTests.cs
+++ tools/CreateRuleFabricBot/CreateRuleFabricBotTests/PayloadTests.cs
@@ -58,7 +58,7 @@ namespace Tests
       ""id"": ""AzureSDKPullRequestLabelFolder_repo_test"",
       ""config"": {
         ""configs"": [
-            { ""labels"": [""Label1""], ""pathFilter"": [""test""], ""exclude"": [ """" ]  }
+            { ""label"": ""Label1"", ""pathFilter"": [""test""], ""exclude"": [ """" ]  }
         ],
       ""taskName"" :""Auto PR based on folder paths ""
       }
@@ -74,9 +74,7 @@ namespace Tests
             PathConfig pc = new PathConfig("folder", "path");
 
             string expectedPayload = @"{
-  ""labels"": [
-    ""path""
-  ],
+  ""label"": ""path"",
   ""pathFilter"": [ ""folder"" ], ""exclude"": [ """" ]
 }";
 

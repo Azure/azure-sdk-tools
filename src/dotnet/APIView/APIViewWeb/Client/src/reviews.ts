a35ccecaa99df3d7f823351dbc069cbbdc3c8da0
Chidozie Ononiwu <chononiw@microsoft.com>
2021-10-25 19:30:40 -0700

    UI improvement to reviews page

--- src/dotnet/APIView/APIViewWeb/Client/src/reviews.ts
+++ src/dotnet/APIView/APIViewWeb/Client/src/reviews.ts
@@ -25,9 +25,35 @@
 //  }
 //});
 
-
 // Add DataTables
-(<any>$('#reviews-table')).DataTable({
-  "pageLength" : 25,
-  "search.smart" : true
-});
+$(() => {
+    const languageSelect = $("#reviews-table-language-filter");
+    const searchBox = $("#reviews-table-search-box");
+
+    var reviewsTable = (<any>$('#reviews-table')).DataTable({
+      "rowGroup": {
+        dataSrc: 0
+      },
+      "dom": 't<"pl-2 pr-2"ipr>',
+      "columnDefs" : [
+        { orderable: false, targets: 5 }
+      ],
+      "pageLength" : 25,
+      "search.smart" : true,
+      "drawCallback": function (settings){
+        // Enable bootstraps tooltip
+        (<any>$('[data-toggle="tooltip"]')).tooltip();
+      }
+    });
+
+    searchBox.on("input", function (e) {
+      reviewsTable.search($(this).val() as string).draw();
+    });
+
+    languageSelect.on('change', function(e) {
+      reviewsTable.columns(4).search($(this).val() as string, true, false, false).draw();
+    });
+  }
+);
+
+

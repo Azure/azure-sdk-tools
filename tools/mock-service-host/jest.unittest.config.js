35f89215c3f7ee3ce4ef252eaec9bc75eacfb4ae
changlong-liu <59815250+changlong-liu@users.noreply.github.com>
2021-10-26 23:42:37 +0800

    fix-publish for mock-service-host (#2138)
    * fix-publish
    
    * exclude-dts-test
    
    * trigger-path
    
    * config-unittest
    
    * fix-pipeline-path

--- tools/mock-service-host/jest.unittest.config.js
+++ tools/mock-service-host/jest.unittest.config.js
@@ -4,5 +4,10 @@ const mainConfig = require('./jest.config')
 
 module.exports = {
     ...mainConfig,
-    testMatch: ['**/test/unittest/**/*.ts', '!**/.history/**']
+    testMatch: [
+        '**/test/unittest/**/*.ts',
+        '!**/test/**/*.d.ts',
+        '!**/test/tools.ts',
+        '!**/.history/**'
+    ]
 }

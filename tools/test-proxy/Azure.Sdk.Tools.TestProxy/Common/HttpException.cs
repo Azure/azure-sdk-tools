5ac9b9dff4b155556fe47b27842633773b57fb93
scbedd <45376673+scbedd@users.noreply.github.com>
2021-11-03 14:56:40 -0700

    update TestMisMatchException to inherit from HttpException. Allowing us to easily surface such exceptions to the API.

--- tools/test-proxy/Azure.Sdk.Tools.TestProxy/Common/HttpException.cs
+++ tools/test-proxy/Azure.Sdk.Tools.TestProxy/Common/HttpException.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace Azure.Sdk.Tools.TestProxy.Common
@@ -9,6 +10,11 @@ namespace Azure.Sdk.Tools.TestProxy.Common
     public class HttpException : Exception
     {
         public HttpStatusCode StatusCode { get; }
+
+        public HttpException()
+        {
+            this.StatusCode = HttpStatusCode.InternalServerError;
+        }
 
         public HttpException(HttpStatusCode statusCode)
         {
@@ -25,5 +31,16 @@ namespace Azure.Sdk.Tools.TestProxy.Common
         {
             this.StatusCode = statusCode;
         }
+
+        public HttpException(HttpStatusCode statusCode, string message, Exception innerException) : base(message, innerException)
+        {
+            this.StatusCode = statusCode;
+        }
+
+        protected HttpException(HttpStatusCode statusCode, SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+            this.StatusCode = statusCode;
+        }
+
     }
 }

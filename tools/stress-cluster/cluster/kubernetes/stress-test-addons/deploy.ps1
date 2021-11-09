c71702684afb58dfb847d6a1e1cc3114f48be48a
ckairen <38804567+ckairen@users.noreply.github.com>
2021-11-08 16:36:04 -0800

    Updating helm chart for stress-test-addons (#2223)
    Updated helm chart for stress-test-addons
    Migrated the deploy script from bash script to powershell script
    Updated the README with the deploy steps

---
+++ tools/stress-cluster/cluster/kubernetes/stress-test-addons/deploy.ps1
@@ -0,0 +1,43 @@
+function Run()
+{
+    Write-Host "`n==> $args`n" -ForegroundColor Green
+    $command, $arguments = $args
+    & $command $arguments
+    if ($LASTEXITCODE) {
+        Write-Error "Command '$args' failed with code: $LASTEXITCODE" -ErrorAction 'Continue'
+    }
+}
+function RunOrExitOnFailure()
+{
+    Run @args
+    if ($LASTEXITCODE) {
+        exit $LASTEXITCODE
+    }
+}
+$ErrorActionPreference = 'Stop'
+Set-PSDebug -trace 1
+$subscriptionId = "2cd617ea-1866-46b1-90e3-fffb087ebf9b"
+$env:AZURE_STORAGE_ACCOUNT="stresstestcharts"
+$env:AZURE_STORAGE_KEY=$(RunOrExitOnFailure az storage account keys list --subscription $subscriptionId --account-name $env:AZURE_STORAGE_ACCOUNT -o tsv --query '[0].value')
+
+Remove-Item -Force ./*.tgz
+
+RunOrExitOnFailure helm package  .
+RunOrExitOnFailure helm repo index --url https://stresstestcharts.blob.core.windows.net/helm/ --merge index.yaml .
+
+# The index.yaml in git should be synced with the index.yaml already in blob storage
+# az storage blob download -c helm -n index.yaml -f index.yaml
+
+$files = (Get-Item *.tgz).Name
+$confirmation = Read-Host "Do you want to update the helm repository to add $files? [y/n]"
+if ( $confirmation -match "[yY]" ) { 
+    RunOrExitOnFailure az storage blob upload --subscription $subscriptionId --container-name helm --file index.yaml --name index.yaml
+    RunOrExitOnFailure az storage blob upload --subscription $subscriptionId --container-name helm --file $files --name $files
+
+    # index.yaml must be kept up to date, otherwise when helm generates the file, it will not
+    # merge it with previous entries, and those packages will become inaccessible as they are no
+    # longer index.
+    echo "COMMIT CHANGES MADE TO 'index.yaml'"
+} else {
+    echo "Abort uploading files $files."
+}

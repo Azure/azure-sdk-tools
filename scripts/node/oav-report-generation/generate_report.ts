a0cc651553f9a9405752d2282fc06f27b39bf3e5
scbedd <45376673+scbedd@users.noreply.github.com>
2021-12-01 16:10:40 -0800

    samples actually work now

--- scripts/node/oav-report-generation/generate_report.ts
+++ scripts/node/oav-report-generation/generate_report.ts
@@ -1,7 +1,38 @@
-class HelloThere {
-    coolio: string;
+#!/usr/bin/env node
+import {Argv} from "yargs";
+const fs = require('fs/promises');
+const path = require('path');
 
-    constructor(){
-        this.coolio = "1";
+class PayloadObject {
+    PayloadLocation: string;
+
+    constructor(payloadLocation: string){
+        this.PayloadLocation = payloadLocation;
+    }
+
+    async getPayload(): Promise<Array<any>> {
+        var data = await fs.readFile(this.PayloadLocation)
+
+        console.log(data.toString());
+
+        return [];
     }
 }
+
+
+// functionality start
+require('yargs')
+    .command('html', "Generate an HTML report", (yargs: Argv) => {
+        yargs.option('payload', {
+            alias: 'p',
+            describe: "The targeted oav output."
+        }).option('swagger', {
+            alias: 's',
+            describe: "The targeted oav swagger."
+        })
+    }, async (args: any) => {
+        console.log(`Input Payload: ${args.payload}. Input Swagger: ${args.swagger}`)
+
+        let obj = new PayloadObject(args.payload);
+        await obj.getPayload();
+    }).argv;
--- scripts/node/oav-report-generation/get_sample_input.ps1
+++ scripts/node/oav-report-generation/get_sample_input.ps1
@@ -1,7 +1,7 @@
 param (
-    $recordingsFolder,
-    $swagger,
-    $output
+    $recordingsFolder = "C:\repo\sdk-for-python\sdk\tables\azure-data-tables\tests\recordings",
+    $swagger = "C:\repo\azure-rest-api-specs\specification\cosmos-db\data-plane\Microsoft.Tables\preview\2019-02-02\table.json",
+    $output = "./oav-output"
 )
 
 mkdir -p $output
@@ -14,5 +14,7 @@ oavc convert --directory $recordingsFolder --out $output
 
 Write-Host "Recordings Converted, starting validation."
 
-oav validate-traffic $output $swagger -l error &> build/validation.txt
+oav validate-traffic $output $swagger -l error > build/validation.txt
+
+# do a quick replace to clean up the output into an actual array
 

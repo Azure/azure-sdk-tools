0d20355e77418aab56a0a381572cfb72eeab4fe0
sima-zhu <sizhu@microsoft.com>
2021-12-06 16:35:42 -0800

    Rename common folder

---
+++ tools/identity-resolver/Helpers/StringHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace common.Helpers
+{
+    public static class StringHelper
+    {
+        public static string MaxLength(string input, int maxLength)
+        {
+            return input.Length > maxLength
+                ? input.Substring(0, maxLength)
+                : input;
+        }
+    }
+}

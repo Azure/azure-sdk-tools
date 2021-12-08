9f390e1ab0d8d2b3a53e010d85e6f549d29ef493
sima-zhu <sizhu@microsoft.com>
2021-12-08 14:57:32 -0800

    rename the tool

--- tools/identity-converter/Program.cs
+++ tools/identity-converter/Program.cs
@@ -6,7 +6,7 @@ using System;
 using System.Threading.Tasks;
 using OutputColorizer;
 
-namespace Azure.Sdk.Tools.IdentityResolver
+namespace Azure.Sdk.Tools.IdentityConverter
 {
     class Program
     {

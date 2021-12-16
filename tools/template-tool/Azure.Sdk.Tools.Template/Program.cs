bfd783ffebc874baeaefeaa66554c41e4687f294
Chidozie Ononiwu <chononiw@microsoft.com>
2021-12-15 17:09:45 -0800

    Add template tools package

---
+++ tools/template-tool/Azure.Sdk.Tools.Template/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Azure.Sdk.Tools.Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

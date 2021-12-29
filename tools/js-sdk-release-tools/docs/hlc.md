d0e95f07f426ef7cfca41dcbf4880dd77b7743e6
Wei Dong <40835867+dw511214992@users.noreply.github.com>
2021-12-29 13:21:15 +0800

    add additional parameters to autorest command (#2476)

--- tools/js-sdk-release-tools/docs/hlc.md
+++ tools/js-sdk-release-tools/docs/hlc.md
@@ -17,6 +17,11 @@ You can use this command to generate codes, and then run rush update, rush build
 cd azure-sdk-for-js
 hlc-code-gen --use=@autorest/typescript@6.0.0-beta.5 --readme=../azure-rest-api-specs/specification/iotspaces/resource-manager/readme.md
 ```
+If you want to add more parameters to autorest command, use can use parameter `additionalArgs`. For example:
+```shell script
+cd azure-sdk-for-js
+hlc-code-gen --use=@autorest/typescript@6.0.0-beta.5 --readme=../azure-rest-api-specs/specification/iotspaces/resource-manager/readme.md --additional-args="--package-name=@azure/arm-sample"
+```
 hint: you can change the `readme` to yours, and get codegen version from [NPM](https://www.npmjs.com/package/@autorest/typescript).
 
 ### Use Track2 Codegen to generate codes in swagger pipeline
--- tools/js-sdk-release-tools/src/hlcCodeGenCli.ts
+++ tools/js-sdk-release-tools/src/hlcCodeGenCli.ts
@@ -6,14 +6,14 @@ import {generateSdkAutomatically} from "./hlc/hlcCore";
 
 const shell = require('shelljs');
 
-async function automationGenerate(absoluteReadmeMd: string, tag?: string, use?: string, useDebugger?: boolean, isStable?: boolean) {
+async function automationGenerate(absoluteReadmeMd: string, tag?: string, use?: string, useDebugger?: boolean, additionalArgs?: string) {
     const regexResult = /^(.*\/azure-rest-api-specs[-pr]*)\/(specification.*)/.exec(absoluteReadmeMd);
     if (!regexResult || regexResult.length !== 3) {
         logger.logError(`Cannot Parse readme file path: ${absoluteReadmeMd}`);
     } else {
         const gitCommitId = await getLastCommitId(regexResult[1]);
         const relativeReadmeMd = regexResult[2];
-        await generateSdkAutomatically(String(shell.pwd()), absoluteReadmeMd, relativeReadmeMd, gitCommitId, tag, use, useDebugger, isStable);
+        await generateSdkAutomatically(String(shell.pwd()), absoluteReadmeMd, relativeReadmeMd, gitCommitId, tag, use, useDebugger, additionalArgs);
     }
 
 }
@@ -22,9 +22,9 @@ const optionDefinitions = [
     { name: 'use',  type: String },
     { name: 'tag', type: String },
     { name: 'readme', type: String },
-    { name: 'debugger', type: String},
-    { name: 'stable', type: Boolean},
+    { name: 'useDebugger', type: String},
+    { name: 'additional-args', type: String },
 ];
 const commandLineArgs = require('command-line-args');
 const options = commandLineArgs(optionDefinitions);
-automationGenerate(options.readme, options.tag, options.use, options.useDebugger? true : false, options.stable);
+automationGenerate(options.readme, options.tag, options.use, options.useDebugger? true : false, options['additional-args']);

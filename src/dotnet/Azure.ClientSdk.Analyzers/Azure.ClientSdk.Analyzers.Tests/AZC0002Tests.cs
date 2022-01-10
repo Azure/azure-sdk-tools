c23d077f1d14a6f846c2b18e2f6ce275cd92739c
Krzysztof Cwalina <kcwalina@microsoft.com>
2022-01-10 09:43:08 -0800

    Added tests

--- src/dotnet/Azure.ClientSdk.Analyzers/Azure.ClientSdk.Analyzers.Tests/AZC0002Tests.cs
+++ src/dotnet/Azure.ClientSdk.Analyzers/Azure.ClientSdk.Analyzers.Tests/AZC0002Tests.cs
@@ -121,6 +121,40 @@ namespace RandomNamespace
                 .RunAsync();
         }
 
+        [Fact]
+        public async Task AZC0002DoesntFireIfThereIsAnOverloadWithRequestContext()
+        {
+            const string code = @"
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RandomNamespace
+{
+    public class SomeClient
+    {
+        public virtual Task GetAsync(string s)
+        {
+            return null;
+        }
+
+        public virtual void Get(string s)
+        {
+        }
+
+        public virtual Task GetAsync(string s, RequestContext context = default)
+        {
+            return null;
+        }
+
+        public virtual void Get(string s, RequestContext context = default)
+        {
+        }
+    }
+}";
+            await Verifier.CreateAnalyzer(code)
+                .WithDisabledDiagnostics("AZC0015")
+                .RunAsync();
+        }
 
         [Fact]
         public async Task AZC0002ProducedWhenCancellationTokenOverloadsDontMatch()
@@ -174,6 +208,32 @@ namespace RandomNamespace
         }
 
         public virtual void Get(CancellationToken cancellationToken = default)
+        {
+        }
+    }
+}";
+            await Verifier.CreateAnalyzer(code)
+                .WithDisabledDiagnostics("AZC0015")
+                .RunAsync();
+        }
+
+        [Fact]
+        public async Task AZC0002NotProducedForMethodsWithRequestContext()
+        {
+            const string code = @"
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RandomNamespace
+{
+    public class SomeClient
+    {
+        public virtual Task GetAsync(RequestContext context = default)
+        {
+            return null;
+        }
+
+        public virtual void Get(RequestContext context = default)
         {
         }
     }

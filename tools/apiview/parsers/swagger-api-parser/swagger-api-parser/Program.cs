f8ae22003353e88eb822f9a271157e0f1791c3a1
Ruoxuan Wang <ruowan@microsoft.com>
2022-01-15 13:46:53 +0800

    add swagger-api-parser solution

---
+++ tools/apiview/parsers/swagger-api-parser/swagger-api-parser/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using APIViewWeb;
+
+namespace swagger_api_parser
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {   
+            var input = args[0];
+            var output = args[1];
+            var fileReadStream = File.OpenRead(input);
+            var ls = new SwaggerLanguageService();
+            var cf = await ls.GetCodeFileInternalAsync(input, fileReadStream, false);
+            var fileWriteStream = File.OpenWrite(output);
+            await cf.SerializeAsync(fileWriteStream);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

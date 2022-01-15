030bbf4ec036e8a6926700496fe100e16a91034a
Ruoxuan Wang <ruowan@microsoft.com>
2022-01-15 14:03:35 +0800

    add log

--- tools/apiview/parsers/swagger-api-parser/swagger-api-parser/Program.cs
+++ tools/apiview/parsers/swagger-api-parser/swagger-api-parser/Program.cs
@@ -9,14 +9,20 @@ namespace swagger_api_parser
     {
         static async Task Main(string[] args)
         {   
-            var input = args[0];
-            var output = args[1];
+            if(args.Length == 0)
+            {
+                throw new ArgumentException("swagger");
+            }
+            var input = Path.GetFullPath(args[0]);
+
+            Console.WriteLine("Input swagger file: {0}", input);
+            var output = input.Replace(".json", ".swagger");
             var fileReadStream = File.OpenRead(input);
             var ls = new SwaggerLanguageService();
             var cf = await ls.GetCodeFileInternalAsync(input, fileReadStream, false);
             var fileWriteStream = File.OpenWrite(output);
             await cf.SerializeAsync(fileWriteStream);
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Generated output file {0}", output);
         }
     }
 }

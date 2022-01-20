ed68bff4f3d0ddb778600515adfb43d709bd9ac2
scbedd <45376673+scbedd@users.noreply.github.com>
2022-01-19 19:09:16 -0800

    progress on understanding this. need to pivot the appropriate data elements and then style the output

--- scripts/node/oav-report-generation/generate_report.ts
+++ scripts/node/oav-report-generation/generate_report.ts
@@ -18,8 +18,23 @@ class CoverageView {
         this.validationResults = validationResults;
         this.generatedDate = new Date();
         this.language = language;
+
+        this.generalErrorResults = new Map();
+
     }
 
+    formatGeneratedDate(): string {
+        let day = this.generatedDate.getDate();
+        let month = this.generatedDate.getMonth() + 1;
+        let year = this.generatedDate.getFullYear();
+        let hours = this.generatedDate.getHours();
+        let minutes = this.generatedDate.getMinutes();
+
+        return year + "-" 
+            + (month < 10 ? "0" + month : month) + "-" 
+            + (day < 10 ? "0" + day : day) + " at " 
+            + hours + ":" + (minutes < 10 ? "0" + minutes : minutes) + (hours < 13 ? "AM" : "PM");
+    }
 
     getTotalErrors(): number {
         return this.validationResults.length;
@@ -27,7 +42,7 @@ class CoverageView {
 
     getGeneralErrors(): Array<oav.TrafficValidationIssue>{
         return this.validationResults.filter((x, idx) => {
-            return x.errors.length > 0
+            return x.errors.length > 0;
         });
     }
 
@@ -37,7 +52,7 @@ class CoverageView {
 
     getRunTimeErrors(): Array<oav.TrafficValidationIssue>{
         return this.validationResults.filter((x, idx) => {
-            return x.runtimeExceptions.length > 0
+            return x.runtimeExceptions.length > 0;
         });
     }
 
@@ -49,6 +64,7 @@ class CoverageView {
     generatedDate: Date;
     validationResults: Array<oav.TrafficValidationIssue>;
     language: string;
+    generalErrorResults: Map<string, Array<oav.TrafficValidationIssue>>;
 }
 
 // functionality start
@@ -60,6 +76,14 @@ require('yargs')
         }).option('swagger', {
             alias: 's',
             describe: "The targeted oav swagger."
+        }).option('package', {
+            default: "azure-data-tables",
+            describe: "The targeted package name"
+        })
+        .option('language', {
+            alias: 'l',
+            default: "python",
+            describe: "The targeted oav swagger."
         })
     }, async (args: any) => {
         console.log(`Input Payload: ${args.payload}. Input Swagger: ${args.swagger}`)
@@ -68,8 +92,16 @@ require('yargs')
         errors = await oav.validateTrafficAgainstSpec(args.swagger, args.payload, {});
         let template = await fs.readFile("template/layout-base.mustache", "utf-8");
 
-        let view = new CoverageView("azure-data-tables", errors, "python");
+        let view = new CoverageView(args.package, errors, args.language);
+
+        let general_errors = view.getGeneralErrors();
+        let runtime_errors = view.getRunTimeErrors();
+        
+        console.log(general_errors);
+        console.log(runtime_errors);
+
         let text = Mustache.render(template, view);
+        debugger;
 
         let writeResult = await fs.writeFile("report.html", text, "utf-8");
     }).argv;

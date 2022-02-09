16269d2ea29807e06e453815f400a041a881c234
Chidozie Ononiwu <chononiw@microsoft.com>
2022-02-09 12:24:48 -0800

    Add + and - to diff lines

--- src/dotnet/APIView/APIViewWeb/Repositories/ReviewManager.cs
+++ src/dotnet/APIView/APIViewWeb/Repositories/ReviewManager.cs
@@ -524,7 +524,7 @@ namespace APIViewWeb.Respositories
 
         public async void UpdateReviewBackground()
         {
-            var reviews = await _reviewsRepository.GetReviewsAsync(false, "All");
+            var reviews = await _reviewsRepository.GetReviewsAsync(false, "Allll");
             await SyncPackageDisplayServiceName(reviews);
             foreach (var review in reviews.Where(r => IsUpdateAvailable(r)))
             {

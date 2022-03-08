179e10197cfd2836552eaa26b4fd4b84ab781960
Mike Harder <mharder@microsoft.com>
2022-03-08 13:25:48 -0800

    Improve vcpkg comments

--- tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/Cpp.cs
+++ tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/Cpp.cs
@@ -15,8 +15,8 @@ namespace Azure.Sdk.Tools.PerfAutomation
     //    Linux: export VCPKG_ROOT=/home/user/vcpkg && export VCPKG_DEFAULT_TRIPLET=x64-linux
     // 2. git clone https://github.com/microsoft/vcpkg
     // 3. cd vcpkg
-    // 4. ./bootstrap-vcpkg
-    // 5. vcpkg install curl LibXml2
+    // 4. ./bootstrap-vcpkg.sh
+    // 5. ./vcpkg install curl LibXml2
     public class Cpp : LanguageBase
     {
         private const string _buildDirectory = "build";

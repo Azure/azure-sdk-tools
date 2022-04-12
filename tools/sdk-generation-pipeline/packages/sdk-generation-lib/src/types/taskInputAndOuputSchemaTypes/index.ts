3193d751035639830264e46d4b91c1002d475cb7
dw511214992 <dw511214992@gmail.com>
2022-04-12 14:10:28 +0800

    move codes of azure-sdk-pipeline to azure-sdk-tools

---
+++ tools/sdk-generation-pipeline/packages/sdk-generation-lib/src/types/taskInputAndOuputSchemaTypes/index.ts
@@ -0,0 +1,7 @@
+export * from './InitOutput';
+export * from './GenerateAndBuildInput';
+export * from './GenerateAndBuildOutput';
+export * from './MockTestInput';
+export * from './LiveTestInput';
+export * from './TestOutput';
+export * from './CodegenToSdkConfig';

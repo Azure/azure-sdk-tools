830bf2e6304e724063158294fe1fa0922ced69f3
sima-zhu <sizhu@microsoft.com>
2022-04-13 11:25:49 -0700

    add helper

---
+++ tools/identity-resolution/Helpers/CacheHelper.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azure.Sdk.Tools.NotificationConfiguration.Helpers
+{
+    public class CacheHelper<TKey, TValue>
+    {
+        private readonly ConcurrentDictionary<TKey, TValue> resultCache = new ConcurrentDictionary<TKey, TValue>(); 
+        private readonly ConcurrentDictionary<TKey, SemaphoreSlim> keyLocks = new ConcurrentDictionary<TKey, SemaphoreSlim>();
+
+
+        public CacheHelper()
+        {
+            
+        }
+
+        public async Task<TValue> GetValueByKey(TKey key, Func<TKey, Task<TValue>> f)
+        {
+            TValue output;
+            if (!resultCache.TryGetValue(key, out output))
+            {
+                output = await f(key);
+                resultCache.TryAdd(key, output);
+            }
+            return output;
+        }
+    }
+}

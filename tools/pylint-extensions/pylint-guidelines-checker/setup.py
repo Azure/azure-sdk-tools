fded0e9e2f255ecc825c5e18123f105c034c6af3
Libba Lawrence <llawrence@microsoft.com>
2022-04-20 14:50:40 -0700

    Moving pylint checker updates from sdk-for-python into sdk-tools.

--- tools/pylint-extensions/pylint-guidelines-checker/setup.py
+++ tools/pylint-extensions/pylint-guidelines-checker/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="pylint-guidelines-checker",
     version="0.0.1",
-    url='http://github.com/Azure/azure-sdk-tools',
+    url='http://github.com/Azure/azure-sdk-for-python',
     license='MIT License',
     description="A pylint plugin which enforces azure sdk guidelines.",
     author='Microsoft Corporation',

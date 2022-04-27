5c27990651736404319833db6eaa6fbbb92cc505
l0lawrence <llawrence@microsoft.com>
2022-04-27 09:25:08 -0700

    adding test file

---
+++ tools/pylint-extensions/pylint-guidelines-checker/tests/test_files/docstring_parameters_violation.py
@@ -0,0 +1,11 @@
+def docstring_violation(self, one_thing: str , two_thing: str):
+    """
+    :param one_thing: OneThing
+    :return: str
+    """
+    return ""
+
+# Message(msg_id='docstring-should-be-keyword', line=None, node=<FunctionDef.docstring_violation l.1 at 0x20ed5c311f0>, args='one_thing', 
+# confidence=Confidence(name='UNDEFINED', description='Warning without any associated confidence level.'))
+
+# Potentially incorrect here ^^

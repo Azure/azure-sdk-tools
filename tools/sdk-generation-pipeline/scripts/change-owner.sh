e0efb96e5b8a81528835d9d67427799d0c380804
dw511214992 <dw511214992@gmail.com>
2022-05-17 17:25:32 +0800

    fix change-owner

--- tools/sdk-generation-pipeline/scripts/change-owner.sh
+++ tools/sdk-generation-pipeline/scripts/change-owner.sh
@@ -7,12 +7,14 @@ SDK_REPO=/sdk-repo
 if [ -d "${SPEC_REPO}" ]; then
   while true
   do
-    USER_GROUP_ID=`stat -c "%u:%g" ${SPEC_REPO}`
-    if [ -d "${WORK_DIR}" ]; then
-      chown -R ${USER_GROUP_ID} ${WORK_DIR}
-    fi
-    if [ -d "${SDK_REPO}" ]; then
-      chown -R ${USER_GROUP_ID} ${SDK_REPO}
+    if [ -f "/tmp/notExit" ]; then
+      USER_GROUP_ID=`stat -c "%u:%g" ${SPEC_REPO}`
+      if [ -d "${WORK_DIR}" ]; then
+        chown -R ${USER_GROUP_ID} ${WORK_DIR}
+      fi
+      if [ -d "${SDK_REPO}" ]; then
+        chown -R ${USER_GROUP_ID} ${SDK_REPO}
+      fi
     fi
     sleep 5s
   done

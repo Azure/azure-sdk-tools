e629cde1c1967bc5645a8d0aeaa24b303d7a4a58
scbedd <45376673+scbedd@users.noreply.github.com>
2022-05-20 13:21:55 -0700

    beginning of activation loop. time to create a test dll to ensure this will actually work!

--- tools/test-proxy/Azure.Sdk.Tools.TestProxy/Store/StoreResolver.cs
+++ tools/test-proxy/Azure.Sdk.Tools.TestProxy/Store/StoreResolver.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Azure.Sdk.Tools.TestProxy.Common;
+
 namespace Azure.Sdk.Tools.TestProxy.Store
 {
     public class StoreResolver
@@ -6,26 +13,100 @@ namespace Azure.Sdk.Tools.TestProxy.Store
 
         public StoreResolver(string[] assemblyDirectories)
         {
-            // TODO: handle null/empty/invalid paths here.
+            foreach(var directory in assemblyDirectories)
+            {
+                if (!File.Exists(directory))
+                {
+                    throw new HttpException(System.Net.HttpStatusCode.BadRequest, $"Provided directory {directory} does not exist.");
+                }
+            }
+
             AssemblyDirectories = assemblyDirectories;
         }
 
         public StoreResolver(string assemblyDirectory)
         {
-            // TODO: handle null/empty/invalid paths here.
+            if (!File.Exists(assemblyDirectory))
+            {
+                throw new HttpException(System.Net.HttpStatusCode.BadRequest, $"Provided directory {assemblyDirectory} does not exist.");
+            }
+
             AssemblyDirectories = new string[] { assemblyDirectory };
+        }
+
+        private Assembly LoadAssembly(string path)
+        {
+            return null;
+        }
+
+        private Type CheckAssemblyForStore(string storeName, Assembly inputAssembly)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string[] GetAssembliesFromFolder(string folder)
+        {
+            return Directory.GetFiles(folder, "*.dll");
         }
 
         /// <summary>
         /// Used to resolve the store given a name and an additional assembly directory. The provided path will be added to the existing set present within AssemblyDirectories property.
+        /// This function will take the FIRST matched assembly type by NAME.
         /// </summary>
         /// <param name="storeName"></param>
         /// <param name="additionalAssemblyDirectory"></param>
         /// <returns></returns>
         public IAssetsStore ResolveStore(string storeName, string additionalAssemblyDirectory)
         {
-            // TODO: implement
-            return new NullStore();
+            // first check if the type is present in executing assembly
+            var invokingAssembly = Assembly.GetExecutingAssembly();
+
+            var storeType = CheckAssemblyForStore(storeName, invokingAssembly);
+
+            if (storeType == null)
+            {
+                foreach(var assemblyDirectory in AssemblyDirectories)
+                {
+                    var assemblyFiles = GetAssembliesFromFolder(assemblyDirectory);
+
+                    foreach(var assemblyFile in assemblyFiles)
+                    {
+                        storeType = CheckAssemblyForStore(storeName, LoadAssembly(assemblyFile));
+
+                        if(storeType != null)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (storeType != null)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (storeType == null)
+            {
+                throw new HttpException(
+                    System.Net.HttpStatusCode.BadRequest,
+                    $"Unable to load the specified IAssetStore class {storeName}. Looked in invoking assembly as well as additional directories: [{String.Join(",", AssemblyDirectories)}]"
+                );
+            }
+
+            try
+            {
+                var generatedStore = Activator.CreateInstance(storeType);
+
+                return (IAssetsStore)generatedStore;
+            }
+            catch (Exception e)
+            {
+                throw new HttpException(
+                    System.Net.HttpStatusCode.BadRequest,
+                    $"Unable to create an instance of type {storeType.Name}. This name was generated from input {storeName}. The invoking assembly and directories [{String.Join(",", AssemblyDirectories)}] were queried for this type."
+                );
+            }
         }
 
         /// <summary>

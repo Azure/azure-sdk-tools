116df5e97aa4159548f2a1a83cf34488894b3b6c
ruowan <ruowan@ruowandeMacBook-Pro.local>
2022-05-24 19:24:11 +0800

    add baseSchema

--- tools/apiview/parsers/swagger-api-parser/swagger-api-parser/SwaggerTypes.cs
+++ tools/apiview/parsers/swagger-api-parser/swagger-api-parser/SwaggerTypes.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 using Newtonsoft.Json;
@@ -7,10 +8,18 @@ namespace swagger_api_parser
     public class SwaggerSpec
     {
         public string swagger { get; set; }
-        public Dictionary<string, Dictionary<string, Operation> > paths { get; set; }
+
+        public string host { get; set; }
+
+        public List<string> schemes { get; set; }
+        public Dictionary<string, Dictionary<string, Operation>> paths { get; set; }
         public Info info { get; set; }
-        
-        
+
+        public Dictionary<string, Parameter> parameters { get; set; }
+
+        [JsonPropertyName("x-ms-paths")] public Dictionary<string, Dictionary<string, Operation>> xMsPaths { get; set; }
+
+        public Dictionary<string, Schema> definitions { get; set; }
     }
 
     public class Info
@@ -24,12 +33,10 @@ namespace swagger_api_parser
 
     public class Operation
     {
-
         public string description { get; set; }
         public string operationId { get; set; }
         public List<Parameter> parameters { get; set; }
         public Dictionary<string, Response> responses { get; set; }
-
     }
 
     public class Parameter
@@ -37,12 +44,10 @@ namespace swagger_api_parser
         public string name { get; set; }
         public bool required { get; set; }
         public string description { get; set; }
-        
-        [JsonPropertyName("in")]
-        public string In { get; set; }
 
+        [JsonPropertyName("in")] public string In { get; set; }
     }
-    
+
 
     public class Response
     {
@@ -51,18 +56,32 @@ namespace swagger_api_parser
     }
 
     public class Header : BaseSchema
-    { }
+    {
+    }
+
+    public class Schema : BaseSchema
+    {
+    }
 
     public class BaseSchema
     {
         public string description { get; set; }
         public string type { get; set; }
         public string format { get; set; }
+        public List<BaseSchema> allOf { get; set; }
+        public Boolean additionalProperties { get; set; }
+        public Boolean readOnly { get; set; }
+        public string discriminator { get; set; }
+        public Dictionary<string, BaseSchema> properties { get; set; }
 
-        [JsonPropertyName("$ref")]
-        public string _ref { get; set; }
+        [JsonPropertyName("x-ms-discriminator-value")]
+        public string xMsDiscriminatorValue { get; set; }
+
+        public List<string> required { get; set; }
+
+
+        [JsonPropertyName("$ref")] public string _ref { get; set; }
 
         public List<BaseSchema> items { get; set; }
     }
-    
 }

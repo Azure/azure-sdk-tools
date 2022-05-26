d488896e01d96e16fa513a3b06a31b87c2a57b2a
Changlong Liu <changlong.liu@microsoft.com>
2022-05-26 18:15:40 +0800

    test

--- tools/mock-service-host/test/unittest/testMock.ts
+++ tools/mock-service-host/test/unittest/testMock.ts
@@ -96,6 +96,11 @@ describe('mockForExample: ', () => {
             { info: { version: '2020-01-01' } },
             'fakeRp'
         )
+        swaggerMocker.patchExampleResponses(example, specItem as SpecItem, spec, {
+            method: 'put',
+            url:
+                'https://localhost:8443/subscriptions/xxx/resourceGroups/yy/providers/Microsoft.ApiManagement/service/serviceName/policies/policyId'
+        })
 
         assert.strictEqual(example.responses['200'].body.test, undefined)
 

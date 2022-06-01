3135a0366f7fd6b2db38a0c961d81ec0ff2212ce
Charles Lowell <10964656+chlowell@users.noreply.github.com>
2022-06-01 10:01:11 -0700

    Fix apiviewgo cross-platform path handling (#3387)

--- src/go/cmd/module.go
+++ src/go/cmd/module.go
@@ -8,13 +8,22 @@ import (
 	"fmt"
 	"go/ast"
 	"io/fs"
+	"os"
+	"path"
 	"path/filepath"
 	"strings"
+
+	"golang.org/x/mod/modfile"
 )
 
 // indexTestdata allows tests to index this module's testdata
 // (we never want to do that for real reviews)
 var indexTestdata bool
+
+// sdkDirName allows tests to set the name of the assumed common
+// directory of all Azure SDK modules, which enables tests to
+// pass without the code below having to compute this directory
+var sdkDirName = "sdk"
 
 // Module collects the data required to describe an Azure SDK module's public API.
 type Module struct {
@@ -22,23 +31,24 @@ type Module struct {
 
 	// packages maps import paths to packages
 	packages map[string]*Pkg
-
-	// path is the module's path on disk
-	path string
 }
 
-// NewModule indexes an Azure SDK module's ASTs. dir must be under azure-sdk-for-go/sdk.
+// NewModule indexes an Azure SDK module's ASTs
 func NewModule(dir string) (*Module, error) {
-	var baseSDKPath, baseImportPath string
-	if before, _, found := strings.Cut(dir, "sdk"+string(filepath.Separator)); found {
-		baseSDKPath = filepath.Join(before, "sdk")
-		baseImportPath = "github.com/Azure/azure-sdk-for-go/sdk/"
-	} else {
-		fmt.Println(dir + " isn't part of the Azure SDK for Go. Output may be incomplete or inaccurate.")
+	mf, err := parseModFile(dir)
+	if err != nil {
+		return nil, err
 	}
-	m := Module{Name: filepath.Base(dir), packages: map[string]*Pkg{}, path: dir}
+	// sdkRoot is the path on disk to the sdk folder e.g. /home/user/me/azure-sdk-for-go/sdk.
+	// Used to find definitions of types imported from other Azure SDK modules.
+	sdkRoot := ""
+	if before, _, found := strings.Cut(dir, fmt.Sprintf("%s%c", sdkDirName, filepath.Separator)); found {
+		sdkRoot = filepath.Join(before, sdkDirName)
+	}
+	m := Module{Name: filepath.Base(dir), packages: map[string]*Pkg{}}
 
-	filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+	baseImportPath := path.Dir(mf.Module.Mod.Path) + "/"
+	err = filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
 		if d.IsDir() {
 			if !indexTestdata && strings.Contains(path, "testdata") {
 				return filepath.SkipDir
@@ -47,11 +57,14 @@ func NewModule(dir string) (*Module, error) {
 			if err == nil {
 				m.packages[baseImportPath+p.Name()] = p
 			} else if !errors.Is(err, ErrNoPackages) {
-				fmt.Printf("error: %v\n", err)
+				return err
 			}
 		}
 		return nil
 	})
+	if err != nil {
+		return nil, err
+	}
 
 	for _, p := range m.packages {
 		p.Index()
@@ -72,25 +85,35 @@ func NewModule(dir string) (*Module, error) {
 			var ok bool
 			if source, ok = m.packages[impPath]; !ok {
 				// must be a package external to this module
-				if source, ok = externalPackages[impPath]; !ok {
+				if source, ok = externalPackages[impPath]; !ok && sdkRoot != "" {
 					// figure out a path to the package, index it
 					if _, after, found := strings.Cut(impPath, "azure-sdk-for-go/sdk/"); found {
-						path := filepath.Join(baseSDKPath, after)
+						path := filepath.Join(sdkRoot, after)
 						pkg, err := NewPkg(path, after)
-						if err != nil {
-							fmt.Printf("couldn't parse %s: %v", impPath, err)
-							continue
+						if err == nil {
+							pkg.Index()
+							externalPackages[impPath] = pkg
+							source = pkg
+						} else {
+							// types from this module will appear in the review without their definitions
+							fmt.Printf("couldn't parse %s: %v\n", impPath, err)
 						}
-						pkg.Index()
-						externalPackages[impPath] = pkg
-						source = pkg
 					}
 				}
 			}
 
+			level := DiagnosticLevelInfo
+			originalName := qn
+			if _, after, found := strings.Cut(qn, m.Name); found {
+				originalName = strings.TrimPrefix(after, "/")
+			} else {
+				// this type is defined in another module
+				level = DiagnosticLevelWarning
+			}
+
 			var t TokenMaker
 			if source == nil {
-				t = p.c.addSimpleType(*p, alias, p.Name(), qn)
+				t = p.c.addSimpleType(*p, alias, p.Name(), originalName)
 			} else if def, ok := source.types[typeName]; ok {
 				switch n := def.n.Type.(type) {
 				case *ast.InterfaceType:
@@ -100,25 +123,29 @@ func NewModule(dir string) (*Module, error) {
 				case *ast.Ident:
 					t = p.c.addSimpleType(*p, alias, p.Name(), def.n.Type.(*ast.Ident).Name)
 				default:
-					fmt.Printf("unexpected node type %T", def.n.Type)
+					fmt.Printf("unexpected node type %T\n", def.n.Type)
+					t = p.c.addSimpleType(*p, alias, p.Name(), originalName)
 				}
 			} else {
 				fmt.Println("found no definition for " + qn)
 			}
 			if t != nil {
-				path := strings.TrimPrefix(qn, baseImportPath)
-				level := DiagnosticLevelInfo
-				if !strings.Contains(path, m.Name) {
-					// this type is defined in another module
-					level = DiagnosticLevelWarning
-				}
 				p.diagnostics = append(p.diagnostics, Diagnostic{
 					Level:    level,
 					TargetID: t.ID(),
-					Text:     aliasFor + path,
+					Text:     aliasFor + originalName,
 				})
 			}
 		}
 	}
 	return &m, nil
+}
+
+func parseModFile(dir string) (*modfile.File, error) {
+	path := filepath.Join(dir, "go.mod")
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return modfile.Parse(path, content, nil)
 }

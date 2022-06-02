466cc5f108ab5f1508dc705a90b12db6242c144b
dw511214992 <dw511214992@gmail.com>
2022-06-02 13:01:50 +0800

    fix

--- tools/sdk-generation-pipeline/packages/sdk-generation-cli/test/unit/GitOperationWrapper.test.ts
+++ tools/sdk-generation-pipeline/packages/sdk-generation-cli/test/unit/GitOperationWrapper.test.ts
@@ -1,10 +1,13 @@
 // eslint-disable-next-line simple-import-sort/imports
-import { closeWithSuccess } from './utils/childProcesses';
+import * as cp from './utils/mockChildProcess';
 import { GitOperationWrapper } from '../../src/utils/GitOperationWrapper';
-import { mockChildProcessModule } from './utils/mockChildProcess';
 
 describe('git operation wrapper', () => {
     let gitOperationWrapper: GitOperationWrapper;
+
+    function assertExecutedCommands(...commands: string[]) {
+        expect(cp.mockChildProcessModule.$mostRecent().$args).toEqual(commands);
+    }
 
     beforeAll(() => {
         gitOperationWrapper = new GitOperationWrapper();
@@ -12,37 +15,37 @@ describe('git operation wrapper', () => {
     );
     it('getFileListInPackageFolder', async () => {
         gitOperationWrapper.getFileListInPackageFolder('.');
-        await closeWithSuccess();
-        expect(mockChildProcessModule.$mostRecent().$args).toEqual(['ls-files', '-cmo', '--exclude-standard']);
+        await cp.closeWithSuccess();
+        assertExecutedCommands('ls-files', '-cmo', '--exclude-standard');
     });
 
     it('getHeadSha', async () => {
         gitOperationWrapper.getHeadSha();
-        await closeWithSuccess();
-        expect(mockChildProcessModule.$mostRecent().$args).toEqual(['rev-parse', 'HEAD']);
+        await cp.closeWithSuccess();
+        assertExecutedCommands('rev-parse', 'HEAD');
     });
 
     it('getHeadRef', async () => {
         gitOperationWrapper.getHeadRef();
-        await closeWithSuccess();
-        expect(mockChildProcessModule.$mostRecent().$args).toEqual(['rev-parse', '--abbrev-ref', 'HEAD']);
+        await cp.closeWithSuccess();
+        assertExecutedCommands('rev-parse', '--abbrev-ref', 'HEAD');
     });
 
     it('safeDirectory', async () => {
         gitOperationWrapper.safeDirectory();
-        await closeWithSuccess();
-        expect(mockChildProcessModule.$mostRecent().$args).toEqual(['config', '--global', '--add', 'safe.directory', '.']);
+        await cp.closeWithSuccess();
+        assertExecutedCommands('config', '--global', '--add', 'safe.directory', '.');
     });
 
     it('disableFileMode', async () => {
         gitOperationWrapper.disableFileMode();
-        await closeWithSuccess();
-        expect(mockChildProcessModule.$mostRecent().$args).toEqual(['config', 'core.fileMode', 'false', '--replace-all']);
+        await cp.closeWithSuccess();
+        assertExecutedCommands('config', 'core.fileMode', 'false', '--replace-all');
     });
 
     it('getChangedPackageDirectory', async () => {
         gitOperationWrapper.getChangedPackageDirectory();
-        await closeWithSuccess();
-        expect(mockChildProcessModule.$mostRecent().$args).toEqual(['ls-files', '-mdo', '--exclude-standard']);
+        await cp.closeWithSuccess();
+        assertExecutedCommands('ls-files', '-mdo', '--exclude-standard');
     });
 });
--- tools/sdk-generation-pipeline/packages/sdk-generation-cli/test/unit/utils/mockChildProcess.ts
+++ tools/sdk-generation-pipeline/packages/sdk-generation-cli/test/unit/utils/mockChildProcess.ts
@@ -129,6 +129,37 @@ export const mockChildProcessModule = (function mockChildProcessModule() {
     }
 }());
 
+export function wait(timeoutOrPromise: number | Promise<unknown> = 10): Promise<void> {
+    if (timeoutOrPromise && typeof timeoutOrPromise === 'object' && typeof timeoutOrPromise.then === 'function') {
+        return timeoutOrPromise.then(() => wait());
+    }
+
+    return new Promise((ok) => setTimeout(ok, typeof timeoutOrPromise === 'number' ? timeoutOrPromise : 10));
+}
+
+async function exitChildProcess(proc: MockChildProcess, data: string | null, exitSignal: number) {
+    if (proc.$emitted('exit')) {
+        throw new Error('exitChildProcess: attempting to exit an already closed process');
+    }
+
+    if (typeof data === 'string') {
+        proc.stdout.$emit('data', Buffer.from(data));
+    }
+
+    proc.$emit('exit', exitSignal);
+    proc.$emit('close', exitSignal);
+}
+
+export async function closeWithSuccess(message = '') {
+    await wait();
+    const match = mockChildProcessModule.$matchingChildProcess((p) => !p.$emitted('exit'));
+    if (!match) {
+        throw new Error(`closeWithSuccess unable to find matching child process`);
+    }
+    await exitChildProcess(match, message, 0);
+    await wait();
+}
+
 jest.mock('child_process', () => mockChildProcessModule);
 
 afterEach(() => {

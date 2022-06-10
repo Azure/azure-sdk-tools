c5cb3da8b1a6c3691baf4ca71b3221070d7df7a4
scbedd <45376673+scbedd@users.noreply.github.com>
2022-06-10 11:35:56 -0700

    adding initial tests. describing a test folder. working through boilerplate functionality before moving on to git shell out

---
+++ tools/test-proxy/Azure.Sdk.Tools.TestProxy.Tests/GitStoreTests.cs
@@ -0,0 +1,93 @@
+﻿using Azure.Sdk.Tools.TestProxy.Common;
+using Azure.Sdk.Tools.TestProxy.Store;
+using Azure.Sdk.Tools.TestProxy.Transforms;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Azure.Sdk.Tools.TestProxy.Tests
+{
+    public class GitStoretests
+    {
+        #region variable defs
+        public static string DefaultAssetsJson =
+@"
+{
+    ""AssetsRepo"" = ""Azure/azure-sdk-assets-integration""
+    ""AssetsRepoPrefixPath"" = ""python/recordings/""
+    ""AssetsRepoId"" = """"
+    ""AssetsRepoBranch"" = ""scenario_new_push""
+    ""SHA"" = ""786b4f3d380d9c36c91f5f146ce4a7661ffee3b9""
+}
+";
+        #endregion
+
+        [Fact]
+        public void TestEvaluateDirectoryGitRootExistsWithNoAssets()
+        {
+            string[] folderStructure = new string[]
+            {
+                "assets.json",
+                "folder1\\",
+                "folder2\\file1.json"
+            };
+
+            var testFolder = TestHelpers.DescribeTestFolder(string.Empty, folderStructure);
+            GitStore store = new GitStore();
+
+            var evaluation = store.EvaluateDirectory(testFolder.FullName);
+            Assert.True(evaluation.IsGitRoot);
+            Assert.False(evaluation.AssetsJsonPresent);
+            Assert.False(evaluation.IsRoot);
+        }
+
+        [Fact]
+        public void TestEvaluateDirectoryFindsGitAssetsAlongsideGitRoot()
+        {
+            string[] folderStructure = new string[]
+            {
+                "assets.json",
+                "folder1\\",
+                "folder2\\file1.json"
+            };
+
+            var testFolder = TestHelpers.DescribeTestFolder(DefaultAssetsJson, folderStructure);
+            GitStore store = new GitStore();
+
+            var evaluation = store.EvaluateDirectory(testFolder.FullName);
+            Assert.True(evaluation.IsGitRoot);
+            Assert.True(evaluation.AssetsJsonPresent);
+            Assert.False(evaluation.IsRoot);
+        }
+
+        [Fact]
+        public void TestEvaluateDirectoryIdentifiesIntermediateDirectory()
+        {
+            string[] folderStructure = new string[]
+            {
+                "assets.json",
+                "folder1\\",
+                "folder2\\file1.json"
+            };
+
+            var testFolder = TestHelpers.DescribeTestFolder(DefaultAssetsJson, folderStructure);
+            var evaluationDirectory = Path.Join(testFolder.FullName, "folder1");
+
+            GitStore store = new GitStore();
+
+            var evaluation = store.EvaluateDirectory(evaluationDirectory);
+            Assert.False(evaluation.IsGitRoot);
+            Assert.False(evaluation.AssetsJsonPresent);
+            Assert.False(evaluation.IsRoot);
+        }
+
+        [Fact]
+        public void TestEvaluateDirectoryFindAssetsAboveFolder()
+        {
+
+        }
+    }
+}
--- tools/test-proxy/Azure.Sdk.Tools.TestProxy/Store/GitStore.cs
+++ tools/test-proxy/Azure.Sdk.Tools.TestProxy/Store/GitStore.cs
@@ -5,8 +5,17 @@ using System;
 
 namespace Azure.Sdk.Tools.TestProxy.Store
 {
+    public class DirectoryEvaluation
+    {
+        public bool IsRoot;
+        public bool IsGitRoot;
+        public bool AssetsJsonPresent;
+    }
+
     public class GitStore : IAssetsStore
     {
+        
+
         public void Push(string pathToAssetsJson, string contextPath) {
             var config = ParseConfigurationFile(pathToAssetsJson);
 
@@ -32,6 +41,31 @@ namespace Azure.Sdk.Tools.TestProxy.Store
             }
 
             return new GitAssetsConfiguration();
+        }
+
+        /// <summary>
+        /// Evaluates a directory and determines whether it contains an assets json, whether it is a git repo root, and if it is a root folder.
+        /// </summary>
+        /// <param name="directoryPath">Path to a directory. If given an actual file path, it will use the directory CONTAINING that file as the directory it is evaluating.</param>
+        /// <returns></returns>
+        public DirectoryEvaluation EvaluateDirectory(string directoryPath)
+        {
+            var fileAttributes = File.GetAttributes(directoryPath);
+            
+            if (!(fileAttributes == FileAttributes.Directory))
+            {
+                directoryPath = Path.GetDirectoryName(directoryPath);
+            }
+
+            var assetsJsonLocation = Path.Join(directoryPath, "assets.json");
+            var gitLocation = Path.Join(directoryPath, ".git");
+
+            return new DirectoryEvaluation()
+            {
+                AssetsJsonPresent = File.Exists(assetsJsonLocation),
+                IsGitRoot = File.Exists(gitLocation),
+                IsRoot = new DirectoryInfo(directoryPath).Parent == null
+            };
         }
     }
 }

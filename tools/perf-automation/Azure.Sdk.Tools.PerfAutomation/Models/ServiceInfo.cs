b2975140f6e7f6ed7b9f39309a77e2e9eb4f2046
Mike Harder <mharder@microsoft.com>
2022-06-16 15:25:06 -0700

    Perf Automation application (#3456)

---
+++ tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/Models/ServiceInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Azure.Sdk.Tools.PerfAutomation.Models
+{
+    public class ServiceInfo
+    {
+        public string Service { get; set; }
+        public IDictionary<Language, ServiceLanguageInfo> Languages { get; set; }
+        public IEnumerable<TestInfo> Tests { get; set; }
+    }
+}

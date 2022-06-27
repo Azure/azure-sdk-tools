1b6416c35cdab749ec6b264282d9d5bec109d398
Chidozie Ononiwu <chononiw@microsoft.com>
2022-06-27 16:03:12 -0700

    Update front end logic for collapsible

--- src/dotnet/APIView/APIViewWeb/Client/src/review.ts
+++ src/dotnet/APIView/APIViewWeb/Client/src/review.ts
@@ -46,26 +46,58 @@
   });
 
   // Collapsible Code Lines
-  $('.row-fold-elipsis, .row-fold-caret').on('click', function() {
+  $('.row-fold-elipsis, .row-fold-caret').on('click', function () {
+    var triggeringClass = $(this)[0].className;
     var headingRow = $(this).parents('.code-line');
     var headingRowClasses = headingRow.attr('class');
-    if (headingRowClasses) {
-      var foldableClassPrefix = headingRowClasses.split(' ').filter(c => c.endsWith('-heading'))[0].replace("-heading","");
-      $(`.${foldableClassPrefix}-content`).toggleClass("d-none");
-    }
-    headingRow.find(".row-fold-elipsis").toggleClass("d-none");
     var caretIcon = headingRow.find(".row-fold-caret").children("i");
-    var caretClasses =  caretIcon.attr("class");
-    if (caretClasses)
-    {
-      var caretDirection = caretClasses.split(' ').filter(c => c.startsWith('fa-angle-'))[0];
-      if (caretDirection.endsWith("right"))
+    var caretClasses = caretIcon.attr("class");
+    var caretDirection = caretClasses ? caretClasses.split(' ').filter(c => c.startsWith('fa-angle-'))[0] : "";
+    var foldableClassPrefix = headingRowClasses ? headingRowClasses.split(' ').filter(c => c.endsWith('-heading'))[0].replace("-heading", "") : "";
+
+
+    if (triggeringClass == "row-fold-caret" && caretDirection == "fa-angle-down") {
+      var classesOfRowsToHide = [`${foldableClassPrefix}-content`];
+
+      do
       {
+        let rowToHide = classesOfRowsToHide.pop();
+        if (rowToHide) {
+          $(`.${rowToHide}`).each(function (index, value) {
+            let contentRowClasses = $(this).attr("class");
+            if (contentRowClasses) {
+              let subHeadingRows = contentRowClasses.split(' ').filter(c => c.endsWith('-heading'));
+              subHeadingRows.forEach(item => {
+                classesOfRowsToHide.push(item.replace("-heading", "-content"));
+              });
+            }
+            $(this).addClass("d-none");
+          });
+          let subHeadingClass = rowToHide.replace("-content", "-heading");
+          let subHeadingRow = $(`.${subHeadingClass}`);
+          let subHeadingCaretIcon = subHeadingRow.find(".row-fold-caret").children("i");
+          let subHeadingCaretClasses = subHeadingCaretIcon.attr("class");
+          let caretDirection = subHeadingCaretClasses ? subHeadingCaretClasses.split(' ').filter(c => c.startsWith('fa-angle-'))[0] : "";
+          if (caretDirection) {
+            subHeadingCaretIcon.removeClass("fa-angle-down");
+            subHeadingCaretIcon.addClass("fa-angle-right");
+          }
+          subHeadingRow.find(".row-fold-elipsis").removeClass("d-none");
+        }
+      }
+      while (classesOfRowsToHide.length > 0);
+    }
+    else {
+      $(`.${foldableClassPrefix}-content`).toggleClass("d-none");
+      // toggle ellipsis
+      headingRow.find(".row-fold-elipsis").toggleClass("d-none");
+
+      // Toggle caret direction
+      if (caretDirection.endsWith("right")) {
         caretIcon.removeClass("fa-angle-right");
         caretIcon.addClass("fa-angle-down");
       }
-      else
-      {
+      else {
         caretIcon.removeClass("fa-angle-down");
         caretIcon.addClass("fa-angle-right");
       }

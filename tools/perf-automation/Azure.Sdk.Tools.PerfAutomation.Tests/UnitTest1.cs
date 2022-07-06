ba5071e04edb707755af1c87ca97b944384b35df
Mike Harder <mharder@microsoft.com>
2022-07-05 19:16:08 -0700

    Add unit test project

---
+++ tools/perf-automation/Azure.Sdk.Tools.PerfAutomation.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Azure.Sdk.Tools.PerfAutomation.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

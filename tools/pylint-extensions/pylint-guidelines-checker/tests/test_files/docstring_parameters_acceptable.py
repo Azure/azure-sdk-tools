c012e5072b1311a1acc145fc006c580eef6b50b9
l0lawrence <llawrence@microsoft.com>
2022-07-08 14:43:58 -0700

    updating test

--- tools/pylint-extensions/pylint-guidelines-checker/tests/test_files/docstring_parameters_acceptable.py
+++ tools/pylint-extensions/pylint-guidelines-checker/tests/test_files/docstring_parameters_acceptable.py
@@ -2,28 +2,39 @@ from typing import Union
 
 def with_variadic_typehints(self, *var, **kwargs):
     """ Typehint
-    :keyword str var: Args
-    :keyword Any kwargs: Kwargs
+    :param str var: Args
+    :keyword something: something
+    :paramtype something: str 
+    :param Any kwargs: Kwargs
     :rtype: None
     :return: None
     """
-    # """ Typehint
-    # :param str var: Args
-    # :param Any kwargs: Kwargs
-    # :rtype: None
-    # :return: None
-    # """
+    # param b/c this is a function
+    # if it is a function = param 
+
+
     # var is a vararg, which is classified differently than an arg
     #instance variable is ivar  -> would var be a param or would it be an args and would kwargs be a param or would it be keyword , and should either of these be in the docstring 
     pass
 
-def with_ivar_typehints(self, something: str, something2: str, union: Union[bool, int]):
+class with_ivar_typehints():
+
+    
     """
     :ivar str something: Something
+    :param str something: Something 
     :ivar str something2: Something2
     :ivar union: Union
-    :vartype union: Union[bool, int]
+    :vartype union: bool or int 
     :rtype: None
     :return: None
     """
-    pass
+    def __init__(self, something: str, something2: str, union: Union[bool, int]):
+
+
+        # self.something = an ivar
+        # something = param 
+        self.something = something
+        self.something2 = something2
+        # something3 is an ivar not a param 
+        self.something3 = something + something2

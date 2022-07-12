ffa446a3275e99f1397ecdfb77b8263aca3fbcd0
Chidozie Ononiwu <chononiw@microsoft.com>
2022-07-11 20:40:03 -0700

    Expand APIView to Support Swagger Reviews
    Update collapsible sectoins into tree structure
    
    morre updates to codefile renderer
    
    Implement codeline sections
    
    Remove OnPostRefreshModelAsync
    
    Updates to code file renderer
    
    Updates to File Renderer
    
    Split codeFile nto smaller sections
    
    Split Leafs from CodeFile Sections
    
    Tidy up display of partialCodeLines
    
    Fine tune collapsible indents
    
    Add SVG hieracy to codelines
    
    More updates to codeline hierachy
    
    Resolve discrepancies in line numbering
    
    Update codefilerenderer to include defintionid for collapsible heading
    
    Make comments hide or expand with rows to which they are related
    
    Collapse navbar initially on swagger pages
    
    expand main codelines as navigation expands
    
    Update CodeFileRenderer for tables
    
    Comments for rows containing tables
    
    Fix scrolling to inline tables
    
    Switch from ViewData to TempData

--- src/dotnet/APIView/APIView/Model/CodeFile.cs
+++ src/dotnet/APIView/APIView/Model/CodeFile.cs
@@ -3,7 +3,9 @@
 
 using APIView;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -44,20 +46,84 @@ namespace ApiView
 
         public CodeFileToken[] Tokens { get; set; } = Array.Empty<CodeFileToken>();
 
+        public List<CodeFileToken[]> LeafSections { get; set; }
+
         public NavigationItem[] Navigation { get; set; }
 
         public CodeDiagnostic[] Diagnostics { get; set; }
 
         public override string ToString()
         {
-            return new CodeFileRenderer().Render(this).ToString();
+            return new CodeFileRenderer().Render(this).CodeLines.ToString();
         }
 
-        public static async Task<CodeFile> DeserializeAsync(Stream stream)
+        public static async Task<CodeFile> DeserializeAsync(Stream stream, bool hasSections = false)
         {
-            return await JsonSerializer.DeserializeAsync<CodeFile>(
+            var codeFile = await JsonSerializer.DeserializeAsync<CodeFile>(
                 stream,
                 JsonSerializerOptions);
+
+            // Spliting out the 'leafSections' of the codeFile is done so as not to have to render large codeFiles at once
+            // Rendering sections in part helps to improve page load time
+            if (hasSections)
+            {
+                var index = 0;
+                var tokens = codeFile.Tokens;
+                var newTokens = new List<CodeFileToken>();
+                var leafSections = new List<CodeFileToken[]>();
+                var section = new List<CodeFileToken>();
+                var isLeaf = false;
+                var numberOfLinesinLeafSection = 0;
+
+                while (index < tokens.Length)
+                {
+                    var token = tokens[index];
+                    if (token.Kind == CodeFileTokenKind.FoldableSectionHeading)
+                    {
+                        section.Add(token);
+                        isLeaf = false;
+                    }
+                    else if (token.Kind == CodeFileTokenKind.FoldableSectionContentStart)
+                    {
+                        section.Add(token);
+                        newTokens.AddRange(section);
+                        section.Clear();
+                        isLeaf = true;
+                        numberOfLinesinLeafSection = 0;
+                    }
+                    else if (token.Kind == CodeFileTokenKind.FoldableSectionContentEnd)
+                    {
+                        if (isLeaf)
+                        {
+                            leafSections.Add(section.ToArray());
+                            section.Clear();
+                            isLeaf = false;
+
+                            // leafSectionPlaceholder will be used to identify the appriopriate index and number of lines in the leafSections
+                            // numberOfLinesinLeafSection help keep line numbering consistent with the main 'non-leaf' sections
+                            var leafSectionPlaceholder = new CodeFileToken(
+                                $"{(leafSections.Count() - 1)}", CodeFileTokenKind.LeafSectionPlaceholder, numberOfLinesinLeafSection);
+                            var newLineToken = new CodeFileToken("", CodeFileTokenKind.Newline);
+                            section.Add(leafSectionPlaceholder);
+                            section.Add(newLineToken);
+                        }
+                        section.Add(token);
+                    }
+                    else
+                    {
+                        if (isLeaf && token.Kind == CodeFileTokenKind.Newline)
+                        {
+                            numberOfLinesinLeafSection++;
+                        }
+                        section.Add(token);
+                    }
+                    index++;
+                }
+                newTokens.AddRange(section);
+                codeFile.Tokens = newTokens.ToArray();
+                codeFile.LeafSections = leafSections;
+            }
+            return codeFile;
         }
 
         public async Task SerializeAsync(Stream stream)

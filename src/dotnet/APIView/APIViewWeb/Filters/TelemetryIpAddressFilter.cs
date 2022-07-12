eebc9159c029715ad321a193161b92a99f8db510
Praveen Kuttappan <praveen.kuttappan@gmail.com>
2022-07-12 11:23:57 -0400

    Remove IP address information from telemetry

---
+++ src/dotnet/APIView/APIViewWeb/Filters/TelemetryIpAddressFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.DataContracts;
+
+namespace APIViewWeb.Filters
+{
+    public class TelemetryIpAddressFilter : ITelemetryProcessor
+    {
+        private ITelemetryProcessor Next { get; set; }
+
+        public TelemetryIpAddressFilter(ITelemetryProcessor next)
+        {
+            this.Next = next;
+        }
+
+        public void Process(ITelemetry item)
+        {
+            if(item.Context?.Location?.Ip != null)
+            {
+                item.Context.Location.Ip = null;
+            }
+
+            this.Next.Process(item);
+        }
+    }
+}
+
+
+

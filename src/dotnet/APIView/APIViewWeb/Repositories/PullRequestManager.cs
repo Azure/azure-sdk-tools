443bbcf297fd09620b957903429deaf9336c3275
praveenkuttappan <prmarott@microsoft.com>
2022-07-19 17:55:57 -0400

    REST APi accepts new project param that can be used to pass devops project name

--- src/dotnet/APIView/APIViewWeb/Repositories/PullRequestManager.cs
+++ src/dotnet/APIView/APIViewWeb/Repositories/PullRequestManager.cs
@@ -88,7 +88,8 @@ namespace APIViewWeb.Repositories
             string codeFileName = null,
             string baselineCodeFileName = null,
             bool commentOnPR = true,
-            string language = null)
+            string language = null,
+            string projet = "public")
         {
             var requestTelemetry = new RequestTelemetry { Name = "Detecting API changes for PR: " + prNumber };
             var operation = _telemetryClient.StartOperation(requestTelemetry);
@@ -114,7 +115,7 @@ namespace APIViewWeb.Repositories
                 
                 using var memoryStream = new MemoryStream();
                 using var baselineStream = new MemoryStream();
-                var codeFile = await _reviewManager.GetCodeFile(repoName, buildId, artifactName, packageName, originalFileName, codeFileName, memoryStream, baselineCodeFileName: baselineCodeFileName, baselineStream: baselineStream);
+                var codeFile = await _reviewManager.GetCodeFile(repoName, buildId, artifactName, packageName, originalFileName, codeFileName, memoryStream, baselineCodeFileName: baselineCodeFileName, baselineStream: baselineStream, project: projet);
                 CodeFile baseLineCodeFile = null;
                 if (baselineStream.Length>0)
                 {

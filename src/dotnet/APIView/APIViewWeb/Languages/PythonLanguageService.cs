d4bfc28b2885fd27726df4f38e3202e10dd70e65
praveenkuttappan <prmarott@microsoft.com>
2022-07-28 12:30:09 -0400

    Disable manual review sandboxing for Python

--- src/dotnet/APIView/APIViewWeb/Languages/PythonLanguageService.cs
+++ src/dotnet/APIView/APIViewWeb/Languages/PythonLanguageService.cs
@@ -19,7 +19,7 @@ namespace APIViewWeb
         public override string Extension { get; } = ".whl";
         public override string VersionString { get; } = "0.3.2";
 
-        public override bool IsReviewGenByPipeline { get; } = true;
+        public override bool IsReviewGenByPipeline { get; } = false;
 
         private readonly string _pythonExecutablePath;
         public override string ProcessName => _pythonExecutablePath;

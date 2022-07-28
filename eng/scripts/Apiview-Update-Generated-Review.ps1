057cd0858abc8b4f7b06c317f58f7b9b7257fb24
praveenkuttappan <prmarott@microsoft.com>
2022-07-28 16:43:42 -0400

    Updated documentation within script

--- eng/scripts/Apiview-Update-Generated-Review.ps1
+++ eng/scripts/Apiview-Update-Generated-Review.ps1
@@ -7,9 +7,18 @@ param (
   [string]$ApiviewUpdateUrl = "https://apiview.dev/review/UpdateApiReview"
 )
 
-# Sample request
+####################################################################################################################
+
+# This script is called by tools - generate-<Language>-apireview pipelines to send a request to APIView server to
+# to notify that generated code file is ready and published. APIView server pulls this code file from artifact and 
+# uploads into APIView. This offline step abstracts and protects APIView server resources so caller doesn't have to
+# know about APIView server. Pipline will publish an artifact 'apiview' before this script is called
+
+# Script will send a request to APIView with build ID, artifact name and repo name as params.
+# Sample request is as follows:
 # https://apistaging.test.dev/review/UpdateApiReview?repoName=azure/azure-sdk-tools&buildId=1742433&artifact=apiview
 
+####################################################################################################################
 
 $query = [System.Web.HttpUtility]::ParseQueryString('')
 $query.Add('artifact', $ArtifactName)

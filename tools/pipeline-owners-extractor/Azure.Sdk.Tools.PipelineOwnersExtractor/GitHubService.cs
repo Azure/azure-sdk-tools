7908531bf2839eaaa08cb16b2db3a31c3aa34368
Nicholi Harris <67086158+nicholiharris@users.noreply.github.com>
2022-07-27 20:11:08 -0700

    Introduced codeowner-extractor. (#3719)
    * Introduced codeowner-extractor.
    
    * Refactor using implicit principal and vault based config (#3751)
    
    * Refactor using implicit principal and vault based config
    * Cleanup logging and skipping logic in Processor. Use bulk github user mapping api.
    * Avoid using arrays with IConfiguration
    * Improve logging in GitHubToAADConverter and pipeline owner extractor
    
    Co-authored-by: Nicholi Harris <t-nharris@microsoft.com>
    Co-authored-by: Patrick Hallisey <pahallis@microsoft.com>

---
+++ tools/pipeline-owners-extractor/Azure.Sdk.Tools.PipelineOwnersExtractor/GitHubService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Azure.Sdk.Tools.CodeOwnersParser;
+using Microsoft.Extensions.Logging;
+
+namespace Azure.Sdk.Tools.PipelineOwnersExtractor
+{
+    /// <summary>
+    /// Interface for interacting with GitHub
+    /// </summary>
+    public class GitHubService
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        private readonly ILogger<GitHubService> logger;
+        private readonly ConcurrentDictionary<string, List<CodeOwnerEntry>> codeOwnersFileCache;
+
+        /// <summary>
+        /// Creates a new GitHubService
+        /// </summary>
+        /// <param name="logger">Logger</param>
+        public GitHubService(ILogger<GitHubService> logger)
+        {
+            this.logger = logger;
+            this.codeOwnersFileCache = new ConcurrentDictionary<string, List<CodeOwnerEntry>>();
+        }
+
+        /// <summary>
+        /// Looks for CODEOWNERS in the main branch of the given repo URL using cache
+        /// </summary>
+        /// <param name="repoUrl">GitHub repository URL</param>
+        /// <returns>Contents fo the located CODEOWNERS file</returns>
+        public async Task<List<CodeOwnerEntry>> GetCodeOwnersFile(Uri repoUrl)
+        {
+            List<CodeOwnerEntry> result;
+            if (codeOwnersFileCache.TryGetValue(repoUrl.ToString(), out result))
+            {
+                return result;
+            }
+
+            result = await GetCodeownersFileImpl(repoUrl);
+            codeOwnersFileCache.TryAdd(repoUrl.ToString(), result);
+            return result;
+        }
+
+        /// <summary>
+        /// Looks for CODEOWNERS in the main branch of the given repo URL
+        /// </summary>
+        /// <param name="repoUrl"></param>
+        /// <returns></returns>
+        private async Task<List<CodeOwnerEntry>> GetCodeownersFileImpl(Uri repoUrl)
+        {
+            // Gets the repo path from the URL
+            var relevantPathParts = repoUrl.Segments.Skip(1).Take(2);
+            var repoPath = string.Join("", relevantPathParts);
+
+            var codeOwnersUrl = $"https://raw.githubusercontent.com/{repoPath}/main/.github/CODEOWNERS";
+            var result = await httpClient.GetAsync(codeOwnersUrl);
+            if (result.IsSuccessStatusCode)
+            {
+                this.logger.LogInformation("Retrieved CODEOWNERS file URL = {0}", codeOwnersUrl);
+                return CodeOwnersFile.ParseContent(await result.Content.ReadAsStringAsync());
+            }
+
+            this.logger.LogWarning("Could not retrieve CODEOWNERS file URL = {0} ResponseCode = {1}", codeOwnersUrl, result.StatusCode);
+            return default;
+        }
+
+    }
+}
---
+++ tools/pipeline-owners-extractor/Azure.Sdk.Tools.PipelineOwnersExtractor/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.Sdk.Tools.NotificationConfiguration.Helpers;
+using Azure.Sdk.Tools.NotificationConfiguration.Services;
+using Azure.Sdk.Tools.PipelineOwnersExtractor.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+
+namespace Azure.Sdk.Tools.PipelineOwnersExtractor
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Initializing PipelineOwnersExtractor");
+
+            using var host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton<TokenCredential, DefaultAzureCredential>();
+                    services.AddSingleton<ISecretClientProvider, SecretClientProvider>();
+                    services.Configure<PipelineOwnerSettings>(context.Configuration);
+                    services.AddSingleton<IPostConfigureOptions<PipelineOwnerSettings>, PostConfigureKeyVaultSettings<PipelineOwnerSettings>>();
+                    services.AddSingleton<GitHubService>();
+                    services.AddSingleton(CreateGithubAadConverter);
+                    services.AddSingleton(CreateAzureDevOpsService);
+                    services.AddSingleton<Processor>();
+                })
+                .Build();
+
+            var processor = host.Services.GetRequiredService<Processor>();
+
+            await processor.ExecuteAsync();
+        }
+
+        private static AzureDevOpsService CreateAzureDevOpsService(IServiceProvider provider)
+        {
+            var logger = provider.GetRequiredService<ILogger<AzureDevOpsService>>();
+            var settings = provider.GetRequiredService<IOptions<PipelineOwnerSettings>>().Value;
+
+            var uri = new Uri($"https://dev.azure.com/{settings.Account}");
+            var credentials = new VssBasicCredential("pat", settings.AzureDevOpsPat);
+            var connection = new VssConnection(uri, credentials);
+
+            return new AzureDevOpsService(connection, logger);
+        }
+
+        private static GitHubToAADConverter CreateGithubAadConverter(IServiceProvider provider)
+        {
+            var logger = provider.GetRequiredService<ILogger<GitHubToAADConverter>>();
+            var settings = provider.GetRequiredService<IOptions<PipelineOwnerSettings>>().Value;
+
+            var credential = new ClientSecretCredential(
+                settings.OpenSourceAadTenantId, 
+                settings.OpenSourceAadAppId,
+                settings.OpenSourceAadSecret);
+
+            return new GitHubToAADConverter(credential, logger);
+        }
+    }
+}

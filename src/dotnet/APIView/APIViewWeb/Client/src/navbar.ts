83cdcce7893f5ee42f6196302feb86178e762971
Chidozie Ononiwu <chononiw@microsoft.com>
2022-07-28 19:13:31 -0700

    Add button for switching themes

--- src/dotnet/APIView/APIViewWeb/Client/src/navbar.ts
+++ src/dotnet/APIView/APIViewWeb/Client/src/navbar.ts
@@ -1,1 +1,26 @@
-﻿
+import Split from "split.js";
+
+addEventListener("load", () => {
+    $(".nav-list-toggle").click(function () {
+        $(this).parents(".nav-list-group").first().toggleClass("nav-list-collapsed");
+    });
+});
+
+$(() => {
+    const themeSelector = $( '#theme-selector' );
+
+    // Add EventListener for Changing CSS Theme
+    themeSelector.on('change', function() {
+        var allThemes = themeSelector.children();
+        var newTheme = themeSelector.children(":selected").val() as string;
+        var themesToRemove = allThemes.filter(function(){
+            return ($(this).val() as string) != newTheme;
+        });
+        var body = $('body');
+
+        themesToRemove.each(function(){
+            body.removeClass(($(this).val() as string));
+        })
+        body.addClass(newTheme);
+    });
+});

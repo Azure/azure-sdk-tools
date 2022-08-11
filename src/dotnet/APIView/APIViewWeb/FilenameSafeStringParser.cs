2b46a9625e1f9888cfd16b7f7fd93807498f49d7
Vailorium <casterthe5587@gmail.com>
2022-08-11 17:12:21 +1200

    add utility method to transform filename unsafe language strings into filename safe strings, replace escaped css classes with safe string version

---
+++ src/dotnet/APIView/APIViewWeb/FilenameSafeStringParser.cs
@@ -0,0 +1,18 @@
+﻿namespace APIViewWeb
+{
+    public class FilenameSafeStringParser
+    {
+        public static string ParseLanguageName(string languageName)
+        {
+            switch(languageName.ToLower())
+            {
+                case "c#":
+                    return "csharp";
+                case "c++":
+                    return "cplusplus";
+                default:
+                    return languageName.ToLower();
+            }
+        }
+    }
+}

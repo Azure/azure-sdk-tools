921b9ac89964c7fbaf595b9dc9c73b3c60166a6a
James Suplizio <jasupliz@microsoft.com>
2022-09-20 16:04:26 -0700

    Initial CLI using CommandLineParser

---
+++ tools/test-proxy/Azure.Sdk.Tools.TestProxy/CommandParserOptions/PushOptions.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine.Text;
+using CommandLine;
+
+namespace Azure.Sdk.Tools.TestProxy.CommandParserOptions
+{
+    /// <summary>
+    /// Any unique options to the push command will reside here.
+    /// </summary>
+    [Verb("push", HelpText = "Push the assets, referenced by assets.json, into git.")]
+    class PushOptions : CLICommandOptions
+    {
+    }
+}
---
+++ tools/test-proxy/Azure.Sdk.Tools.TestProxy/CommandParserOptions/RestoreOptions.cs
@@ -0,0 +1,12 @@
+﻿using CommandLine;
+
+namespace Azure.Sdk.Tools.TestProxy.CommandParserOptions
+{
+    /// <summary>
+    /// Any unique options to the restore command will reside here.
+    /// </summary>
+    [Verb("restore", HelpText = "Restore the assets, referenced by assets.json, from git.")]
+    class RestoreOptions : CLICommandOptions
+    {
+    }
+}

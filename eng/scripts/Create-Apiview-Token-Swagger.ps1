9a4b03050e916e3ad8b708f0c17319cf17e48e90
praveenkuttappan <praveen.kuttappan@gmail.com>
2022-09-22 14:45:30 -0400

    Create pipeline to convert swagger file to Apiview code file

---
+++ eng/scripts/Create-Apiview-Token-Swagger.ps1
@@ -0,0 +1,14 @@
+[CmdletBinding()]
+param (
+  [Parameter(Mandatory = $true)]
+  [string]$SourcePath,
+  [Parameter(Mandatory = $true)]
+  [string]$OutPath
+)
+
+Write-Host "Generating API review token file: $($SourcePath)"
+$FileName = Split-Path -Leaf $SourcePath
+$OutFileName = $FileName -replace ".swagger", "_swagger.json"
+$OutFilePath = Join-Path -Path $OutPath $OutFileName
+Write-Host "Converting Swagger file $($SourcePath) to APIview code file $($OutFilePath)"
+SwaggerApiParser $SourcePath --output $OutFilePath

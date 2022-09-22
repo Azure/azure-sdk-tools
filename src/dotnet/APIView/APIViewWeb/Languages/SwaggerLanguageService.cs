225565c4774ef48cdc49706a407b97804a3ccdb3
praveenkuttappan <praveen.kuttappan@gmail.com>
2022-09-22 17:07:03 -0400

    Update Swagger language service

--- src/dotnet/APIView/APIViewWeb/Languages/SwaggerLanguageService.cs
+++ src/dotnet/APIView/APIViewWeb/Languages/SwaggerLanguageService.cs
@@ -8,13 +8,26 @@ using ApiView;
 
 namespace APIViewWeb
 {
-    public class SwaggerLanguageService : JsonLanguageService
+    public class SwaggerLanguageService : LanguageProcessor
     {
         public override string Name { get; } = "Swagger";
+
+        public override string Extension { get; } = ".swagger";
+
+        public override string VersionString { get; } = "1.0.0";
+
+        public override string ProcessName => throw new NotImplementedException();
+
+        public new bool IsReviewGenByPipeline = true;
 
         public override async Task<CodeFile> GetCodeFileAsync(string originalName, Stream stream, bool runAnalysis)
         {
             return await CodeFile.DeserializeAsync(stream, true);
+        }
+
+        public override string GetProcessorArguments(string originalName, string tempDirectory, string jsonPath)
+        {
+            throw new NotImplementedException();
         }
     }
 }

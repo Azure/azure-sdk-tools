3a40cbcb539369d3d7f37bb33fa9a2a74ab84ad4
Baas-hub <benjbaas@gmail.com>
2022-09-23 12:15:44 +1200

    Basic model implementation

---
+++ src/dotnet/APIView/APIViewWeb/Repositories/CosmosUserRepository.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using APIViewWeb.Models;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+
+namespace APIViewWeb
+{
+    public class CosmosUserRepository
+    {
+        private readonly Container _userContainer;
+
+        public CosmosUserRepository(IConfiguration configuration)
+        {
+            var client = new CosmosClient(configuration["Cosmos:ConnectionString"]);
+            _userContainer = client.GetContainer("APIView", "Users");
+        }
+
+        public async Task<UserModel> getUserAsync(ClaimsPrincipal User)
+        {
+            return await _userContainer.ReadItemAsync<UserModel>(User.GetGitHubLogin(), new PartitionKey("users"));
+        }
+
+        public async Task upsertUserAsync(ClaimsPrincipal User, UserModel userModel)
+        {
+            if(User.GetGitHubLogin().Equals(userModel.UserName))
+            {
+                await _userContainer.UpsertItemAsync(userModel, new PartitionKey("users"));
+            }
+        }
+    }
+}

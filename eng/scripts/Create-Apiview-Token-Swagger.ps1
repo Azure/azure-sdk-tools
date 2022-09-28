6f2f3f7effb11f0e4ba97e6f7639d69e0949b374
praveenkuttappan <praveen.kuttappan@gmail.com>
2022-09-28 12:16:44 -0400

    Updated pipeline for swagger parser

--- eng/scripts/Create-Apiview-Token-Swagger.ps1
+++ eng/scripts/Create-Apiview-Token-Swagger.ps1
@@ -4,7 +4,7 @@ param (
   [string]$SourcePath,
   [Parameter(Mandatory = $true)]
   [string]$OutPath,
-  [string]$ParserPath = (Join-Path $env:Pipeline.Workspace "SwaggerApiParser")
+  [string]$ParserPath
 )
 
 Write-Host "Generating API review token file: $($SourcePath)"
@@ -12,5 +12,10 @@ $FileName = Split-Path -Leaf $SourcePath
 $OutFileName = $FileName -replace ".swagger", "_swagger.json"
 $OutFilePath = Join-Path -Path $OutPath $OutFileName
 Write-Host "Converting Swagger file $($SourcePath) to APIview code file $($OutFilePath)"
+if ($ParserPath -eq $null)
+{
+  $ParserPath = (Join-Path -Path $env:Pipeline.Workspace "SwaggerApiParser"
+}
+Write-Host "Parser Path: $($ParserPath)"
 Get-Item $ParserPath
 &$($ParserPath)/SwaggerApiParser $SourcePath --output $OutFilePath

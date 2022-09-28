b5df9262495a950820f18221c08a144581882536
praveenkuttappan <praveen.kuttappan@gmail.com>
2022-09-28 12:00:44 -0400

    Updated pipeline for swagger parser

--- eng/scripts/Create-Apiview-Token-Swagger.ps1
+++ eng/scripts/Create-Apiview-Token-Swagger.ps1
@@ -4,7 +4,7 @@ param (
   [string]$SourcePath,
   [Parameter(Mandatory = $true)]
   [string]$OutPath,
-  [string]$ParserPath = $env:SwaggerParserInstallPath,
+  [string]$ParserPath = (Join-Path $env:Pipeline.Workspace "SwaggerApiParser")
 )
 
 Write-Host "Generating API review token file: $($SourcePath)"

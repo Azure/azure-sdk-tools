54582cc9c2f968345fc1095a297fbde90d31ae80
scbedd <45376673+scbedd@users.noreply.github.com>
2022-10-12 15:00:41 -0700

    forcing a dump of the contents after restore

--- tools/test-proxy/scripts/test-scripts/CLIIntegration.Tests.ps1
+++ tools/test-proxy/scripts/test-scripts/CLIIntegration.Tests.ps1
@@ -41,6 +41,8 @@ Describe "AssetsModuleTests" {
 
             $CommandArgs = "restore --assets-json-path $assetsJsonRelativePath"
             Invoke-ProxyCommand -TestProxyExe $TestProxyExe -CommandArgs $CommandArgs -MountDirectory $testFolder
+
+            Get-ChildItem -Recurse -Force $testFolder
             $LASTEXITCODE | Should -Be 0
             $localAssetsFilePath = Get-AssetsFilePath -AssetsJsonContent $recordingJson -AssetsJsonFile $assetsFile
             Test-DirectoryFileCount -Directory $localAssetsFilePath -ExpectedNumberOfFiles 3

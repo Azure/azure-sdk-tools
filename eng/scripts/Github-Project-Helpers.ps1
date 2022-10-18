38df134632f161651d3a46fc36c7f5d06d1e3b96
Ben Broderick Phillips <bebroder@microsoft.com>
2022-10-18 01:19:49 -0400

    Add automation for updating github branch protection required status checks

--- eng/scripts/Github-Project-Helpers.ps1
+++ eng/scripts/Github-Project-Helpers.ps1
@@ -51,3 +51,93 @@ function Remove-GithubIssueFromProject([string]$projectId, [string]$projectItemI
 
   return $projectDeletedItemId
 }
+
+function Get-BranchProtectionChecks([string]$org = "Azure", [string]$repo, [string]$branch = "main")
+{
+  $resp = gh api -H "Accept: application/vnd.github+json" /repos/$org/$repo/branches/$branch/protection/required_status_checks
+  if ($LASTEXITCODE) {
+      exit $LASTEXITCODE
+  }
+  return $resp | ConvertFrom-Json -AsHashtable -Depth 100
+}
+
+function Remove-BranchProtectionCheck()
+{
+  [CmdletBinding(SupportsShouldProcess)]
+  param(
+    [string]$org = "Azure",
+    [string]$repo,
+    [string]$branch = "main",
+    [string]$ruleName
+  )
+  $ErrorActionPreference = 'Stop'
+
+  $rules = Get-BranchProtectionChecks -org $org -repo $repo -branch $branch
+  $dateTag = Get-Date -Format "yyyyMMdd.hhmmss"
+  $backupFile = "$org.$repo.$branch.required-checks.$dateTag.json"
+  Write-Host "Backing up original rules to $backupFile"
+  $rules | ConvertTo-Json -Depth 100 | Out-File -WhatIf:$false $backupFile
+
+  $rules.checks = [array]($rules.checks | Where-Object { $_.context -ne $ruleName })
+  # Deprecated, but update for parity just in case
+  $rules.contexts = [array]($rules.contexts | Where-Object { $_ -ne $ruleName })
+  if (!$rules.checks) {
+      $rules.checks = @()
+  }
+  if (!$rules.contexts) {
+      $rules.contexts = @()
+  }
+
+  Write-Host "Removing check '$ruleName'"
+  Write-Host "New rules: $($rules.checks | ConvertTo-Json -Compress)"
+  $body = $rules | ConvertTo-Json -Depth 100 -Compress
+  if($PSCmdlet.ShouldProcess("Remove check $ruleName for ${branch}?")) {
+    $_ = $body | gh api -X PATCH -H "Accept: application/vnd.github+json" /repos/$org/$repo/branches/$branch/protection/required_status_checks --input -
+    if ($LASTEXITCODE) {
+      exit $LASTEXITCODE
+    }
+  }
+}
+
+function Add-BranchProtectionCheck()
+{
+  [CmdletBinding(SupportsShouldProcess)]
+  param(
+    [string]$org = "Azure",
+    [string]$repo,
+    [string]$branch = "main",
+    [string]$ruleName
+  )
+  $ErrorActionPreference = 'Stop'
+
+  $rules = Get-BranchProtectionChecks -org $org -repo $repo -branch $branch
+  $rules.checks = [array]($rules.checks | Where-Object { $_.context -ne $ruleName -and !$_.app_id })
+  # Deprecated, but update for parity just in case
+  $rules.contexts = [array]($rules.contexts | Where-Object { $_ -ne $ruleName })
+  if (!$rules.checks) {
+      $rules.checks = @()
+  }
+  if (!$rules.contexts) {
+      $rules.contexts = @()
+  }
+
+  if ($rules.checks.context -notcontains $ruleName) {
+   # For now, support "any source" only for required check source
+   # Setting the `app_id` field to -1 results in "any source" being set for the check
+   $rules.checks = $rules.checks + @( @{ context = $ruleName ; app_id = -1 } )
+   # Deprecated, but update for parity just in case
+   $rules.contexts = $rules.contexts + $ruleName
+  } else {
+    Write-Host "Rule $ruleName already exists for $branch"
+    return
+  }
+
+  Write-Host "New rules: $($rules.checks | ConvertTo-Json -Compress)"
+  $body = $rules | ConvertTo-Json -Depth 100 -Compress
+  if($PSCmdlet.ShouldProcess("Add rule $ruleName for ${branch}?")) {
+    $_ = $body | gh api -X PATCH -H "Accept: application/vnd.github+json" /repos/$org/$repo/branches/$branch/protection/required_status_checks --input -
+    if ($LASTEXITCODE) {
+      exit $LASTEXITCODE
+    }
+  }
+}

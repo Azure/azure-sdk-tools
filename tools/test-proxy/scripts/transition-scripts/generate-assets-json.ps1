2755362b3c8f84e57c0d7c83946187ac2fdba6b5
Scott Beddall <45376673+scbedd@users.noreply.github.com>
2022-10-19 14:16:30 -0700

    Resolve Issues with Transition Script (#4467)
    * relative path WAS including assets.json
    * account for single ";" separating elements in the breadcrumb file
    * tool invocation passes storage-location properly
    
    Co-authored-by: Ben Broderick Phillips <ben@benbp.net>

--- tools/test-proxy/scripts/transition-scripts/generate-assets-json.ps1
+++ tools/test-proxy/scripts/transition-scripts/generate-assets-json.ps1
@@ -56,7 +56,7 @@ $GitExe = "git"
 
 # The built test proxy on a dev machine will have the version 1.0.0-dev.20221013.1
 # whereas the one installed from nuget will have the version 20221013.1 (minus the 1.0.0-dev.)
-$MinTestProxyVersion = "20221017.1"
+$MinTestProxyVersion = "20221017.4"
 
 $DefaultAssetsRepo = "Azure/azure-sdk-assets"
 if ($UseTestRepo) {
@@ -287,7 +287,7 @@ Function Invoke-ProxyCommand {
   }
 
   Write-Host "$TestProxyExe $CommandArgs"
-  [array] $output = & "$TestProxyExe" $CommandArgs.Split(" ")
+  [array] $output = & "$TestProxyExe" $CommandArgs.Split(" ") --storage-location="$updatedDirectory"
   # echo the command output
   foreach ($line in $output) {
     Write-Host "$line"
@@ -301,13 +301,14 @@ Function Get-AssetsRoot {
   )
   $repoRoot = Get-Repo-Root
   $relPath = [IO.Path]::GetRelativePath($repoRoot, $AssetsJsonFile).Replace("`\", "/")
+  $assetsJsonDirectory = Split-Path $relPath
   $breadcrumbFile = Join-Path $repoRoot ".assets" ".breadcrumb"
 
   $breadcrumbString = Get-Content $breadcrumbFile | Where-Object { $_.StartsWith($relPath) }
-  $assetRepo = $breadcrumbString.Split(";;")[1]
+  $assetRepo = $breadcrumbString.Split(";")[1]
   $assetsPrefix = (Get-Content $AssetsJsonFile | Out-String | ConvertFrom-Json).AssetsRepoPrefixPath
 
-  return Join-Path $repoRoot ".assets" $assetRepo $assetsPrefix $relPath
+  return Join-Path $repoRoot ".assets" $assetRepo $assetsPrefix $assetsJsonDirectory
 }
 
 Function Move-AssetsFromLangRepo {
@@ -321,8 +322,8 @@ Function Move-AssetsFromLangRepo {
   [string] $currentDir = Get-Location
 
   foreach ($fromFile in $filesToMove) {
-    Write-Host $fromFile
     $relPath = [IO.Path]::GetRelativePath($currentDir, $fromFile)
+
     $toFile = Join-Path -Path $AssetsRoot -ChildPath $relPath
     # Write-Host "Moving from=$fromFile"
     # Write-Host "          to=$toFile"
@@ -372,7 +373,7 @@ if ($InitialPush) {
     $CommandArgs = "restore --assets-json-path $assetsJsonRelPath"
     Invoke-ProxyCommand -TestProxyExe $TestProxyExe -CommandArgs $CommandArgs -TargetDirectory $repoRoot
 
-    $assetsRoot = Get-AssetsRoot -AssetsJsonFile $assetsJsonFile
+    $assetsRoot = (Get-AssetsRoot -AssetsJsonFile $assetsJsonFile)
     Write-Host "assetsRoot=$assetsRoot"
 
     Move-AssetsFromLangRepo -AssetsRoot $assetsRoot

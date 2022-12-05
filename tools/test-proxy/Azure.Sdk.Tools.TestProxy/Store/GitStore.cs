eddc4a92970f91c1a68c729be5712016adbb85b3
scbedd <45376673+scbedd@users.noreply.github.com>
2022-12-05 14:52:48 -0800

    resolve lastexitcode shenanigans for last time hopefully

--- tools/test-proxy/Azure.Sdk.Tools.TestProxy/Store/GitStore.cs
+++ tools/test-proxy/Azure.Sdk.Tools.TestProxy/Store/GitStore.cs
@@ -93,7 +93,8 @@ namespace Azure.Sdk.Tools.TestProxy.Store
                 _consoleWrapper.WriteLine($"The targeted assets.json \"{config.AssetsJsonRelativeLocation}\" has not been restored prior to attempting push. " +
                     $"Are you certain you're pushing the correct assets.json? Please invoke \'test-proxy restore \"{config.AssetsJsonRelativeLocation}\"\' prior to invoking a push operation.");
 
-                Environment.Exit(1);
+                Environment.ExitCode = -1;
+                return;
             }
 
             var pendingChanges = DetectPendingChanges(config);

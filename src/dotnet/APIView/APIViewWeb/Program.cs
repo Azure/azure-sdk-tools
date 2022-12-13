a1220845c2caf74b07a2dadb250266fd4dd03884
praveenkuttappan <praveen.kuttappan@gmail.com>
2022-12-13 14:16:25 -0500

    Update authentication to access configuration in KV

--- src/dotnet/APIView/APIViewWeb/Program.cs
+++ src/dotnet/APIView/APIViewWeb/Program.cs
@@ -20,12 +20,15 @@ namespace APIViewWeb
                     config.AddUserSecrets(typeof(Program).Assembly);
                     IConfiguration settings = config.Build();
                     string connectionString = settings.GetValue<string>("APPCONFIG");
+                    string clientID = settings.GetValue<string>("APPCONFIG_CLIENT_ID");
+                    string tenantId = settings.GetValue<string>("APPCONFIG_TENANT_ID");
+                    string clientSecret = settings.GetValue<string>("APPCONFIG_CLIENT_SECRET");
                     // Load configuration from Azure App Configuration
                     config.AddAzureAppConfiguration(options =>
                     {
                         options.Connect(connectionString).ConfigureKeyVault(kv => 
                         { 
-                            kv.SetCredential(new DefaultAzureCredential());
+                            kv.SetCredential(new ClientSecretCredential(tenantId, clientID, clientSecret));
                         });
                     });
                 })

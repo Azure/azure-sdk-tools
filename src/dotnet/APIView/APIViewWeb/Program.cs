7be2314cfdaff82ba527d568e2e50c40e87e04d7
Praven Kuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2022-12-14 06:52:22 -0500

    Add new app config connection string (#4946)
    * Update APIVIew pipeline to set additional env variables for testing

--- src/dotnet/APIView/APIViewWeb/Program.cs
+++ src/dotnet/APIView/APIViewWeb/Program.cs
@@ -19,7 +19,7 @@ namespace APIViewWeb
                     config.AddEnvironmentVariables(prefix: "APIVIEW_");
                     config.AddUserSecrets(typeof(Program).Assembly);
                     IConfiguration settings = config.Build();
-                    string connectionString = settings.GetConnectionString("AppConfig");
+                    string connectionString = settings.GetValue<string>("APPCONFIG");
                     // Load configuration from Azure App Configuration
                     config.AddAzureAppConfiguration(options =>
                     {

6a3e598d749550ce621176b650a58f42fc940ddf
Libba Lawrence <llawrence@microsoft.com>
2023-01-17 13:15:52 -0800

    [pylint] type property name too long (#5097)
    * type property name too long
    
    * readme
    
    * ignore private names
    
    * change class checker name
    
    * catching other class related var/names
    
    * update tests
    
    * fixes
    
    * docstring param

--- tools/pylint-extensions/pylint-guidelines-checker/pylint_guidelines_checker.py
+++ tools/pylint-extensions/pylint-guidelines-checker/pylint_guidelines_checker.py
@@ -2028,6 +2028,98 @@ class NoAzureCoreTracebackUseRaiseFrom(BaseChecker):
                 msgid="no-raise-with-traceback", node=node, confidence=None
             )
 
+class NameExceedsStandardCharacterLength(BaseChecker):
+    __implements__ = IAstroidChecker
+
+    """Rule to check that the character length of type and property names are not over 40 characters."""
+    name = "name-too-long"
+    priority = -1
+    msgs = {
+        "C4751": (
+            "Name is over standard character length of 40.",
+            "name-too-long",
+            "Only use names that are less than 40 characters."
+        ),
+    }
+
+    STANDARD_CHARACTER_LENGTH = 40
+
+    def visit_classdef(self,node):
+        """Visit every class and check that the
+         class name is within the character length limit.
+
+        :param node: node
+        :type node: ast.ClassDef
+         
+         """
+
+        try:
+            self.iterate_through_names(node, True)    
+        except:
+            pass   
+
+
+    def visit_functiondef(self, node):
+        """Visit every function and check that the function and 
+        its variable names are within the character length limit.
+        
+        :param node: node
+        :type node: ast.FunctionDef
+        
+        """
+        try:
+            self.iterate_through_names(node, False)    
+        except:
+            pass      
+
+    def iterate_through_names(self, node, ignore_function):
+        """Helper function to iterate through names.
+
+            :param node: node
+            :type node: ast.ClassDef or ast.FunctionDef
+            :param ignore_function: Whether the function is being called 
+             from `visit_classdef` or not. If it is called from `visit_classdef`
+             ignore_function should be `True` to avoid repeat warnings on functions. 
+            :type ignore_function: bool
+            :return: None
+        """
+        if len(node.name) > self.STANDARD_CHARACTER_LENGTH and not node.name.startswith("_"):
+            self.add_message(
+                msgid="name-too-long",
+                node=node,
+                confidence=None,
+            )
+
+        for i in node.body:
+            if not (isinstance(i, astroid.FunctionDef) or isinstance(i, astroid.AsyncFunctionDef)) and not ignore_function:
+                try:
+                    if len(i.name) > self.STANDARD_CHARACTER_LENGTH and not i.name.startswith("_"):
+                        self.add_message(
+                            msgid="name-too-long",
+                            node=i,
+                            confidence=None,
+                        )        
+                except:
+                    # Gets the names of ast.Assign statements
+                    for j in i.targets:
+                        if isinstance(j, astroid.AssignName):
+                            if len(j.name) > self.STANDARD_CHARACTER_LENGTH and not j.name.startswith("_"):
+                                self.add_message(
+                                    msgid="name-too-long",
+                                    node=j,
+                                    confidence=None,
+                                )  
+                        elif isinstance(j, astroid.AssignAttr):
+                            # for self.names
+                            if len(j.attrname) > self.STANDARD_CHARACTER_LENGTH and not j.attrname.startswith("_"):
+                                self.add_message(
+                                    msgid="name-too-long",
+                                    node=j,
+                                    confidence=None,
+                                )  
+
+    visit_asyncfunctiondef = visit_functiondef    
+
 # if a linter is registered in this function then it will be checked with pylint
 def register(linter):
     linter.register_checker(ClientsDoNotUseStaticMethods(linter))
@@ -2052,6 +2144,7 @@ def register(linter):
     linter.register_checker(ClientListMethodsUseCorePaging(linter))
     linter.register_checker(NonAbstractTransportImport(linter))
     linter.register_checker(NoAzureCoreTracebackUseRaiseFrom(linter))
+    linter.register_checker(NameExceedsStandardCharacterLength(linter))
 
 
     # disabled by default, use pylint --enable=check-docstrings if you want to use it
--- tools/pylint-extensions/pylint-guidelines-checker/tests/test_pylint_custom_plugins.py
+++ tools/pylint-extensions/pylint-guidelines-checker/tests/test_pylint_custom_plugins.py
@@ -3137,3 +3137,128 @@ class TestRaiseWithTraceback(pylint.testutils.CheckerTestCase):
                 )
         ):
             self.checker.visit_importfrom(node)
+
+class TestTypePropertyNameLength(pylint.testutils.CheckerTestCase):
+    """Test that we are checking the type and property name lengths"""
+    CHECKER_CLASS = checker.NameExceedsStandardCharacterLength
+
+    def test_class_name_too_long(self):
+        class_node = astroid.extract_node(
+        """
+            class ThisClassNameShouldEndUpBeingTooLongForAClient():
+                def __init__(self, **kwargs):
+                    pass
+        """
+        )
+        with self.assertAddsMessages(
+                pylint.testutils.MessageTest(
+                    msg_id="name-too-long",
+                    line=2,
+                    node=class_node,
+                    col_offset=0, 
+                    end_line=2, 
+                    end_col_offset=52
+                )
+        ):
+            self.checker.visit_classdef(class_node)
+
+    def test_function_name_too_long(self):
+        class_node, function_node = astroid.extract_node(
+        """
+            class ClassNameGoodClient(): #@
+                def this_function_name_should_be_too_long_for_rule(self, **kwargs): #@
+                    pass
+        """
+        )
+        with self.assertNoMessages():
+            self.checker.visit_classdef(class_node)
+        with self.assertAddsMessages(
+                pylint.testutils.MessageTest(
+                    msg_id="name-too-long",
+                    line=3,
+                    node=function_node,
+                    col_offset=4, 
+                    end_line=3, 
+                    end_col_offset=54
+                )
+        ):
+            self.checker.visit_functiondef(function_node)
+
+    def test_variable_name_too_long(self):
+        class_node, function_node, property_node = astroid.extract_node(
+        """
+            class ClassNameGoodClient(): #@
+                def this_function_good(self, **kwargs): #@
+                    this_lists_name_is_too_long_to_work_with_linter_rule = [] #@
+        """
+        )
+        with self.assertNoMessages():
+            self.checker.visit_classdef(class_node)
+        with self.assertAddsMessages(
+                pylint.testutils.MessageTest(
+                    msg_id="name-too-long",
+                    line=4,
+                    node=property_node.targets[0],
+                    col_offset=8, 
+                    end_line=4, 
+                    end_col_offset=65
+                )
+        ):
+            self.checker.visit_functiondef(function_node)
+
+    def test_private_name_too_long(self):
+        class_node, function_node, property_node = astroid.extract_node(
+        """
+            class ClassNameGoodClient(): #@
+                def _this_function_is_private_but_over_length_reqs(self, **kwargs): #@
+                    this_lists_name = [] #@
+        """
+        )
+        with self.assertNoMessages():
+            self.checker.visit_classdef(class_node)
+            self.checker.visit_functiondef(function_node)
+
+    def test_instance_attr_name_too_long(self):
+        class_node, function_node, property_node = astroid.extract_node(
+        """
+            class ClassNameGoodClient(): #@
+                def __init__(self, this_name_is_too_long_to_use_anymore_reqs, **kwargs): #@
+                    self.this_name_is_too_long_to_use_anymore_reqs = 10 #@
+        """
+        )
+        with self.assertNoMessages():
+            self.checker.visit_classdef(class_node)
+        with self.assertAddsMessages(
+                pylint.testutils.MessageTest(
+                    msg_id="name-too-long",
+                    line=4,
+                    node=property_node.targets[0],
+                    col_offset=8, 
+                    end_line=4, 
+                    end_col_offset=65
+                )
+        ):
+            self.checker.visit_functiondef(function_node)
+
+    def test_class_var_name_too_long(self):
+        class_node, class_var_node, function_node, property_node = astroid.extract_node(
+        """
+            class ClassNameGoodClient(): #@
+                this_name_is_too_long_to_use_anymore_reqs = 10 #@
+                def __init__(self, dog, **kwargs): #@
+                    self.dog=dog #@
+        """
+        )
+        with self.assertAddsMessages(
+                pylint.testutils.MessageTest(
+                    msg_id="name-too-long",
+                    line=3,
+                    node=class_node.body[0].targets[0],
+                    col_offset=4, 
+                    end_line=3, 
+                    end_col_offset=45
+                )
+        ):
+            self.checker.visit_functiondef(class_node)
+        with self.assertNoMessages():
+            self.checker.visit_functiondef(function_node)

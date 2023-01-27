d9465f3282ed752f366ebf67d0258389e1ddad9e
scbedd <45376673+scbedd@users.noreply.github.com>
2023-01-26 18:23:20 -0800

    I am happy with this formatting. time to finish off go, cpp, .net, and java

--- scripts/python/assets-automation/generate_assets_report.py
+++ scripts/python/assets-automation/generate_assets_report.py
@@ -1,4 +1,4 @@
-import os, argparse, glob, json, datetime
+import os, argparse, glob, json, datetime, time
 
 from subprocess import run
 from typing import List, Dict, Any
@@ -39,6 +39,7 @@ DOCUMENT: str = """
 """
 
 TABLE_HEIGHT: int = 10
+BATCH_SIZE = TABLE_HEIGHT * 2
 
 
 class ScanResult:
@@ -50,7 +51,9 @@ class ScanResult:
 
 
 def get_repo(language: str) -> str:
+    where = f"https://github.com/azure/azure-sdk-for-{language.lower()}"
     target_folder = os.path.join(generated_folder, language)
+    print(f"Cloning repo for {language} from {where}", end="...")
 
     if not os.path.exists(target_folder):
         os.makedirs(target_folder)
@@ -62,10 +65,11 @@ def get_repo(language: str) -> str:
             "1",
             "--branch",
             "main",
-            f"https://github.com/azure/azure-sdk-for-{language.lower()}",
+            where,
             target_folder,
         ]
         run(command, cwd=generated_folder)
+    print(YES)
 
     return target_folder
 
@@ -95,12 +99,59 @@ def evaluate_python_package(package_path: str) -> int:
     return result
 
 
+def generate_python_report() -> ScanResult:
+    language = "Python"
+    repo = get_repo(language)
+    print(f"Evaluating repo for {language} @ {repo}", end="...")
+
+    try:
+        result = ScanResult(language)
+
+        results = discover_targeted_packages("azure*", repo)
+
+        # filter the results here
+        result.packages = [os.path.basename(pkg) for pkg in results]
+
+        for pkg in results:
+            evaluation = evaluate_python_package(pkg)
+            if evaluation == 1:
+                result.packages_using_proxy.append(os.path.basename(pkg))
+            elif evaluation == 2:
+                result.packages_using_external.append(os.path.basename(pkg))
+
+        print(YES)
+    except Exception as e:
+        print(NO)
+
+    return result
+
+
+def evaluate_go_package(package_path: str) -> int:
+    pass
+
+
+# evaluate by finding a testdata/recordings
 def generate_go_report() -> ScanResult:
+    result = ScanResult("Go")
+    return result
+
+
+def evaluate_net_package(package_path: str) -> int:
     pass
 
 
 def generate_net_report() -> ScanResult:
+    result = ScanResult(".NET")
+    return result
+
+
+def evaluate_cpp_package(package_path: str) -> int:
     pass
+
+
+def generate_cpp_report() -> ScanResult:
+    result = ScanResult("Cpp")
+    return result
 
 
 def evaluate_js_package(package_path: str) -> int:
@@ -120,19 +171,33 @@ def evaluate_js_package(package_path: str) -> int:
 
 
 def generate_js_report() -> ScanResult:
-    repo = get_repo("JS")
-    target_folder = os.path.join(repo, "sdk", "**", "package.json")
-    result = ScanResult("JS")
+    language = "JS"
+    repo = get_repo(language)
+    print(f"Evaluating repo for {language} @ {repo}", end="...")
 
-    results = glob.glob(target_folder, recursive=True)
-    result.packages = [os.path.basename(os.path.dirname(pkg)) for pkg in results]
+    try:
+        target_folder = os.path.join(repo, "sdk", "**", "package.json")
+        result = ScanResult(language)
 
-    for pkg in results:
-        evaluation = evaluate_js_package(pkg)
-        if evaluation == 1:
-            result.packages_using_proxy.append(os.path.basename(os.path.dirname(pkg)))
-        elif evaluation == 2:
-            result.packages_using_external.append(os.path.basename(os.path.dirname(pkg)))
+        results = glob.glob(target_folder, recursive=True)
+
+        result.packages = [
+            os.path.basename(os.path.dirname(pkg))
+            for pkg in results
+            if "samples" not in os.path.normpath(pkg).split(os.sep)
+            and os.path.basename(os.path.dirname(pkg)) != "samples-react"
+        ]
+
+        for pkg in results:
+            evaluation = evaluate_js_package(pkg)
+            if evaluation == 1:
+                result.packages_using_proxy.append(os.path.basename(os.path.dirname(pkg)))
+            elif evaluation == 2:
+                result.packages_using_proxy.append(os.path.basename(os.path.dirname(pkg)))
+                result.packages_using_external.append(os.path.basename(os.path.dirname(pkg)))
+        print(YES)
+    except Exception as e:
+        print(NO)
 
     return result
 
@@ -151,13 +216,18 @@ def generate_detailed_table(origin: ScanResult, package_set: List[str]):
 
 def write_output(result: ScanResult) -> None:
     with open(result.language.lower() + ".md", "w", encoding="utf-8") as f:
-        f.writelines(f"# {result.language} Transition Details - {datetime.date.today()}")
+        date = datetime.date.today()
+        time_of_day = datetime.datetime.today().strftime("%I:%M%p")
+        f.writelines(
+            f"# {result.language} Transition Details as of {date}@{time_of_day} {datetime.datetime.today().astimezone().tzname()}"
+        )
 
-        # batch by sets of 20
-        for i in range(0, len(result.packages), 20):
-            packages = result.packages[i : i + 20]
-            table_set_1 = packages[0:TABLE_HEIGHT]
-            table_set_2 = packages[TABLE_HEIGHT:]
+        if result.packages:
+            # batch by sets of 20
+            batch_size = (len(result.packages) // 2) + (len(result.packages) % 2)
+
+            table_set_1 = result.packages[0:batch_size]
+            table_set_2 = result.packages[batch_size:]
 
             document_addition = DOCUMENT.format(
                 generate_detailed_table(result, table_set_1), generate_detailed_table(result, table_set_2)
@@ -166,27 +236,29 @@ def write_output(result: ScanResult) -> None:
             f.write(document_addition)
 
 
+# original version of write-output that had two nicely batched tables
+# def write_output(result: ScanResult) -> None:
+#     with open(result.language.lower() + ".md", "w", encoding="utf-8") as f:
+#         date = datetime.date.today()
+#         time_of_day = datetime.datetime.today().strftime("%I:%M%p")
+#         f.writelines(f"# {result.language} Transition Details as of {date}@{time_of_day} {datetime.datetime.today().astimezone().tzname()}")
+
+#         # batch by sets of 20
+#         for i in range(0, len(result.packages), BATCH_SIZE):
+#             packages = result.packages[i : i + BATCH_SIZE]
+#             table_set_1 = packages[0:TABLE_HEIGHT]
+#             table_set_2 = packages[TABLE_HEIGHT:]
+
+#             document_addition = DOCUMENT.format(
+#                 generate_detailed_table(result, table_set_1), generate_detailed_table(result, table_set_2)
+#             )
+
+#             f.write(document_addition)
+
+
 def write_summary(results: List[ScanResult]) -> None:
     with open("summary.md", "w", encoding="utf-8") as f:
         f.writelines(f"# Test Proxy Transition Summary - {datetime.date.today()}")
-
-
-def generate_python_report() -> ScanResult:
-    repo = get_repo("Python")
-
-    result = ScanResult("Python")
-
-    results = discover_targeted_packages("azure*", repo)
-    result.packages = [os.path.basename(pkg) for pkg in results]
-
-    for pkg in results:
-        evaluation = evaluate_python_package(pkg)
-        if evaluation == 1:
-            result.packages_using_proxy.append(os.path.basename(pkg))
-        elif evaluation == 2:
-            result.packages_using_external.append(os.path.basename(pkg))
-
-    return result
 
 
 if __name__ == "__main__":
@@ -209,4 +281,7 @@ if __name__ == "__main__":
     net = generate_net_report()
     write_output(go)
 
-    write_summary([python, js, go, net])
+    cpp = generate_cpp_report()
+    write_output(cpp)
+
+    write_summary([python, js, go, net, cpp])

ecc26fb56b0e4818e9ed885422a099ddb5e91868
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2023-02-10 15:51:23 -0500

    Remove unused import

--- tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/Program.cs
+++ tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/Program.cs
@@ -13,7 +13,6 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using YamlDotNet.Core;
 using YamlDotNet.Serialization;
-using static Azure.Sdk.Tools.PerfAutomation.Program;
 
 namespace Azure.Sdk.Tools.PerfAutomation
 {

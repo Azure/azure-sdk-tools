79cecede2ab06284370c6e4a8ec1b40847e1ac39
Konrad Jamrozik <kojamroz@microsoft.com>
2023-02-23 14:13:15 -0800

    Restore and overhaul simple target path tests into `RetrieveCodeOwnersProgramTests` (#5518)
    * extract RunProgramMain; rename test to OutputsCorrectCodeownersOnGlobTargetPath
    * add OutputsCorrectCodeownersOnSimpleTargetPaths
    * converted OutputsCorrectCodeownersOnSimpleTargetPath to parameterized UT
    * dedup test battery + improve comments

--- tools/code-owners-parser/Azure.Sdk.Tools.RetrieveCodeOwners.Tests/RetrieveCodeOwnersProgramTests.cs
+++ tools/code-owners-parser/Azure.Sdk.Tools.RetrieveCodeOwners.Tests/RetrieveCodeOwnersProgramTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 using Azure.Sdk.Tools.CodeOwnersParser;
 using NUnit.Framework;
@@ -21,50 +22,115 @@ namespace Azure.Sdk.Tools.RetrieveCodeOwners.Tests;
 public class RetrieveCodeOwnersProgramTests
 {
     /// <summary>
-    /// Given:
+    /// A battery of test cases exercising the Azure.Sdk.Tools.RetrieveCodeOwners.Program.Main executable.
     ///
-    ///   file system contents as seen in TestData/InputDir
-    /// 
-    ///   codeownersFilePathOrUrl contents as seen in TestData/glob_path_CODEOWNERS
+    /// Each test case is composed of a targetPath and expected CodeownersEntry that is to match against
+    /// the targetPath when the executable is executed.
     ///
-    ///   targetPath of /**
+    /// These test battery is used in the following ways:
     ///
-    ///   excludeNonUserAliases set to false
-    /// 
-    /// When:
-    ///   The retrieve-codeowners tool is executed on these inputs.
+    /// 1. In OutputsCorrectCodeownersOnSimpleTargetPath parameterized unit test, each test case is exercised
+    /// by running the executable with targetPath provided as input targetPath.
     ///
-    /// Then:
-    ///   The tool should return on STDOUT owners matched as seen in the
-    ///   "expectedEntries" dictionary.
-    /// 
+    /// 2. In OutputsCorrectCodeownersOnGlobTargetPath the entire test battery is asserted against
+    /// by running the executable with targetPaths set to "/**", thus entering the glob-matching mode and finding
+    /// all the targetPaths present in this battery.
+    ///
+    /// Preconditions for running tests against this battery:
+    /// - directory "./TestData/InputDir" and file  "./TestData/test_CODEOWNERS" contain appropriate contents
+    /// - the exercised executable is passed as input appropriate arguments pointing to the file system;
+    ///   consult the aforementioned tests for concrete values.
+    /// </summary>
+    private static readonly TestCase[] testCases =
+    {
+        // @formatter:off
+        //   targetPath        expected CodeownersEntry
+        new ("a.txt"         , new CodeownersEntry("/*",            new List<string> { "star" })),
+        new ("b.txt"         , new CodeownersEntry("/*",            new List<string> { "star" })),
+        new ("foo/a.txt"     , new CodeownersEntry("/foo/**/a.txt", new List<string> { "foo_2star_a" })),
+        new ("foo/b.txt"     , new CodeownersEntry("/**",           new List<string> { "2star" })),
+        new ("foo/bar/a.txt" , new CodeownersEntry("/foo/*/a.txt",  new List<string> { "foo_star_a_1", "foo_star_a_2" })),
+        new ("foo/bar/b.txt" , new CodeownersEntry("/**",           new List<string> { "2star" })),
+        new ("baz/cor/c.txt" , new CodeownersEntry("/baz*",         new List<string> { "baz_star" })),
+        new ("baz_.txt"      , new CodeownersEntry("/baz*",         new List<string> { "baz_star" })),
+        new ("qux/abc/d.txt" , new CodeownersEntry("/qux/",         new List<string> { "qux" })),
+        new ("cor.txt"       , new CodeownersEntry("/*",            new List<string> { "star" })),
+        new ("cor2/a.txt"    , new CodeownersEntry("/**",           new List<string> { "2star" })),
+        new ("cor/gra/a.txt" , new CodeownersEntry("/**",           new List<string> { "2star" }))
+        // @formatter:on
+    };
+
+    private static Dictionary<string, CodeownersEntry> TestCasesAsDictionary
+        => testCases.ToDictionary(
+            testCase => testCase.TargetPath,
+            testCase => testCase.ExpectedCodeownersEntry);
+
+    /// <summary>
+    /// Please see comment on RetrieveCodeOwnersProgramTests.testCases
+    /// </summary>
+    [TestCaseSource(nameof(testCases))]
+    public void OutputsCorrectCodeownersOnSimpleTargetPath(TestCase testCase)
+    {
+        const string targetDir = "./TestData/InputDir";
+        const string codeownersFilePathOrUrl = "./TestData/test_CODEOWNERS";
+        const bool excludeNonUserAliases = false;
+
+        var targetPath = testCase.TargetPath;
+        var expectedEntry = testCase.ExpectedCodeownersEntry;
+
+        // Act
+        (string actualOutput, string actualErr, int returnCode) = RunProgramMain(
+            targetPath,
+            codeownersFilePathOrUrl,
+            excludeNonUserAliases,
+            targetDir);
+
+        CodeownersEntry actualEntry = TryDeserializeActualEntryFromSimpleTargetPath(actualOutput, actualErr);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(actualEntry, Is.EqualTo(expectedEntry), $"path: {targetPath}");
+            Assert.That(returnCode, Is.EqualTo(0));
+            Assert.That(actualErr, Is.EqualTo(string.Empty));
+        });
+    }
+
+    /// <summary>
+    /// Please see comment on RetrieveCodeOwnersProgramTests.testCases
     /// </summary>
     [Test]
-    public void OutputsCodeowners()
+    public void OutputsCorrectCodeownersOnGlobTargetPath()
     {
         const string targetDir = "./TestData/InputDir";
         const string targetPath = "/**";
         const string codeownersFilePathOrUrl = "./TestData/test_CODEOWNERS";
         const bool excludeNonUserAliases = false;
 
-        var expectedEntries = new Dictionary<string, CodeownersEntry>
+        Dictionary<string, CodeownersEntry> expectedEntriesByPath = TestCasesAsDictionary;
+
+        // Act
+        (string actualOutput, string actualErr, int returnCode) = RunProgramMain(
+            targetPath,
+            codeownersFilePathOrUrl,
+            excludeNonUserAliases,
+            targetDir);
+
+        Dictionary<string, CodeownersEntry> actualEntriesByPath = TryDeserializeActualEntriesFromGlobTargetPath(actualOutput, actualErr);
+
+        Assert.Multiple(() =>
         {
-            // @formatter:off
-            ["a.txt"]         = new CodeownersEntry("/*",            new List<string> { "star" }),
-            ["b.txt"]         = new CodeownersEntry("/*",            new List<string> { "star" }),
-            ["foo/a.txt"]     = new CodeownersEntry("/foo/**/a.txt", new List<string> { "foo_2star_a" }),
-            ["foo/b.txt"]     = new CodeownersEntry("/**",           new List<string> { "2star" }),
-            ["foo/bar/a.txt"] = new CodeownersEntry("/foo/*/a.txt",  new List<string> { "foo_star_a_1", "foo_star_a_2" }),
-            ["foo/bar/b.txt"] = new CodeownersEntry("/**",           new List<string> { "2star" }),
-            ["baz/cor/c.txt"] = new CodeownersEntry("/baz*",         new List<string> { "baz_star" }),
-            ["baz_.txt"]      = new CodeownersEntry("/baz*",         new List<string> { "baz_star" }),
-            ["qux/abc/d.txt"] = new CodeownersEntry("/qux/",         new List<string> { "qux" }),
-            ["cor.txt"]       = new CodeownersEntry("/*",            new List<string> { "star" }),
-            ["cor2/a.txt"]    = new CodeownersEntry("/**",           new List<string> { "2star" }),
-            ["cor/gra/a.txt"] = new CodeownersEntry("/**",           new List<string> { "2star" }),
-            // @formatter:on
-        };
-        
+            AssertEntries(actualEntriesByPath, expectedEntriesByPath);
+            Assert.That(returnCode, Is.EqualTo(0));
+            Assert.That(actualErr, Is.EqualTo(string.Empty));
+        });
+    }
+
+    private static (string actualOutput, string actualErr, int returnCode) RunProgramMain(
+        string targetPath,
+        string codeownersFilePathOrUrl,
+        bool excludeNonUserAliases,
+        string targetDir)
+    {
         string actualOutput, actualErr;
         int returnCode;
         using (var consoleOutput = new ConsoleOutput())
@@ -80,17 +146,31 @@ public class RetrieveCodeOwnersProgramTests
             actualErr = consoleOutput.GetStderr();
         }
 
-        var actualEntries = TryDeserializeActualEntries(actualOutput, actualErr);
-
-        Assert.Multiple(() =>
-        {
-            AssertEntries(actualEntries, expectedEntries);
-            Assert.That(returnCode, Is.EqualTo(0));
-            Assert.That(actualErr, Is.EqualTo(string.Empty));
-        });
+        return (actualOutput, actualErr, returnCode);
     }
 
-    private static Dictionary<string, CodeownersEntry> TryDeserializeActualEntries(
+    private static CodeownersEntry TryDeserializeActualEntryFromSimpleTargetPath(
+        string actualOutput,
+        string actualErr)
+    {
+        CodeownersEntry actualEntry;
+        try
+        {
+            actualEntry =
+                JsonSerializer.Deserialize<CodeownersEntry>(actualOutput)!;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            Console.WriteLine("actualOutput: " + actualOutput);
+            Console.WriteLine("actualErr: " + actualErr);
+            throw;
+        }
+
+        return actualEntry;
+    }
+
+    private static Dictionary<string, CodeownersEntry> TryDeserializeActualEntriesFromGlobTargetPath(
         string actualOutput,
         string actualErr)
     {
@@ -124,4 +204,11 @@ public class RetrieveCodeOwnersProgramTests
 
         Assert.That(actualEntries, Has.Count.EqualTo(expectedEntries.Count));
     }
+
+    /// <summary>
+    /// Please see comment on RetrieveCodeOwnersProgramTests.testCases
+    /// </summary>
+    public record TestCase(
+        string TargetPath,
+        CodeownersEntry ExpectedCodeownersEntry);
 }

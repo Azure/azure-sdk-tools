bfb0edee90adb4b2afd0117aa332a38bffa3a0b8
Konrad Jamrozik <kojamroz@microsoft.com>
2023-03-06 23:09:53 -0800

    Fix CodeOwnerFileLocation path, fix casing, and dedup param defaults

--- eng/common-tests/get-codeowners/get-codeowners.tests.ps1
+++ eng/common-tests/get-codeowners/get-codeowners.tests.ps1
@@ -3,26 +3,21 @@ Import-Module Pester
 BeforeAll {
   . $PSScriptRoot/../../common/scripts/get-codeowners/get-codeowners-functions.ps1
 
-    $ToolVersion = "1.0.0-dev.20230214.3"
-    $ToolPath = (Join-Path ([System.IO.Path]::GetTempPath()) "codeowners-tool")
-    $DevOpsFeed = "https://pkgs.dev.azure.com/azure-sdk/public/_packaging/azure-sdk-for-net/nuget/v3/index.json"
-    $VsoVariable = ""
-
-    function TestGetCodeowners([string]$targetPath, [string]$codeownersFileLocation, [bool]$includeNonUserAliases = $false, [string[]]$expectedOwners) {
-      Write-Host "Test: Owners for path '$targetPath' in CODEOWNERS file at path '$codeownersFileLocation' should be '$expectedOwners'"
+    function TestGetCodeowners(
+      [string] $TargetPath, 
+      [string] $CodeownersFileLocation, 
+      [string[]] $ExpectedOwners
+    ) 
+    {
+      Write-Host "Test: Owners for path '$TargetPath' in CODEOWNERS file at path '$CodeownersFileLocation' should be '$ExpectedOwners'"
       
       $actualOwners = Get-Codeowners `
-        -ToolVersion $ToolVersion `
-        -ToolPath $ToolPath `
-        -DevOpsFeed $DevOpsFeed `
-        -VsoVariable $VsoVariable `
-        -targetPath $targetPath `
-        -codeownersFileLocation $codeownersFileLocation `
-        -includeNonUserAliases $IncludeNonUserAliases
+        -TargetPath $TargetPath `
+        -CodeownersFileLocation $CodeownersFileLocation `
     
-      $actualOwners.Count | Should -Be $expectedOwners.Count
-      for ($i = 0; $i -lt $expectedOwners.Count; $i++) {
-        $expectedOwners[$i] | Should -Be $actualOwners[$i]
+      $actualOwners.Count | Should -Be $ExpectedOwners.Count
+      for ($i = 0; $i -lt $ExpectedOwners.Count; $i++) {
+        $ExpectedOwners[$i] | Should -Be $actualOwners[$i]
       }
     }
 }
@@ -35,16 +30,15 @@ Describe "Get Codeowners" -Tag "UnitTest" {
         expectedOwners = @("konrad-jamrozik", "weshaggard", "benbp")
       },
       @{
-        codeownersPath = "$PSScriptRoot/../../../tools/code-owners-parser/Azure.Sdk.Tools.RetrieveCodeOwners.Tests/TestData/test_CODEOWNERS"; 
+        CodeownersPath = "$PSScriptRoot/../../../tools/code-owners-parser/Azure.Sdk.Tools.RetrieveCodeOwners.Tests/TestData/test_CODEOWNERS"; 
         targetPath = "tools/code-owners-parser/Azure.Sdk.Tools.RetrieveCodeOwners.Tests/TestData/InputDir/a.txt"; 
         expectedOwners = @("2star")
       }      
     ) {
       $resolvedCodeownersPath = (Resolve-Path $codeownersPath)
       TestGetCodeowners `
-        -targetPath $targetPath `
-        -codeownersFileLocation $resolvedCodeownersPath `
-        -includeNonUserAliases $true `
-        -expectedOwners $expectedOwners
+        -TargetPath $targetPath `
+        -CodeownersFileLocation $resolvedCodeownersPath `
+        -ExpectedOwners $expectedOwners
     }
 }

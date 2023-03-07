aa515a52acb11e555bc90cae1ef76135d06daab5
Konrad Jamrozik <kojamroz@microsoft.com>
2023-03-07 11:49:05 -0800

    move get-codeowners back to scripts/ and rename -functions to .lib

--- eng/common-tests/get-codeowners/get-codeowners.tests.ps1
+++ eng/common-tests/get-codeowners/get-codeowners.tests.ps1
@@ -1,7 +1,7 @@
 Import-Module Pester
 
 BeforeAll {
-  . $PSScriptRoot/../../common/scripts/get-codeowners/get-codeowners-functions.ps1
+  . $PSScriptRoot/../../common/scripts/get-codeowners.lib.ps1
 
     function TestGetCodeowners(
       [string] $TargetPath, 
@@ -25,11 +25,13 @@ BeforeAll {
 Describe "Get Codeowners" -Tag "UnitTest" {
     It "Should get Codeowners" -TestCases @(
       @{
+        # The $PSScriptRoot is assumed to be azure-sdk-tools/eng/common-tests/get-codeowners/get-codeowners.tests.ps1
         codeownersPath = "$PSScriptRoot/../../../.github/CODEOWNERS"; 
         targetPath = "eng/common/scripts/get-codeowners/get-codeowners.ps1"; 
         expectedOwners = @("konrad-jamrozik", "weshaggard", "benbp")
       },
       @{
+        # The $PSScriptRoot is assumed to be azure-sdk-tools/eng/common-tests/get-codeowners/get-codeowners.tests.ps1
         CodeownersPath = "$PSScriptRoot/../../../tools/code-owners-parser/Azure.Sdk.Tools.RetrieveCodeOwners.Tests/TestData/test_CODEOWNERS"; 
         targetPath = "tools/code-owners-parser/Azure.Sdk.Tools.RetrieveCodeOwners.Tests/TestData/InputDir/a.txt"; 
         expectedOwners = @("2star")

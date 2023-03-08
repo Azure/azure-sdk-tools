a4f3be808675e3b4be2c51f3d4f172022fd3bc8f
praveenkuttappan <praveen.kuttappan@gmail.com>
2023-03-08 15:46:40 -0500

    Disable delete option for PR API reviews

---
+++ src/dotnet/APIView/APIViewWeb/Repositories/UnDeletableReviewException.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+namespace APIViewWeb.Repositories
+{
+    internal class UnDeletableReviewException : Exception
+    {
+    }
+}

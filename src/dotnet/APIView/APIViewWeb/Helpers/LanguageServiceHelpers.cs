b802e452432a6a8fd789a04402ca73de61af780d
Chidozie Ononiwu <chononiw@microsoft.com>
2023-03-14 16:27:16 -0700

    CADL to TypeSpec rename

--- src/dotnet/APIView/APIViewWeb/Helpers/LanguageServiceHelpers.cs
+++ src/dotnet/APIView/APIViewWeb/Helpers/LanguageServiceHelpers.cs
@@ -2,6 +2,6 @@ namespace APIViewWeb.Helpers
 {
     public class LanguageServiceHelpers
     {
-        public static string[] SupportedLanguages = new string[] { "C", "C++", "C#", "Cadl", "Go", "Java", "JavaScript", "Json", "Kotlin", "Python", "Swagger", "Swift", "Xml" };
+        public static string[] SupportedLanguages = new string[] { "C", "C++", "C#", "TypeSpec", "Go", "Java", "JavaScript", "Json", "Kotlin", "Python", "Swagger", "Swift", "Xml" };
     }
 }

f7ab00cb4c33dda8994c107bf9f229dbbe040930
scbedd <45376673+scbedd@users.noreply.github.com>
2023-03-15 13:00:16 -0700

    re-commit reverted change that was necessary

--- tools/test-proxy/Azure.Sdk.Tools.TestProxy/Startup.cs
+++ tools/test-proxy/Azure.Sdk.Tools.TestProxy/Startup.cs
@@ -132,6 +132,7 @@ namespace Azure.Sdk.Tools.TestProxy
             new GitProcessHandler().VerifyGitMinVersion();
             DefaultOptions defaultOptions = (DefaultOptions)commandObj;
 
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
             var semanticVersion = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
             System.Console.WriteLine($"Running proxy version is Azure.Sdk.Tools.TestProxy {semanticVersion}");
 

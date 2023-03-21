f55970d31bcc0d2f7c30779fb34f8c415b2e6d65
Caio Saldanha <camaiaor@microsoft.com>
2023-03-21 13:06:48 -0700

    Addressed feedback

--- src/dotnet/APIView/APIViewUnitTests/ProgramTests.cs
+++ src/dotnet/APIView/APIViewUnitTests/ProgramTests.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ApiView;
@@ -8,9 +10,21 @@ using Xunit;
 
 namespace APIViewUnitTests
 {
-    public class AppTests
+    public class ProgramTests
     {
-        private string InputPath => Path.Combine("SampleTestFiles", "APIView.dll");
+        public static IEnumerable<object[]> ExactFormattingFiles
+        {
+            get
+            {
+                var assembly = typeof(CodeFileBuilderTests).Assembly;
+                return assembly.GetManifestResourceNames()
+                    .Where(r => r.Contains("ExactFormatting"))
+                    .Select(r => new object[] { r })
+                    .ToArray();
+            }
+        }
+
+        private string InputPath => Path.Combine(Path.GetTempPath(), "input.dll");
 
         private string OutputPath => Path.Combine(Path.GetTempPath(), "output.json");
 
@@ -27,7 +41,7 @@ namespace APIViewUnitTests
                 args[i] = InputPath;
             }
 
-            Assert.ThrowsAsync<ArgumentException>(async () => await App.RunAsync(args));
+            Assert.ThrowsAsync<ArgumentException>(async () => await Program.RunAsync(args));
         }
 
         [Fact]
@@ -39,7 +53,7 @@ namespace APIViewUnitTests
                 OutputPath
             };
 
-            Assert.ThrowsAsync<FileNotFoundException>(async () => await App.RunAsync(args));
+            Assert.ThrowsAsync<FileNotFoundException>(async () => await Program.RunAsync(args));
         }
 
         [Fact]
@@ -51,15 +65,25 @@ namespace APIViewUnitTests
                 Path.Combine("missing_folder", "output.json")
             };
 
-            Assert.ThrowsAsync<FolderNotFoundException>(async () => await App.RunAsync(args));
+            Assert.ThrowsAsync<FolderNotFoundException>(async () => await Program.RunAsync(args));
         }
 
-        [Fact]
-        public async Task RunAsyncGeneratesOutputFile()
+        [Theory]
+        [MemberData(nameof(ExactFormattingFiles))]
+        public async Task RunAsyncGeneratesOutputFile(string name)
         {
+            var manifestResourceStream = typeof(CodeFileBuilderTests).Assembly.GetManifestResourceStream(name);
+            using var streamReader = new StreamReader(manifestResourceStream);
+            var code = streamReader.ReadToEnd();
+
+            using (var fileStream = new FileStream(InputPath, FileMode.Create, FileAccess.Write))
+            {
+                await Common.BuildDllAsync(fileStream, code);
+            }
+
             var args = new string[] { InputPath, OutputPath };
 
-            await App.RunAsync(args);
+            await Program.RunAsync(args);
 
             var output = await File.ReadAllTextAsync(OutputPath);
 

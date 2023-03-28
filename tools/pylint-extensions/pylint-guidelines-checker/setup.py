bf59880e223c6b751e19aff8323e71c5fdcff559
Scott Beddall <scbedd@microsoft.com>
2023-03-27 22:07:59 -0700

    prepare pylint guidelines checker for release on pypi

--- tools/pylint-extensions/pylint-guidelines-checker/setup.py
+++ tools/pylint-extensions/pylint-guidelines-checker/setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
 setup(
     name="pylint-guidelines-checker",
     version="0.0.8",
@@ -9,4 +13,7 @@ setup(
     author='Microsoft Corporation',
     author_email='azpysdkhelp@microsoft.com',
     py_modules=['pylint_guidelines_checker'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    python_requires=">=3.7",
 )

2fbcab2515f6b8a7af62d0c6a7f6030d44260ff5
scbedd <45376673+scbedd@users.noreply.github.com>
2023-05-08 16:45:46 -0700

    add project structure for new stuff

---
+++ tools/assets-automation/assets-maintenance-tool/Azure.Sdk.Tools.Assets.BackupTool/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Azure.Sdk.Tools.Assets.BackupTool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ tools/assets-automation/assets-maintenance-tool/Azure.Sdk.Tools.Assets.CleanupTool/Program.cs
@@ -0,0 +1,10 @@
+namespace Azure.Sdk.Tools.Assets.CleanupTool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

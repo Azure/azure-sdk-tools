8cdc711753ad20219d544da4c7897c389a98a2d7
Chidozie Ononiwu <chononiw@microsoft.com>
2023-06-01 18:12:32 -0700

    Add SerializePatterenedObjectsCreatesTokensFromDiffPropertyType Test

--- tools/apiview/parsers/swagger-api-parser/SwaggerApiParser/Specs/Schema.cs
+++ tools/apiview/parsers/swagger-api-parser/SwaggerApiParser/Specs/Schema.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json.Serialization;
-using System.Text.Json;
+
 using SwaggerApiParser.SwaggerApiView;
 
 namespace SwaggerApiParser.Specs

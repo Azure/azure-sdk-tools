5c7b5737f5ee6c17d7ecb5253e3b5aa38900a83a
scbedd <45376673+scbedd@users.noreply.github.com>
2023-06-08 13:16:03 -0700

    cleanup usings

--- tools/assets-automation/assets-maintenance-tool/Azure.Sdk.Tools.Assets.MaintenanceTool/Model/RunConfiguration.cs
+++ tools/assets-automation/assets-maintenance-tool/Azure.Sdk.Tools.Assets.MaintenanceTool/Model/RunConfiguration.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Azure.Sdk.Tools.Assets.MaintenanceTool.Model;
 

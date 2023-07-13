891eb14e4c7c617a96d906a35326c01ab9a0da7d
Yeojun Han <t-hanyeojun@microsoft.com>
2023-07-12 19:27:40 -0700

    Add autorefresh APIView review approvals

--- src/dotnet/APIView/APIViewWeb/Hubs/SignalRHub.cs
+++ src/dotnet/APIView/APIViewWeb/Hubs/SignalRHub.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using APIViewWeb.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
@@ -19,6 +20,7 @@ namespace APIViewWeb.Hubs
             if (!string.IsNullOrEmpty(name))
             {
                 Groups.AddToGroupAsync(Context.ConnectionId, name);
+                Clients.Caller.SendAsync("ReceiveConnectionId", Context.ConnectionId);
             }
             return base.OnConnectedAsync();
         }

1f4c34f3c59c9ea1ee3db168e74d936dec260de4
samvaity <savaity@microsoft.com>
2023-07-14 16:13:56 -0700

    udpate for containerURI

--- tools/test-proxy/Azure.Sdk.Tools.TestProxy.Tests/SanitizerTestExample.cs
+++ tools/test-proxy/Azure.Sdk.Tools.TestProxy.Tests/SanitizerTestExample.cs
@@ -75,5 +75,57 @@ namespace Azure.Sdk.Tools.TestProxy.Tests
             var newBody = Encoding.UTF8.GetString(session.Session.Entries[2].Response.Body);
             Assert.Contains("redacted.com", newBody);
         }
+
+
+        [Fact]
+        public void SanitizerWorksAgainstSample1()
+        {
+            var session = TestHelpers.LoadRecordSession("Test.RecordEntries/sample_entry.json");
+
+            var uriRegexSanitizer = new BodyKeySanitizer(jsonPath: "containerUri", value: "redacted", regex: "https://([a-zA-Z0-9-]{3,63}).blob.core.windows.net/[a-zA-Z0-9-]*\\?sv=.*");
+
+            session.Session.Sanitize(uriRegexSanitizer);
+            var newBody = Encoding.UTF8.GetString(session.Session.Entries[2].Response.Body);
+
+            Assert.Contains("redacted", newBody);
+        }
+
+        [Fact]
+        public async Task APISanitizerWorksAgainstSample1()
+        {
+            var session = TestHelpers.LoadRecordSession("Test.RecordEntries/sample_entry.json");
+
+            // this is what your json body will look like coming over the wire. Notice the double escapes to prevent JSON parse break.
+            // it is an identical sanitizer registration to the one above
+            var overTheWire = "{ \"value\": \"redacted\", \"regex\": \"https://([a-zA-Z0-9-]{3,63}).blob.core.windows.net/[a-zA-Z0-9-]*\\?sv=.*\", \"jsonPath\": \"containerUri\" }";
+
+            // Target the type of sanitizer using this. (This is similar to selecting a constructor above)
+            var sanitizerName = "BodyKeySanitizer";
+
+
+            #region API registration and running of sanitizer
+            // feel free to ignore this setup, bunch of implementation details to register as if coming from external request
+            RecordingHandler testRecordingHandler = new RecordingHandler(Directory.GetCurrentDirectory());
+            testRecordingHandler.Sanitizers.Clear();
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Headers["x-abstraction-identifier"] = sanitizerName;
+            httpContext.Request.Body = TestHelpers.GenerateStreamRequestBody(overTheWire);
+            httpContext.Request.ContentLength = httpContext.Request.Body.Length;
+            var controller = new Admin(testRecordingHandler, new NullLoggerFactory())
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = httpContext
+                }
+            };
+            await controller.AddSanitizer();
+            var registeredSanitizer = testRecordingHandler.Sanitizers[0];
+            Assert.NotNull(registeredSanitizer);
+            #endregion
+
+            session.Session.Sanitize(registeredSanitizer);
+            var newBody = Encoding.UTF8.GetString(session.Session.Entries[2].Response.Body);
+            Assert.Contains("redacted", newBody);
+        }
     }
 }

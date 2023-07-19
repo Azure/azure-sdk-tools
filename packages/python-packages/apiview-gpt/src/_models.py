0679c00e3bc988ed841056c664b0efa37a34b1ba
Travis Prescott <tjprescott@users.noreply.github.com>
2023-07-19 08:54:15 -0700

    Combine multiple violations for the same line. (#6536)

--- packages/python-packages/apiview-gpt/src/_models.py
+++ packages/python-packages/apiview-gpt/src/_models.py
@@ -3,6 +3,7 @@ from typing import List
 
 class Violation(BaseModel):
     rule_ids: List[str] = Field(description="unique rule ID or IDs that were violated.")
+    line_no: int = Field(description="the line number of the violation.")
     bad_code: str = Field(description="the original code that was bad.")
     suggestion: str = Field(description="the suggested fix for the bad code.")
     comment: str = Field(description="a comment about the violation.")

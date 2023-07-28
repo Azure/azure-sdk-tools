cb3dd1e0215629a35d6031cbc131af939d93ee38
Scott Beddall <scbedd@microsoft.com>
2023-07-27 17:47:49 -0700

    not outputting the proper stuff

--- tools/test-proxy/scripts/test-scripts/assets.Tests.Helpers.ps1
+++ tools/test-proxy/scripts/test-scripts/assets.Tests.Helpers.ps1
@@ -317,13 +317,13 @@ Function Get-AssetsFilePath {
   $folders = Get-ChildItem $startingPath -Directory -Exclude "breadcrumb"
 
   Write-Host "What we see overall in the assets folder is"
-  Get-ChildItem $startingPath -Recurse -Exclude "breadcrumb" | % { Write-Host $_.FullName } | Out-Null
+  Get-ChildItem $startingPath -Recurse | % { Write-Host $_.FullName } | Out-Null
 
   Write-Host "What we see when we GCI and exclude breadcrumb"
-  Get-ChildItem -Directory -Exclude "breadcrumb" | % { Write-Host $_.FullName } | Out-Null
+  Get-ChildItem $startingPath -Directory -Exclude "breadcrumb" | % { Write-Host $_.FullName } | Out-Null
 
   Write-Host "What we see when we GCI"
-  Get-ChildItem -Directory | % { Write-Host $_.FullName } | Out-Null
+  Get-ChildItem $startingPath -Directory | % { Write-Host $_.FullName } | Out-Null
 
   # There should only be one folder
   if (1 -ne $numDirs) {

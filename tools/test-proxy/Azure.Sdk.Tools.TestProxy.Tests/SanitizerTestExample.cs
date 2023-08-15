ffc9c9a19af5d3c66d8bbe73a2675529c8cb7b1a
samvaity <savaity@microsoft.com>
2023-08-15 15:16:06 -0700

    cleanup

--- tools/test-proxy/Azure.Sdk.Tools.TestProxy.Tests/SanitizerTestExample.cs
+++ tools/test-proxy/Azure.Sdk.Tools.TestProxy.Tests/SanitizerTestExample.cs
@@ -49,45 +49,6 @@ namespace Azure.Sdk.Tools.TestProxy.Tests
             ReadCommentHandling = JsonCommentHandling.Skip,
             AllowTrailingCommas = true,
         };
-        
-        [Fact]
-        public async Task ThisShouldWork()
-        {
-            var session = TestHelpers.LoadRecordSession("Test.RecordEntries/sample_entry.json");
-
-            // this is what your json body will look like coming over the wire. Notice the double escapes to prevent JSON parse break.
-            // it is an identical sanitizer registration to the one above
-            var overTheWire = "{ \"value\": \"REDACTED\", \"jsonPath\": \"$..to\" }";
-
-            // Target the type of sanitizer using this. (This is similar to selecting a constructor above)
-            var sanitizerName = "BodyKeySanitizer";
-
-    
-            #region API registration and running of sanitizer
-            // feel free to ignore this setup, bunch of implementation details to register as if coming from external request
-            RecordingHandler testRecordingHandler = new RecordingHandler(Directory.GetCurrentDirectory());
-            testRecordingHandler.Sanitizers.Clear();
-            var httpContext = new DefaultHttpContext();
-            httpContext.Request.Headers["x-abstraction-identifier"] = sanitizerName;
-            httpContext.Request.Body = TestHelpers.GenerateStreamRequestBody(overTheWire);
-            httpContext.Request.ContentLength = httpContext.Request.Body.Length;
-            var controller = new Admin(testRecordingHandler, new NullLoggerFactory())
-            {
-                ControllerContext = new ControllerContext()
-                {
-                    HttpContext = httpContext
-                }
-            };
-            await controller.AddSanitizer();
-            var registeredSanitizer = testRecordingHandler.Sanitizers[0];
-            Assert.NotNull(registeredSanitizer);
-            #endregion
-
-            session.Session.Sanitize(registeredSanitizer);
-            var newBody = Encoding.UTF8.GetString(session.Session.Entries[0].Response.Body);
-            System.Console.WriteLine(newBody);
-            Assert.Contains("REDACTED", newBody);
-        }
 
         [Fact]
         public void TestSetRecordingOptionsValidTlsCert()

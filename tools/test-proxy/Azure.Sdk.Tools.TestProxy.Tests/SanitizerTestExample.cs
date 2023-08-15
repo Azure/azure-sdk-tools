785597eb1e51e508009b1b33041d97a2f80e76c6
samvaity <savaity@microsoft.com>
2023-08-15 11:20:09 -0700

    add cert test

--- tools/test-proxy/Azure.Sdk.Tools.TestProxy.Tests/SanitizerTestExample.cs
+++ tools/test-proxy/Azure.Sdk.Tools.TestProxy.Tests/SanitizerTestExample.cs
@@ -63,6 +63,17 @@ namespace Azure.Sdk.Tools.TestProxy.Tests
             var newBody = Encoding.UTF8.GetString(session.Session.Entries[0].Response.Body);
             System.Console.WriteLine(newBody);
             Assert.Contains("REDACTED", newBody);
+        }
+
+        [Fact]
+        public void TestSetRecordingOptionsValidTlsCert()
+        {
+            var certValue = "-----BEGIN CERTIFICATE-----\nMIIBgTCCASegAwIBAgIRAP8o8bVU8taW6SIlq68ooFAwCgYIKoZIzj0EAwIwFjEU\nMBIGA1UEAwwLQ0NGIE5ldHdvcmswHhcNMjMwNzE5MTQzNTM4WhcNMjMxMDE3MTQz\nNTM3WjAWMRQwEgYDVQQDDAtDQ0YgTmV0d29yazBZMBMGByqGSM49AgEGCCqGSM49\nAwEHA0IABD4ujJba2GkR0bAD+AS+dbUBenPAC6iqXJbM2q+JJWCN1O/GdUfmVZag\nan5OQxn417cKp4dGiExyVpEdeg0/LyKjVjBUMBIGA1UdEwEB/wQIMAYBAf8CAQAw\nHQYDVR0OBBYEFBHQ0lGEDifiYVaYfZkjOCLf2maTMB8GA1UdIwQYMBaAFBHQ0lGE\nDifiYVaYfZkjOCLf2maTMAoGCCqGSM49BAMCA0gAMEUCIDyeyrpYZLGrklG9Z1jy\naKX0U/P5CBmL2jE+1boYEFeyAiEA/hPrtNfhdYX9JrVz8MDWzlojkCClSGwbjn1H\nZMW/wNY=\n-----END CERTIFICATE-----";
+            var inputObj = string.Format("{{\"Transport\": {{\"TLSValidationCert\": \"{0}\"}}}}", certValue);
+            var testRecordingHandler = new RecordingHandler(Path.Join(Path.GetTempPath(), Guid.NewGuid().ToString()));
+            var inputBody = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, object>>(inputObj, SerializerOptions);
+
+            testRecordingHandler.SetRecordingOptions(inputBody, null);
         }    
     }
 }

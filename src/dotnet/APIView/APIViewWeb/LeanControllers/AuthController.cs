267ca57531f262d2a05c8e0a80b1294c06ca7b8d
Chidozie Ononiwu <chononiw@microsoft.com>
2023-10-12 13:38:30 -0700

    Serve APIView Index Page Via SPA UI
    Good Rebase
    
    Add client SPA
    
    APIView V2 index page
    
    Sort by lastUpdated on, stop scrolling when end is reached
    
    Geting Revisions list working fine
    
    Fix dynamic height calculation for revisions table
    
    Fix APIRevisions filters
    
    Soft Delete of APIRevisions working
    
    Implement Review Creation
    
    File Upload Implemented
    
    APIView V@ Index Page Working
    
    APIViewV2 Index PAge Working
    
    Add Option in Profile for Beta UI
    
    Enable SPA deployment
    
    Update Assets Path
    
    Remove Console Logs
    
    Add Filter for Showing Reviews Aprroved for First Release
    
    Update First Release apporval indicator
    
    Update ARIRevision Filters
    
    Improvements to Requested Review Page

---
+++ src/dotnet/APIView/APIViewWeb/LeanControllers/AuthController.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using APIViewWeb.Helpers;
+using APIViewWeb.Managers;
+using APIViewWeb.Managers.Interfaces;
+using APIViewWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Serialization.HybridRow;
+using Microsoft.Extensions.Logging;
+
+namespace APIViewWeb.LeanControllers
+{
+    public class AuthController : BaseApiController
+    {
+        private readonly ILogger<AuthController> _logger;
+        private readonly IUserProfileManager _userProfileManager;
+
+        public AuthController(ILogger<AuthController> logger, IUserProfileManager userProfileManager)
+        {
+            _logger = logger;
+            _userProfileManager = userProfileManager;
+        }
+
+        [HttpGet]
+        public ActionResult IsLogged()
+        {
+            var result = new
+            {
+                IsLoggedIn = true
+            };
+            return new LeanJsonResult(result, StatusCodes.Status200OK);
+        }
+
+        [HttpGet]
+        [Route("AppVersion")]
+        public ActionResult AppVersion()
+        {
+            var result = new
+            {
+                Hash = Startup.VersionHash
+            };
+            return new LeanJsonResult(result, StatusCodes.Status200OK);
+        }
+
+        [HttpGet]
+        [Route("profile")]
+        public async Task<ActionResult<UserProfileModel>> GetUserPreference()
+        {
+            return await _userProfileManager.TryGetUserProfileAsync(User);
+        }
+    }
+}

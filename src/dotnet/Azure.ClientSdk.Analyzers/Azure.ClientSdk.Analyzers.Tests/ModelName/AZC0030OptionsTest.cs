fbc61edec3341513c999b1826180024c125facb9
Mingzhe Huang (from Dev Box) <mingzhehuang@microsoft.com>
2023-11-07 15:36:57 +0800

    fix(.net analyzer): ignore property bag with `Options` suffix
    - change `GenerlSuffixAnalyzer` to ignore model classes with `Options` suffix if there is no serialization
    - add test cases
    
    fix #7247

---
+++ src/dotnet/Azure.ClientSdk.Analyzers/Azure.ClientSdk.Analyzers.Tests/ModelName/AZC0030OptionsTest.cs
@@ -0,0 +1,93 @@
+using System.Threading.Tasks;
+using Xunit;
+
+using VerifyCS = Azure.ClientSdk.Analyzers.Tests.AzureAnalyzerVerifier<
+    Azure.ClientSdk.Analyzers.ModelName.GeneralSuffixAnalyzer>;
+
+// Test cases for `Options` suffix. We allow `Options` suffix for property bags.
+// So need to skip property bags which do not have any serialization codes.
+namespace Azure.ClientSdk.Analyzers.Tests.ModelName
+{
+    public class AZC0030OptionsSuffixTests
+    {
+        private const string diagnosticId = "AZC0030";
+
+        [Fact]
+        public async Task WithoutAnySerialization()
+        {
+            var test = @"using System.Text.Json;
+namespace Azure.ResourceManager.Models;
+
+public class DiskOptions
+{
+}";
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [Fact]
+        public async Task WithDeserializationMethod()
+        {
+            var test = @"using System.Text.Json;
+namespace Azure.ResourceManager
+{
+    public class ResponseOptions
+    {
+        public static ResponseOptions DeserializeResponseOptions(JsonElement element)
+        {
+            return null;
+        }
+    }
+}";
+            var expected = VerifyCS.Diagnostic(diagnosticId).WithSpan(4, 18, 4, 33).WithArguments("ResponseOptions", "Options", "'ResponseConfig'");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [Fact]
+        public async Task WithSerializationMethod()
+        {
+            var test = @"using System.Text.Json;
+namespace Azure.ResourceManager.Models
+{
+    internal interface IUtf8JsonSerializable
+    {
+        void Write(Utf8JsonWriter writer);
+    };
+
+    public class DiskOptions: IUtf8JsonSerializable
+    {
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) {}
+    }
+}";
+            var expected = VerifyCS.Diagnostic(diagnosticId).WithSpan(9, 18, 9, 29).WithArguments("DiskOptions", "Options", "'DiskConfig'");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [Fact]
+        public async Task WithPublicSerialization()
+        {
+            var test = @"using System.Text.Json;
+namespace Azure.ResourceManager.Models
+{
+    public class ModelReaderWriterOptions
+    {
+        public string Foo;
+    }
+
+    public interface IJsonModel<out T>
+    {
+        void Write(Utf8JsonWriter writer, ModelReaderWriterOptions options);
+    };
+
+    namespace SubTest
+    {
+        public class DiskOptions: IJsonModel<DiskOptions>
+        {
+            void IJsonModel<DiskOptions>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options) {}
+        }
+    }
+}";
+            var expected = VerifyCS.Diagnostic(diagnosticId).WithSpan(16, 22, 16, 33).WithArguments("DiskOptions", "Options", "'DiskConfig'");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+    }
+}

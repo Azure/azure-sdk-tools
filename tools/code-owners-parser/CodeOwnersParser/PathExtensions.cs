f8a0e69a808aced08ae2d79e107f606ff276f9f3
James Suplizio <jasupliz@microsoft.com>
2023-11-16 10:24:26 -0800

    Remove CodeOwnersParser (#7307)

--- tools/code-owners-parser/CodeOwnersParser/PathExtensions.cs
+++
@@ -1,7 +0,0 @@
-namespace Azure.Sdk.Tools.CodeOwnersParser;
-
-public static class PathExtensions
-{
-    public static bool IsGlobFilePath(this string path)
-        => GlobFilePath.IsGlobFilePath(path);
-}

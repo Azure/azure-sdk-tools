1125cdbead7874f31c2055cc07a226d9a81dca38
Jeremy Meng <yumeng@microsoft.com>
2023-11-22 14:36:04 -0800

    Refactoring
    Extract code to save stream to disk and run processors into protected methods in
    LanguageProcessor so they can be called in derived types.

--- src/dotnet/APIView/APIViewWeb/Languages/JavaScriptLanguageService.cs
+++ src/dotnet/APIView/APIViewWeb/Languages/JavaScriptLanguageService.cs
@@ -16,23 +16,11 @@ namespace APIViewWeb
         public override string Name { get; } = "JavaScript";
         public override string[] Extensions { get; } = { ".api.json", ".zip" };
         public override string ProcessName { get; } = "node";
-        public override string VersionString { get; } = "1.0.7";
+        public override string VersionString { get; } = "1.0.8";
 
         public override async Task<CodeFile> GetCodeFileAsync(string originalName, Stream stream, bool runAnalysis)
         {
-            var tempPath = Path.GetTempPath();
-            var randomSegment = Guid.NewGuid().ToString("N");
-            var tempDirectory = Path.Combine(tempPath, "ApiView", randomSegment);
-            Directory.CreateDirectory(tempDirectory);
-            var originalFilePath = Path.Combine(tempDirectory, originalName);
-
-            var jsonFilePath = Path.ChangeExtension(originalFilePath, ".json");
-
-            using (var file = File.Create(originalFilePath))
-            {
-                await stream.CopyToAsync(file);
-            }
-
+            var (tempDirectory, originalFilePath, jsonFilePath) = await PrepareTemporaryDirectory(originalName, stream);
             if (originalName.EndsWith(".zip"))
             {
                 ZipFile.ExtractToDirectory(originalFilePath, tempDirectory);
@@ -40,33 +28,7 @@ namespace APIViewWeb
 
             try
             {
-                var arguments = GetProcessorArguments(originalName, tempDirectory, jsonFilePath);
-                var processStartInfo = new ProcessStartInfo(ProcessName, arguments);
-                processStartInfo.WorkingDirectory = tempDirectory;
-                processStartInfo.RedirectStandardError = true;
-                processStartInfo.RedirectStandardOutput = true;
-
-                using (var process = Process.Start(processStartInfo))
-                {
-                    process.WaitForExit();
-                    if (process.ExitCode != 0)
-                    {
-                        throw new InvalidOperationException(
-                            "Processor failed: " + Environment.NewLine +
-                            "stdout: " + Environment.NewLine +
-                            process.StandardOutput.ReadToEnd() + Environment.NewLine +
-                            "stderr: " + Environment.NewLine +
-                            process.StandardError.ReadToEnd() + Environment.NewLine);
-                    }
-                }
-
-                using (var codeFileStream = File.OpenRead(jsonFilePath))
-                {
-                    var codeFile = await CodeFile.DeserializeAsync(codeFileStream);
-                    codeFile.VersionString = VersionString;
-                    codeFile.Language = Name;
-                    return codeFile;
-                }
+                return await ExecuteProcessorAsync(originalName, tempDirectory, jsonFilePath);
             }
             finally
             {
@@ -81,12 +43,12 @@ namespace APIViewWeb
       "export.js");
 
             var apiJsonFiles = Directory.EnumerateFiles(tempDirectory, "*.api.json");
-            if (apiJsonFiles.Count() > 1)
+            if (originalName.EndsWith(".zip"))
             {
                 return $"{jsPath} \"{tempDirectory}\" \"{jsonFilePath}\"";
             } else
             {
-                return $"{jsPath} \"{apiJsonFiles.Single()}\" \"{jsonFilePath}\"";
+                return $"{jsPath} \"{originalName}\" \"{jsonFilePath}\"";
             }
 
 

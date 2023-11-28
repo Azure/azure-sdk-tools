c57379788eb7d36ceee96baf54c189f4cdf5d722
Jeremy Meng <yumeng@microsoft.com>
2023-11-28 21:49:06 +0000

    Add list of items for each export to the main view
    This allows diffing of the exported API surface.
    
    - Add functions to navigation list
    
    - Format

--- tools/apiview/parsers/js-api-parser/export.ts
+++ tools/apiview/parsers/js-api-parser/export.ts
@@ -4,16 +4,20 @@ import {
     ApiItemKind,
     ApiDeclaredItem,
     ExcerptTokenKind,
-    ReleaseTag
-} from '@microsoft/api-extractor-model';
+    ReleaseTag,
+} from "@microsoft/api-extractor-model";
 
 import { stat, readdir, writeFile } from "fs/promises";
 
-import { IApiViewFile, IApiViewNavItem } from './models';
-import { TokensBuilder } from './tokensBuilder';
-import path = require('path');
+import { IApiViewFile, IApiViewNavItem } from "./models";
+import { TokensBuilder } from "./tokensBuilder";
+import path = require("path");
 
-function appendMembers(builder: TokensBuilder, navigation: IApiViewNavItem[], item: ApiItem) {
+function appendMembers(
+    builder: TokensBuilder,
+    navigation: IApiViewNavItem[],
+    item: ApiItem
+) {
     builder.lineId(item.canonicalReference.toString());
     builder.indent();
     const releaseTag = getReleaseTag(item);
@@ -27,14 +31,24 @@ function appendMembers(builder: TokensBuilder, navigation: IApiViewNavItem[], it
 
     if (item instanceof ApiDeclaredItem) {
         if (item.kind === ApiItemKind.Namespace) {
-            builder.splitAppend(`declare namespace ${item.displayName} `, item.canonicalReference.toString(), item.displayName);
+            builder.splitAppend(
+                `declare namespace ${item.displayName} `,
+                item.canonicalReference.toString(),
+                item.displayName
+            );
         }
         for (const token of item.excerptTokens) {
             if (token.kind === ExcerptTokenKind.Reference) {
-                builder.typeReference(token.canonicalReference.toString(), token.text);
-            }
-            else {
-                builder.splitAppend(token.text, item.canonicalReference.toString(), item.displayName);
+                builder.typeReference(
+                    token.canonicalReference.toString(),
+                    token.text
+                );
+            } else {
+                builder.splitAppend(
+                    token.text,
+                    item.canonicalReference.toString(),
+                    item.displayName
+                );
             }
         }
     }
@@ -48,10 +62,13 @@ function appendMembers(builder: TokensBuilder, navigation: IApiViewNavItem[], it
         case ApiItemKind.Namespace:
         case ApiItemKind.Enum:
             typeKind = item.kind.toLowerCase();
-            break
+            break;
+        case ApiItemKind.Function:
+            typeKind = "method";
+            break;
         case ApiItemKind.TypeAlias:
             typeKind = "struct";
-            break
+            break;
     }
 
     if (typeKind) {
@@ -59,47 +76,38 @@ function appendMembers(builder: TokensBuilder, navigation: IApiViewNavItem[], it
             Text: item.displayName,
             NavigationId: item.canonicalReference.toString(),
             Tags: {
-                "TypeKind": typeKind
+                TypeKind: typeKind,
             },
-            ChildItems: []
+            ChildItems: [],
         };
         navigation.push(navigationItem);
     }
 
-    if (item.kind === ApiItemKind.Interface ||
+    if (
+        item.kind === ApiItemKind.Interface ||
         item.kind === ApiItemKind.Class ||
         item.kind === ApiItemKind.Namespace ||
-        item.kind === ApiItemKind.Enum) {
+        item.kind === ApiItemKind.Enum
+    ) {
         if (item.members.length > 0) {
-            builder
-                .punct("{")
-                .newline()
-                .incIndent()
+            builder.punct("{").newline().incIndent();
 
             for (const member of item.members) {
                 appendMembers(builder, navigationItem.ChildItems, member);
             }
 
-            builder
-                .decIndent()
-                .indent()
-                .punct("}")
-                .newline();
+            builder.decIndent().indent().punct("}").newline();
+        } else {
+            builder.punct("{").space().punct("}").newline();
         }
-        else {
-            builder
-                .punct("{")
-                .space()
-                .punct("}")
-                .newline();
-        }
-    }
-    else {
+    } else {
         builder.newline();
     }
 }
 
-function getReleaseTag(item: ApiItem & { releaseTag?: ReleaseTag }): "alpha" | "beta" | undefined {
+function getReleaseTag(
+    item: ApiItem & { releaseTag?: ReleaseTag }
+): "alpha" | "beta" | undefined {
     switch (item.releaseTag) {
         case ReleaseTag.Beta:
             return "beta";
@@ -111,19 +119,28 @@ function getReleaseTag(item: ApiItem & { releaseTag?: ReleaseTag }): "alpha" | "
 }
 
 function noOp() {}
-const logger = process.env.DEBUG?.startsWith("export") ?
-    {
-        log: console.log,
-        error: console.error,
-        warn: console.warn,
-        info: console.info,
-    } : {
-        log: noOp,
-        error: noOp,
-        warn: noOp,
-        info: noOp,
-    }
+const logger = process.env.DEBUG?.startsWith("export")
+    ? {
+          log: console.log,
+          error: console.error,
+          warn: console.warn,
+          info: console.info,
+      }
+    : {
+          log: noOp,
+          error: noOp,
+          warn: noOp,
+          info: noOp,
+      };
 
+function kindToKeyword(kind: ApiItemKind): string {
+    switch (kind) {
+        case ApiItemKind.TypeAlias:
+            return "type";
+        default:
+            return kind.toLowerCase();
+    }
+}
 
 async function main() {
     const inputPath = process.argv[2];
@@ -145,18 +162,17 @@ async function main() {
             const matches = file.match(pattern);
             let suffix = "<default>";
             if (matches) {
-                suffix = matches.groups["suffix"]
+                suffix = matches.groups["suffix"];
             }
             const navItem: IApiViewNavItem = {
                 Text: suffix,
                 NavigationId: file,
-                Tags: {},
+                Tags: {
+                    TypeKind: "namespace",
+                },
                 ChildItems: [],
             };
             navigation.push(navItem);
-            builder
-                .newline()
-                .incIndent();
 
             logger.info(`Processing ${file}`);
             const apiModel = new ApiModel();
@@ -170,32 +186,75 @@ async function main() {
                 }
             }
 
-            builder
-                .decIndent()
-                .newline();
-
             if (!name) {
                 name = apiModel.packages[0].name;
                 packageName = apiModel.packages[0].name;
             }
         }
+        // append children of each subpath export to enable api comparison
+        for (const file of files) {
+            if (!file.endsWith(".api.json")) {
+                continue;
+            }
+            const matches = file.match(pattern);
+            let suffix = "<default>";
+            if (matches) {
+                suffix = matches.groups["suffix"];
+            }
+            builder.splitAppend(
+                `export "${suffix}" `,
+                `${name}!export:${suffix}`,
+                file
+            );
+            builder.indent();
+            builder.punct("{").newline().incIndent();
+            const apiModel = new ApiModel();
+            apiModel.loadPackage(path.join(inputPath, file));
+            for (const modelPackage of apiModel.packages) {
+                for (const entryPoint of modelPackage.entryPoints) {
+                    for (const member of entryPoint.members) {
+                        builder.punct(" ");
+                        builder.punct(" ");
+                        builder.punct(" ");
+                        builder.punct(" ");
+                        builder.punct(" ");
+                        builder.keyword(kindToKeyword(member.kind));
+                        builder.punct(" ");
+                        builder.typeReference(
+                            member.canonicalReference.toString(),
+                            member.displayName
+                        );
+                        // builder.splitAppend(
+                        //     `export ${member.kind} ${member.displayName}`,
+                        //     "typeid",
+                        //     "typename"
+                        // );
+                        builder.newline();
+                    }
+                }
+            }
+            builder.decIndent().indent().punct("}").newline();
+        }
+
         const apiViewFile: IApiViewFile = {
             Name: name,
             Navigation: navigation,
             Tokens: builder.tokens,
             PackageName: packageName,
-            VersionString: "1.0.6",
+            VersionString: "1.0.8",
             Language: "JavaScript",
             PackageVersion: "",
-        }
+        };
         await writeFile(process.argv[3], JSON.stringify(apiViewFile));
-
     } else {
         const apiModel = new ApiModel();
         const fileName = process.argv[2];
         var PackageversionString = "";
         if (fileName.includes("_")) {
-            PackageversionString = fileName.split("_").pop().replace(".api.json", "");
+            PackageversionString = fileName
+                .split("_")
+                .pop()
+                .replace(".api.json", "");
         }
         apiModel.loadPackage(fileName);
 
@@ -218,22 +277,24 @@ async function main() {
             PackageName: apiModel.packages[0].name,
             VersionString: "1.0.8",
             Language: "JavaScript",
-            PackageVersion: PackageversionString
-        }
+            PackageVersion: PackageversionString,
+        };
 
-        writeFile(
+        await writeFile(
             process.argv[3],
-            JSON.stringify(apiViewFile),
+            JSON.stringify(apiViewFile)
             // err => {
             //     if (err) {
             //         console.error(err);
             //         return;
             //     };
             // }
-        )
+        );
     }
 }
 
-main().then(() => {
-    console.log("Completed");
-}).catch(console.error);
+main()
+    .then(() => {
+        console.log("Completed");
+    })
+    .catch(console.error);

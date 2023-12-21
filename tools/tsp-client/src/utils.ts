5ba1b956b6bbdd2f920b9821126cb8d7860fcaa0
Catalina Peralta <catalinaperaltah@hotmail.com>
2023-12-21 09:42:38 -0800

    fix type in formatAdditionalDirectories()

--- tools/tsp-client/src/utils.ts
+++ tools/tsp-client/src/utils.ts
@@ -1,4 +1,4 @@
-export function formatAdditionalDirectories(additionalDirectories?: string): string {
+export function formatAdditionalDirectories(additionalDirectories?: string[]): string {
     let additionalDirOutput = "";
     for (const dir of additionalDirectories ?? []) {
         additionalDirOutput += `\n- ${dir}`;

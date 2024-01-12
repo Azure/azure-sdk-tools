f670af735084cc77273f78f95e850b0d789c5f8b
Catalina Peralta <catalinaperaltah@hotmail.com>
2024-01-12 15:28:57 -0800

    remove unused fetch function

--- tools/tsp-client/src/network.ts
+++ tools/tsp-client/src/network.ts
@@ -1,15 +1,4 @@
-import { createDefaultHttpClient, createPipelineRequest } from "@azure/core-rest-pipeline";
 import { stat } from "fs/promises";
-
-const httpClient = createDefaultHttpClient();
-
-export async function fetch(url: string, method: "GET" | "HEAD" = "GET"): Promise<string> {
-  const result = await httpClient.sendRequest(createPipelineRequest({ url, method }));
-  if (result.status !== 200) {
-    throw new Error(`failed to fetch ${url}: ${result.status}`);
-  }
-  return String(result.bodyAsText);
-}
 
 export function isValidUrl(url: string) {
   try {

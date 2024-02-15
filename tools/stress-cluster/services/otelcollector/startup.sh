dc32cdbd733a14ab268e3bf7fe17dc488efa2ee5
Richard Park <ripark@microsoft.com>
2024-02-14 18:03:09 -0800

    Remove cat, not needed for production.

--- tools/stress-cluster/services/otelcollector/startup.sh
+++ tools/stress-cluster/services/otelcollector/startup.sh
@@ -4,7 +4,4 @@ set -ex
 
 source $ENV_FILE
 echo "    connection_string: \"$APPLICATIONINSIGHTS_CONNECTION_STRING\"" >> /otel-collector-config.yml
-
-cat /otel-collector-config.yml
-
 /otelcol-contrib --config otel-collector-config.yml $@

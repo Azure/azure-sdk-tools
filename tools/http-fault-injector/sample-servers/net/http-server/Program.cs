aefc8c65426211adaabf435f5a2cf2df492ec2ff
Mike Harder (from Dev Box) <mharder@microsoft.com>
2024-02-15 16:43:58 -0800

    Add sample server

---
+++ tools/http-fault-injector/sample-servers/net/http-server/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace Azure.Sdk.Tools.HttpFaultInjector.HttpServerSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Any, 5000);
+                    options.Listen(IPAddress.Any, 5001, listenOptions =>
+                    {
+                        listenOptions.UseHttps("testCert.pfx", "testPassword");
+                    });
+                })
+                .Configure(app => app.Run(context =>
+                {
+                    return context.Response.WriteAsync("Hello World!");
+                }))
+                .Build()
+                .Run();
+        }
+    }
+}

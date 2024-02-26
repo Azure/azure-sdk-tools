7d9603e4f803ea448f33b37d0f7fb1b5e6948c64
James Suplizio <jasupliz@microsoft.com>
2024-02-26 09:36:54 -0800

    Add new Scheduled rule: EnforceMaxLifeOfIssues (#7693)
    * Add new Scheduled rule: EnforceMax2YearLifeOfIssues
    
    * Fix a minor error in the metadata file. AzureSdkOwners must be part of a block that contains a ServiceLabel entry which was missing from the example.
    
    * Revert metadata.md file changes in this PR to update in a differnt PR
    
    * Change rule name to EnforceMaxLifeOfIssues
    
    * Update the scheduled-event-processor.yml sample yml's event name to remove the 2
    
    * Update sample yml, RULES.md and Trigger EnforceMaxLifeOfIssues comment.
    
    * scheduled if statement needs to match the actual cron

--- tools/github-event-processor/Azure.Sdk.Tools.GitHubEventProcessor.Tests/Static/ScheduledEventProcessingTests.cs
+++ tools/github-event-processor/Azure.Sdk.Tools.GitHubEventProcessor.Tests/Static/ScheduledEventProcessingTests.cs
@@ -275,5 +275,53 @@ namespace Azure.Sdk.Tools.GitHubEventProcessor.Tests.Static
                 Assert.AreEqual(0, totalUpdates, $"{rule} is {ruleState} and should not have produced any updates.");
             }
         }
+
+        /// <summary>
+        /// Test the EnforceMaxLifeOfIssues scheduled event.
+        /// Each item returned from the query will have three updates:
+        ///     Issue will be closed
+        ///     Issue will have a comment added
+        ///     Issue will be locked
+        /// </summary>
+        /// <param name="rule">String, RulesConstants for the rule being tested</param>
+        /// <param name="payloadFile">JSon payload file for the event being tested</param>
+        /// <param name="ruleState">Whether or not the rule is on/off</param>
+        [Category("static")]
+        [TestCase(RulesConstants.EnforceMaxLifeOfIssues, "Tests.JsonEventPayloads/ScheduledEvent_payload.json", RuleState.On)]
+        [TestCase(RulesConstants.EnforceMaxLifeOfIssues, "Tests.JsonEventPayloads/ScheduledEvent_payload.json", RuleState.Off)]
+        public async Task TestEnforceMaxLifeOfIssues(string rule, string payloadFile, RuleState ruleState)
+        {
+            // Need something divisible by 3 Because EnforceMaxLifeOfIssues does 3 updates per issue
+            // creating 100 results should only result in 34 issues being closed and 34 comments created
+            // and 34 issues being locked = 102 expected updates.
+            int expectedUpdates = 102;
+            var mockGitHubEventClient = new MockGitHubEventClient(OrgConstants.ProductHeaderName);
+            mockGitHubEventClient.RulesConfiguration.Rules[rule] = ruleState;
+            var rawJson = TestHelpers.GetTestEventPayload(payloadFile);
+            ScheduledEventGitHubPayload scheduledEventPayload = SimpleJsonSerializer.Deserialize<ScheduledEventGitHubPayload>(rawJson);
+            // Create the fake issues to update. 
+            mockGitHubEventClient.CreateSearchIssuesResult(expectedUpdates, scheduledEventPayload.Repository, ItemState.Open);
+            await ScheduledEventProcessing.EnforceMaxLifeOfIssues(mockGitHubEventClient, scheduledEventPayload);
+
+            var totalUpdates = await mockGitHubEventClient.ProcessPendingUpdates(scheduledEventPayload.Repository.Id);
+            // Verify the RuleCheck 
+            Assert.AreEqual(ruleState == RuleState.On, mockGitHubEventClient.RulesConfiguration.RuleEnabled(rule), $"Rule '{rule}' enabled should have been {ruleState == RuleState.On} but RuleEnabled returned {ruleState != RuleState.On}.'");
+            if (RuleState.On == ruleState)
+            {
+                // Create the fake issues to update. 
+                Assert.AreEqual(expectedUpdates, totalUpdates, $"The number of updates should have been {expectedUpdates} but was instead, {totalUpdates}");
+                // There should be expectedUpdates/3 issueUpdates, expectedUpdates/3 comments and expectedUpdates/3 issues to lock
+                int numIssueUpdates = mockGitHubEventClient.GetGitHubIssuesToUpdate().Count;
+                Assert.AreEqual(expectedUpdates / 3, numIssueUpdates, $"The number of issue updates should have been {expectedUpdates / 3} but was instead, {numIssueUpdates}");
+                int numComments = mockGitHubEventClient.GetComments().Count;
+                Assert.AreEqual(expectedUpdates / 3, numComments, $"The number of comments should have been {expectedUpdates / 3} but was instead, {numComments}");
+                int numIssuesToLock = mockGitHubEventClient.GetGitHubIssuesToLock().Count;
+                Assert.AreEqual(expectedUpdates / 3, numIssuesToLock, $"The number of issues to lock should have been {expectedUpdates / 3} but was instead, {numIssuesToLock}");
+            }
+            else
+            {
+                Assert.AreEqual(0, totalUpdates, $"{rule} is {ruleState} and should not have produced any updates.");
+            }
+        }
     }
 }

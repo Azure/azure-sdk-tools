b5299139ea71e77eca829553a034f9e15b042985
praveenkuttappan <praveenkuttappan@gmail.com>
2024-03-06 10:40:37 -0500

    Update label and add new param for package version

--- src/dotnet/APIView/APIViewWeb/Managers/APIRevisionsManager.cs
+++ src/dotnet/APIView/APIViewWeb/Managers/APIRevisionsManager.cs
@@ -829,11 +829,12 @@ namespace APIViewWeb.Managers
             return result;
         }
 
-        public async Task<APIRevisionListItemModel> UpdateRevisionPackageVersionAsync(APIRevisionListItemModel revision, string packageVersion)
+        public async Task<APIRevisionListItemModel> UpdateRevisionMetadataAsync(APIRevisionListItemModel revision, string packageVersion, string label)
         {
             if (packageVersion != null && !packageVersion.Equals(revision.Files[0].PackageVersion))
             {
                 revision.Files[0].PackageVersion = packageVersion;
+                revision.Label = label;
                 await _apiRevisionsRepository.UpsertAPIRevisionAsync(revision);
             }
             return revision;

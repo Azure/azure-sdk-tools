49966e50a9ed5d5b14442c20b58e522d4e4300e3
Richard Park <51494936+richardpark-msft@users.noreply.github.com>
2024-03-06 13:57:59 -0800

    Also remove that line that cats in the appinsights connection string, since we can just use normal environment injection.

--- tools/stress-cluster/services/otelcollector/startup.sh
+++ tools/stress-cluster/services/otelcollector/startup.sh
@@ -3,5 +3,4 @@
 set -ex
 
 source $ENV_FILE
-echo "    connection_string: \"$APPLICATIONINSIGHTS_CONNECTION_STRING\"" >> /otel-collector-config.yml
 /otelcol-contrib --config otel-collector-config.yml $@

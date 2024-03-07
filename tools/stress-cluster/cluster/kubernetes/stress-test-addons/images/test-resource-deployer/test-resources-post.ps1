bb946e15fec5c65878f6682b66947e8d41964c93
ripark <ripark@microsoft.com>
2024-03-07 02:07:45 +0000

    Bring back the trailing newline, little refactoring.

--- tools/stress-cluster/cluster/kubernetes/stress-test-addons/images/test-resource-deployer/test-resources-post.ps1
+++ tools/stress-cluster/cluster/kubernetes/stress-test-addons/images/test-resource-deployer/test-resources-post.ps1
@@ -30,28 +30,38 @@ foreach ($entry in $DeploymentOutputs.GetEnumerator()) {
     }
 }
 
+$envLines += ""
 $environmentText = $envLines -join "`n"
+
+Write-Output "Writing $outputFile"
 
 $bytes = [System.Text.Encoding]::UTF8.GetBytes($environmentText)
 Set-Content $outputFile -Value $bytes -AsByteStream -Force
 
-function Write-Sourceable-BashFile([string]$sourceableBashFilePath) {
+function Write-Sourceable-BashFile([string]$sourceableBashFilePath, [string[]]$envLines) {
     Write-Output "Writing $sourceableBashFilePath"
+
+    $lines = @(
+        "# This file is intended to be sourced by a bash script",
+        "# Example:",
+        "#   . `$ENV_FILE + '.sh'"
+    )
 
     # the .sh file equivalent with all variables exported and properly quoted so it
     # can be sourced in a bash script.    
-    $sourceableBashFileText = "# This file is intended to be sourced by a bash script`n" + 
-    "# Example: . $ENV_FILE + '.sh'`n"
+    $sourceableBashFileText = 
 
     foreach ($envLine in $envLines) {
         Write-Output "Processing $envLine"
         # grab any lines that don't start with the comment char, and squeeze out the spaces between the key and value.
         # ex: "EVENTHUB_CONNECTION_STRING=<some connection string with embedded ='s, etc..>"
         if ($envLine -match "^([^#].+?)\s*=\s*(.+?)$") {        
-            $sourceableBashFileText += [string]::Format("export {0}='{1}'`n", $Matches[1], $Matches[2])
+            $lines += [string]::Format("export {0}='{1}'", $Matches[1], $Matches[2])
         }
     }
 
+    $lines += ""
+    $sourceableBashFileText = $lines -join "`n"
     $bytes = [System.Text.Encoding]::UTF8.GetBytes($sourceableBashFileText)
     Set-Content $sourceableBashFilePath -Value $bytes -AsByteStream -Force
 }

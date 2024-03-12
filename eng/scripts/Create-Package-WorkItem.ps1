ae2d823d31512a41bf28e2b405f4be9150f02d95
Praveen Kuttappan <prmarott@microsoft.com>
2024-03-12 17:19:54 -0400

    Changes to sparse checkout language repo

--- eng/scripts/Create-Package-WorkItem.ps1
+++ eng/scripts/Create-Package-WorkItem.ps1
@@ -9,8 +9,6 @@ param (
   [Parameter(Mandatory = $true)]
   [string]$PackageDisplayName,
   [Parameter(Mandatory = $true)]
-  [string]$PackageType,
-  [Parameter(Mandatory = $true)]
   [string]$PackageVersion,
   [Parameter(Mandatory = $true)]
   [string]$ReleaseDate,
@@ -39,60 +37,56 @@ function Get-Repo-Name($language)
     return ""
 }
 
-function Clone-Repo($repoName, $targetDir)
+function Sparse-Checkout($repoName)
 {
-    git clone "https://github.com/azure/$repoName.git"  $targetDir
-    Write-Host "Cloned repo [$repoName] to [$($targetDir)]"
-    if (-not(Test-Path $targetDir))
-    {
-        Write-Error "Failed to find cloned repo [$($targetDir)] to find pacakge root path from package name. Package root path is required to create or update package work item."
-        exit 1
-    }
+    Write-Host "Sparse checkout repo [$repoName]."
+    git sparse-checkout init
+    git sparse-checkout set "sdk" "eng"
+    git checkout
 }
 
-function Get-Package-Root-Path($packageName, $language)
+
+$repoName = Get-Repo-Name $language
+if (!$repoName)
 {
-    # clone language repo
-    $repoName = Get-Repo-Name $language
-    if (!$repoName)
-    {
-        Write-Error "GitHub repo name is not found for language [$language]. Failed to find package root path."
-        exit 1
-    }
-    $clonedRepoPath = Join-Path $WorkingDir $repoName
-    Clone-Repo $repoName $clonedRepoPath
-    
-    # load common script helpers from cloned language repo
-    . (Join-Path $clonedRepoPath "eng/common/scripts/common.ps1")
-    Write-Host "Language repo root path: [$RepoRoot]"
+    Write-Error "GitHub repo name is not found for language [$language]. Failed to find package root path."
+    exit 1
+}
+
+# clone language repo
+$clonedRepoPath = Join-Path $WorkingDir $repoName
+Write-Host "Cloning repo [$repoName] to [$WorkingDir]."
+git clone --no-checkout --filter=tree:0 "https://github.com/azure/$repoName.git" $clonedRepoPath
+Push-Location $clonedRepoPath
+
+try
+{
+    Sparse-Checkout $repoName
+    . "eng/common/scripts/common.ps1"
+
     # Parse package properties from cloned repo and find the package repo path
-    $packageProperties = Get-PkgProperties -PackageName $PackageName -ServiceDirectory ""
+    $packageProperties = Get-PkgProperties -PackageName $PackageName
     if (!$packageProperties)
     {
-        Write-Error "Could not find a package with name [ $packageName ] in repo."
+        Write-Error "Failed to find package properties for package name [$PackageName]."
         exit 1
     }
 
-    Write-Host "Package Name [ $($packageProperties.Name) ]"
-    return $packageProperties.ServiceDirectory
-}
-
-if ($PackageRootPath -eq "")
-{
-  $PackageRootPath = Get-Package-Root-Path $PackageName $Language
-}
-
-Write-Host "Package Root Path: [$PackageRootPath]"
-# Create or update package work item  
-&$EngCommonScriptsDir/Update-DevOps-Release-WorkItem.ps1 `
+    # Create or update package work item  
+    $EngCommonScriptsDir/Update-DevOps-Release-WorkItem.ps1 `
     -language $Language `
     -packageName $PackageName `
     -version $PackageVersion `
     -plannedDate $ReleaseDate `
-    -packageRepoPath $PackageRootPath `
-    -packageType $PackageType `
+    -packageRepoPath $PkgProperties.ServiceDirectory `
+    -packageType $PkgProperties.SdkType `
+    -packageNewLibrary $PkgProperties.IsNewSdk `
     -serviceName $ServiceName `
     -packageDisplayName $PackageDisplayName `
     -relatedWorkItemId $RelatedWorkItemId `
     -tag $Tag `
     -devops_pat $Devops_pat
+}
+finally {
+    Pop-Location
+}

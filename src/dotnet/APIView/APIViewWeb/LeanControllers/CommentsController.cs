36565b802234543bbc81e940a3743fc1eab957ca
Chidozie Ononiwu <chononiw@microsoft.com>
2024-04-08 11:04:50 -0700

    Add Comments

---
+++ src/dotnet/APIView/APIViewWeb/LeanControllers/CommentsController.cs
@@ -0,0 +1,37 @@
+using APIViewWeb.Extensions;
+using APIViewWeb.Helpers;
+using APIViewWeb.LeanModels;
+using APIViewWeb.Managers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace APIViewWeb.LeanControllers
+{
+    public class CommentsController : BaseApiController
+    {
+        private readonly ILogger<CommentsController> _logger;
+        private readonly ICommentsManager _commentsManager;
+
+        public CommentsController(ILogger<CommentsController> logger, ICommentsManager commentManager)
+        {
+            _logger = logger;
+            _commentsManager = commentManager;
+        }
+
+        /// <summary>
+        /// Retrieve comments for a review.
+        /// </summary>
+        /// <param name="reviewId"></param>
+        /// <returns></returns>
+        [HttpGet(Name = "GetComments")]
+        [Route("{reviewId}")]
+        public async Task<ActionResult<IEnumerable<CommentItemModel>>> GetCommentsAsync(string reviewId)
+        {
+            var comments = await _commentsManager.GetCommentsAsync(reviewId);
+            return new LeanJsonResult(comments, StatusCodes.Status200OK);
+        }
+    }
+}
---
+++ src/dotnet/APIView/ClientSPA/src/app/_components/shared/comment/comment.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CommentComponent } from './comment.component';
+
+describe('CommentComponent', () => {
+  let component: CommentComponent;
+  let fixture: ComponentFixture<CommentComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CommentComponent]
+    });
+    fixture = TestBed.createComponent(CommentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

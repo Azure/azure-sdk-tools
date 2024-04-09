37dc1e2fc89507b3ea1566fcf678ffee83680207
Chidozie Ononiwu <chononiw@microsoft.com>
2024-04-09 14:33:21 -0700

    Comments Partially Implemented

--- src/dotnet/APIView/ClientSPA/src/app/_components/shared/comment-thread/comment-thread.component.ts
+++ src/dotnet/APIView/ClientSPA/src/app/_components/shared/comment-thread/comment-thread.component.ts
@@ -1,5 +1,8 @@
 import { Component, Injector, Input } from '@angular/core';
+import { UserProfile } from 'src/app/_models/auth_service_models';
 import { CommentItemModel } from 'src/app/_models/review';
+import { AuthService } from 'src/app/_services/auth/auth.service';
+import { ConfigService } from 'src/app/_services/config/config.service';
 
 @Component({
   selector: 'app-comment-thread',
@@ -11,5 +14,15 @@ import { CommentItemModel } from 'src/app/_models/review';
 })
 export class CommentThreadComponent {
   @Input() comments: CommentItemModel[] | undefined = []
+  userProfile : UserProfile | undefined;
   commentEditText: string | undefined;
+
+  constructor(private authService: AuthService, private configService: ConfigService) { }
+
+  ngOnInit(): void {
+    this.authService.getUserProfile().subscribe(
+      (userProfile : any) => {
+        this.userProfile = userProfile;
+      });
+  }
 }

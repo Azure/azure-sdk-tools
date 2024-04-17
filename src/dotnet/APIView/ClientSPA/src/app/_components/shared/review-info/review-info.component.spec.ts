35507a43a9d1f311e3a1c54c4184c5e912c21afa
Chidozie Ononiwu <chononiw@microsoft.com>
2024-04-16 21:38:16 -0700

    Compute Diff on Client Side

--- src/dotnet/APIView/ClientSPA/src/app/_components/shared/review-info/review-info.component.spec.ts
+++ src/dotnet/APIView/ClientSPA/src/app/_components/shared/review-info/review-info.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReviewInfoComponent } from './review-info.component';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 
 describe('ReviewInfoComponent', () => {
   let component: ReviewInfoComponent;
@@ -8,7 +10,21 @@ describe('ReviewInfoComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ReviewInfoComponent]
+      declarations: [ReviewInfoComponent],
+      imports: [
+        BreadcrumbModule
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: convertToParamMap({ reviewId: 'test' }),
+              queryParamMap: convertToParamMap({ activeApiRevisionId: 'test', diffApiRevisionId: 'test' })
+            }
+          }
+        }
+      ]
     });
     fixture = TestBed.createComponent(ReviewInfoComponent);
     component = fixture.componentInstance;

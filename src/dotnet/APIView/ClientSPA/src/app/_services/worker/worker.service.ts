0c3965ddd24655fee937882f2c70d15cc75a9480
Chidozie Ononiwu <chononiw@microsoft.com>
2024-04-19 09:57:06 -0700

    Move workers into services

---
+++ src/dotnet/APIView/ClientSPA/src/app/_services/worker/worker.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WorkerService {
+  private apiTreeBuilder : Worker;
+  private tokenBuilder : Worker;
+
+  constructor() {
+    this.apiTreeBuilder = new Worker(new URL('../../_workers/review-page.worker', import.meta.url));
+    this.tokenBuilder = new Worker(new URL('../../_workers/review-page.worker', import.meta.url));
+  }
+
+  postToApiTreeBuilder(message: any) {
+    this.apiTreeBuilder.postMessage(message);
+  }
+
+  postToTokenBuilder(message: any) {
+    this.tokenBuilder.postMessage(message);
+  }
+
+  onMessageFromApiTreeBuilder(): Observable<any> {
+    return new Observable(observer => {
+      this.apiTreeBuilder.onmessage = ({ data }) => {
+        observer.next(data);
+      };
+    });
+  }
+
+  onMessageFromTokenBuilder(): Observable<any> {
+    return new Observable(observer => {
+      this.tokenBuilder.onmessage = ({ data }) => {
+        observer.next(data);
+      };
+    });
+  }
+}

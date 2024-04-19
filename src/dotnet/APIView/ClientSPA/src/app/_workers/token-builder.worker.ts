9d9f62472386521635411d8ed333edfd9e4e715a
Chidozie Ononiwu <chononiw@microsoft.com>
2024-04-19 11:05:42 -0700

    Split workers into separate files

--- src/dotnet/APIView/ClientSPA/src/app/_workers/token-builder.worker.ts
+++ src/dotnet/APIView/ClientSPA/src/app/_workers/token-builder.worker.ts
@@ -5,81 +5,10 @@ import { APITreeNode, StructuredToken } from "../_models/revision";
 import { ComputeTokenDiff } from "../_helpers/worker-helpers";
 
 addEventListener('message', ({ data }) => {
-  if (data.directive === ReviewPageWorkerMessageDirective.BuildAPITree) {
-    let navTreeNodes: any[] = [];
-    let treeNodeId : string[] = [];
-
-    data.apiTree.forEach((apiTreeNode: APITreeNode) => {
-      navTreeNodes.push(buildAPITree(apiTreeNode as APITreeNode, treeNodeId));
-    });
-
-    const createNavigationMessage =  {
-      directive: ReviewPageWorkerMessageDirective.CreatePageNavigation,
-      navTree : navTreeNodes
-    };
-    postMessage(createNavigationMessage);
-  }
-
   if (data.directive === ReviewPageWorkerMessageDirective.BuildTokens) {
     buildTokens(data.apiTreeNode, data.huskNodeId, data.position);
   }
 });
-
-
-function buildAPITree(apiTreeNode: APITreeNode, treeNodeId : string[], indent: number = 0) : any {
-  let idPart = getTokenNodeIdPart(apiTreeNode);
-  treeNodeId.push(idPart);
-  const nodeId = treeNodeId.join("-");
-
-  let nodeData: CodeHuskNode = {
-    name: apiTreeNode.name,
-    id: nodeId,
-    indent: indent,
-    position: "top"
-  };
-
-  let createCodeLineHuskMessage : CreateCodeLineHuskMessage =  {
-    directive: ReviewPageWorkerMessageDirective.CreateCodeLineHusk,
-    nodeData: nodeData,
-  };
-  postMessage(createCodeLineHuskMessage);
-
-  let buildTokensMessage : BuildTokensMessage =  { 
-    directive: ReviewPageWorkerMessageDirective.PassToTokenBuilder,
-    apiTreeNode: apiTreeNode,
-    huskNodeId: nodeId,
-    position: "top"
-  };
-  postMessage(buildTokensMessage);
-
-  let treeNode: any = {
-    label: nodeData.name,
-    data: nodeData,
-    expanded: true,
-    children: []
-  }
-
-  let children : any[] = [];
-  apiTreeNode.children.forEach(child => {
-    const childTreeNodes = buildAPITree(child, treeNodeId, indent + 1);
-    if (child.children.length > 0) {
-      children.push(childTreeNodes);
-    }
-  });
-
-  if (apiTreeNode.bottomTokens.length > 0) {
-    nodeData.position = "bottom";
-    createCodeLineHuskMessage.nodeData = nodeData;
-    postMessage(createCodeLineHuskMessage);
-
-    buildTokensMessage.position = "bottom";
-    postMessage(buildTokensMessage);
-  }
-
-  treeNode.children = children;
-  treeNodeId.pop();
-  return treeNode;
-}
 
 function buildTokens(apiTreeNode: APITreeNode, id: string, position: string) {
   if (apiTreeNode.diffKind === "NoneDiff") {
@@ -88,22 +17,6 @@ function buildTokens(apiTreeNode: APITreeNode, id: string, position: string) {
   else {
     buildTokensForDiffNodes(apiTreeNode, id, position);
   }
-}
-
-function getTokenNodeIdPart(apiTreeNode: APITreeNode) {
-  const kind = apiTreeNode.kind;
-  const subKind = apiTreeNode.properties["SubKind"];
-  const id = apiTreeNode.id;
-
-  let idPart = kind;
-  if (subKind) {
-    idPart = `${idPart}-${subKind}`;
-  }
-
-  if (id) {
-    idPart = `${idPart}-${id}`;
-  }
-  return idPart.toLocaleLowerCase();
 }
 
 function createHashFromTokenLine(tokenLines: StructuredToken[]) {
@@ -143,13 +56,8 @@ function buildTokensForDiffNodes(apiTreeNode: APITreeNode, id: string, position:
       afterTokenLine.push(token);
     }
 
-    console.log("beforeLine - %o", beforeTokenLine)
-    console.log("afterLine - %o", afterTokenLine)
     const diffTokenLineResult = ComputeTokenDiff(beforeTokenLine, afterTokenLine) as [StructuredToken[], StructuredToken[], boolean];
 
-    console.log("diffTokenLineResult0 - %o", diffTokenLineResult[0])
-    console.log("diffTokenLineResult1 - %o", diffTokenLineResult[1])
-    console.log("diffTokenLineResult2 - %o", diffTokenLineResult[2])
     let createLinesOfTokensMessage : CreateLinesOfTokensMessage =  {
       directive: ReviewPageWorkerMessageDirective.CreateLineOfTokens,
       tokenLine : diffTokenLineResult[0],

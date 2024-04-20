5d1e8621b4ad068a1026a25a9b53e8ae1626981e
Chidozie Ononiwu <chononiw@microsoft.com>
2024-04-19 17:18:50 -0700

    Start Implementing Lazy Loading

--- src/dotnet/APIView/ClientSPA/src/app/_components/code-panel/code-panel.component.ts
+++ src/dotnet/APIView/ClientSPA/src/app/_components/code-panel/code-panel.component.ts
@@ -1,8 +1,8 @@
 import { ApplicationRef, ChangeDetectorRef, Component, ElementRef, Injector, Input, ViewChild, ViewContainerRef, } from '@angular/core';
 import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
-import { finalize } from 'rxjs/operators';
+import { debounce, finalize } from 'rxjs/operators';
 import { CommentItemModel } from 'src/app/_models/review';
-import { CodeHuskNode, CreateLinesOfTokensMessage } from 'src/app/_models/revision';
+import { CodeHuskNode, CreateLinesOfTokensMessage, ReviewPageWorkerMessageDirective } from 'src/app/_models/revision';
 import { CommentThreadComponent } from '../shared/comment-thread/comment-thread.component';
 
 @Component({
@@ -20,6 +20,7 @@ export class CodePanelComponent {
 
   lineNumberCount : number = 0;
   isLoading: boolean = true;
+  codeLineFragments : Map<string, any> = new Map<string, any>();
 
   private destroyApiTreeNode$ = new Subject<void>();
   private destroyTokenLineData$ = new Subject<void>();
@@ -48,69 +49,94 @@ export class CodePanelComponent {
     this.tokenLineData.pipe(takeUntil(this.destroyTokenLineData$), finalize(() => {
     })).subscribe(lineData => {
       if (lineData != null) {
-        const node = document.getElementById(`${lineData.nodeId}-${lineData.position}`);
-        const line = document.createElement('div');
-        const lineActions = document.createElement('div');
-        const lineContent = document.createElement('div');
-        const lineNumber = document.createElement('span');
-        const commentButton = document.createElement('span');
-        const commentIcon = document.createElement('i');
-        let commentThreadData : CommentItemModel[] = [];
+        const nodeId = `${lineData.nodeId}-${lineData.position}`;
+        if (lineData.directive === ReviewPageWorkerMessageDirective.CreateLineOfTokens) {
+          const apiTreeNode = document.getElementById(nodeId);
 
-        if (lineData.nodeId && lineData.tokenLine) {
-          for (let token of lineData.tokenLine) {
-            const span = document.createElement('span');
-            token.renderClasses.forEach((c: string) => span.classList.add(c));
-            span.textContent = token.value;
-            if (token.id){
-              const tokenId = token.id;
-              span.setAttribute('data-token-id', tokenId);
+          const line = document.createElement('div');
+          const lineActions = document.createElement('div');
+          const lineContent = document.createElement('div');
+          const lineNumber = document.createElement('span');
+          const commentButton = document.createElement('span');
+          const commentIcon = document.createElement('i');
+          let commentThreadData : CommentItemModel[] = [];
 
-              if (this.reviewComments && this.reviewComments.length > 0)
+          if (lineData.nodeId && lineData.tokenLine) {
+            for (let token of lineData.tokenLine) {
+              const span = document.createElement('span');
+              token.renderClasses.forEach((c: string) => span.classList.add(c));
+              span.textContent = token.value;
+              if (token.id){
+                const tokenId = token.id;
+                span.setAttribute('data-token-id', tokenId);
+
+                if (this.reviewComments && this.reviewComments.length > 0)
+                {
+                  commentThreadData = this.reviewComments.filter(c => c.elementId === tokenId);
+                }
+              }
+
+              if (token.diffKind === "Added" || token.diffKind === "Removed") {
+                span.classList.add(`token-${token.diffKind.toLowerCase()}`);
+              }
+
+              lineContent.appendChild(span);
+            }
+            
+            if (lineData.tokenLine.length > 0)
+            {
+              this.lineNumberCount++;
+              let codeLineFragment : DocumentFragment;
+              if (this.codeLineFragments.has(nodeId)) {
+                codeLineFragment = this.codeLineFragments.get(nodeId)?.documentFragment!;
+              }
+              else {
+                codeLineFragment = document.createDocumentFragment();
+                const nodeFragmentData = { 
+                  documentFragment: codeLineFragment,
+                  lastLineNumber: this.lineNumberCount
+                };
+                this.codeLineFragments.set(nodeId, nodeFragmentData);
+              }
+              
+              lineNumber.textContent = `${this.lineNumberCount}`;
+              lineNumber.classList.add('line-number');
+              line.classList.add('code-line');
+              if (lineData.diffKind === "Added" || lineData.diffKind === "Removed") {
+                line.classList.add(`code-line-${lineData.diffKind.toLowerCase()}`);
+              }
+              lineContent.classList.add('code-line-content');
+              lineContent.style.paddingLeft = `${apiTreeNode!.dataset["indent"] as unknown as number * 20}px`;
+              lineActions.classList.add('line-actions');
+              commentIcon.classList.add('bi', 'bi-plus-square-fill');
+              commentButton.appendChild(commentIcon);
+              commentButton.classList.add('comment-button');
+              if (lineData.position === "top") {
+                commentButton.classList.add('commentable');
+              }
+
+              lineActions.appendChild(lineNumber);
+              lineActions.appendChild(commentButton);
+              line.appendChild(lineActions);
+              line.appendChild(lineContent);
+              codeLineFragment!.appendChild(line);
+
+              if (commentThreadData.length > 0)
               {
-                commentThreadData = this.reviewComments.filter(c => c.elementId === tokenId);
+                const commentThreadNode = this.commentThreadRef.createComponent(CommentThreadComponent);
+                commentThreadNode.instance.comments = commentThreadData;
+                codeLineFragment!.appendChild(commentThreadNode.location.nativeElement);
+                // this.changeDeterctorRef.detectChanges();
               }
             }
-
-            if (token.diffKind === "Added" || token.diffKind === "Removed") {
-              span.classList.add(`token-${token.diffKind.toLowerCase()}`);
-            }
-
-            lineContent.appendChild(span);
           }
-          
-          if (lineData.tokenLine.length > 0)
-          {
-            this.lineNumberCount++;
-            lineNumber.textContent = `${this.lineNumberCount}`;
-            lineNumber.classList.add('line-number');
-            line.classList.add('code-line');
-            if (lineData.diffKind === "Added" || lineData.diffKind === "Removed") {
-              line.classList.add(`code-line-${lineData.diffKind.toLowerCase()}`);
-            }
-            lineContent.classList.add('code-line-content');
-            lineContent.style.paddingLeft = `${node!.dataset["indent"] as unknown as number * 20}px`;
-            lineActions.classList.add('line-actions');
-            commentIcon.classList.add('bi', 'bi-plus-square-fill');
-            commentButton.appendChild(commentIcon);
-            commentButton.classList.add('comment-button');
-            if (lineData.position === "top") {
-              commentButton.classList.add('commentable');
-            }
+        }
 
-            lineActions.appendChild(lineNumber);
-            lineActions.appendChild(commentButton);
-            line.appendChild(lineActions);
-            line.appendChild(lineContent);
-            node!.appendChild(line);
-
-            if (commentThreadData.length > 0)
-            {
-              const commentThreadNode = this.commentThreadRef.createComponent(CommentThreadComponent);
-              commentThreadNode.instance.comments = commentThreadData;
-              node!.appendChild(commentThreadNode.location.nativeElement);
-              this.changeDeterctorRef.detectChanges();
-            }
+        if (lineData.directive === ReviewPageWorkerMessageDirective.AppendTokenLinesToNode) {
+          const apiTreeNode = document.getElementById(nodeId);
+          const nodeFragmentData = this.codeLineFragments.get(nodeId);
+          if (nodeFragmentData.lastLineNumber < 1000) {
+            apiTreeNode!.appendChild(nodeFragmentData.documentFragment!);
           }
         }
       }

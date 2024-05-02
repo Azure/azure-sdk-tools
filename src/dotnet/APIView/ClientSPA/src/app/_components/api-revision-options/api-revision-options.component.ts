c840e39f90fe46cd52b8f3aef993636444fddace
Chidozie Ononiwu <chononiw@microsoft.com>
2024-05-02 12:27:23 -0700

    Add Review Page Options

---
+++ src/dotnet/APIView/ClientSPA/src/app/_components/api-revision-options/api-revision-options.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-api-revision-options',
+  templateUrl: './api-revision-options.component.html',
+  styleUrls: ['./api-revision-options.component.scss']
+})
+export class ApiRevisionOptionsComponent {
+
+}

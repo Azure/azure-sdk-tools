d9a6efea563f77f1690a158c39f8e76fcba2e6e5
Chidozie Ononiwu <chononiw@microsoft.com>
2024-05-03 12:14:03 -0700

    terminate worker after 5 seconds of inactivity

--- src/dotnet/APIView/ClientSPA/src/app/_services/worker/worker.service.ts
+++ src/dotnet/APIView/ClientSPA/src/app/_services/worker/worker.service.ts
@@ -5,22 +5,41 @@ import { Observable, Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class WorkerService {
-  private apiTreeBuilder : Worker;
+  private apiTreeBuilder : Worker | null = null;
   private apiTreeMessages = new Subject<any>();
+  private workerTimeout : any;
 
   constructor() {
+    this.startWorker();
+  }
+
+  startWorker() {
     this.apiTreeBuilder = new Worker(new URL('../../_workers/apitree-builder.worker', import.meta.url));
 
     this.apiTreeBuilder.onmessage = ({ data }) => {
       this.apiTreeMessages.next(data);
+      this.resetWorkerTimeout();
     };
   }
 
   postToApiTreeBuilder(message: any) {
-    this.apiTreeBuilder.postMessage(message);
+    if (!this.apiTreeBuilder) {
+      this.startWorker();
+    }
+
+    this.apiTreeBuilder!.postMessage(message);
+    this.resetWorkerTimeout();
   }
 
   onMessageFromApiTreeBuilder(): Observable<any> {
     return this.apiTreeMessages.asObservable();
+  }
+
+  private resetWorkerTimeout() {
+    clearTimeout(this.workerTimeout);
+    this.workerTimeout = setTimeout(() => {
+      this.apiTreeBuilder!.terminate();
+      this.apiTreeBuilder = null;
+    }, 5000); // Terminate worker after 5 seconds of inactivity
   }
 }

1e048aadcdbf168bf56bccddb91ce874e6e6933b
Chidozie Ononiwu <chononiw@microsoft.com>
2024-05-13 21:05:31 -0700

    Regular Diffind with lazy loaded lines

--- src/dotnet/APIView/ClientSPA/src/app/_workers/apitree-builder.worker.ts
+++ src/dotnet/APIView/ClientSPA/src/app/_workers/apitree-builder.worker.ts
@@ -2,7 +2,7 @@
 
 import { ComputeTokenDiff } from "../_helpers/worker-helpers";
 import { CodeDiagnostic, CodePanelData, CommentItemModel } from "../_models/review";
-import { CodePanelRowData, CodePanelRowDatatype, InsertCodePanelRowDataMessage, ReviewPageWorkerMessageDirective, StructuredToken } from "../_models/revision";
+import { CodePanelRowData, CodePanelRowDatatype, DiffLineInProcess, InsertCodePanelRowDataMessage, ReviewPageWorkerMessageDirective, StructuredToken } from "../_models/revision";
 import { APITreeNode } from "../_models/revision";
 
 let insertLineNumber = 0;
@@ -135,18 +135,31 @@ function buildTokens(apiTreeNode: APITreeNode, id: string, position: string, ind
  * @param position 
  */
 function buildTokensForDiffNodes(apiTreeNode: APITreeNode, id: string, position: string, indent: number = 0) {
-  const beforeTokens = (position === "top") ? apiTreeNode.topTokens : apiTreeNode.bottomTokens;
-  const afterTokens = (position === "top") ? apiTreeNode.topDiffTokens : apiTreeNode.bottomDiffTokens;
+  const lineGroupOrder = ["documentation"];
+  const nodeId = getTokenNodeIdHash(apiTreeNode, position);
+  let beforeTokens = (position === "top") ? apiTreeNode.topTokens : apiTreeNode.bottomTokens;
+  let afterTokens = (position === "top") ? apiTreeNode.topDiffTokens : apiTreeNode.bottomDiffTokens;
+
+  if (apiTreeNode.diffKind === "Added") {
+    afterTokens = (position === "top") ? apiTreeNode.topTokens : apiTreeNode.bottomTokens;
+    beforeTokens = [];
+  }
+  
+  const beforeTokenLines  : Array<DiffLineInProcess> = [];
+  const afterTokenLines  : Array<DiffLineInProcess> = [];
 
   let beforeIndex = 0;
   let afterIndex = 0;
 
-  while (beforeIndex < beforeTokens.length || afterIndex < afterTokens.length) {
+  while (beforeIndex < beforeTokens.length || afterIndex < afterTokens.length || beforeTokenLines.length > 0 || afterTokenLines.length > 0) {
     const beforeTokenLine : Array<StructuredToken> = [];
     const afterTokenLine : Array<StructuredToken> = [];
 
     const beforeLineClasses = new Set<string>();
     const afterLineClasses = new Set<string>();
+
+    let beforeLineGroupId : string | undefined = undefined;
+    let afterLineGroupId : string | undefined = undefined;
 
     while (beforeIndex < beforeTokens.length) {
       const token = beforeTokens[beforeIndex++];     
@@ -154,11 +167,24 @@ function buildTokensForDiffNodes(apiTreeNode: APITreeNode, id: string, position:
         break;
       }
 
-      if ("GroupId" in token.properties) {
-        beforeLineClasses.add(token.properties["GroupId"]);
+      if ("groupId" in token.properties) {
+        beforeLineGroupId = token.properties["groupId"];
+        beforeLineClasses.add(beforeLineGroupId);
       }
+      else {
+        beforeLineGroupId = undefined;
+      }
+
       beforeTokenLine.push(token);
     }
+
+    if (beforeTokenLine.length > 0 ) {
+      beforeTokenLines.push({
+        groupId: beforeLineGroupId,
+        lineTokens: beforeTokenLine
+      })
+    }
+
 
     while (afterIndex < afterTokens.length) {
       const token = afterTokens[afterIndex++];
@@ -166,48 +192,98 @@ function buildTokensForDiffNodes(apiTreeNode: APITreeNode, id: string, position:
         break;
       }
 
-      if ("GroupId" in token.properties) {
-        afterLineClasses.add(token.properties["GroupId"]);
+      if ("groupId" in token.properties) {
+        afterLineGroupId = token.properties["groupId"];
+        afterLineClasses.add(afterLineGroupId);
       }
+      else {
+        afterLineGroupId = undefined;
+      }
+
       afterTokenLine.push(token);
     }
 
-    const diffTokenLineResult = ComputeTokenDiff(beforeTokenLine, afterTokenLine) as [StructuredToken[], StructuredToken[], boolean];
-
-    let insertLinesOfTokensMessage : InsertCodePanelRowDataMessage =  {
-      directive: ReviewPageWorkerMessageDirective.InsertCodeLineData,
-      codePanelRowData: {
-        rowType: CodePanelRowDatatype.CodeLine,
-        lineNumber: 0,
-        lineTokens : diffTokenLineResult[0],
-        rowClasses : beforeLineClasses,
-        nodeId : id,
-        indent : indent,
-        diffKind : "Unchanged",
-        rowSize : 21
-      } 
-    };
-
-    if (diffTokenLineResult[2] === true) {
-      insertLinesOfTokensMessage.codePanelRowData.diffKind = "Removed";
-      insertLinesOfTokensMessage.codePanelRowData.rowClasses = beforeLineClasses;
-      
-      insertLineNumber++;
-      insertLinesOfTokensMessage.codePanelRowData.lineNumber = insertLineNumber;
-      postMessage(insertLinesOfTokensMessage);
-
-      insertLinesOfTokensMessage.codePanelRowData.lineTokens = diffTokenLineResult[1];
-      insertLinesOfTokensMessage.codePanelRowData.diffKind = "Added";
-      insertLinesOfTokensMessage.codePanelRowData.rowClasses = afterLineClasses;
-
-      insertLineNumber++;
-      insertLinesOfTokensMessage.codePanelRowData.lineNumber = insertLineNumber;
-      postMessage(insertLinesOfTokensMessage);
+    if (afterTokenLine.length > 0) {
+      afterTokenLines.push({
+        groupId: afterLineGroupId,
+        lineTokens: afterTokenLine
+      })
     }
-    else {
-      insertLineNumber++;
-      insertLinesOfTokensMessage.codePanelRowData.lineNumber = insertLineNumber;
-      postMessage(insertLinesOfTokensMessage);
+
+    if (beforeTokenLines.length > 0 || afterTokenLines.length > 0) {
+      let beforeDiffTokens : Array<StructuredToken> = [];
+      let afterDiffTokens : Array<StructuredToken> = [];
+
+      if (beforeTokenLines.length > 0 && afterTokenLines.length > 0) {
+        if (beforeTokenLines[0].groupId === afterTokenLines[0].groupId) {
+          beforeDiffTokens = beforeTokenLines.shift()?.lineTokens!;
+          afterDiffTokens = afterTokenLines.shift()?.lineTokens!;
+        }
+        else {
+          const beforeTokenLineBuildOrder = lineGroupOrder.indexOf(beforeTokenLines[0].groupId!);
+          const afterTokenLineBuildOrder = lineGroupOrder.indexOf(afterTokenLines[0].groupId!);
+          if ((afterTokenLineBuildOrder < 0) || (beforeTokenLineBuildOrder >= 0 && beforeTokenLineBuildOrder < afterTokenLineBuildOrder)) {
+            beforeDiffTokens = beforeTokenLines.shift()?.lineTokens!;
+          }
+          else {
+            afterDiffTokens = afterTokenLines.shift()?.lineTokens!;
+          }
+        }
+      }
+      else if(beforeTokenLines.length > 0) {
+        beforeDiffTokens = beforeTokenLines.shift()?.lineTokens!;
+      }
+      else {
+        afterDiffTokens = afterTokenLines.shift()?.lineTokens!;
+      }
+
+      const diffTokenLineResult = ComputeTokenDiff(beforeDiffTokens, afterDiffTokens) as [StructuredToken[], StructuredToken[], boolean];
+
+      let insertLinesOfTokensMessage : InsertCodePanelRowDataMessage =  {
+        directive: ReviewPageWorkerMessageDirective.InsertCodeLineData,
+        codePanelRowData: {
+          rowType: CodePanelRowDatatype.CodeLine,
+          lineNumber: 0,
+          lineTokens : diffTokenLineResult[0],
+          rowClasses : beforeLineClasses,
+          nodeId : nodeId,
+          indent : indent,
+          diffKind : "Unchanged",
+          rowSize : 21
+        } 
+      };
+
+      if (diffTokenLineResult[2] === true) {
+        if (diffTokenLineResult[0].length > 0) {
+          insertLinesOfTokensMessage.codePanelRowData.diffKind = "Removed";
+          beforeLineClasses.add("removed");
+          insertLinesOfTokensMessage.codePanelRowData.rowClasses = beforeLineClasses;
+          
+          insertLineNumber++;
+          insertLinesOfTokensMessage.codePanelRowData.lineNumber = insertLineNumber;
+          postMessage(insertLinesOfTokensMessage);
+          beforeLineClasses.clear();
+        }
+
+        if (diffTokenLineResult[1].length > 0) {
+          insertLinesOfTokensMessage.codePanelRowData.lineTokens = diffTokenLineResult[1];
+          insertLinesOfTokensMessage.codePanelRowData.diffKind = "Added";
+          afterLineClasses.add("added");
+          insertLinesOfTokensMessage.codePanelRowData.rowClasses = afterLineClasses;
+
+          insertLineNumber++;
+          insertLinesOfTokensMessage.codePanelRowData.lineNumber = insertLineNumber;
+          postMessage(insertLinesOfTokensMessage);
+          afterLineClasses.clear();
+        }
+      }
+      else {
+        if (diffTokenLineResult[0].length > 0) {
+          insertLineNumber++;
+          insertLinesOfTokensMessage.codePanelRowData.lineNumber = insertLineNumber;
+          postMessage(insertLinesOfTokensMessage);
+        }
+      }
     }
   }
 }
@@ -230,8 +306,8 @@ function buildTokensForNonDiffNodes(apiTreeNode: APITreeNode, id: string, positi
 
 
   for (let token of tokens) {
-    if ("GroupId" in token.properties) {
-      lineClasses.add(token.properties["GroupId"]);
+    if ("groupId" in token.properties) {
+      lineClasses.add(token.properties["groupId"]);
     }
     
     if (token.kind === "LineBreak") {
@@ -364,5 +440,3 @@ function collectUserCommentsforLine(tokenIdsInLine: Set<string>, nodeId: string,
   }
   return insertCommentMessage;
 }
-
-

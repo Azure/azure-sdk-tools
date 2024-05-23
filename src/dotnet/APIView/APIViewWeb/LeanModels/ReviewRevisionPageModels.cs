7cfacf408441b6d054d715f8f0f77ee38b7b49aa
Chidozie Ononiwu <chononiw@microsoft.com>
2024-05-23 09:52:57 -0700

    Sending from Client Side

--- src/dotnet/APIView/APIViewWeb/LeanModels/ReviewRevisionPageModels.cs
+++ src/dotnet/APIView/APIViewWeb/LeanModels/ReviewRevisionPageModels.cs
@@ -14,6 +14,20 @@ namespace APIViewWeb.LeanModels
         RedirectToSPAUI
     }
 
+    public enum CodePanelRowDatatype
+    {
+        CodeLine,
+        Documentation,
+        Diagnostics,
+        CommentThread
+    }
+
+    public enum LinesOfTokensPosition
+    {
+        Top,
+        Bottom
+    }
+
     public class ReviewContentModel
     {
         public ReviewListItemModel Review { get; set; }
@@ -50,5 +64,41 @@ namespace APIViewWeb.LeanModels
         public IEnumerable<CommentItemModel> Comments { get; set; }
         public List<APITreeNodeForAPI> APIForest { get; set; } = new List<APITreeNodeForAPI>();
         public CodeDiagnostic[] Diagnostics { get; set; }
+    }
+
+    public class CodePanelRowData
+    {
+        public CodePanelRowDatatype RowType { get; set; }
+        public int? LineNumber { get; set; }
+        public List<StructuredToken> RowsOfTokens { get; set; } = new List<StructuredToken>();
+        public string NodeId { get; set; }
+        public string NodeIdHashed { get; set; }
+        public LinesOfTokensPosition linesOfTokensPosition { get; set; }
+        public HashSet<string> RowClasses { get; set; } = new HashSet<string>();
+        public int? Indent { get; set; }
+        public DiffNodeKind DiffKind { get; set; }
+        public int RowSize { get; set; }
+        public string ToggleDocumentationClasses { get; set; }
+        public string ToggleCommentsClasses { get; set; }
+        public CodeDiagnostic Diagnostics { get; set; }
+        public List<CommentItemModel> Comments { get; set; } = new List<CommentItemModel>();
+    }
+
+    public class CodePanelNodeMetaData
+    {
+        public List<CodePanelRowData> Documentation { get; set; } = new List<CodePanelRowData>();
+        public List<CodePanelRowData> Diagnostics { get; set; } = new List<CodePanelRowData>();
+        public List<CommentItemModel> Comments { get; set; } = new List<CommentItemModel>();
+        public string ParentNodeId { get; set; }
+        public string ChildrenNodeIds { get; set; }
+        public bool IsDiffNode { get; set; }
+        public bool IsDiffInDescendants { get; set; }
+        public bool IsClosingNode { get; set; }
+    }
+
+    public class CodePanelData
+    {
+        public List<CodePanelRowData> Rows { get; set; } = new List<CodePanelRowData>();
+        public Dictionary<string, CodePanelNodeMetaData> NodeMetaData { get; set; } = new Dictionary<string, CodePanelNodeMetaData>();
     }
 }

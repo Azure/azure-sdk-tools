3a7dd480e37068de989b9fb5e6eca9c4ec425032
Chidozie Ononiwu <chononiw@microsoft.com>
2024-05-30 11:06:49 -0700

    Retrieve MessagePack

--- src/dotnet/APIView/APIViewWeb/Repositories/Interfaces/IBlobCodeFileRepository.cs
+++ src/dotnet/APIView/APIViewWeb/Repositories/Interfaces/IBlobCodeFileRepository.cs
@@ -8,7 +8,7 @@ namespace APIViewWeb.Repositories
     public interface IBlobCodeFileRepository
     {
         public Task<RenderedCodeFile> GetCodeFileAsync(APIRevisionListItemModel revision, bool updateCache = true);
-        public Task<RenderedCodeFile> GetCodeFileAsync(string revisionId, string codeFileId, bool updateCache = true);
+        public Task<RenderedCodeFile> GetCodeFileAsync(string revisionId, string codeFileId, string codeFileExtension = "", bool updateCache = true);
         public Task UpsertCodeFileAsync(string revisionId, string codeFileId, CodeFile codeFile);
         public Task DeleteCodeFileAsync(string revisionId, string codeFileId);
     }

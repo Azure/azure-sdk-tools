39b4cc34536a171d24375b35f3fa981ea6210fea
Chidozie Ononiwu <chononiw@microsoft.com>
2024-06-11 17:31:26 -0700

    Comment API Calls

--- src/dotnet/APIView/ClientSPA/src/app/_services/comments/comments.service.ts
+++ src/dotnet/APIView/ClientSPA/src/app/_services/comments/comments.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ConfigService } from '../config/config.service';
 import { Observable } from 'rxjs';
@@ -17,19 +17,90 @@ export class CommentsService {
   }
 
   createComment(reviewId: string, apiRevisionId: string, elementId: string, commentText: string, commentType: CommentType, resolutionLocked : boolean = false) {
-    const data = {
-      reviewId: reviewId,
-      apiRevisionId: apiRevisionId,
-      elementId: elementId,
-      commentText: commentText,
-      commentType: commentType,
-      resolutionLocked: resolutionLocked
-    };
+    let params = new HttpParams();
+    params = params.append('reviewId', reviewId);
+    params = params.append('apiRevisionId', apiRevisionId);
+    params = params.append('elementId', elementId);
+    params = params.append('commentText', commentText);
+    params = params.append('commentType', commentType);
+    params = params.append('resolutionLocked', resolutionLocked);
 
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
     })
 
-    return this.http.post(this.baseUrl, data, { headers: headers, withCredentials: true });
+    return this.http.post(this.baseUrl, {}, { 
+      headers: headers,
+      observe: 'response',
+      params: params,
+      withCredentials: true });
+  }
+
+  updateComment(reviewId: string, commentId: string, commentText: string) {
+    let params = new HttpParams();
+    params = params.append('commentText', commentText);
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+
+    return this.http.patch(this.baseUrl + `/${reviewId}/${commentId}/updateCommentText`, {}, { 
+      headers: headers,
+      observe: 'response',
+      params: params,
+      withCredentials: true });
+  }
+
+  resolveComments(reviewId: string, elementId: string) {
+    let params = new HttpParams();
+    params = params.append('elementId', elementId);
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+
+    return this.http.patch(this.baseUrl + `/${reviewId}/resolveComments`, {}, { 
+      headers: headers,
+      observe: 'response',
+      params: params,
+      withCredentials: true });
+  }
+
+  unresolveComments(reviewId: string, elementId: string) {
+    let params = new HttpParams();
+    params = params.append('elementId', elementId);
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+
+    return this.http.patch(this.baseUrl + `/${reviewId}/unResolveComments`, {}, { 
+      headers: headers,
+      observe: 'response',
+      params: params,
+      withCredentials: true });
+  }
+
+  toggleCommentUpVote(reviewId: string, commentId: string) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+
+    return this.http.patch(this.baseUrl + `/${reviewId}/${commentId}/toggleCommentUpVote`, {}, { 
+      headers: headers,
+      observe: 'response',
+      withCredentials: true
+    });
+  }
+
+  deleteComment(reviewId: string, commentId: string) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+
+    return this.http.delete(this.baseUrl + `/${reviewId}/${commentId}`, { 
+      headers: headers,
+      observe: 'response',
+      withCredentials: true });
   }
 }

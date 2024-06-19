61ad0765763d8c7e60ea851d95db44fe16f6c36e
Ray Chen <raychen@microsoft.com>
2024-06-20 00:43:46 +0800

    Deleted codes of sdk-generation-pipeline path (#8472)
    * Deleted codes of sdk-generation-pipeline path
    
    * Revert change of other paths under CODEOWNER

--- tools/sdk-generation-pipeline/packages/sdk-generation-lib/src/utils/db/taskResultDao.ts
+++
@@ -1,6 +0,0 @@
-import { TaskResult } from '../../types/taskResult';
-
-export interface TaskResultDao {
-    getFromBuild(pipelineBuildId: string);
-    put(pipelineBuildId: string, taskResult: TaskResult);
-}

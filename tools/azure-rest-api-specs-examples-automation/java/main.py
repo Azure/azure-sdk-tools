f0c9ed8ee3abb40414d5c509ed7309f89ad99dc2
Weidong Xu <weidxu@microsoft.com>
2024-07-02 15:27:52 +0800

    nit

--- tools/azure-rest-api-specs-examples-automation/java/main.py
+++ tools/azure-rest-api-specs-examples-automation/java/main.py
@@ -200,8 +200,8 @@ def set_specs_path(new_specs_path: str):
 
 
 def try_find_resource_manager_example(example_dir: str, example_filename: str) -> str:
-    try:
-        if '/resource-manager/' not in example_dir:
+    if '/resource-manager/' not in example_dir:
+        try:
             match = re.match(r'specification/([^/]+)/.*/examples/([^/]+)(.*)', example_dir)
             if match:
                 # example: specification/mongocluster/DocumentDB.MongoCluster.Management/examples/2024-03-01-preview
@@ -219,9 +219,9 @@ def try_find_resource_manager_example(example_dir: str, example_filename: str) -
                 if len(candidate_resource_manager_filename) > 0:
                     example_path, _ = path.split(candidate_resource_manager_filename[0])
                     example_dir = path.relpath(example_path, specs_path).replace('\\', '/')
-    except NameError:
-        # specs_path not defined
-        pass
+        except NameError:
+            # specs_path not defined
+            pass
 
     return example_dir
 

fdb1d70534fac8f28957efcea152fb96787bad87
Weidong Xu <weidxu@microsoft.com>
2024-07-02 14:50:59 +0800

    avoid glob root_dir in python 3.10

--- tools/azure-rest-api-specs-examples-automation/java/main.py
+++ tools/azure-rest-api-specs-examples-automation/java/main.py
@@ -214,11 +214,11 @@ def try_find_resource_manager_example(example_dir: str, example_filename: str) -
                 additional_path = match.group(3)
 
                 glob_resource_manager_filename = f'specification/{service}/resource-manager/**/{api_version}/examples{additional_path}/{example_filename}'
-                candidate_resource_manager_filename = glob.glob(glob_resource_manager_filename,
-                                                                root_dir=specs_path,
+                candidate_resource_manager_filename = glob.glob(path.join(specs_path, glob_resource_manager_filename),
                                                                 recursive=True)
                 if len(candidate_resource_manager_filename) > 0:
-                    example_dir, _ = path.split(candidate_resource_manager_filename[0].replace('\\', '/'))
+                    example_path, _ = path.split(candidate_resource_manager_filename[0])
+                    example_dir = path.relpath(example_path, specs_path).replace('\\', '/')
     except NameError:
         # specs_path not defined
         pass

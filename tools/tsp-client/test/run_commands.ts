9e81f5227bcb96567ffec66659bd855abf3d11df
Catalina Peralta <caperal@microsoft.com>
2024-07-11 20:03:04 -0700

    improve script

--- tools/tsp-client/test/run_commands.ts
+++ tools/tsp-client/test/run_commands.ts
@@ -1,14 +1,14 @@
-// 
 // The script will run the init, update, sync, and generate commands on the test/examples directory.
+// The script will copy the emitter-package.json found in test/utils/ to the eng directory at the root of the repo then clean it up.
 // The init command will download the specification from the tspconfig.yaml url and generate the client library in the test/examples directory.
 // The update command will sync and generate the client library under test/examples/sdk/contosowidgetmanager/contosowidgetmanager-rest.
-// The sync command will sync the specification from the tsp-location.yaml under test/examples/sdk/sync-generate.
-// The generate command will generate the client library under test/examples/sdk/sync-generate.
+// The sync command will sync the specification from the tsp-location.yaml under test/examples/sdk/contosowidgetmanager/contosowidgetmanager-rest.
+// The generate command will generate the client library under test/examples/sdk/contosowidgetmanager/contosowidgetmanager-rest.
 // The script will log the success of each command.
 // The script will throw an error if any of the commands fail.
 
 import { spawn } from "child_process";
-import { cp } from "fs/promises";
+import { cp, unlink } from "fs/promises";
 import { join, resolve } from "path";
 import { getRepoRoot } from "../src/git";
 
@@ -24,6 +24,7 @@ export async function runCommand(workingDir: string, args: string[]): Promise<vo
       });
       npm.once("exit", (code) => {
         if (code === 0) {
+          console.log(`${args[0]} ---------------> RAN SUCCESSFULLY`);
           resolve();
         } else {
           reject(new Error(`npx ${args[0]} failed exited with code ${code}`));
@@ -48,39 +49,24 @@ async function main() {
     
     // Copy the emitter-package.json to the eng directory at the root of the repo
     const repoRoot = await getRepoRoot(".");
-    cp(emitterPackageJson, join(repoRoot, "eng/emitter-package.json")).then(() => {
-        console.log("emitter-package.json -> copied successfully");
 
-        // Run the init + update command
-        runCommand(baseDir, ["init", "-c", tspConfig, "-o", examplesDir]).then(() => {
-            console.log("init -> ran successfully");
-            // Run the update command
-            runCommand(baseDir, ["update", "-o", constosoJsSdkDir]).then(() => {
-                console.log("update -> ran successfully");
-                }
-            ).catch((error) => {
-                console.error(error);
-            });
-        }).catch((error) => {
-            console.error(error);
-        });
+    await cp(emitterPackageJson, join(repoRoot, "eng/emitter-package.json"));
+    console.log("emitter-package.json ---------------> copied successfully");
 
-        // Run the sync + generate commands
-        runCommand(baseDir, ["sync", "-o", constosoSyncGenerateDir]).then(() => {
-            console.log("sync -> ran successfully");
-            // Run the generate command
-            runCommand(baseDir, ["generate", "-o", constosoSyncGenerateDir]).then(() => {
-                console.log("generate -> ran successfully");
-            }).catch((error) => {
-                console.error(error);
-            });
-        }).catch((error) => {
-            console.error(error);
-        });
+    // Run the init command
+    await runCommand(baseDir, ["init", "-c", tspConfig, "-o", examplesDir]);
 
-    }).catch((error) => {
-        console.error(error);
-    });
+    // Run the update command
+    await runCommand(baseDir, ["update", "-o", constosoJsSdkDir]);
+    
+    // Run the sync commands
+    await runCommand(baseDir, ["sync", "-o", constosoSyncGenerateDir])
+
+    // Run the generate command
+    await runCommand(baseDir, ["generate", "-o", constosoSyncGenerateDir])
+
+    await unlink(join(repoRoot, "eng/emitter-package.json"));
+    console.log("emitter-package.json ---------------> deleted successfully");
 }
 
 main().catch((e) => {

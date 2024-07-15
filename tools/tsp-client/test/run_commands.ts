08e70c30c9e9d02066eb6439c682b89f715c735a
Catalina Peralta <caperal@microsoft.com>
2024-07-15 15:04:46 -0700

    more clean up

--- tools/tsp-client/test/run_commands.ts
+++ tools/tsp-client/test/run_commands.ts
@@ -1,4 +1,4 @@
-// The script will run the init, update, sync, and generate commands on the test/examples directory.
+// The script will run the tsp-client commands on the test/examples directory.
 // The script will copy the emitter-package.json found in test/utils/ to the eng directory at the root of the repo then clean it up.
 // The init command will download the specification from the tspconfig.yaml url and generate the client library in the test/examples directory.
 // The update command will sync and generate the client library under test/examples/sdk/contosowidgetmanager/contosowidgetmanager-rest.
@@ -12,67 +12,71 @@ import { cp, unlink } from "fs/promises";
 import { join, resolve } from "path";
 import { getRepoRoot } from "../src/git";
 
-
 export async function runCommand(workingDir: string, args: string[]): Promise<void> {
-    const baseArgs = ["tsx", "./src/index.ts", "--no-prompt"];
-    const commandArgs = baseArgs.concat(args);
-    return new Promise((resolve, reject) => {
-      const npm = spawn("npx", commandArgs, {
-        cwd: workingDir,
-        stdio: "inherit",
-        shell: true,
-      });
-      npm.once("exit", (code) => {
-        if (code === 0) {
-          console.log(`${args[0]} ---------------> RAN SUCCESSFULLY`);
-          resolve();
-        } else {
-          reject(new Error(`npx ${args[0]} failed exited with code ${code}`));
-        }
-      });
-      npm.once("error", (err) => {
-        reject(new Error(`npx ${args[0]} failed with error: ${err}`));
-      });
+  const baseArgs = ["tsx", "./src/index.ts", "--no-prompt"];
+  const commandArgs = baseArgs.concat(args);
+  return new Promise((resolve, reject) => {
+    const npm = spawn("npx", commandArgs, {
+      cwd: workingDir,
+      stdio: "inherit",
+      shell: true,
     });
+    npm.once("exit", (code) => {
+      if (code === 0) {
+        console.log(`${args[0]} ---------------> RAN SUCCESSFULLY`);
+        resolve();
+      } else {
+        reject(new Error(`npx ${args[0]} failed exited with code ${code}`));
+      }
+    });
+    npm.once("error", (err) => {
+      reject(new Error(`npx ${args[0]} failed with error: ${err}`));
+    });
+  });
 }
 
 async function main() {
-    // Variables for the target directories
-    const baseDir = resolve(".");
-    const examplesDir = resolve("./test/examples/");
-    const constosoJsSdkDir = resolve("./test/examples/sdk/contosowidgetmanager/contosowidgetmanager-rest/");
-    const baseSpecDir = "./test/examples/specification/convert/";
-    const mgmtSpecLink = "https://github.com/Azure/azure-rest-api-specs/blob/589cb3afdc76fe9a989ef36952eec816906d481f/specification/sphere/resource-manager/readme.md";
-    const contosoSpecDir = "./test/examples/specification/contosowidgetmanager/Contoso.WidgetManager/";
-    const tspConfig = "https://github.com/Azure/azure-rest-api-specs/blob/db63bea839f5648462c94e685d5cc96f8e8b38ba/specification/contosowidgetmanager/Contoso.WidgetManager/tspconfig.yaml";
+  // Variables for the target directories
+  const baseDir = resolve(".");
+  const examplesDir = resolve("./test/examples/");
+  const constosoJsSdkDir = resolve(
+    "./test/examples/sdk/contosowidgetmanager/contosowidgetmanager-rest/",
+  );
+  const baseSpecDir = "./test/examples/specification/convert/";
+  const mgmtSpecLink =
+    "https://github.com/Azure/azure-rest-api-specs/blob/589cb3afdc76fe9a989ef36952eec816906d481f/specification/sphere/resource-manager/readme.md";
+  const contosoSpecDir =
+    "./test/examples/specification/contosowidgetmanager/Contoso.WidgetManager/";
+  const tspConfig =
+    "https://github.com/Azure/azure-rest-api-specs/blob/db63bea839f5648462c94e685d5cc96f8e8b38ba/specification/contosowidgetmanager/Contoso.WidgetManager/tspconfig.yaml";
 
-    const emitterPackageJson = resolve("./test/utils/emitter-package.json");
-    
-    // Copy the emitter-package.json to the eng directory at the root of the repo
-    const repoRoot = await getRepoRoot(".");
+  const emitterPackageJson = resolve("./test/utils/emitter-package.json");
 
-    await cp(emitterPackageJson, join(repoRoot, "eng/emitter-package.json"));
-    console.log("emitter-package.json ---------------> copied successfully");
+  // Copy the emitter-package.json to the eng directory at the root of the repo
+  const repoRoot = await getRepoRoot(".");
 
-    // Run the init command
-    await runCommand(baseDir, ["init", "-c", tspConfig, "-o", examplesDir]);
+  await cp(emitterPackageJson, join(repoRoot, "eng/emitter-package.json"));
+  console.log("emitter-package.json ---------------> copied successfully");
 
-    // Run the update command
-    await runCommand(baseDir, ["update", "-o", constosoJsSdkDir]);
-    
-    // Run the sync commands
-    await runCommand(baseDir, ["sync", "-o", constosoJsSdkDir])
+  // Run the init command
+  await runCommand(baseDir, ["init", "-c", tspConfig, "-o", examplesDir]);
 
-    // Run the generate command
-    await runCommand(baseDir, ["generate", "-o", constosoJsSdkDir])
+  // Run the update command
+  await runCommand(baseDir, ["update", "-o", constosoJsSdkDir]);
 
-    // Run the convert command on a mgmt spec
-    await runCommand(baseDir, ["convert", "-o", baseSpecDir, "--swagger-readme", mgmtSpecLink]);
+  // Run the sync commands
+  await runCommand(baseDir, ["sync", "-o", constosoJsSdkDir]);
 
-    await unlink(join(repoRoot, "eng/emitter-package.json"));
-    console.log("emitter-package.json ---------------> deleted successfully");
+  // Run the generate command
+  await runCommand(baseDir, ["generate", "-o", constosoJsSdkDir]);
+
+  // Run the convert command on a mgmt spec
+  await runCommand(baseDir, ["convert", "-o", baseSpecDir, "--swagger-readme", mgmtSpecLink]);
+
+  await unlink(join(repoRoot, "eng/emitter-package.json"));
+  console.log("emitter-package.json ---------------> deleted successfully");
 }
 
 main().catch((e) => {
-    throw e;
+  throw e;
 });

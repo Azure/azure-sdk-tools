9f4535002d40a9285c32f864fbcbcd19f8b2063b
Catalina Peralta <caperal@microsoft.com>
2024-07-16 18:26:47 -0700

    more utils tests

--- tools/tsp-client/test/utils.spec.ts
+++ tools/tsp-client/test/utils.spec.ts
@@ -1,8 +1,13 @@
 import { describe, it } from "node:test";
-import { getServiceDir } from "../src/utils.js";
+import {
+  formatAdditionalDirectories,
+  getAdditionalDirectoryName,
+  getServiceDir,
+  makeSparseSpecDir,
+} from "../src/utils.js";
 import { parse as parseYaml } from "yaml";
 import { assert } from "chai";
-import { readFile } from "node:fs/promises";
+import { readFile, stat } from "node:fs/promises";
 
 describe("get the right service dir from tspconfig.yaml", function () {
   it("Get custom emitter service-dir", async function () {
@@ -17,5 +22,29 @@ describe("get the right service dir from tspconfig.yaml", function () {
     const configYaml = parseYaml(data);
     const serviceDir = getServiceDir(configYaml, "@azure-tools/typespec-python");
     assert.strictEqual(serviceDir, "sdk/contosowidgetmanager");
+  });
+});
+
+describe("Verify other utils functions", function () {
+  it("Check makeSparseSpecDir", async function () {
+    const specDir = await makeSparseSpecDir("./test");
+    assert.ok(specDir.startsWith("./test/../sparse-spec"));
+
+    const createdDir = await stat(specDir);
+    assert.ok(createdDir.isDirectory());
+  });
+
+  it("Check getAdditionalDirectoryName", async function () {
+    const dir = getAdditionalDirectoryName("/specification/foo");
+    assert.equal(dir, "foo");
+
+    const dir2 = getAdditionalDirectoryName("/specification/foo/");
+    assert.equal(dir2, "foo");
+  });
+
+  it("Check formatAdditionalDirectories", async function () {
+    const result = formatAdditionalDirectories(["/specification/foo", "/specification/bar"]);
+    const expected = "\n- /specification/foo\n- /specification/bar";
+    assert.equal(result, expected);
   });
 });

a7a63b39620a5321bffbf7ef1a446c86ff3b1cd6
Catalina Peralta <caperal@microsoft.com>
2024-07-18 18:57:26 -0700

    add test

--- tools/tsp-client/test/tspLocation.spec.ts
+++ tools/tsp-client/test/tspLocation.spec.ts
@@ -1,8 +1,10 @@
 import { assert } from "chai";
 import { normalizeDirectory, readTspLocation } from "../src/fs.js";
 import { describe, it } from "node:test";
+import { writeTspLocationYaml } from "../src/utils.js";
+import { stat } from "node:fs/promises";
 
-describe("Parse tsp-location.yaml", function () {
+describe("Verify tsp-location.yaml", function () {
   it("Normalize tsp-location.yaml directories", async function () {
     const tspLocation = await readTspLocation("./test/examples/sdk/badtsplocation");
     const directory = normalizeDirectory(tspLocation.directory);
@@ -10,8 +12,30 @@ describe("Parse tsp-location.yaml", function () {
     const additionalDirectories = tspLocation.additionalDirectories?.map(normalizeDirectory);
     assert.equal(additionalDirectories?.length, 2);
     if (additionalDirectories) {
-        assert.equal(additionalDirectories[0], "specification/contosowidgetmanager/Contoso.WidgetManager.Shared");
-        assert.equal(additionalDirectories[1], "specification/contosowidgetmanager/Contoso.WidgetManager.Utils");
+      assert.equal(
+        additionalDirectories[0],
+        "specification/contosowidgetmanager/Contoso.WidgetManager.Shared",
+      );
+      assert.equal(
+        additionalDirectories[1],
+        "specification/contosowidgetmanager/Contoso.WidgetManager.Utils",
+      );
     }
+  });
+
+  it("Write tsp-location.yaml", async function () {
+    const tspLocation = {
+      directory: "specification/contosowidgetmanager/Contoso.WidgetManager",
+      commit: "1234567",
+      repo: "foo",
+      additionalDirectories: [
+        "specification/contosowidgetmanager/Contoso.WidgetManager.Shared",
+        "specification/contosowidgetmanager/Contoso.WidgetManager.Utils",
+      ],
+    };
+    await writeTspLocationYaml(tspLocation, "./test/examples/");
+
+    const tspLocationFile = await stat("./test/examples/tsp-location.yaml");
+    assert.isTrue(tspLocationFile.isFile());
   });
 });

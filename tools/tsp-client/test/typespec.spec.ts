b894e340a5c7350c428ece1da7950953fc2702f7
Catalina Peralta <caperal@microsoft.com>
2024-07-18 18:19:20 -0700

    update tsp compile test

--- tools/tsp-client/test/typespec.spec.ts
+++ tools/tsp-client/test/typespec.spec.ts
@@ -1,23 +1,27 @@
 import { assert } from "chai";
-import { compileTsp } from "../src/typespec.js";
 import { describe, it } from "node:test";
-import { joinPaths } from "@typespec/compiler";
+import { compileTsp, discoverMainFile } from "../src/typespec.js";
+import { joinPaths, resolvePath } from "@typespec/compiler";
 
 describe("Check diagnostic reporting", function () {
   it("Check diagnostic format", async function () {
-        compileTsp({
-            emitterPackage: "@azure-tools/typespec-ts",
-            outputPath: joinPaths(process.cwd(), "examples"),
-            resolvedMainFilePath: joinPaths(process.cwd(), "examples", "specification", "diagnostics", "main.tsp"),
-            additionalEmitterOptions: "",
-            saveInputs: false
-        }).then(
-            () => {
-                assert.fail("Expected error but got none");
-            },
-            (e) => {
-                assert.ok(e.message.includes("test/examples/specification/diagnostics/main.tsp:38:19 - error @typespec/versioning/using-versioned-library: Namespace 'Contoso.WidgetManager' is referencing types from versioned namespace 'Azure.Core' but didn't specify which versions with @useDependency."));
-            }
-        );
+    const mainFile = await discoverMainFile(
+      resolvePath(process.cwd(), "test", "examples", "specification", "diagnostics"),
+    );
+    try {
+      const succeeded = await compileTsp({
+        emitterPackage: "@azure-tools/typespec-ts",
+        outputPath: joinPaths(process.cwd(), "examples"),
+        resolvedMainFilePath: mainFile,
+        additionalEmitterOptions: "",
+        saveInputs: false,
+      });
+
+      // False is returned if diagnostics are reported.
+      // TODO: add more checks for specific diagnostics reported.
+      assert.isFalse(succeeded);
+    } catch {
+      assert.fail("Unexpected failure.");
+    }
   });
 });

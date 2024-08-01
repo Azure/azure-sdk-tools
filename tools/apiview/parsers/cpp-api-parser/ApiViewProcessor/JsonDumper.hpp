19dfece25bb8c7b520bcc40543c813e77319707c
Larry Osterman <LarryOsterman@users.noreply.github.com>
2024-07-31 17:25:34 -0700

    Removed debug code

--- tools/apiview/parsers/cpp-api-parser/ApiViewProcessor/JsonDumper.hpp
+++ tools/apiview/parsers/cpp-api-parser/ApiViewProcessor/JsonDumper.hpp
@@ -176,9 +176,6 @@ public:
   virtual void InsertTypeName(std::string_view const& type, std::string_view const& navigationId)
       override
   {
-    if (navigationId == "Azure::Security::KeyVault::Administration::Models")
-      __debugbreak();
-
     m_json["Tokens"].push_back(
         {{"DefinitionId", navigationId},
          {"NavigateToId", navigationId},
@@ -190,9 +187,6 @@ public:
       std::string_view const& member,
       std::string_view const& memberFullName) override
   {
-    if (memberFullName == "Azure::Security::KeyVault::Administration::Models")
-      __debugbreak();
-
     m_json["Tokens"].push_back(
         {{"DefinitionId", memberFullName},
          {"NavigateToId", nullptr},

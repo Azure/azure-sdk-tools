d9ff98b505ad25ab817d251802182dff38a29f71
Travis Prescott <trpresco@microsoft.com>
2024-08-06 11:13:52 -0700

    Co-authored-by: catalinaperalta <catalinaperaltah@hotmail.com>
    Update package.json

--- tools/tsp-client/src/commands.ts
+++ tools/tsp-client/src/commands.ts
@@ -284,6 +284,18 @@ export async function generateCommand(argv: any) {
   }
 }
 
+export async function compareCommand(argv: any, args: string[]) {
+  let outputDir = argv["output-dir"];
+  const openApiDiffPath = await getPathToDependency("openapi-diff");
+  const command = [openApiDiffPath, ...args];
+  try {
+    await nodeCommand(outputDir, command);
+  } catch (err) {
+    Logger.error(`Error occurred while attempting to compare: ${err}`);
+    process.exit(1);
+  }
+}
+
 export async function updateCommand(argv: any) {
   const outputDir = argv["output-dir"];
   const repo = argv["repo"];

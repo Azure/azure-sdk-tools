6685f395c78b0f7f85193cb404bacc9bb98bcef6
Praveen Kuttappan <prmarott@microsoft.com>
2024-08-26 09:07:22 -0400

    Update param in XML documentation

--- src/dotnet/APIView/APIViewWeb/Managers/ReviewManager.cs
+++ src/dotnet/APIView/APIViewWeb/Managers/ReviewManager.cs
@@ -398,7 +398,7 @@ namespace APIViewWeb.Managers
         /// <param name="updateDisabledLanguages"></param>
         /// <param name="backgroundBatchProcessCount"></param>
         /// <param name="verifyUpgradabilityOnly"></param>
-        /// <param name="packageNameFilterForUpgrade"
+        /// <param name="packageNameFilterForUpgrade"></param>
         /// <returns></returns>
         public async Task UpdateReviewsInBackground(HashSet<string> updateDisabledLanguages, int backgroundBatchProcessCount, bool verifyUpgradabilityOnly, string packageNameFilterForUpgrade = "")
         {

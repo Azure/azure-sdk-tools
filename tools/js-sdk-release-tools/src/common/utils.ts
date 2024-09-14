76a2aa095b343861ec9d6dc9542fc04a53a74e23
wanl <wanl@microsoft.com>
2024-09-14 10:57:24 +0800

    .

--- tools/js-sdk-release-tools/src/common/utils.ts
+++ tools/js-sdk-release-tools/src/common/utils.ts
@@ -14,8 +14,8 @@ import { SpawnOptions, spawn } from 'child_process';
 // so do NOT change the emitter
 const emitterName = '@azure-tools/typespec-ts';
 
-// 15 minutes in milliseconds unit
-export const defaultChildProcessTimeout = 15 * 60 * 1000;
+// 1 hour in milliseconds unit
+export const defaultChildProcessTimeout = 60 * 60 * 1000;
 
 // TODO: remove it after we generate and use options by ourselves
 const messageToTspConfigSample =

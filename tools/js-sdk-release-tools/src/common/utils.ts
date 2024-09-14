62601706877f05a3b2355d570c55682f44bc04a5
wanl <wanl@microsoft.com>
2024-09-14 10:40:16 +0800

    Fix Autorest Hang Issue
    Issue: autorest may hang when generation failed
    Fix: add timeout to child process to avoid hang

--- tools/js-sdk-release-tools/src/common/utils.ts
+++ tools/js-sdk-release-tools/src/common/utils.ts
@@ -14,6 +14,9 @@ import { SpawnOptions, spawn } from 'child_process';
 // so do NOT change the emitter
 const emitterName = '@azure-tools/typespec-ts';
 
+// 15 minutes in milliseconds unit
+export const defaultChildProcessTimeout = 15 * 60 * 1000;
+
 // TODO: remove it after we generate and use options by ourselves
 const messageToTspConfigSample =
     'Please refer to https://github.com/Azure/azure-rest-api-specs/blob/main/specification/contosowidgetmanager/Contoso.WidgetManager/tspconfig.yaml for the right schema.';

fc71989df331e66211f4305902b5d4e8ae5b664f
Lukew3531 <luke_r_wakefield@hotmail.com>
2024-09-25 11:37:00 +0200

    Indent error correction

--- tools/pylint-extensions/azure-pylint-guidelines-checker/tests/test_pylint_custom_plugins.py
+++ tools/pylint-extensions/azure-pylint-guidelines-checker/tests/test_pylint_custom_plugins.py
@@ -3320,6 +3320,7 @@ class TestDoNotLogErrorsEndUpRaising(pylint.testutils.CheckerTestCase):
 
 
 
+
 class TestDoNotImportAsyncio(pylint.testutils.CheckerTestCase):
     """Test that we are blocking imports of asyncio directly allowing indirect imports."""
     CHECKER_CLASS = checker.DoNotImportAsyncio

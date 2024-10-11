e631218477ef5f3367c8106e3f197c534fe766d1
swathipil <swathip@microsoft.com>
2024-10-10 22:57:33 -0700

    add complex ivar type to tests

--- packages/python-packages/apiview-stub-generator/tests/dataclass_parsing_test.py
+++ packages/python-packages/apiview-stub-generator/tests/dataclass_parsing_test.py
@@ -9,13 +9,7 @@ from apistubgentest.models import (
     DataClassSimple, DataClassWithFields, DataClassDynamic, DataClassWithKeywordOnly, DataClassWithPostInit
 )
 
-from ._test_util import _check, _tokenize, _merge_lines, _render_lines, _render_string
-
-
-def _check_all(actual, expect, obj):
-    for (idx, exp) in enumerate(expect):
-        act = actual[idx]
-        _check(act.lstrip(), exp, obj)
+from ._test_util import _tokenize, _merge_lines, _render_lines, _check_all
 
 
 class TestDataClassParsing:

708273eb84e2ab4da47a90ae67d1ab78034e2b31
Chidozie Ononiwu <chononiw@microsoft.com>
2024-10-14 11:40:47 -0700

    Revert Icon Url Change

--- src/dotnet/APIView/ClientSPA/workbox-config.js
+++ src/dotnet/APIView/ClientSPA/workbox-config.js
@@ -1,27 +1,38 @@
+const version = 'v1.0.0';
+const staticCacheName = `static-${version}`;
+const globPatterns = [
+  'favicon.ico',
+  'index.html',
+  'manifest.webmanifest',
+  '*.css',
+  '*.js',
+  'assets/**/*.{svg,cur,jpg,jpeg,png,apng,webp,avif,gif,otf,ttf,woff,woff2,json}'
+];
+
 module.exports = {
-    globDirectory: '../APIViewWeb/wwwroot/spa',
-    globPatterns: [
-      'favicon.ico',
-      'index.html',
-      'manifest.webmanifest',
-      '*.css',
-      '*.js',
-      'assets/**/*.{svg,cur,jpg,jpeg,png,apng,webp,avif,gif,otf,ttf,woff,woff2,json}'
-    ],
-    swDest: '../APIViewWeb/wwwroot/spa/sw.js',
-    runtimeCaching: [
-      {
-        urlPattern: "https://apiviewstagingtest.com/api/reviews/*/content**",
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'revisioncontent',
-          expiration: {
-            maxEntries: 50,
-            maxAgeSeconds: 24 * 60 * 60, // 1 day
-          },
-        },
-      }
-    ],
-    skipWaiting: true,
-    clientsClaim: true,
-  };
+  globDirectory: '../APIViewWeb/wwwroot/spa',
+  globPatterns: globPatterns,
+  swDest: '../APIViewWeb/wwwroot/spa/sw.js',
+  skipWaiting: true,
+  clientsClaim: true,
+  cleanupOutdatedCaches: true,
+};
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => caches.delete(cacheName))
+      );
+    }).then(() => {
+      return caches.open(staticCacheName).then(cache => {
+        return cache.addAll(globPatterns);
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});

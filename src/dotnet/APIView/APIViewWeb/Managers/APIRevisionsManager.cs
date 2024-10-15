ef5e4ec671023ea02d0c217116ab22d64027202c
Praveen Kuttappan <prmarott@microsoft.com>
2024-10-15 05:53:57 -0400

    Upgrade the revisions on the fly

--- src/dotnet/APIView/APIViewWeb/Managers/APIRevisionsManager.cs
+++ src/dotnet/APIView/APIViewWeb/Managers/APIRevisionsManager.cs
@@ -72,7 +72,13 @@ namespace APIViewWeb.Managers
         /// <returns></returns>
         public async Task<PagedList<APIRevisionListItemModel>> GetAPIRevisionsAsync(ClaimsPrincipal user, PageParams pageParams, APIRevisionsFilterAndSortParams filterAndSortParams)
         {
-             return await _apiRevisionsRepository.GetAPIRevisionsAsync(user, pageParams, filterAndSortParams);
+            var revisions = await _apiRevisionsRepository.GetAPIRevisionsAsync(user, pageParams, filterAndSortParams);
+            List<APIRevisionListItemModel> upgradedList = [];
+            foreach (var item in revisions)
+            {
+                upgradedList.Add(await UpgradeAPIRevisionIfRequired(item));
+            }
+            return revisions;
         }
 
         /// <summary>
@@ -85,6 +91,12 @@ namespace APIViewWeb.Managers
         public async Task<IEnumerable<APIRevisionListItemModel>> GetAPIRevisionsAsync(string reviewId, string packageVersion = "", APIRevisionType apiRevisionType = APIRevisionType.All)
         {
             var apiRevisions = await _apiRevisionsRepository.GetAPIRevisionsAsync(reviewId);
+            List<APIRevisionListItemModel> upgradedList = [];
+            foreach (var item in apiRevisions)
+            {
+                upgradedList.Add(await UpgradeAPIRevisionIfRequired(item));
+            }
+            apiRevisions = upgradedList;
 
             if (apiRevisionType != APIRevisionType.All)
                 apiRevisions = apiRevisions.Where(r => r.APIRevisionType == apiRevisionType);

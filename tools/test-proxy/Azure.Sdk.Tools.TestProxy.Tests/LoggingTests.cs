4c71ca644e4df7b95f87452ed6204744ac804159
Harsha Nalluru <sanallur@microsoft.com>
2024-10-23 19:24:12 -0700

    fix logging test

--- tools/test-proxy/Azure.Sdk.Tools.TestProxy.Tests/LoggingTests.cs
+++ tools/test-proxy/Azure.Sdk.Tools.TestProxy.Tests/LoggingTests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Azure.Sdk.Tools.TestProxy.Common;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace Azure.Sdk.Tools.TestProxy.Tests
@@ -52,7 +53,7 @@ namespace Azure.Sdk.Tools.TestProxy.Tests
                 HttpResponse response = new DefaultHttpContext().Response;
                 await testRecordingHandler.HandlePlaybackRequest(recordingId, request, response);
 
-                AssertLogs(logger, 4, 12, 12, "playback");
+                AssertLogs(logger, 4, 10, 12, "playback");
             }
             finally
             {
@@ -91,7 +92,7 @@ namespace Azure.Sdk.Tools.TestProxy.Tests
             await testRecordingHandler.StopRecording(recordingId);
             try
             {
-                AssertLogs(logger, 2, 10, 12, "record");
+                AssertLogs(logger, 2, 9, 12, "record");
             }
             finally
             {
@@ -117,19 +118,19 @@ namespace Azure.Sdk.Tools.TestProxy.Tests
                 logger.Logs[2 + offset].ToString());
             if (testType == "playback")
             {
-                offset = offset + 2;
+                offset = offset + 1;
             }
             Assert.Equal("URI: [ https://Sanitized.table.core.windows.net/Tables]" +
                          Environment.NewLine + "Headers: [{\"Accept\":[\"application/json;odata=minimalmetadata\"],\"Accept-Encoding\":[\"gzip, deflate\"],\"Authorization\":[\"Sanitized\"],\"Connection\":[\"keep-alive\"]," +
                          "\"Content-Length\":[\"" + expectedContentLength + "\"],\"Content-Type\":[\"application/octet-stream\"],\"DataServiceVersion\":[\"3.0\"],\"Date\":[\"Tue, 18 May 2021 23:27:42 GMT\"]," +
                          "\"User-Agent\":[\"azsdk-python-data-tables/12.0.0b7 Python/3.8.6 (Windows-10-10.0.19041-SP0)\"],\"x-ms-client-request-id\":[\"Sanitized\"]," +
                          "\"x-ms-date\":[\"Tue, 18 May 2021 23:27:42 GMT\"],\"x-ms-version\":[\"2019-02-02\"]}]" + Environment.NewLine,
-                logger.Logs[3 + offset].ToString());
+                logger.Logs[3 + offset].ToString()); 
             if (testType == "playback")
             {
                 offset = offset - 1;
             }
-            Assert.Equal("RequestUri is modified" + Environment.NewLine + "before:" + Environment.NewLine + " https://fakeazsdktestaccount.table.core.windows.net/Tables" + Environment.NewLine + "after:" + Environment.NewLine + " https://Sanitized.table.core.windows.net/Tables" + Environment.NewLine , logger.Logs[6 + offset].ToString());
+            Assert.Equal("Central sanitizer rule AZSDK4001 modified the entry"+Environment.NewLine+"RequestUri is modified" + Environment.NewLine + "before:" + Environment.NewLine + " https://fakeazsdktestaccount.table.core.windows.net/Tables" + Environment.NewLine + "after: " + Environment.NewLine + " https://Sanitized.table.core.windows.net/Tables" + Environment.NewLine , logger.Logs[5 + offset].ToString());
         }
 
         private static async Task AddSanitizerAsync(RecordingHandler testRecordingHandler, TestLogger logger)

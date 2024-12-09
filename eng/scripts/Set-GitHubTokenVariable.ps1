ed18e72b887ba1a69b60540a3918129f6d230e8f
Patrick Hallisey <pahallis@microsoft.com>
2024-12-09 10:11:03 -0800

    Token powershell and template

---
+++ eng/scripts/Set-GitHubTokenVariable.ps1
@@ -0,0 +1,24 @@
+param(
+    [string]$AppId,
+    [string]$AppKey,
+    [string]$Repository,
+    [string]$VariableName
+)
+
+$now = [System.DateTimeOffset]::UtcNow
+$payload = [ordered]@{ iss = $AppId; iat = $now.ToUnixTimeSeconds(); exp = $now.AddMinutes(1).ToUnixTimeSeconds() }
+
+$jwt = ./eng/scripts/New-JsonWebToken.ps1 -payload $payload -privateKeyPem $AppKey
+
+$headers = @{
+    "Authorization" = "Bearer $jwt"
+    "Accept" = "application/vnd.github+json"
+    "X-GitHub-Api-Version" = "2022-11-28"
+}
+
+$url = "https://api.github.com/repos/$Repository/installation"
+$installationResponse = Invoke-RestMethod -Method GET -Uri $url -Headers $headers
+$tokenResponse = Invoke-RestMethod -Method POST -Uri $installationResponse.access_tokens_url -Headers $headers
+
+Write-Host "Setting devops secret $VariableName to installation access token for $Repository expiring at $($tokenResponse.expires_at.ToLocalTime())"
+Write-Host "##vso[task.setvariable variable=$VariableName;issecret=true]$($tokenResponse.token)"

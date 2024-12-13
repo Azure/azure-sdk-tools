19b87aab9d584c6575ae3f1f280c73b43949a222
Patrick Hallisey <pahallis@microsoft.com>
2024-12-12 17:00:54 -0800

    Try repo work

--- eng/common/scripts/Get-GitHubInstallationToken.ps1
+++ eng/common/scripts/Get-GitHubInstallationToken.ps1
@@ -1,14 +1,15 @@
 #requires -Version 7.0
 
 param(
+    [Parameter(Mandatory)]
     [string]$AppId,
+    [Parameter(Mandatory)]
     [string]$AppKey,
+    [Parameter(Mandatory, ParameterSetName = 'Repository')]
     [string]$Repository,
-    [string]$VariableName
+    [Parameter(Mandatory, ParameterSetName = 'Organization')]
+    [string]$Organization
 )
-
-Write-Host "PowerShell version:"
-$PSVersionTable | Out-Host
 
 $now = [System.DateTimeOffset]::UtcNow
 $payload = [ordered]@{ iss = $AppId; iat = $now.ToUnixTimeSeconds(); exp = $now.AddMinutes(1).ToUnixTimeSeconds() }
@@ -21,9 +22,13 @@ $headers = @{
     "X-GitHub-Api-Version" = "2022-11-28"
 }
 
-$url = "https://api.github.com/repos/$Repository/installation"
+if ($Organization) {
+    $url = "https://api.github.com/orgs/$Organization/installation"
+} else {
+    $url = "https://api.github.com/repos/$Repository/installation"
+}
+
 $installationResponse = Invoke-RestMethod -Method GET -Uri $url -Headers $headers
 $tokenResponse = Invoke-RestMethod -Method POST -Uri $installationResponse.access_tokens_url -Headers $headers
 
-Write-Host "Setting devops secret $VariableName to installation access token for $Repository expiring at $($tokenResponse.expires_at.ToLocalTime())"
-Write-Host "##vso[task.setvariable variable=$VariableName;issecret=true]$($tokenResponse.token)"
+Write-Output $tokenResponse

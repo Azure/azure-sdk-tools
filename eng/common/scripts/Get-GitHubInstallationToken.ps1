a23e7f6a191af71b9d9ecd68f2b59669589d0d49
Patrick Hallisey <pahallis@microsoft.com>
2024-12-18 13:05:08 -0800

    Add support for user tokens to Get-GitHubInstallationToken.ps1

--- eng/common/scripts/Get-GitHubInstallationToken.ps1
+++ eng/common/scripts/Get-GitHubInstallationToken.ps1
@@ -1,6 +1,7 @@
 #requires -Version 7.0
 
-param(
+[CmdletBinding()]
+param (
     [Parameter(Mandatory)]
     [string]$AppId,
     [Parameter(Mandatory)]
@@ -8,8 +9,14 @@ param(
     [Parameter(Mandatory, ParameterSetName = 'Repository')]
     [string]$Repository,
     [Parameter(Mandatory, ParameterSetName = 'Organization')]
-    [string]$Organization
+    [string]$Organization,
+    [Parameter(Mandatory, ParameterSetName = 'User')]
+    [string]$User,
+    [switch]$Token
 )
+
+Set-StrictMode -Version 2.0
+$ErrorActionPreference = "Stop"
 
 $now = [System.DateTimeOffset]::UtcNow
 $payload = [ordered]@{ iss = $AppId; iat = $now.ToUnixTimeSeconds(); exp = $now.AddMinutes(1).ToUnixTimeSeconds() }
@@ -22,13 +29,18 @@ $headers = @{
     "X-GitHub-Api-Version" = "2022-11-28"
 }
 
-if ($Organization) {
-    $url = "https://api.github.com/orgs/$Organization/installation"
-} else {
-    $url = "https://api.github.com/repos/$Repository/installation"
+$url = switch ($PSCmdlet.ParameterSetName) {
+    "Organization" { "https://api.github.com/orgs/$Organization/installation" }
+    "User" { "https://api.github.com/users/$User/installation" }
+    "Repository" { "https://api.github.com/repos/$Repository/installation" }
+    Default { Write-Error "No installation specified" }
 }
 
 $installationResponse = Invoke-RestMethod -Method GET -Uri $url -Headers $headers
 $tokenResponse = Invoke-RestMethod -Method POST -Uri $installationResponse.access_tokens_url -Headers $headers
 
-Write-Output $tokenResponse
+if($Token) {
+    Write-Output $tokenResponse.token
+} else {    
+    Write-Output $tokenResponse
+}

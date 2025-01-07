64428f15dcfe06ecb59f53e3d8dd161512739b12
Harsha Nalluru <sanallur@microsoft.com>
2025-01-07 00:34:17 +0000

    outputs folder and cleanup main

--- tools/apiview/parsers/rust-api-parser/outputs/docs.rs
+++ tools/apiview/parsers/rust-api-parser/outputs/docs.rs
@@ -7,6 +7,8 @@ pub mod docs {
                 pub fn sample_function2()
                 /// This is a sample struct
                 pub struct SampleStruct2 {
+                    pub field1: String,
+                    pub field2: i32,
                 }
             }
             pub fn use_sample_struct2()
@@ -18,10 +20,12 @@ pub mod docs {
         pub fn sample_function()
         /// This is a sample struct
         pub struct SampleStruct {
+            pub field: i32,
         }
     }
     pub trait MyTrait {
+        fn example_method(self: &Self);
     }
-    pub fn foo()
-    pub fn bar()
+    pub fn foo<T, V>(v: &T)
+    pub fn bar<T, V>(v: &T)
 }

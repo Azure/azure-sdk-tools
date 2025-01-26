d92a1ce5968eba9550112d212329d902ef5f2637
Harsha Nalluru <sanallur@microsoft.com>
2025-01-26 01:39:23 +0000

    split nonTraitImpls and traitImpls

--- tools/apiview/parsers/rust-api-parser/src/process-items/processImpl.ts
+++ tools/apiview/parsers/rust-api-parser/src/process-items/processImpl.ts
@@ -1,26 +1,27 @@
 import { ReviewLine, TokenKind, ReviewToken } from "../utils/apiview-models";
 import { Item, Crate, Impl, Struct } from "../utils/rustdoc-json-types/jsonTypes";
+import { processItem } from "./processItem";
 
 export function processImpl(item: Omit<Item, "inner"> & { inner: { struct: Struct; } }, apiJson: Crate, reviewLine: ReviewLine) {
     type ImplItem = Omit<Item, "inner"> & { inner: { impl: Impl } };
 
-    const derivedTokens = (impls: number[], apiJson: Crate, filterCallback: (implItem: ImplItem) => boolean) =>
+    const traitImpls = (impls: number[], apiJson: Crate) =>
         impls.map(implId => apiJson.index[implId] as ImplItem)
-            .filter((implItem) => typeof implItem?.inner == "object" && "impl" in implItem?.inner && implItem.inner.impl.blanket_impl === null)
-            .filter(filterCallback)
-            .map(implItem => ({
-                Kind: TokenKind.TypeName, Value: typeof implItem?.inner == "object" && "impl" in implItem?.inner ? implItem.inner.impl.trait.name : "unknown",
+            .filter((implItem) => typeof implItem?.inner == "object" && "impl" in implItem?.inner && implItem.inner.impl.blanket_impl === null && implItem.inner.impl.trait)
+            .map<ReviewToken>(implItem => ({
+                Kind: TokenKind.TypeName,
+                Value: implItem.inner.impl.trait.name,
                 RenderClasses: [
                     "trait"
                 ],
-                NavigateToId: implItem.inner.impl.trait.id.toString(),
-                NavigationDisplayName: implItem.inner.impl.trait.name || undefined,
+                // NavigateToId: implItem.inner.impl.trait.id.toString(),
+                // NavigationDisplayName: implItem.inner.impl.trait.name || undefined,
                 HasSuffixSpace: false
             }));
 
-    const addTokens = (tokens: ReviewToken[], kind: string) => {
+    const addTokens = (tokens: ReviewToken[]) => {
         reviewLine.Tokens.push({ Kind: TokenKind.Punctuation, Value: '#[', HasSuffixSpace: false });
-        reviewLine.Tokens.push({ Kind: TokenKind.Keyword, Value: kind, HasSuffixSpace: false });
+        reviewLine.Tokens.push({ Kind: TokenKind.Keyword, Value: "derive", HasSuffixSpace: false });
         reviewLine.Tokens.push({ Kind: TokenKind.Punctuation, Value: '(', HasSuffixSpace: false });
         tokens.forEach((token, index) => {
             reviewLine.Tokens.push(token);
@@ -32,9 +33,19 @@ export function processImpl(item: Omit<Item, "inner"> & { inner: { struct: Struc
         reviewLine.Tokens.push({ Kind: TokenKind.Punctuation, Value: '\n', HasSuffixSpace: false });
     };
 
-    const automatically_derived = derivedTokens(item.inner.struct.impls, apiJson, implItem => implItem.attrs.includes("#[automatically_derived]"));
-    const custom_derived = derivedTokens(item.inner.struct.impls, apiJson, implItem => !implItem.attrs.includes("#[automatically_derived]"));
+    const traits = traitImpls(item.inner.struct.impls, apiJson);
+    if (traits.length) addTokens(traits);
 
-    if (automatically_derived.length) addTokens(automatically_derived, 'derive');
-    if (custom_derived.length) addTokens(custom_derived, 'custom_derive');
+    const nonTraitImpls = (impls: number[], apiJson: Crate): void =>
+        impls.map(implId => apiJson.index[implId] as ImplItem)
+            .filter(implItem => implItem?.inner && "impl" in implItem.inner && implItem.inner.impl.blanket_impl === null && implItem.inner.impl.trait === null)
+            .forEach(implItem => {
+                const items = implItem.inner.impl.items;
+                if (!reviewLine.Children.length) reviewLine.Children = [];
+                items.forEach(item => {
+                    processItem(apiJson, apiJson.index[item], reviewLine.Children);
+                });
+            });
+
+    nonTraitImpls(item.inner.struct.impls, apiJson);
 }

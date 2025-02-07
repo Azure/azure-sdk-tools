fd8555bbfdfdb1333058b036ab228018be8719ac
Scott Beddall <scbedd@microsoft.com>
2025-02-07 15:00:23 -0800

    add base check

--- eng/common-tests/helpers/Package-Helpers.tests.ps1
+++ eng/common-tests/helpers/Package-Helpers.tests.ps1
@@ -1,10 +1,8 @@
 Import-Module Pester
 
-# todo, move this to package helpers tests versus job matrix functions
-
 Describe "Matrix-Collation" {
     BeforeAll {
-        . $PSScriptRoot/../../../common/scripts/Helpers/Package-Helpers.ps1
+        . $PSScriptRoot/../../common/scripts/Helpers/Package-Helpers.ps1
 
         $ClientCoreMatrixConfig = @{
             Name = "clientcore_ci_test_base"
@@ -43,5 +41,39 @@ Describe "Matrix-Collation" {
 
         $groupingResults | Should -Not -BeNullOrEmpty
         $groupingResults.Keys | Should -HaveCount 2
+
+        $group1 = $groupingResults.Keys | Select-Object -First 1
+        $groupingResults[$group1] | Should -HaveCount 2
+        $groupingResults[$group1][0].Name | Should -Be "package2"
+        $groupingResults[$group1][1].Name | Should -Be "package3"
+
+        $group2 = $groupingResults.Keys | Select-Object -Last 1
+        $groupingResults[$group2] | Should -HaveCount 2
+        $groupingResults[$group2][0].Name | Should -Be "package1"
+        $groupingResults[$group2][1].Name | Should -Be "package3"
+    }
+
+    It "Should properly group items with no setting" {
+        $Pkgs = @(
+            @{
+                Name = "package1"
+                CIMatrixConfigs = @()
+            }
+            @{
+                Name = "package2"
+                CIMatrixConfigs = @()
+            },
+            @{
+                Name = "package3"
+                CIMatrixConfigs = @()
+            }
+        )
+
+        $groupingResults = Group-ByObjectKey -Items $Pkgs -GroupByProperty "CIMatrixConfigs"
+
+        $groupingResults | Should -Not -BeNullOrEmpty
+        $groupingResults.Keys | Should -HaveCount 1
+        $key = $groupingResults.Keys | Select-Object -First 1
+        $groupingResults[$key] | Should -HaveCount 3
     }
 }

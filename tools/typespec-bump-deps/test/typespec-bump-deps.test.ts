45d79c474e373e7a61b4e16599a0f22fe3d97373
Weidong Xu <weidxu@microsoft.com>
2025-03-04 13:17:27 +0800

    use vitest for UT

--- tools/typespec-bump-deps/test/typespec-bump-deps.test.ts
+++ tools/typespec-bump-deps/test/typespec-bump-deps.test.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { describe, it } from "mocha";
+import { describe, it } from "vitest";
 import { updatePackageJson, getVersionRange } from "../src/cli/typespec-bump-deps.js";
 
 describe("typespec-bump-deps cli", () => {

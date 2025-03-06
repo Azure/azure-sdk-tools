b642eeeccd5171593c62c5dc0202335120cab6b7
Chidozie Ononiwu <31145988+chidozieononiwu@users.noreply.github.com>
2025-03-06 13:03:09 -0800

    Expose Endpoint for retrieving APIRevisions created by Pull Request (#9986)
    * Expose Endpoint for retrieving APIRevisions created by Pull Request
    
    * Return 404 when no result is found
    
    * Update GetPullRequestReviews API endpoint
    
    * Make GetPullRequestReviews endpoint anonymous

--- src/dotnet/APIView/APIViewWeb/LeanControllers/PullRequestsController.cs
+++ src/dotnet/APIView/APIViewWeb/LeanControllers/PullRequestsController.cs
@@ -1,9 +1,12 @@
+using APIViewWeb.DTOs;
 using APIViewWeb.Extensions;
 using APIViewWeb.Helpers;
 using APIViewWeb.Managers;
 using APIViewWeb.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +18,17 @@ namespace APIViewWeb.LeanControllers
     {
         private readonly ILogger<PullRequestsController> _logger;
         private readonly IPullRequestManager _pullRequestManager;
+        private readonly IConfiguration _configuration;
+        private readonly IEnumerable<LanguageService> _languageServices;
 
-        public PullRequestsController(ILogger<PullRequestsController> logger, IPullRequestManager pullRequestManager)
+        public PullRequestsController(
+            ILogger<PullRequestsController> logger,
+            IPullRequestManager pullRequestManager, IConfiguration configuration, IEnumerable<LanguageService> languageServices)
         {
             _logger = logger;
             _pullRequestManager = pullRequestManager;
+            _configuration = configuration;
+            _languageServices = languageServices;
         }
 
         /// <summary>
@@ -51,6 +60,50 @@ namespace APIViewWeb.LeanControllers
                 results = await _pullRequestManager.GetPullRequestsModelAsync(creatingPR.PullRequestNumber, creatingPR.RepoName);
             }
             return new LeanJsonResult(results, StatusCodes.Status200OK);
+        }
+
+        /// <summary>
+        /// Retrieves API Revision information associated with a Pull Request and commitSHA
+        /// </summary>
+        /// <param name="pullRequestNumber"></param>
+        /// <param name="repoName"></param>
+        /// <param name="commitSHA"></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpGet(Name = "GetPullRequestReviews")]
+        public async Task<ActionResult<IEnumerable<PullRequestModel>>> GetPullRequestReviews(int pullRequestNumber, string repoName, string commitSHA)
+        {
+            IEnumerable<PullRequestModel> pullRequestModels = await _pullRequestManager.GetPullRequestsModelAsync(pullRequestNumber: pullRequestNumber, repoName: repoName);
+            var prsForCommit = pullRequestModels.Where(c => c.Commits.Contains(commitSHA));
+
+            var host = _configuration["APIVIew-Host-Url"];
+            var spaHost = _configuration["APIVIew-SPA-Host-Url"];
+            var reviewSpaUrlTemplate = "{0}review/{1}?activeApiRevisionId={1}";
+            var reviewUrlTemplate = "{0}Assemblies/Review/{0}?revisionId={0}";
+
+            List<PullRequestReviewDto> pullRequestReviewDtos = new List<PullRequestReviewDto>();
+
+            if (prsForCommit.Any())
+            {
+                foreach (var pr in prsForCommit)
+                {
+                    var prDto = new PullRequestReviewDto();
+                    var languageService = LanguageServiceHelpers.GetLanguageService(language: pr.Language, languageServices: _languageServices);
+                    if (languageService.UsesTreeStyleParser) // Languages using treestyle parser are also using the spa UI
+                    {
+                        prDto.Url = string.Format(reviewSpaUrlTemplate, spaHost, pr.ReviewId, pr.APIRevisionId);
+                    }
+                    else 
+                    {
+                        prDto.Url = string.Format(reviewUrlTemplate, host, pr.ReviewId, pr.APIRevisionId);
+                    }
+                    prDto.PackageName = pr.PackageName;
+                    prDto.Language = pr.Language;
+                    pullRequestReviewDtos.Add(prDto);
+                }
+                return new LeanJsonResult(pullRequestReviewDtos, StatusCodes.Status200OK);
+            }
+            return StatusCode(StatusCodes.Status404NotFound);
         }
     }
 }

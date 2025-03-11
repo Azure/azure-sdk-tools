4728257d7a587acf37ae186ee19c7c71461f2fdc
Scott Beddall <scbedd@microsoft.com>
2025-03-11 14:48:31 -0700

    we now iterate across the scenarios

--- eng/common-tests/matrix-generator/pr-matrix-samples/pr-matrix-generation-acceptance.tests.ps1
+++ eng/common-tests/matrix-generator/pr-matrix-samples/pr-matrix-generation-acceptance.tests.ps1
@@ -1,6 +1,7 @@
 Import-Module Pester
 
-
+# Load scenarios before we enter the Describe block so they're available for -ForEach
+$scenarios = Get-Content (Join-Path $PSScriptRoot net_scenarios.json) | ConvertFrom-Json
 
 Describe "Acceptance tests for .NET PR Matrix Generation" {
     BeforeAll {
@@ -8,16 +9,14 @@ Describe "Acceptance tests for .NET PR Matrix Generation" {
         $RepoRoot = Get-Repo -Repo "Azure/azure-sdk-for-net" -Reference "331c07a1ab59ed0042972ca6d0df830df235280f"
     }
 
-    # todo: parametrize this test to via file
-    It "Should evaluate .NET core diffs correctly" {
-        # todo actually parameterize this instead of going after the first one specifically only
-        $scenarios = Get-Content (Join-Path $PSScriptRoot net_scenarios.json) -Raw | ConvertFrom-Json
-        $scenario = $scenarios[0]
-
+    It "Should evaluate .NET core diffs correctly - <name>" -ForEach $scenarios {
+        $scenario = $_
         $outputProps = Invoke-PackageProps -InputDiff $scenario.diff -Repo "$RepoRoot"
-
+        $expectedOutputs = $scenario.expected_package_output | Sort-Object -Property Name
         $detectedOutputs = Get-ChildItem -Path $outputProps -Recurse -Filter "*.json" -Exclude "pr-diff.json" `
             | ForEach-Object { Get-Content -Raw $_ | ConvertFrom-Json }
+            | Sort-Object -Property Name
 
+        $detectedOutputs | Should -Be $expectedOutputs
     }
 }

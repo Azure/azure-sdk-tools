02cbbd58762e74b9b8191a1854020250cd35c2dc
Scott Beddall <scbedd@microsoft.com>
2025-03-12 11:19:25 -0700

    need to get this fix through pretty urgently. this is messing with java and python

--- eng/common-tests/matrix-generator/pr-matrix-samples/pr-matrix-generation-acceptance.tests.ps1
+++ eng/common-tests/matrix-generator/pr-matrix-samples/pr-matrix-generation-acceptance.tests.ps1
@@ -1,7 +1,8 @@
 Import-Module Pester
 
 # Load scenarios before we enter the Describe block so they're available for -ForEach
-$scenarios = Get-Content (Join-Path $PSScriptRoot net_scenarios.json) | ConvertFrom-Json
+$netScenarios = Get-Content (Join-Path $PSScriptRoot net_scenarios.json) | ConvertFrom-Json
+$pythonScenarios = Get-Content (Join-Path $PSScriptRoot python_scenarios.json) | ConvertFrom-Json
 
 Describe "Acceptance tests for .NET PR Matrix Generation" {
     BeforeAll {
@@ -9,7 +10,25 @@ Describe "Acceptance tests for .NET PR Matrix Generation" {
         $RepoRoot = Get-Repo -Repo "Azure/azure-sdk-for-net" -Reference "331c07a1ab59ed0042972ca6d0df830df235280f"
     }
 
-    It "Should evaluate .NET core diffs correctly - <name>" -ForEach $scenarios {
+    It "Should evaluate .NET core diffs correctly - <name>" -ForEach $netScenarios {
+        $scenario = $_
+        $outputProps = Invoke-PackageProps -InputDiff $scenario.diff -Repo "$RepoRoot"
+        $expectedOutputs = $scenario.expected_package_output | Sort-Object -Property Name
+        $detectedOutputs = Get-ChildItem -Path $outputProps -Recurse -Filter "*.json" -Exclude "pr-diff.json" `
+            | ForEach-Object { Get-Content -Raw $_ | ConvertFrom-Json }
+            | Sort-Object -Property Name
+
+        ($detectedOutputs | ConvertTo-Json -Depth 100) | Should -Be ($expectedOutputs | ConvertTo-Json -Depth 100)
+    }
+}
+
+Describe "Acceptance tests for Python PR Matrix Generation" {
+    BeforeAll {
+        . $PSScriptRoot/pr-matrix-generation-acceptance.helpers.ps1
+        $RepoRoot = Get-Repo -Repo "Azure/azure-sdk-for-python" -Reference "516977e3a0f9ba22d5611608f64b836fefffc37e"
+    }
+
+    It "Should evaluate python diffs correctly - <name>" -ForEach $pythonScenarios {
         $scenario = $_
         $outputProps = Invoke-PackageProps -InputDiff $scenario.diff -Repo "$RepoRoot"
         $expectedOutputs = $scenario.expected_package_output | Sort-Object -Property Name

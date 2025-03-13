eac485d1bef0407aecd2c1e42187c8b44387a513
Scott Beddall <scbedd@microsoft.com>
2025-03-12 21:48:08 -0700

    saving progress for the evening

--- eng/common-tests/matrix-generator/pr-matrix-samples/pr-matrix-generation-acceptance.helpers.ps1
+++ eng/common-tests/matrix-generator/pr-matrix-samples/pr-matrix-generation-acceptance.helpers.ps1
@@ -39,6 +39,7 @@ Function Get-Repo {
 
     $tempDir = [System.IO.Path]::GetTempPath()
     $repoPath = Join-Path $tempDir $Repo.Replace("/", "-")
+    $thisRepo = Resolve-Path (Join-Path $PSScriptRoot ".." ".." ".." "..")
 
     if (!(Test-Path $repoPath)) {
         New-Item -ItemType Directory -Path $repoPath | Out-Null
@@ -57,7 +58,7 @@ Function Get-Repo {
             Pop-Location | Out-Null
         }
     }
-    Copy-EngCommon -OutputRepo $repoPath -InputRepo (Resolve-Path (Join-Path $PSScriptRoot ".." ".." ".." ".."))
+    Copy-EngCommon -InputRepo $thisRepo -OutputRepo $repoPath
 
     return $repoPath
 }
@@ -89,4 +90,46 @@ Function Invoke-PackageProps {
     }
 
     return $uniqueTempDir
+}
+
+Function OrderArtifactDetails {
+    param (
+        [PSCustomObject]$PackagePropObject
+    )
+    $sortedDetails = [ordered]@{}
+
+    # Sort the existing hashtable by key name, then rebuild it
+    $PackagePropObject.ArtifactDetails.PSObject.Properties | Sort-Object Name | ForEach-Object {
+        $sortedDetails[$_.Name] = $_.Value
+    }
+
+    $PackagePropObject.ArtifactDetails = $sortedDetails
+}
+
+Function Compare-PackageResults {
+    param(
+        [PSCustomObject[]]$Actual,
+        [PSCustomObject[]]$Expected
+    )
+    # we would LOVE to be able to run a simple comparison here, but the artifact details are not in consistent order (nor should HAVE to be)
+    # so we need to do a little more work to do a valid comparison
+    if ($Actual.Count -ne $Expected.Count) {
+        $packages = ($Actual | ForEach-Object { $_.ArtifactName }) -join ", "
+        $exception = "Expected $($Expected.Count) results, but got $($Actual.Count). Visible Packages: [ $packages ]"
+        throw $exception
+    }
+
+    # sort the results by name so we can compare them easily
+    $sortedActual = $Actual | Sort-Object -Property Name
+    $sortedExpected = $Expected | Sort-Object -Property Name
+
+    for ($i = 0; $i -lt $sortedActual.Count; $i++) {
+        $actual = $sortedActual[$i]
+        $expected = $sortedExpected[$i]
+
+        OrderArtifactDetails -PackagePropObject $actual
+        OrderArtifactDetails -PackagePropObject $expected
+    }
+
+    $sortedActual | Should -Be $sortedExpected
 }
--- eng/common-tests/matrix-generator/pr-matrix-samples/pr-matrix-generation-acceptance.tests.ps1
+++ eng/common-tests/matrix-generator/pr-matrix-samples/pr-matrix-generation-acceptance.tests.ps1
@@ -1,17 +1,14 @@
 Import-Module Pester
 
-
-$NET_REPO = "Azure/azure-sdk-for-net"
-$NET_REPO_REF = "331c07a1ab59ed0042972ca6d0df830df235280f"
-$PYTHON_REPO = "Azure/azure-sdk-for-python"
-$PYTHON_REPO_REF = "d762abb4d84d78b58db91a45646351c5789ae211"
-
 # Load scenarios before we enter the Describe block so they're available for -ForEach
 $netScenarios = Get-Content (Join-Path $PSScriptRoot net_scenarios.json) | ConvertFrom-Json
 $pythonScenarios = Get-Content (Join-Path $PSScriptRoot python_scenarios.json) | ConvertFrom-Json
 
 Describe "Acceptance tests for .NET PR Matrix Generation" -Tag "Integration" {
     BeforeAll {
+        $NET_REPO = "Azure/azure-sdk-for-net"
+        $NET_REPO_REF = "331c07a1ab59ed0042972ca6d0df830df235280f"
+
         . $PSScriptRoot/pr-matrix-generation-acceptance.helpers.ps1
         $RepoRoot = Get-Repo -Repo $NET_REPO -Reference $NET_REPO_REF
     }
@@ -30,6 +27,9 @@ Describe "Acceptance tests for .NET PR Matrix Generation" -Tag "Integration" {
 
 Describe "Acceptance tests for Python PR Matrix Generation" -Tag "Integration" {
     BeforeAll {
+        $PYTHON_REPO_REF = "d762abb4d84d78b58db91a45646351c5789ae211"
+        $PYTHON_REPO = "Azure/azure-sdk-for-python"
+
         . $PSScriptRoot/pr-matrix-generation-acceptance.helpers.ps1
         $RepoRoot = Get-Repo -Repo $PYTHON_REPO -Reference $PYTHON_REPO_REF
     }
@@ -37,12 +37,19 @@ Describe "Acceptance tests for Python PR Matrix Generation" -Tag "Integration" {
     It "Should evaluate python diffs correctly - <name>" -ForEach $pythonScenarios {
         Write-Host "Operating against repo: $RepoRoot"
         $scenario = $_
-        $outputProps = Invoke-PackageProps -InputDiff $scenario.diff -Repo "$RepoRoot"
-        $expectedOutputs = $scenario.expected_package_output | Sort-Object -Property Name
-        $detectedOutputs = Get-ChildItem -Path $outputProps -Recurse -Filter "*.json" -Exclude "pr-diff.json" `
-            | ForEach-Object { Get-Content -Raw $_ | ConvertFrom-Json }
-            | Sort-Object -Property Name
 
-        ($detectedOutputs | ConvertTo-Json -Depth 100) | Should -Be ($expectedOutputs | ConvertTo-Json -Depth 100)
+        if (-not $scenario.diff) {
+            Write-Host "Skipping scenario with no diff"
+            return
+        }
+        else {
+            $outputProps = Invoke-PackageProps -InputDiff $scenario.diff -Repo "$RepoRoot"
+            $expectedOutputs = $scenario.expected_package_output | Sort-Object -Property Name
+            $detectedOutputs = Get-ChildItem -Path $outputProps -Recurse -Filter "*.json" -Exclude "pr-diff.json" `
+                | ForEach-Object { Get-Content -Raw $_ | ConvertFrom-Json }
+                | Sort-Object -Property Name
+
+            Compare-PackageResults -Actual $detectedOutputs -Expected $expectedOutputs
+        }
     }
 }

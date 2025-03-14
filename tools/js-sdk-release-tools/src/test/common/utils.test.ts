e1ebec4c080acb36223497e3a76d6fbff7fe3b54
Kyle Zhang <v-zhanh@microsoft.com>
2025-03-14 17:09:27 +0800

    up cases

--- tools/js-sdk-release-tools/src/test/common/utils.test.ts
+++ tools/js-sdk-release-tools/src/test/common/utils.test.ts
@@ -6,12 +6,13 @@ import { deepStrictEqual, strictEqual } from "assert";
 describe("resolveOptions", () => {
     test("loads config at the given path", async () => {
         const configPath = path.join(__dirname, "config/myConfig.yaml");
+        const emitterOutputDir = path.posix.join(__dirname, "/config").replace(/\\/g, '/');
         const options = await resolveOptions(configPath);
         deepStrictEqual(options.options, {
             "@azure-tools/typespec-autorest": {
                 "azure-resource-provider-folder": "resource-manager",
                 "emitter-output-dir":
-                    "D:/GithubRepos/tmpRepo/azure-sdk-tools/tools/js-sdk-release-tools/src/test/command/config/..",
+                `${emitterOutputDir}/..`,
                 "output-file":
                     "resource-manager/{service-name}/{version-status}/{version}/openapi.json",
             },

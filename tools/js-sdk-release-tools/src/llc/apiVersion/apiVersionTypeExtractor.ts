a45c8fb86fcfb8c6f7f137bf2ae5acf90083cc9a
Wanpeng Li <wanl@microsoft.com>
2025-03-17 17:58:24 +0800

    WIP

--- tools/js-sdk-release-tools/src/llc/apiVersion/apiVersionTypeExtractor.ts
+++ tools/js-sdk-release-tools/src/llc/apiVersion/apiVersionTypeExtractor.ts
@@ -4,6 +4,10 @@ import { ApiVersionType } from "../../common/types";
 import { IApiVersionTypeExtractor } from "../../common/interfaces";
 import { join } from "path";
 import { exists } from "fs-extra";
+import { tryGetNpmView } from "../../common/npmUtils";
+import { getNpmPackageName } from "../../common/utils";
+import { getVersion, isBetaVersion } from "../../utils/version";
+import { logger } from "../../utils/logger";
 
 export const getApiVersionType: IApiVersionTypeExtractor = async (
     packageRoot: string
@@ -15,11 +19,17 @@ export const getApiVersionType: IApiVersionTypeExtractor = async (
         if (typeFromClient !== ApiVersionType.None) return typeFromClient;
     }
     
-    const parametersPath = join(packageRoot, "src/parameters.ts");
-    if (!(await exists(parametersPath))) {
-        throw new Error(`Failed to find parameters file '${parametersPath}'.`);
+    logger.info('No client found, fallback to get api version type in operation\'s parameter');
+    const parametersFolder = ["src/", "generated/", "src/generated"];
+    for (const folder of parametersFolder) {
+        const typeFromOperations = getApiVersionTypeFromOperations(packageRoot, folder, findParametersPath);
+        if (typeFromOperations !== ApiVersionType.None) return typeFromOperations;
     }
-    const typeFromOperations = getApiVersionTypeFromOperations(parametersPath);
-    if (typeFromOperations !== ApiVersionType.None) return typeFromOperations;
-    return ApiVersionType.Stable;
+
+    logger.info('No operation found, fallback to get api version type from latest version in NPM');
+    const packageName = getNpmPackageName(packageRoot);
+    const npmViewResult = await tryGetNpmView(packageName);
+    const latestVersion = getVersion(npmViewResult, "latest");
+    const isBeta = isBetaVersion(latestVersion);
+    return isBeta ? ApiVersionType.Preview : ApiVersionType.Stable;
 };

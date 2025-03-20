9e6154a6e53eef329d43f3229f6da163fcc6ba8b
Kyle Zhang <v-zhanh@microsoft.com>
2025-03-20 18:07:59 +0800

    up

--- tools/js-sdk-release-tools/src/utils/version.ts
+++ tools/js-sdk-release-tools/src/utils/version.ts
@@ -1,6 +1,6 @@
 import {logger} from "./logger.js";
 import pkg from 'semver';
-const { semverInc } = pkg;
+const { inc: semverInc } = pkg;
 
 export function getVersion(npmViewResult: Record<string, any> | undefined, tag: string) {
     const distTags: Record<string, any> | undefined = npmViewResult?.['dist-tags'];

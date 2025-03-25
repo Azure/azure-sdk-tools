7b6bb9f80790acfc0b2d6f84c0f03ab7f09498f6
Scott Beddall <scbedd@microsoft.com>
2025-03-24 18:08:53 -0700

    populating our scenarios fully. preparing to make the go changes

--- eng/common-tests/matrix-generator/pr-matrix-samples/pr-matrix-generation-acceptance.helpers.ps1
+++ eng/common-tests/matrix-generator/pr-matrix-samples/pr-matrix-generation-acceptance.helpers.ps1
@@ -1,5 +1,36 @@
 $PackagePropertiesPath = Join-Path "eng" "common" "scripts" "Save-Package-Properties.ps1"
 
+Function Copy-EngCommon {
+    param (
+        [string]$OutputRepo,
+        [string]$InputRepo
+    )
+    Set-StrictMode -Version 3.0
+    $ErrorActionPreference = "Stop"
+
+    $InputRepo = Resolve-Path $InputRepo
+    $OutputRepo = Resolve-Path $OutputRepo
+
+    $inputFiles = Get-ChildItem -Path (Join-Path $InputRepo "eng/common") -Recurse -File
+
+    foreach ($file in $inputFiles) {
+        $relativePath = $file.FullName.Substring($InputRepo.Length + 1)
+        $outputPath = Join-Path $OutputRepo $relativePath
+
+        if (-not (Test-Path $outputPath) -or
+            (Compare-Object (Get-Content $file.FullName) (Get-Content $outputPath))) {
+
+            $outputDirectory = Split-Path -Parent $outputPath
+            if (-not (Test-Path $outputDirectory)) {
+                New-Item -ItemType Directory -Path $outputDirectory -Force | Out-Null
+            }
+
+            Copy-Item -Path $file.FullName -Destination $outputPath -Force
+            Write-Host "Copied $relativePath"
+        }
+    }
+}
+
 Function Get-Repo {
     param (
         [string]$Repo,
@@ -8,20 +39,26 @@ Function Get-Repo {
 
     $tempDir = [System.IO.Path]::GetTempPath()
     $repoPath = Join-Path $tempDir $Repo.Replace("/", "-")
+    $thisRepo = Resolve-Path (Join-Path $PSScriptRoot ".." ".." ".." "..")
 
     if (!(Test-Path $repoPath)) {
         New-Item -ItemType Directory -Path $repoPath | Out-Null
 
         try {
+            Write-Host "Push-Location $repoPath"
             Push-Location $repoPath
+            Write-Host "git clone --no-checkout `"https://github.com/$Repo.git`" ."
             git clone --no-checkout "https://github.com/$Repo.git" .
+            Write-Host "git fetch origin $Reference"
             git fetch origin $Reference
+            Write-Host "git checkout $Reference"
             git checkout $Reference
         }
         finally {
             Pop-Location | Out-Null
         }
     }
+    Copy-EngCommon -InputRepo $thisRepo -OutputRepo $repoPath
 
     return $repoPath
 }
@@ -40,12 +77,56 @@ Function Invoke-PackageProps {
     $InputDiff | ConvertTo-Json -Depth 100 | Set-Content -Path $prDiffFile -Force
 
     try {
+        Write-Host "Push-Location $Repo"
         Push-Location $Repo
+        Write-Host "&`"$PackagePropertiesPath`" -outDirectory `"$uniqueTempDir`" -prDiff `"$prDiffFile`""
+        Write-Host "Package Diff:"
+        Write-Host (Get-Content -Raw $prDiffFile)
         &"$PackagePropertiesPath" -outDirectory "$uniqueTempDir" -prDiff "$prDiffFile"
+        Remove-Item $prDiffFile -Force
     }
     finally {
         Pop-Location | Out-Null
     }
 
     return $uniqueTempDir
+}
+
+Function OrderArtifactDetails {
+    param (
+        [PSCustomObject]$PackagePropObject
+    )
+    $sortedDetails = [ordered]@{}
+
+    # Sort the existing hashtable by key name, then rebuild it
+    $PackagePropObject.ArtifactDetails.PSObject.Properties | Sort-Object Name | ForEach-Object {
+        $sortedDetails[$_.Name] = $_.Value
+    }
+
+    $PackagePropObject.ArtifactDetails = $sortedDetails
+}
+
+Function Compare-PackageResults {
+    param(
+        [PSCustomObject[]]$Actual,
+        [PSCustomObject[]]$Expected
+    )
+    # we would LOVE to be able to run a simple comparison here, but the artifact details are not in consistent order (nor should HAVE to be)
+    # so we need to do a little more work to do a valid comparison
+    if ($Actual.Count -ne $Expected.Count) {
+        $packages = ($Actual | ForEach-Object { $_.ArtifactName }) -join ", "
+        $exception = "Expected $($Expected.Count) results, but got $($Actual.Count). Visible Packages: [ $packages ]"
+        throw $exception
+    }
+
+    # sort the results by name so we can compare them easily
+    $sortedActual = $Actual | Sort-Object -Property Name
+    $sortedExpected = $Expected | Sort-Object -Property Name
+
+    for ($i = 0; $i -lt $sortedActual.Count; $i++) {
+        OrderArtifactDetails -PackagePropObject $sortedActual[$i]
+        OrderArtifactDetails -PackagePropObject $sortedExpected[$i]
+    }
+
+    $sortedActual | ConvertTo-Json -Depth 100 | Should -Be ($sortedExpected | ConvertTo-Json -Depth 100)
 }
--- eng/common-tests/matrix-generator/pr-matrix-samples/pr-matrix-generation-acceptance.tests.ps1
+++ eng/common-tests/matrix-generator/pr-matrix-samples/pr-matrix-generation-acceptance.tests.ps1
@@ -3,14 +3,20 @@ Import-Module Pester
 # Load scenarios before we enter the Describe block so they're available for -ForEach
 $netScenarios = Get-Content (Join-Path $PSScriptRoot net_scenarios.json) | ConvertFrom-Json
 $pythonScenarios = Get-Content (Join-Path $PSScriptRoot python_scenarios.json) | ConvertFrom-Json
+$jsScenarios = Get-Content (Join-Path $PSScriptRoot js_scenarios.json) | ConvertFrom-Json
+$goScenarios = Get-Content (Join-Path $PSScriptRoot go_scenarios.json) | ConvertFrom-Json
+$javaScenarios = Get-Content (Join-Path $PSScriptRoot java_scenarios.json) | ConvertFrom-Json
 
-Describe "Acceptance tests for .NET PR Matrix Generation" {
+Describe "Acceptance tests for .NET PR Matrix Generation" -Tag "Integration" {
     BeforeAll {
+        $NET_REPO = "Azure/azure-sdk-for-net"
+        $NET_REPO_REF = "331c07a1ab59ed0042972ca6d0df830df235280f"
+
         . $PSScriptRoot/pr-matrix-generation-acceptance.helpers.ps1
-        $RepoRoot = Get-Repo -Repo "Azure/azure-sdk-for-net" -Reference "331c07a1ab59ed0042972ca6d0df830df235280f"
+        $RepoRoot = Get-Repo -Repo $NET_REPO -Reference $NET_REPO_REF
     }
 
-    It "Should evaluate .NET core diffs correctly - <name>" -ForEach $netScenarios {
+    It "Should evaluate targeted .NET packages correctly - <name>" -ForEach $netScenarios {
         $scenario = $_
         $outputProps = Invoke-PackageProps -InputDiff $scenario.diff -Repo "$RepoRoot"
         $expectedOutputs = $scenario.expected_package_output | Sort-Object -Property Name
@@ -22,20 +28,118 @@ Describe "Acceptance tests for .NET PR Matrix Generation" {
     }
 }
 
-Describe "Acceptance tests for Python PR Matrix Generation" {
+Describe "Acceptance tests for Python PR Matrix Generation" -Tag "Integration" {
     BeforeAll {
+        $PYTHON_REPO_REF = "7656cf20f78b7653522040e372a37ff03338b1a2"
+        $PYTHON_REPO = "Azure/azure-sdk-for-python"
+
         . $PSScriptRoot/pr-matrix-generation-acceptance.helpers.ps1
-        $RepoRoot = Get-Repo -Repo "Azure/azure-sdk-for-python" -Reference "516977e3a0f9ba22d5611608f64b836fefffc37e"
+        $RepoRoot = Get-Repo -Repo $PYTHON_REPO -Reference $PYTHON_REPO_REF
     }
 
-    It "Should evaluate python diffs correctly - <name>" -ForEach $pythonScenarios {
+    It "Should evaluate targeted pyton packages correctly - <name>" -ForEach $pythonScenarios {
+        Write-Host "Operating against repo: $RepoRoot"
         $scenario = $_
-        $outputProps = Invoke-PackageProps -InputDiff $scenario.diff -Repo "$RepoRoot"
-        $expectedOutputs = $scenario.expected_package_output | Sort-Object -Property Name
-        $detectedOutputs = Get-ChildItem -Path $outputProps -Recurse -Filter "*.json" -Exclude "pr-diff.json" `
-            | ForEach-Object { Get-Content -Raw $_ | ConvertFrom-Json }
-            | Sort-Object -Property Name
 
-        ($detectedOutputs | ConvertTo-Json -Depth 100) | Should -Be ($expectedOutputs | ConvertTo-Json -Depth 100)
+        if (-not $scenario.diff) {
+            Write-Host "Skipping scenario with no diff"
+            return
+        }
+        else {
+            $outputProps = Invoke-PackageProps -InputDiff $scenario.diff -Repo "$RepoRoot"
+            $expectedOutputs = $scenario.expected_package_output | Sort-Object -Property Name
+            $detectedOutputs = Get-ChildItem -Path $outputProps -Recurse -Filter "*.json" -Exclude "pr-diff.json" `
+                | ForEach-Object { Get-Content -Raw $_ | ConvertFrom-Json }
+                | Sort-Object -Property Name
+
+            Compare-PackageResults -Actual $detectedOutputs -Expected $expectedOutputs
+        }
+    }
+}
+
+Describe "Acceptance tests for JS PR Matrix Generation" -Tag "Integration" {
+    BeforeAll {
+        $JS_REPO_REF = "e2598ca60018edc7b0c3a5b3a28ae7fb40b85894"
+        $JS_REPO = "Azure/azure-sdk-for-js"
+
+        . $PSScriptRoot/pr-matrix-generation-acceptance.helpers.ps1
+        $RepoRoot = Get-Repo -Repo $JS_REPO -Reference $JS_REPO_REF
+    }
+
+    It "Should evaluate targeted js packages correctly - <name>" -ForEach $jsScenarios {
+        Write-Host "Operating against repo: $RepoRoot"
+        $scenario = $_
+
+        if (-not $scenario.diff) {
+            Write-Host "Skipping scenario with no diff"
+            return
+        }
+        else {
+            $outputProps = Invoke-PackageProps -InputDiff $scenario.diff -Repo "$RepoRoot"
+            $expectedOutputs = $scenario.expected_package_output | Sort-Object -Property Name
+            $detectedOutputs = Get-ChildItem -Path $outputProps -Recurse -Filter "*.json" -Exclude "pr-diff.json" `
+                | ForEach-Object { Get-Content -Raw $_ | ConvertFrom-Json }
+                | Sort-Object -Property Name
+
+            Compare-PackageResults -Actual $detectedOutputs -Expected $expectedOutputs
+        }
+    }
+}
+
+Describe "Acceptance tests for Go PR Matrix Generation" -Tag "Integration" {
+    BeforeAll {
+        $GO_REPO_REF = "524f41ef0bdb788e8df3e77fcf47f982421ee5d9"
+        $GO_REPO = "Azure/azure-sdk-for-go"
+
+        . $PSScriptRoot/pr-matrix-generation-acceptance.helpers.ps1
+        $RepoRoot = Get-Repo -Repo $GO_REPO -Reference $GO_REPO_REF
+    }
+
+    It "Should evaluate targeted go packages correctly - <name>" -ForEach $goScenarios {
+        Write-Host "Operating against repo: $RepoRoot"
+        $scenario = $_
+
+        if (-not $scenario.diff) {
+            Write-Host "Skipping scenario with no diff"
+            return
+        }
+        else {
+            $outputProps = Invoke-PackageProps -InputDiff $scenario.diff -Repo "$RepoRoot"
+            $expectedOutputs = $scenario.expected_package_output | Sort-Object -Property Name
+            $detectedOutputs = Get-ChildItem -Path $outputProps -Recurse -Filter "*.json" -Exclude "pr-diff.json" `
+                | ForEach-Object { Get-Content -Raw $_ | ConvertFrom-Json }
+                | Sort-Object -Property Name
+
+            Compare-PackageResults -Actual $detectedOutputs -Expected $expectedOutputs
+        }
+    }
+}
+
+Describe "Acceptance tests for Java PR Matrix Generation" -Tag "Integration" {
+    BeforeAll {
+        $JAVA_REPO_REF = "296f1fad306a49601ec61280eb4af1f33934ccde"
+        $JAVA_REPO = "Azure/azure-sdk-for-java"
+
+        . $PSScriptRoot/pr-matrix-generation-acceptance.helpers.ps1
+        $RepoRoot = Get-Repo -Repo $JAVA_REPO -Reference $JAVA_REPO_REF
+    }
+
+    It "Should evaluate targeted java packages correctly - <name>" -ForEach $javaScenarios {
+        Write-Host "Operating against repo: $RepoRoot"
+        $scenario = $_
+
+        if (-not $scenario.diff) {
+            Write-Host "Skipping scenario with no diff"
+            return
+        }
+        else {
+            $outputProps = Invoke-PackageProps -InputDiff $scenario.diff -Repo "$RepoRoot"
+            $expectedOutputs = $scenario.expected_package_output | Sort-Object -Property Name
+            $detectedOutputs = Get-ChildItem -Path $outputProps -Recurse -Filter "*.json" -Exclude "pr-diff.json" `
+                | ForEach-Object { Get-Content -Raw $_ | ConvertFrom-Json }
+                | Sort-Object -Property Name
+
+            Compare-PackageResults -Actual $detectedOutputs -Expected $expectedOutputs
+        }
     }
 }

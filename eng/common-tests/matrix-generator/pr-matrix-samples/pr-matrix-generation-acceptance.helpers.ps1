b9b42115791839c706962bb35d51b8384351f669
Scott Beddall <45376673+scbedd@users.noreply.github.com>
2025-04-01 15:42:35 -0700

    Add additional handling for Create-PRJobMatrix for `go`, Integrate `save-package-properties` acceptance tests into `eng/common` test run (#10038)
    * add a category to parse triggerPaths when there is no artifact present (this is the go use-case)
    * add integration tests for Save-Package-Properties scenarios
    * bugfixes around various situations in new PackageProps class

--- eng/common-tests/matrix-generator/pr-matrix-samples/pr-matrix-generation-acceptance.helpers.ps1
+++ eng/common-tests/matrix-generator/pr-matrix-samples/pr-matrix-generation-acceptance.helpers.ps1
@@ -1,5 +1,36 @@
 $PackagePropertiesPath = Join-Path "eng" "common" "scripts" "Save-Package-Properties.ps1"
 
+Function Copy-EngCommon {
+    param (
+        [string]$OutputRepo,
+        [string]$InputRepo
+    )
+    Set-StrictMode -Version 3.0
+    $ErrorActionPreference = "Stop"
+
+    $InputRepo = Resolve-Path $InputRepo
+    $OutputRepo = Resolve-Path $OutputRepo
+
+    $inputFiles = Get-ChildItem -Path (Join-Path $InputRepo "eng/common") -Recurse -File
+
+    foreach ($file in $inputFiles) {
+        $relativePath = $file.FullName.Substring($InputRepo.Length + 1)
+        $outputPath = Join-Path $OutputRepo $relativePath
+
+        if (-not (Test-Path $outputPath) -or
+            (Compare-Object (Get-Content $file.FullName) (Get-Content $outputPath))) {
+
+            $outputDirectory = Split-Path -Parent $outputPath
+            if (-not (Test-Path $outputDirectory)) {
+                New-Item -ItemType Directory -Path $outputDirectory -Force | Out-Null
+            }
+
+            Copy-Item -Path $file.FullName -Destination $outputPath -Force
+            Write-Host "Copied $relativePath"
+        }
+    }
+}
+
 Function Get-Repo {
     param (
         [string]$Repo,
@@ -8,20 +39,30 @@ Function Get-Repo {
 
     $tempDir = [System.IO.Path]::GetTempPath()
     $repoPath = Join-Path $tempDir $Repo.Replace("/", "-")
+    $thisRepo = Resolve-Path (Join-Path $PSScriptRoot ".." ".." ".." "..")
 
     if (!(Test-Path $repoPath)) {
         New-Item -ItemType Directory -Path $repoPath | Out-Null
 
         try {
+            Write-Host "Push-Location $repoPath"
             Push-Location $repoPath
-            git clone --no-checkout "https://github.com/$Repo.git" .
-            git fetch origin $Reference
-            git checkout $Reference
+            Write-Host "git clone --no-checkout --filter=tree:0 `"https://github.com/$Repo.git`" ."
+            git clone --no-checkout --filter=tree:0 "https://github.com/$Repo.git" .
+            Write-Host "git config gc.auto"
+            git config gc.auto
+            Write-Host "git sparse-checkout init"
+            git sparse-checkout init
+            Write-Host "git sparse-checkout set --no-cone '/*'"
+            git sparse-checkout set --no-cone '/*'
+            Write-Host "git -c advice.detachedHead=false checkout $Reference"
+            git -c advice.detachedHead=false checkout $Reference
         }
         finally {
             Pop-Location | Out-Null
         }
     }
+    Copy-EngCommon -InputRepo $thisRepo -OutputRepo $repoPath
 
     return $repoPath
 }
@@ -33,19 +74,90 @@ Function Invoke-PackageProps {
     )
 
     $env:GITHUB_ACTIONS="true"
-    $uniqueTempDir = Join-Path -Path ([System.IO.Path]::GetTempPath()) -ChildPath ([System.IO.Path]::GetRandomFileName())
-    New-Item -Path $uniqueTempDir -ItemType Directory | Out-Null
+    $PackageInfoFolder = Join-Path $Repo "PackageInfo"
 
-    $prDiffFile = Join-Path $uniqueTempDir "pr-diff.json"
+    if (Test-Path $PackageInfoFolder) {
+        Remove-Item -Path $PackageInfoFolder -Recurse -Force
+    }
+
+    $prDiffFile = Join-Path $Repo "pr-diff.json"
     $InputDiff | ConvertTo-Json -Depth 100 | Set-Content -Path $prDiffFile -Force
 
     try {
+        Write-Host "Push-Location $Repo"
         Push-Location $Repo
-        &"$PackagePropertiesPath" -outDirectory "$uniqueTempDir" -prDiff "$prDiffFile"
+        Write-Host "&`"$PackagePropertiesPath`" -outDirectory `"$PackageInfoFolder`" -prDiff `"$prDiffFile`""
+        Write-Host "Package Diff:"
+        Write-Host (Get-Content -Raw $prDiffFile)
+        &"$PackagePropertiesPath" -outDirectory "$PackageInfoFolder" -prDiff "$prDiffFile"
+        Remove-Item $prDiffFile -Force
     }
     finally {
         Pop-Location | Out-Null
     }
 
-    return $uniqueTempDir
+    return $PackageInfoFolder
+}
+
+function Order-JsonObject {
+    param (
+        [Parameter(Mandatory = $true)]
+        [hashtable]$JsonObject
+    )
+
+    $orderedObject = @{}
+
+    foreach ($key in ($JsonObject.Keys | Sort-Object)) {
+        $value = $JsonObject[$key]
+
+        if ($value -is [hashtable]) {
+            $orderedObject[$key] = Order-JsonObject -JsonObject $value
+        } elseif ($value -is [array]) {
+            $orderedArray = @()
+            foreach ($item in $value | Sort-Object) {
+                if ($item -is [hashtable]) {
+                    $orderedArray += Order-JsonObject -JsonObject $item
+                } else {
+                    $orderedArray += $item
+                }
+            }
+            $orderedObject[$key] = $orderedArray
+        } else {
+            $orderedObject[$key] = $value
+        }
+    }
+
+    return $orderedObject
+}
+
+Function Compare-PackageResults {
+    param(
+        [HashTable[]]$Actual,
+        [HashTable[]]$Expected
+    )
+    # we would LOVE to be able to run a simple comparison here, but the artifact details are not in consistent order (nor should HAVE to be)
+    # so we need to do a little more work to do a valid comparison
+    if ($Actual.Count -ne $Expected.Count) {
+        $packages = ($Actual | ForEach-Object { $_.ArtifactName }) -join ", "
+        $exception = "Expected $($Expected.Count) results, but got $($Actual.Count). Visible Packages: [ $packages ]"
+        throw $exception
+    }
+
+    # sort the results by name so we can compare them easily
+    $sortedActual = $Actual | Sort-Object -Property Name
+    $sortedExpected = $Expected | Sort-Object -Property Name
+
+    if ($sortedActual -is [HashTable]) {
+        $sortedActual = Order-JsonObject $sortedActual
+        $sortedExpected = Order-JsonObject $sortedExpected
+    }
+    else {
+        for ($i = 0; $i -lt $sortedActual.Count; $i++) {
+            $sortedActual[$i] = Order-JsonObject $sortedActual[$i]
+            $sortedExpected[$i] = Order-JsonObject $sortedExpected[$i]
+        }
+    }
+
+
+    $sortedActual | ConvertTo-Json -Depth 100 | Should -Be ($sortedExpected | ConvertTo-Json -Depth 100)
 }

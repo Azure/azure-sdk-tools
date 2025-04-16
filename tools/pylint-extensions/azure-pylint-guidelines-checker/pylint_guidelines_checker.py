d7ef0d469f4f52e8b6c4c251b08a637f66d05ad6
Libba Lawrence <llawrence@microsoft.com>
2025-04-16 15:18:07 -0700

    b

--- tools/pylint-extensions/azure-pylint-guidelines-checker/pylint_guidelines_checker.py
+++ tools/pylint-extensions/azure-pylint-guidelines-checker/pylint_guidelines_checker.py
@@ -2879,7 +2879,7 @@ class DoNotImportAsyncio(BaseChecker):
         """Check that we aren't importing asyncio."""
         try:
             for name, _ in node.names:
-                if name == "asyncio" and not node.root().name.startswith(self.IGNORE_PACKAGES):
+                if name == "asyncio" and not any(node.root().name.startswith(pkg) for pkg in self.IGNORE_PACKAGES):
                     self.add_message(
                         msgid=f"do-not-import-asyncio",
                         node=node,

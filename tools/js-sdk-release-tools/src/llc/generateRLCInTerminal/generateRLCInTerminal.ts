cc6d59bcaffb9ec75cd977837a52b585522563f7
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-04-18 14:35:35 +0800

    update script

--- tools/js-sdk-release-tools/src/llc/generateRLCInTerminal/generateRLCInTerminal.ts
+++ tools/js-sdk-release-tools/src/llc/generateRLCInTerminal/generateRLCInTerminal.ts
@@ -25,7 +25,7 @@ export async function buildGeneratedCodes(sdkrepo: string, packagePath: string, 
     logger.info(`Start to build '${packageName}', except for tests and samples, which may be written manually`);
     // To build generated codes except test and sample, we need to change tsconfig.json.
     changeConfigOfTestAndSample(packagePath, ChangeModel.Change, SdkType.Rlc);
-    execSync(`node common/scripts/install-run-rush.js build -t ${packageName}`, {stdio: 'inherit'});
+    execSync(`node common/scripts/install-run-rush.js build -t ${packageName} --verbose`, {stdio: 'inherit'});
     changeConfigOfTestAndSample(packagePath, ChangeModel.Revert, SdkType.Rlc);
     shell.cd(packagePath);
     logger.info(`Start to Generate changelog.`);

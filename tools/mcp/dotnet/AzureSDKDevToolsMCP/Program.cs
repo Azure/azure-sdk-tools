3740ee9b64fb56d836a97cb095b64094862bbd18
Praven Kuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2025-04-23 11:27:53 -0400

    MCP servers to support agent based TypeSpec to SDK release process (#10340)
    * MCP Dev tools  to run TypeSpec SDK workflow

---
+++ tools/mcp/dotnet/AzureSDKDevToolsMCP/Program.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using AzureSDKDevToolsMCP.Tools;
+using AzureSDKDevToolsMCP.Helpers;
+using AzureSDKDevToolsMCP.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Server;
+using AzureSDKDSpecTools.Services;
+using AzureSDKDSpecTools.Helpers;
+using Microsoft.Extensions.Logging;
+
+
+var builder = Host.CreateApplicationBuilder(args);
+
+
+#pragma warning disable IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
+builder.Services
+ .AddMcpServer()
+ .WithStdioServerTransport()
+ .WithToolsFromAssembly();
+#pragma warning restore IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
+
+builder.Services.AddLogging(configure => configure.AddConsole());
+builder.Services.AddSingleton<IGitHubService, GitHubService>();
+builder.Services.AddSingleton<IGitHelper, GitHelper>();
+builder.Services.AddSingleton<IDevOpsService, DevOpsService>();
+builder.Services.AddSingleton<ITypeSpecHelper, TypeSpecHelper>();
+await builder.Build().RunAsync();

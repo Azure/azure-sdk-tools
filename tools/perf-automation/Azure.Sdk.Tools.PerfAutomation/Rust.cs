8dd6177957b1bf3a9472732fa55071658052f9cb
George Arama <gearama@microsoft.com>
2025-04-24 16:01:25 -0700

    commennts

--- tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/Rust.cs
+++ tools/perf-automation/Azure.Sdk.Tools.PerfAutomation/Rust.cs
@@ -51,11 +51,8 @@ namespace Azure.Sdk.Tools.PerfAutomation
             IDictionary<string, string> packageVersions,
             bool debug)
         {
+            // just make sure we have the target directory clened up of previous results in case of a test issue in a previous test / run
             _targetResultsDirectory = await CleanupAsync(project);
-
-            var outputBuilder = new StringBuilder();
-            var errorBuilder = new StringBuilder();
-
             return (String.Empty, String.Empty, String.Empty);
         }
         public override async Task<IterationResult> RunAsync(
@@ -69,6 +66,7 @@ namespace Azure.Sdk.Tools.PerfAutomation
             string profilerOptions,
             object context)
         {
+            // set up the params for cargo
             string finalParams = $"bench -- \"{testName}\"";
             ProcessResult result = new ProcessResult(0, String.Empty, String.Empty);
             if (IsTest)
@@ -86,7 +84,7 @@ namespace Azure.Sdk.Tools.PerfAutomation
                 result = await Util.RunAsync(_cargoName, finalParams, WorkingDirectory);
             }
 
-            //time: [48.680 µs 48.913 µs 49.152 µs] 
+            //parse the samples file for the test and calculate the ops per second
             var opsPerSecond = ExtractOpsPerSecond(testName);
 
             return new IterationResult
@@ -100,6 +98,7 @@ namespace Azure.Sdk.Tools.PerfAutomation
 
         public override async Task<string> CleanupAsync(string project)
         {
+            // we need to find the folder that contains SDK  directory. SDK is a sibling of target
             var currentDirectory = WorkingDirectory;
             bool sdkFolderFound = false;
             while (!sdkFolderFound)
@@ -114,7 +113,7 @@ namespace Azure.Sdk.Tools.PerfAutomation
                     currentDirectory = (new DirectoryInfo(currentDirectory)).Parent.FullName;
                 }
             }
-
+            // now that we have the path to the target directory wwe cand determine the results directory .../target/criterion
             var resultsDirectory = Path.Combine(currentDirectory, _resultsDirectory);
 
             if (IsTest)
@@ -125,28 +124,33 @@ namespace Azure.Sdk.Tools.PerfAutomation
             {
                 Util.DeleteIfExists(resultsDirectory);
             }
+            // return the results directory so we can use it later on and avoid the discovery
             return resultsDirectory;
         }
 
         private double ExtractOpsPerSecond(string testName)
         {
-            var results = ParseFromJsonFile(Path.Combine(_targetResultsDirectory,testName,"new\\sample.json"));
+            // the results are in the target directory under target/criterion/<testName>/new
+            var results = ParseFromJsonFile(Path.Combine(_targetResultsDirectory, testName, "new\\sample.json"));
             double nanos = 0.0;
-            for(int i =0; i< results.Iters.Length; i++)
+            // the timings are in nanos, also we have to divide the time by the number of iterations to get the time for one operation
+            for (int i = 0; i < results.Iters.Length; i++)
             {
-                nanos = (nanos*i + (results.Times[i] / results.Iters[i])) / (i + 1);
+                nanos = (nanos * i + (results.Times[i] / results.Iters[i])) / (i + 1);
             }
             // once we have the timing of one operation( in nano seconds) divinde one sec in nanos by the time to get ops/sec
-            return Math.Pow(10,9)/nanos;
+            return Math.Pow(10, 9) / nanos;
         }
         private Models.Rust.Samples ParseFromJsonFile(string filePath)
         {
+            // open the file 
             if (!File.Exists(filePath))
             {
                 throw new FileNotFoundException($"The file '{filePath}' does not exist.");
             }
 
             var jsonContent = File.ReadAllText(filePath);
+            // deserialize in a Samples struct
             return JsonSerializer.Deserialize<Models.Rust.Samples>(jsonContent, new JsonSerializerOptions
             {
                 PropertyNameCaseInsensitive = true // Allows case-insensitive matching of JSON property names

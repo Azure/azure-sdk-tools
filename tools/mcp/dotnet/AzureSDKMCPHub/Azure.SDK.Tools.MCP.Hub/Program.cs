d6ade2a02a90d7c408dbabdc607a9ba7b8bf65b3
Ben Broderick Phillips <bebroder@microsoft.com>
2025-04-30 19:23:48 -0400

    Add DI/sse dependencies and update mcp builder run

--- tools/mcp/dotnet/AzureSDKMCPHub/Azure.SDK.Tools.MCP.Hub/Program.cs
+++ tools/mcp/dotnet/AzureSDKMCPHub/Azure.SDK.Tools.MCP.Hub/Program.cs
@@ -1,37 +1,40 @@
+using Azure.SDK.Tools.MCP.Hub.Services.Azure;
 
-using System.Reflection;
-using Azure.SDK.Tools.MCP.Contract;
+namespace Azure.SDK.Tools.MCP.Hub;
 
-namespace Azure.SDK.Tools.MCP.Hub
+public sealed class Program
 {
-    public sealed class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            // todo: parse a command bundle here. pass it to CreateHostBuilder once we have an actual type
-            // todo: can we have a "start" verb and a <tool> verb? EG if someone calls <server.exe> HelloWorld
-            //   "This is a hello world input" -> we invoke just that tool
-            //   "<server.exe> start" -> runs server responding to vscode copilot chat 
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static HostApplicationBuilder CreateHostBuilder(string[] args)
-        {
-            // todo: implement our own module discovery that takes the `--tools` or `--tools-exclude` when booting
-            var builder = Host.CreateApplicationBuilder(args);
-            builder.Logging.AddConsole(consoleLogOptions =>
-            {
-                consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Error;
-            });
-            builder.Services
-                .AddMcpServer()
-                .WithStdioServerTransport()
-                // todo: we can definitely honor the --tools param here to filter down the provided tools
-                // for now, lets just use WithtoolsFromAssembly to actually run this thing
-                .WithToolsFromAssembly();
-
-            return builder;
-        }
-
+        // todo: parse a command bundle here. pass it to CreateHostBuilder once we have an actual type
+        // todo: can we have a "start" verb and a <tool> verb? EG if someone calls <server.exe> HelloWorld
+        //   "This is a hello world input" -> we invoke just that tool
+        //   "<server.exe> start" -> runs server responding to vscode copilot chat
+        var host = CreateAppBuilder(args).Build();
+        host.MapMcp();
+        host.Run();
     }
+
+    public static WebApplicationBuilder CreateAppBuilder(string[] args)
+    {
+        // todo: implement our own module discovery that takes the `--tools` or `--tools-exclude` when booting
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Logging.AddConsole(consoleLogOptions =>
+        {
+            consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Error;
+        });
+
+        builder.Services.AddSingleton<IAzureService, AzureService>();
+
+        builder.Services
+            .AddMcpServer()
+            //.WithStdioServerTransport()
+            .WithHttpTransport()
+            // todo: we can definitely honor the --tools param here to filter down the provided tools
+            // for now, lets just use WithtoolsFromAssembly to actually run this thing
+            .WithToolsFromAssembly();
+
+        return builder;
+    }
+
 }

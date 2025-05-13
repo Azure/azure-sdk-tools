eb7fa6dfc67e52822d58484c084ea20fa46b29ed
Scott Beddall <45376673+scbedd@users.noreply.github.com>
2025-05-12 17:34:24 -0700

    Refactor to a single ServiceCollection (#10595)
    * HostServerTool now inherits the ServerApp built at runtime

--- tools/azsdk-cli/Azure.Sdk.Tools.Cli/Program.cs
+++ tools/azsdk-cli/Azure.Sdk.Tools.Cli/Program.cs
@@ -10,18 +10,8 @@ public class Program
 {
     public static async Task<int> Main(string[] args)
     {
-        var services = new ServiceCollection();
-        services.AddLogging(builder =>
-        {
-            builder.AddConsole();
-            builder.SetMinimumLevel(LogLevel.Information);
-        });
-        services.AddSingleton<CommandFactory>();
-        ServiceRegistrations.RegisterCommonServices(services);
-
-        var serviceProvider = services.BuildServiceProvider();
-        var commandFactory = serviceProvider.GetRequiredService<CommandFactory>();
-        var rootCommand = commandFactory.CreateRootCommand(args);
+        ServerApp = CreateAppBuilder(args).Build();
+        var rootCommand = CommandFactory.CreateRootCommand(args, ServerApp.Services);
 
         var parsedCommands = new CommandLineBuilder(rootCommand)
                .UseDefaults()            // adds help, version, error reporting, suggestions…
@@ -29,5 +19,63 @@ public class Program
                .Build();
 
         return await parsedCommands.InvokeAsync(args);
+    }
+
+    // todo: make this honor subcommands of `start` and the like, instead of simply looking presence of `start` verb
+    public static bool IsCLI(string[] args) => !args.Select(x => x.Trim().ToLowerInvariant()).Any(x => x == "start");
+
+    public static WebApplication ServerApp;
+
+    public static WebApplicationBuilder CreateAppBuilder(string[] args)
+    {
+        var isCLI = IsCLI(args);
+
+        // Any args that ASP.NET doesn't recognize will be _ignored_ by the CreateBuilder, so we don't need to ONLY
+        // pass unmatched ASP.NET config values like --ASPNET_URLS to the builder. It'll just quietly ignore everything
+        // it doesn't recognize.
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        builder.Logging.AddConsole(consoleLogOptions =>
+        {
+            consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Error;
+        });
+        
+        // register common services
+        ServiceRegistrations.RegisterCommonServices(builder.Services);
+
+        // add the console logger
+        builder.Services.AddLogging(l =>
+        {
+            l.AddConsole();
+            l.SetMinimumLevel(LogLevel.Information);
+        });
+
+        if (isCLI)
+        {
+            // todo: register the command line formatter
+        }
+        else
+        {
+            // todo: register the server formatter
+        }
+
+        var toolTypes = SharedOptions.GetFilteredToolTypes(args);
+        if (toolTypes.Count == 0)
+        {
+            builder.Services
+                .AddMcpServer()
+                .WithStdioServerTransport()
+                .WithToolsFromAssembly();
+        }
+        else
+        {
+
+            builder.Services
+                .AddMcpServer()
+                .WithStdioServerTransport()
+                .WithTools(toolTypes);
+        }
+
+        return builder;
     }
 }
--- tools/azsdk-cli/Azure.Sdk.Tools.Cli/Tools/HostServer/HostServerTool.cs
+++ tools/azsdk-cli/Azure.Sdk.Tools.Cli/Tools/HostServer/HostServerTool.cs
@@ -11,7 +11,6 @@ namespace Azure.Sdk.Tools.Cli.Tools.HostServer
     public class HostServerTool : MCPTool
     {
         private readonly ILogger<HostServerTool> _logger;
-        private readonly Argument<string[]> _unmatchedAspNetArguments = new Argument<string[]>();
 
         public HostServerTool(ILogger<HostServerTool> logger)
         {
@@ -21,7 +20,6 @@ namespace Azure.Sdk.Tools.Cli.Tools.HostServer
         public override Command GetCommand()
         {
             Command command = new Command("start");
-            command.AddArgument(_unmatchedAspNetArguments);
 
             command.SetHandler(async ctx =>
             {
@@ -33,16 +31,10 @@ namespace Azure.Sdk.Tools.Cli.Tools.HostServer
 
         public override async Task<int> HandleCommand(InvocationContext ctx, CancellationToken ct)
         {
-            var unmatched = ctx.ParseResult.GetValueForArgument<string[]>(_unmatchedAspNetArguments);
-            var toolsOption = ctx.ParseResult.GetValueForOption<string>(SharedOptions.ToolOption);
-            var tools = toolsOption != null
-                ? toolsOption.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
-                : new string[] { };
 
-            var host = CreateAppBuilder(tools, unmatched).Build();
             try
             {
-                await host.RunAsync(ct);
+                await Program.ServerApp.RunAsync(ct);
                 return 0;
             }
             catch (Exception ex)
@@ -50,41 +42,6 @@ namespace Azure.Sdk.Tools.Cli.Tools.HostServer
                 _logger.LogError("Exception during web app run: {ex}", ex);
                 return 1;
             }
-        }
-
-        public static WebApplicationBuilder CreateAppBuilder(string[] tools, string[] unmatchedArgs)
-        {
-            WebApplicationBuilder builder = WebApplication.CreateBuilder(unmatchedArgs);
-
-            builder.Logging.AddConsole(consoleLogOptions =>
-            {
-                consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Error;
-            });
-
-            ServiceRegistrations.RegisterCommonServices(builder.Services);
-
-            // For testing SSE can be easier to use. Comment above and uncomment below. Eventually this will be
-            // behind a command line flag or we could try to run in both modes at once if possible.
-            //.WithHttpTransport()
-
-            if (tools.Length == 0)
-            {
-                builder.Services
-                    .AddMcpServer()
-                    .WithStdioServerTransport()
-                    .WithToolsFromAssembly();
-            }
-            else
-            {
-                var toolTypes = SharedOptions.GetFilteredToolTypes(tools);
-
-                builder.Services
-                    .AddMcpServer()
-                    .WithStdioServerTransport()
-                    .WithTools(toolTypes);
-            }
-
-            return builder;
         }
     }
 }

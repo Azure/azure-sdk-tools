9b46dee00f9a96726af177025c60acb64729874c
Scott Beddall <scbedd@microsoft.com>
2025-05-27 22:04:04 +0000

    update the comment

--- eng/scripts/download_targeted_packages.py
+++ eng/scripts/download_targeted_packages.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
         description="Download wheels and source distributions (if possible) for a set of targeted specifiers."
     )
 
-    parser.add_argument("target_packages", nargs="?", help=("An string of targeted specifiers. Ex. 'astroid==2.15.8, dill==0.3.9'"))
+    parser.add_argument("target_packages", nargs="?", help=("A string of targeted specifiers. Ex. 'astroid==2.15.8, dill==0.3.9'"))
 
     parser.add_argument(
         "-d",

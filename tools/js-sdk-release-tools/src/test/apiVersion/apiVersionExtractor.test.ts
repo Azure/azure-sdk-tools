c8e01fd2dfecd74ed2ba4301f29e46feabcd4fc9
Kyle Zhang <v-zhanh@microsoft.com>
2025-06-03 17:50:31 +0800

    update

--- tools/js-sdk-release-tools/src/test/apiVersion/apiVersionExtractor.test.ts
+++ tools/js-sdk-release-tools/src/test/apiVersion/apiVersionExtractor.test.ts
@@ -1,66 +1,57 @@
-import { describe, expect, test, vi } from "vitest";
-import { getApiVersionType } from "../../mlc/apiVersion/apiVersionTypeExtractor.js";
-import { getApiVersionType as getApiVersionTypeInRLC } from "../../llc/apiVersion/apiVersionTypeExtractor.js";
-import { join } from "path";
-import { ApiVersionType } from "../../common/types.js";
-import { tryFindApiVersionInRestClient } from "../../xlc/apiVersion/utils.js";
+import { describe, expect, test, vi } from 'vitest';
+import { getApiVersionType } from '../../mlc/apiVersion/apiVersionTypeExtractor.js';
+import { getApiVersionType as getApiVersionTypeInRLC } from '../../llc/apiVersion/apiVersionTypeExtractor.js';
+import { join } from 'path';
+import { ApiVersionType } from '../../common/types.js';
+import { tryFindApiVersionInRestClient } from '../../xlc/apiVersion/utils.js';
 
-describe("Modular client api-version Extractor", () => {
-    test("new createClient function", async () => {
-        const clientPath = join(
-            __dirname,
-            "testCases/new/src/rest/newClient.ts",
-        );
+describe('Modular client api-version Extractor', () => {
+    test('new createClient function', async () => {
+        const clientPath = join(__dirname, 'testCases/new/src/rest/newClient.ts');
         const version = tryFindApiVersionInRestClient(clientPath);
-        expect(version).toBe("2024-03-01-preview");
+        expect(version).toBe('2024-03-01-preview');
     });
 
-    test("get api version type from new createClient function", async () => {
-        const root = join(__dirname, "testCases/new/");
+    test('get api version type from new createClient function', async () => {
+        const root = join(__dirname, 'testCases/new/');
         const version = await getApiVersionType(root);
         expect(version).toBe(ApiVersionType.Preview);
     });
 
-    test("old createClient function 1", async () => {
-        const clientPath = join(
-            __dirname,
-            "testCases/old1/src/rest/oldClient.ts",
-        );
+    test('old createClient function 1', async () => {
+        const clientPath = join(__dirname, 'testCases/old1/src/rest/oldClient.ts');
         const version = tryFindApiVersionInRestClient(clientPath);
-        expect(version).toBe("v1.1-preview.1");
+        expect(version).toBe('v1.1-preview.1');
     });
 
-    test("get api version type from old createClient function 1", async () => {
-        const root = join(__dirname, "testCases/old1/");
+    test('get api version type from old createClient function 1', async () => {
+        const root = join(__dirname, 'testCases/old1/');
         const version = await getApiVersionType(root);
         expect(version).toBe(ApiVersionType.Preview);
     });
 
-    test("old createClient function 2", async () => {
-        const clientPath = join(
-            __dirname,
-            "testCases/old2/src/rest/oldClient.ts",
-        );
+    test('old createClient function 2', async () => {
+        const clientPath = join(__dirname, 'testCases/old2/src/rest/oldClient.ts');
         const version = tryFindApiVersionInRestClient(clientPath);
-        expect(version).toBe("2024-03-01");
+        expect(version).toBe('2024-03-01');
     });
 
-    test("get api version type from old createClient function 2", async () => {
-        const root = join(__dirname, "testCases/old2/");
+    test('get api version type from old createClient function 2', async () => {
+        const root = join(__dirname, 'testCases/old2/');
         const version = await getApiVersionType(root);
         expect(version).toBe(ApiVersionType.Stable);
     });
 });
 
-describe("Rest client file fallbacks", () => {
-    describe("Modular client", () => {
-        test("src/api/xxxContext.ts exists", async () => {
-            const root = join(__dirname, "testCases/new-context/");
+describe('Rest client file fallbacks', () => {
+    describe('Modular client', () => {
+        test('src/api/xxxContext.ts exists', async () => {
+            const root = join(__dirname, 'testCases/new-context/');
             const version = await getApiVersionType(root);
             expect(version).toBe(ApiVersionType.Preview);
         });
         test("src/api/xxxContext.ts doesn't exists, fallback to src/rest/xxxClient.ts", async () => {
-            const root = join(__dirname, "testCases/new/");
+            const root = join(__dirname, 'testCases/new/');
             const version = await getApiVersionType(root);
             expect(version).toBe(ApiVersionType.Preview);
         });
@@ -83,33 +74,30 @@ describe("Rest client file fallbacks", () => {
             expect(version2).toBe(ApiVersionType.Stable);
             spy.mockRestore();
         });
-    });
-    describe("RLC", () => {
-        test("src/xxxContext.ts exists", async () => {
-            const root = join(__dirname, "testCases/rlc-context/");
+    });    
+    describe('RLC', () => {
+        test('src/xxxContext.ts exists', async () => {
+           const root = join(__dirname, 'testCases/rlc-context/');
             const version = await getApiVersionTypeInRLC(root);
             expect(version).toBe(ApiVersionType.Preview);
         });
         test("src/xxxContext.ts doesn't exists, fallback to src/xxxClient.ts", async () => {
-            const root = join(__dirname, "testCases/rlc-client/");
+            const root = join(__dirname, 'testCases/rlc-client/');
             const version = await getApiVersionTypeInRLC(root);
             expect(version).toBe(ApiVersionType.Preview);
         });
         test("get source folder from readme", async () => {
-            const root = join(__dirname, "testCases/rlc-source-from-readme/");
+            const root = join(__dirname, 'testCases/rlc-source-from-readme/');
             const version = await getApiVersionTypeInRLC(root);
             expect(version).toBe(ApiVersionType.Preview);
         });
         test("get source folder from src", async () => {
-            const root = join(__dirname, "testCases/rlc-source-from-src/");
+            const root = join(__dirname, 'testCases/rlc-source-from-src/');
             const version = await getApiVersionTypeInRLC(root);
             expect(version).toBe(ApiVersionType.Preview);
         });
         test("get api version in parameters.ts that has `api-version: string`", async () => {
-            const root = join(
-                __dirname,
-                "testCases/rlc-source-from-src-streaming/",
-            );
+            const root = join(__dirname, 'testCases/rlc-source-from-src-streaming/');
             const version = await getApiVersionTypeInRLC(root);
             expect(version).toBe(ApiVersionType.Stable);
         });

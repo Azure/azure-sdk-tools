7b798f61b994ea68bee9a24c121c397bcf53eaed
Travis Prescott <trpresco@microsoft.com>
2025-06-06 11:39:01 -0700

    Finally!

--- packages/python-packages/apiview-copilot/src/agent/_agent.py
+++ packages/python-packages/apiview-copilot/src/agent/_agent.py
@@ -1,25 +1,30 @@
 from dotenv import load_dotenv
-from azure.identity import DefaultAzureCredential
-from semantic_kernel.agents import AzureAIAgent, AzureAIAgentSettings
+from azure.identity.aio import DefaultAzureCredential
+from semantic_kernel.agents import AzureAIAgent, AzureAIAgentSettings, RunPollingOptions
+from datetime import timedelta
 from .plugins import ChunkingPlugin
 
 load_dotenv(override=True)
 
-credentials = DefaultAzureCredential()
-
 
 async def get_review_agent() -> AzureAIAgent:
-    async with (AzureAIAgent.create_client(credential=credentials) as client,):
+    ai_agent_settings = AzureAIAgentSettings()
+    async with (
+        DefaultAzureCredential() as credentials,
+        AzureAIAgent.create_client(
+            credential=credentials, endpoint=ai_agent_settings.endpoint, api_version=ai_agent_settings.api_version
+        ) as client,
+    ):
         agent_definition = await client.agents.create_agent(
             name="ApiReviewAgent",
             description="An agent for reviewing API code.",
-            model=AzureAIAgentSettings().model_deployment_name,
+            model=ai_agent_settings.model_deployment_name,
             instructions="Review the provided API code and provide feedback.",
         )
-
         agent = AzureAIAgent(
             client=client,
             definition=agent_definition,
             plugins=[ChunkingPlugin()],
+            polling_options=RunPollingOptions(run_polling_interval=timedelta(seconds=1)),
         )
         return agent

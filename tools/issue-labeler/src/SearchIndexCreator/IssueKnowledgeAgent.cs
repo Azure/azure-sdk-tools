b17e307e2986f405a66136e22dc4fbe78fc5e005
Radhika Gupta <radhgupta@microsoft.com>
2025-06-10 15:10:22 -0700

    Knowledge Agent retrieval pipeline

---
+++ tools/issue-labeler/src/SearchIndexCreator/IssueKnowledgeAgent.cs
@@ -0,0 +1,67 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Search.Documents.Indexes;
+using Microsoft.Extensions.Configuration;
+using Azure.Search.Documents.Indexes.Models;
+using Azure;
+
+namespace SearchIndexCreator
+{
+    public class IssueKnowledgeAgent
+    {
+        private readonly SearchIndexClient _indexClient;
+        private readonly IConfiguration _config;
+
+        public IssueKnowledgeAgent(SearchIndexClient indexClient, IConfiguration config)
+        {
+            _indexClient = indexClient;
+            _config = config;
+        }
+
+        public async Task CreateOrUpdateAsync()
+        {
+            //create the knowledge agent
+            Console.WriteLine("Creating/Updating the knowledge agent...");
+            var IndexName = _config["IndexName"];
+            var openAiParameters = new AzureOpenAIVectorizerParameters
+            {
+                ResourceUri = new Uri(_config["OpenAIEndpoint"]),
+                DeploymentName = _config["KnowledgeAgentModelName"],
+                ModelName = _config["KnowledgeAgentModelName"]
+            };
+            var agentModel = new KnowledgeAgentAzureOpenAIModel(azureOpenAIParameters: openAiParameters);
+            var targetIndex = new KnowledgeAgentTargetIndex(IndexName)
+            {
+                DefaultRerankerThreshold = 1.0f
+            };
+
+
+            var agent = new KnowledgeAgent(
+                name: _config["KnowledgeAgentName"],
+                models: new[] { agentModel },
+                targetIndexes: new[] { targetIndex }
+            );
+            await _indexClient.CreateOrUpdateKnowledgeAgentAsync(agent);
+            Console.WriteLine($"Search agent '{_config["KnowledgeAgentName"]}' created or updated successfully");
+        }
+
+        public async Task<bool> KnowledgeAgentExistsAsync()
+        {
+            var agentName = _config["KnowledgeAgentName"];
+            try
+            {
+                var agent = await _indexClient.GetKnowledgeAgentAsync(agentName);
+                return agent != null;
+            }
+            catch (RequestFailedException ex) when (ex.Status == 404)
+            {
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}

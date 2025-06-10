860683d6ae53083251415c3bf4e8bbbbd3cef82e
Scott Beddall (from Dev Box) <scbedd@microsoft.com>
2025-06-10 11:33:01 -0700

    remove test option

--- tools/azsdk-cli/Azure.Sdk.Tools.Cli/Commands/SharedCommandGroups.cs
+++ tools/azsdk-cli/Azure.Sdk.Tools.Cli/Commands/SharedCommandGroups.cs
@@ -5,25 +5,13 @@ using System.CommandLine.Parsing;
 
 namespace Azure.Sdk.Tools.Cli.Commands
 {
-    /// <summary>
-    /// These should be referenced in the CommandHierarchy array for each command if necessary.
-    /// Every access returns new instances so you never reuse the same Option or List when building up the command tree.
-    /// </summary>
     public static class SharedCommandGroups
     {
-        public static readonly Option<string> CheckThisOption = new Option<string>(
-            aliases: new[] { "--meep", "-m" },
-            description: "This should be settable anywhere in the assigned command",
-            getDefaultValue: () => "oops"
-        )
-        {
-            IsRequired = false
-        };
 
         public static readonly CommandGroup AzurePipelines = new CommandGroup(
             Verb: "azp",
             Description: "Azure Pipelines Tool",
-            Options: new List<Option> { CheckThisOption }
+            Options: new List<Option>()
         );
 
         public static readonly CommandGroup EngSys = new CommandGroup(

70dc6ce40c7d5054b13acf9797ac191587ae34fc
Chidozie Ononiwu <chononiw@microsoft.com>
2025-06-24 14:07:45 -0700

    Surface errors in production temporalily

--- src/dotnet/APIView/ClientSPA/src/main.ts
+++ src/dotnet/APIView/ClientSPA/src/main.ts
@@ -3,6 +3,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+window.addEventListener('error', event => {
+  console.error('Global error:', event.error || event.message);
+});
+window.addEventListener('unhandledrejection', event => {
+  console.error('Unhandled promise rejection:', event.reason);
+});
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));

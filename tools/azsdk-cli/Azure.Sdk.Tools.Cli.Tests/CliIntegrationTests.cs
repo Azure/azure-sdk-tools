494b5a66fd59f84287aa2bde76eab77e668c08f1
Ben Broderick Phillips <bebroder@microsoft.com>
2025-07-11 17:18:28 -0400

    Add simple fail test

--- tools/azsdk-cli/Azure.Sdk.Tools.Cli.Tests/CliIntegrationTests.cs
+++ tools/azsdk-cli/Azure.Sdk.Tools.Cli.Tests/CliIntegrationTests.cs
@@ -32,6 +32,30 @@ internal class CliIntegrationTests
     }
 
     [Test]
+    public async Task TestSimpleFailCase()
+    {
+        var (cmd, logger) = GetTestInstanceWithLogger<HelloWorldTool>();
+
+        var output = "";
+        outputServiceMock
+            .Setup(s => s.Output(It.IsAny<string>()))
+            .Callback<string>(s => output = s);
+
+        var exitCode = await cmd.InvokeAsync(["hello-world", "HI. MY NAME IS", "--fail"]);
+        Assert.That(exitCode, Is.EqualTo(1));
+
+        var expectedExitCode = new Random().Next(2);
+        var expected = $"[ERROR] RESPONDING TO 'HI. MY NAME IS' with FAIL: {expectedExitCode}";
+
+        outputServiceMock
+            .Verify(s => s.Output(It.IsAny<string>()), Times.Once);
+
+        var input = output.Replace("\r", "");
+
+        Assert.That(output, Is.EqualTo(expected));
+    }
+
+    [Test]
     public async Task TestWindows()
     {
         if (!OperatingSystem.IsWindows())

bc1dd01d2b0265ea81ef48c9767725a9de990378
Jacob Lauzon <jalauzon@microsoft.com>
2025-07-16 14:37:07 -0700

    Just remove the test, add Python support

--- tools/perf-automation/Azure.Sdk.Tools.PerfAutomation.Tests/ProcessUtilTests.cs
+++ tools/perf-automation/Azure.Sdk.Tools.PerfAutomation.Tests/ProcessUtilTests.cs
@@ -18,18 +18,6 @@ namespace Azure.Sdk.Tools.PerfAutomation.Tests
             return ("sleep", $"{timeInSeconds}");
         }
 
-        private static (string filename, string arguments) GetLoadCommand(int timeInSeconds)
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                string loadCommandWin = $"$sw = [Diagnostics.Stopwatch]::StartNew(); while ($sw.Elapsed.TotalSeconds -lt {timeInSeconds}) {{ 1..10000 | ForEach-Object {{ [Math]::Sqrt($_) }} }}";
-                return ("powershell", loadCommandWin);
-            }
-            string loadCommandUnix = $"-c 'dd if=/dev/zero of=/dev/null bs=1M count={timeInSeconds * 100} 2>/dev/null'";
-            return ("sh", loadCommandUnix);
-        }
-
-
         [Test]
         public async Task RunAsync([Values(true, false)] bool stats)
         {
@@ -62,15 +50,6 @@ namespace Azure.Sdk.Tools.PerfAutomation.Tests
             {
                 await ProcessUtil.RunAsync(filename, arguments, cancellationToken: cts.Token, trackStatistics: stats);
             });
-        }
-
-        [Test]
-        public async Task RunAsync_Stats()
-        {
-            (string filename, string arguments) = GetSleepCommand(3);
-            ProcessResult result = await ProcessUtil.RunAsync(filename, arguments, trackStatistics: true);
-            Assert.That(result.AverageCpu, Is.GreaterThan(0));
-            Assert.That(result.AverageMemory, Is.GreaterThan(0));
         }
     }
 }

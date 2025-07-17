57437a916fa71b2816801f4bc73190e70e726330
Travis Prescott <trpresco@microsoft.com>
2025-07-17 08:14:45 -0700

    Remove TypeSpec APIView from azure-sdk-tools

--- tools/apiview/emitters/typespec-apiview/src/testing/index.ts
+++
@@ -1,6 +0,0 @@
-import { createTestLibrary, findTestPackageRoot, TypeSpecTestLibrary } from "@typespec/compiler/testing";
-
-export const ApiViewTestLibrary: TypeSpecTestLibrary = createTestLibrary({
-  name: "@azure-tools/typespec-apiview",
-  packageRoot: await findTestPackageRoot(import.meta.url),
-});

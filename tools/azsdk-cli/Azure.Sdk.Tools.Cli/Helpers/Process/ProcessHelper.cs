2d18f5db96b543c1368313a48db5b687768fa606
Christopher Radek <cradek@microsoft.com>
2025-07-24 14:15:29 -0700

    support passing in process timeout to process helper

--- tools/azsdk-cli/Azure.Sdk.Tools.Cli/Helpers/Process/ProcessHelper.cs
+++ tools/azsdk-cli/Azure.Sdk.Tools.Cli/Helpers/Process/ProcessHelper.cs
@@ -8,14 +8,44 @@ namespace Azure.Sdk.Tools.Cli.Helpers
     public interface IProcessHelper
     {
         public ProcessResult RunProcess(string command, string[] args, string workingDirectory);
+        public ProcessResult RunProcess(string command, string[] args, string workingDirectory, int processTimeout);
     }
 
     public class ProcessHelper(ILogger<ProcessHelper> logger) : IProcessHelper
     {
 
+        private readonly int DefaultProcessTimeoutMs = 120_000; // Default timeout of 2 minutes
+
         private readonly ILogger<ProcessHelper> logger = logger;
 
+        /// <summary>
+        /// Runs a process with the specified command and arguments in the given working directory.
+        /// </summary>
+        /// <param name="command">The command to run.</param>
+        /// <param name="args">The arguments to pass to the command.</param>
+        /// <param name="workingDirectory">The working directory for the process.</param>
+        /// <returns>A <see cref="ProcessResult"/> containing the output and exit code of the process.</returns>
+        /// <remarks>
+        /// The default timeout for the process is set to 120 seconds (2 minutes).
+        /// If the process does not complete within this time, it will be terminated.
+        /// </remarks>
         public ProcessResult RunProcess(string command, string[] args, string workingDirectory)
+        {
+            return RunProcess(command, args, workingDirectory, DefaultProcessTimeoutMs);
+        }
+
+        /// <summary>
+        /// Runs a process with the specified command and arguments in the given working directory.
+        /// </summary>
+        /// <param name="command">The command to run.</param>
+        /// <param name="args">The arguments to pass to the command.</param>
+        /// <param name="workingDirectory">The working directory for the process.</param>
+        /// <param name="processTimeout">The timeout in milliseconds for the process to complete.</param>
+        /// <returns>A <see cref="ProcessResult"/> containing the output and exit code of the process.</returns>
+        /// <remarks>
+        /// If the process does not complete within the specified timeout, it will be terminated.
+        /// </remarks>
+        public ProcessResult RunProcess(string command, string[] args, string workingDirectory, int processTimeout)
         {
             var processStartInfo = new ProcessStartInfo
             {
@@ -60,7 +90,7 @@ namespace Azure.Sdk.Tools.Cli.Helpers
                 process.BeginOutputReadLine();
                 process.BeginErrorReadLine();
 
-                process.WaitForExit(100_000);
+                process.WaitForExit(processTimeout);
                 exitCode = process.ExitCode;
 
                 if (process.ExitCode != 0)

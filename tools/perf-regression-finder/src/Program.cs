79bbb6027a702dbf677c6bd2753396e861d49aa9
Serena Akpoyibo <t-akpoyibos@microsoft.com>
2025-07-28 16:06:01 -0700

    Added agent content

---
+++ tools/perf-regression-finder/src/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.Agents.AzureAI;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel;
+
+namespace perf_semantic_kernel
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Build configuration from appsettings.json
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            // Initialize and run the agent provider
+            var sematicAzureAi = new SemanticAzureAi(config);
+            await sematicAzureAi.RunChatLoopAsync();
+           
+
+        }
+    }
+}

a5d678ff424df87b7b8e601ee9b2ce35603040c3
Wanpeng Li <wanl@microsoft.com>
2025-08-01 12:23:56 +0800

    remove

--- tools/sdk-ai-bots/azure-sdk-qa-bot/src/config/config.ts
+++ tools/sdk-ai-bots/azure-sdk-qa-bot/src/config/config.ts
@@ -6,7 +6,6 @@ const config = {
   MicrosoftAppPassword: process.env.BOT_PASSWORD,
   // RAG
   ragApiKey: process.env.RAG_API_KEY,
-  ragEndpoint: process.env.RAG_ENDPOINT,
   // Computer Vision
   azureComputerVisionEndpoint: process.env.AZURE_COMPUTER_VISION_ENDPOINT,
   azureComputerVisionApiKey: process.env.AZURE_COMPUTER_VISION_API_KEY,

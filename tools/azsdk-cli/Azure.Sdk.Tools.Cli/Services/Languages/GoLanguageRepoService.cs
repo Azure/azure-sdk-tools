ad871eecd82a8569f231f4d003b9a65e55f9b9ee
ripark <ripark@microsoft.com>
2025-08-07 23:20:10 +0000

    Add an option to echo, and fixing calls to use the working directory now that Libba's plumbed that through.

--- tools/azsdk-cli/Azure.Sdk.Tools.Cli/Services/Languages/GoLanguageRepoService.cs
+++ tools/azsdk-cli/Azure.Sdk.Tools.Cli/Services/Languages/GoLanguageRepoService.cs
@@ -12,7 +12,7 @@ namespace Azure.Sdk.Tools.Cli.Services;
 public class GoLanguageRepoService : LanguageRepoService
 {
     private readonly string compilerName = "go";
-    private readonly string formatterName = "gofmt";
+    private readonly string formatterName = "goimports";
     private readonly string linterName = "golangci-lint";
 
     public GoLanguageRepoService(string packagePath) : base(packagePath)
@@ -25,11 +25,13 @@ public class GoLanguageRepoService : LanguageRepoService
         }
     }
 
+    #region Go specific functions, not part of the LanguageRepoService
+
     public async Task<ICLICheckResponse> CreateEmptyPackage(string moduleName)
     {
         try
         {
-            var (output, exitCode) = await RunCommandAsync(new() { FileName = compilerName, ArgumentList = { "mod", "init", moduleName } });
+            var (output, exitCode) = await RunCommandAsync(new() { FileName = compilerName, ArgumentList = { "mod", "init", moduleName }, WorkingDirectory = _packagePath });
             return CreateResponse(nameof(CreateEmptyPackage), exitCode, output);
         }
         catch (Exception ex)
@@ -38,13 +40,15 @@ public class GoLanguageRepoService : LanguageRepoService
         }
     }
 
+    #endregion
+
     public override async Task<ICLICheckResponse> AnalyzeDependenciesAsync()
     {
         try
         {
             var (output, exitCode) = await RunCommandsAsync([
-                new() { FileName = compilerName, ArgumentList = { "get", "-u", "all" } },   // update all the dependencies to the latest first
-                new() { FileName = compilerName, ArgumentList = { "mod", "tidy" } }         // now tidy, to cleanup any deps that aren't needed.
+                new() { FileName = compilerName, ArgumentList = { "get", "-u", "all" }, WorkingDirectory = _packagePath  },   // update all the dependencies to the latest first
+                new() { FileName = compilerName, ArgumentList = { "mod", "tidy" }, WorkingDirectory = _packagePath  }         // now tidy, to cleanup any deps that aren't needed.
             ]);
 
             return CreateResponse(nameof(AnalyzeDependenciesAsync), exitCode, output);
@@ -58,7 +62,7 @@ public class GoLanguageRepoService : LanguageRepoService
     {
         try
         {
-            var (output, exitCode) = await RunCommandAsync(new() { FileName = formatterName, ArgumentList = { "-w" } });
+            var (output, exitCode) = await RunCommandAsync(new() { FileName = formatterName, ArgumentList = { "-w", "." }, WorkingDirectory = _packagePath });
             return CreateResponse(nameof(FormatCodeAsync), exitCode, output);
         }
         catch (Exception ex)
@@ -71,7 +75,7 @@ public class GoLanguageRepoService : LanguageRepoService
     {
         try
         {
-            var (output, exitCode) = await RunCommandAsync(new() { FileName = linterName, ArgumentList = { "./..." } });
+            var (output, exitCode) = await RunCommandAsync(new() { FileName = linterName, ArgumentList = { "./..." }, WorkingDirectory = _packagePath });
             return CreateResponse(nameof(LintCodeAsync), exitCode, output);
         }
         catch (Exception ex)
@@ -84,7 +88,7 @@ public class GoLanguageRepoService : LanguageRepoService
     {
         try
         {
-            var (output, exitCode) = await RunCommandAsync(new() { FileName = "go", ArgumentList = { "test", "-v", "-timeout", "1h", "./..." } });
+            var (output, exitCode) = await RunCommandAsync(new() { FileName = compilerName, ArgumentList = { "test", "-v", "-timeout", "1h", "./..." }, WorkingDirectory = _packagePath });
             return CreateResponse(nameof(RunTestsAsync), exitCode, output);
         }
         catch (Exception ex)
@@ -98,7 +102,7 @@ public class GoLanguageRepoService : LanguageRepoService
         try
         {
             // does this need to ensure that tests _also_ build, or is it okay to assume that RunTestsAsync() will always be called?
-            var (output, exitCode) = await RunCommandAsync(new() { FileName = "go", ArgumentList = { "build" } });
+            var (output, exitCode) = await RunCommandAsync(new() { FileName = compilerName, ArgumentList = { "build" }, WorkingDirectory = _packagePath });
             return CreateResponse(nameof(BuildProjectAsync), exitCode, output);
         }
         catch (Exception ex)
@@ -111,16 +115,19 @@ public class GoLanguageRepoService : LanguageRepoService
     /// Runs multiple commands using <see cref="RunCommandAsync"/>. It returns after the first command that returns a non-zero exit code.
     /// </summary>
     /// <param name="psis">A collection of <see cref="ProcessStartInfo"/> objects representing the commands to run.</param>
+    /// <param name="echo">If true, echoes all output to the console as well as capturing it to a string.</param>
     /// <param name="ct">Optional <see cref="CancellationToken"/> to cancel the operation.</param>
     /// <returns>A tuple containing the combined output of all commands, and the exit code of the last command run.</returns>
-    private static async Task<(string Output, int ExitCode)> RunCommandsAsync(IEnumerable<ProcessStartInfo> psis, CancellationToken ct = default)
+    private static async Task<(string Output, int ExitCode)> RunCommandsAsync(IEnumerable<ProcessStartInfo> psis, bool echo = false, CancellationToken ct = default)
     {
         var allOutput = new StringBuilder();
         var lastExitCode = 0;
 
+        var options = new RunCommandOptions() { PrependCommandLine = true, Echo = echo };
+
         foreach (var psi in psis)
         {
-            var (output, exitCode) = await RunCommandAsync(psi, true, ct);
+            var (output, exitCode) = await RunCommandAsync(psi, options, ct);
             allOutput.Append(output);
 
             if (exitCode != 0)
@@ -133,12 +140,25 @@ public class GoLanguageRepoService : LanguageRepoService
         return (allOutput.ToString(), lastExitCode);
     }
 
+
+    public class RunCommandOptions
+    {
+        /// <summary>
+        /// Echo all output to the console as well as capturing it to a string
+        /// </summary>
+        public bool Echo { get; set; }
+
+        /// <summary>
+        /// Prepends the command line to the returned output for the command.
+        /// </summary>
+        public bool PrependCommandLine { get; set; }
+    }
+
     /// <summary>
     /// Helper method to run command line tools asynchronously.
     /// </summary>
     /// <param name="psi">ProcessStartInfo for the process. NOTE: this parameter is modified.</param>
-    /// <param name="prependCommandLine">If true, prepends the command line to the output.</param>
-    private static async Task<(string Output, int ExitCode)> RunCommandAsync(ProcessStartInfo psi, bool prependCommandLine = true, CancellationToken ct = default)
+    private static async Task<(string Output, int ExitCode)> RunCommandAsync(ProcessStartInfo psi, RunCommandOptions options = default, CancellationToken ct = default)
     {
         psi.RedirectStandardOutput = true;
         psi.RedirectStandardError = true;
@@ -150,27 +170,58 @@ public class GoLanguageRepoService : LanguageRepoService
 
         var outputBuilder = new StringBuilder();
 
-        process.OutputDataReceived += (sender, e) =>
+        if (options?.Echo == true)
         {
-            if (e.Data != null)
-            {
-                lock (outputBuilder)
-                {
-                    outputBuilder.AppendLine(e.Data);
-                }
-            }
-        };
+            process.OutputDataReceived += (sender, e) =>
+                    {
+                        if (e.Data != null)
+                        {
+                            Console.WriteLine(e.Data);
 
-        process.ErrorDataReceived += (sender, e) =>
-        {
-            if (e.Data != null)
+                            lock (outputBuilder)
+                            {
+                                outputBuilder.AppendLine(e.Data);
+                            }
+                        }
+                    };
+
+            process.ErrorDataReceived += (sender, e) =>
             {
-                lock (outputBuilder)
+                Console.WriteLine(e.Data);
+
+                if (e.Data != null)
                 {
-                    outputBuilder.AppendLine(e.Data);
+                    lock (outputBuilder)
+                    {
+                        outputBuilder.AppendLine(e.Data);
+                    }
                 }
-            }
-        };
+            };
+        }
+        else
+        {
+            process.OutputDataReceived += (sender, e) =>
+                    {
+                        if (e.Data != null)
+                        {
+                            lock (outputBuilder)
+                            {
+                                outputBuilder.AppendLine(e.Data);
+                            }
+                        }
+                    };
+
+            process.ErrorDataReceived += (sender, e) =>
+            {
+                if (e.Data != null)
+                {
+                    lock (outputBuilder)
+                    {
+                        outputBuilder.AppendLine(e.Data);
+                    }
+                }
+            };
+        }
 
         process.Start();
         process.BeginOutputReadLine();
@@ -178,7 +229,7 @@ public class GoLanguageRepoService : LanguageRepoService
 
         await process.WaitForExitAsync(ct);
 
-        if (prependCommandLine)
+        if (options?.PrependCommandLine == true)
         {
             var commandLine = GetApproximateCommandLine(psi);
             outputBuilder.Insert(0, $"Command line: {commandLine}{Environment.NewLine}");

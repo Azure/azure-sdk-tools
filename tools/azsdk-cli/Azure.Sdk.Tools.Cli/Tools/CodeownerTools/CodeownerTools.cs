8a4c8b4f6ffa9e775752ba2df028fde4964f18e5
Michelle <t-michellema@microsoft.com>
2025-08-12 16:15:27 -0700

    added null conditional operator

--- tools/azsdk-cli/Azure.Sdk.Tools.Cli/Tools/CodeownerTools/CodeownerTools.cs
+++ tools/azsdk-cli/Azure.Sdk.Tools.Cli/Tools/CodeownerTools/CodeownerTools.cs
@@ -105,7 +105,7 @@ namespace Azure.Sdk.Tools.Cli.Tools
                         pathValue ?? "",
                         serviceLabelValue ?? "",
                         serviceOwnersValue?.ToList() ?? new List<string>(),
-                        sourceOwnersValue.ToList() ?? new List<string>(),
+                        sourceOwnersValue?.ToList() ?? new List<string>(),
                         isAddingValue,
                         workingBranchValue ?? "");
                     output.Output(addResult);

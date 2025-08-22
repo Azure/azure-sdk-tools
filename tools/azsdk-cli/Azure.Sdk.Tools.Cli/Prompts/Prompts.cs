e1eb3b676dcb31a5d2b5e9af2c2ce87cc9ca9771
ripark <ripark@microsoft.com>
2025-08-21 18:21:00 -0700

    Adding in some basic prompts, taken from Mariana's list in eng.ms.

---
+++ tools/azsdk-cli/Azure.Sdk.Tools.Cli/Prompts/Prompts.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel;
+using ModelContextProtocol.Server;
+
+namespace Azure.Sdk.Tools.Cli.Prompts
+{
+    public class Prompts
+    {
+        [McpServerPrompt, Description("Start the next steps, after TypeSpec changes")]
+        public static Task<string> NextSteps()
+        {
+            return Task.FromResult("I have completed my TypeSpec changes. What's the next step to generate SDK?");
+        }
+
+        [McpServerPrompt, Description("Get status for a spec PR")]
+        public static Task<string> GetStatusForSpecPR(string prNumber)
+        {
+            return Task.FromResult($"I have submitted a spec PR{prNumber}. What's the status of it");
+        }
+    }
+}

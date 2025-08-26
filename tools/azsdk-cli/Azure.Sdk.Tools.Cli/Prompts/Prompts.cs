d4f95d224bed2380386a34b58b68ea0572e2f266
ripark <ripark@microsoft.com>
2025-08-25 17:46:45 -0700

    Change prompt to show release readiness instead, which produces more predictable (and interesting) results.

--- tools/azsdk-cli/Azure.Sdk.Tools.Cli/Prompts/Prompts.cs
+++ tools/azsdk-cli/Azure.Sdk.Tools.Cli/Prompts/Prompts.cs
@@ -11,10 +11,10 @@ namespace Azure.Sdk.Tools.Cli.Prompts
             return Task.FromResult("I have completed my TypeSpec changes. What's the next step to generate SDK?");
         }
 
-        [McpServerPrompt, Description("Get status for a spec PR")]
-        public static Task<string> GetStatusForSpecPR(string prNumber)
+        [McpServerPrompt, Description("Get release readiness for a package")]
+        public static Task<string> ExampleGetReleaseReadiness()
         {
-            return Task.FromResult($"I have submitted a spec PR#{prNumber}. What's the status of it");
+            return Task.FromResult($"What's my release readiness for the C# package for Azure.Identity?");
         }
     }
 }

b601f4ff1e30250863bb7a239ea1479fc2575149
Travis Prescott <trpresco@microsoft.com>
2025-08-27 09:53:42 -0700

    Temporary testing.

--- packages/python-packages/apiview-copilot/evals/_runner.py
+++ packages/python-packages/apiview-copilot/evals/_runner.py
@@ -13,7 +13,7 @@ from src._settings import SettingsManager
 # set before azure.ai.evaluation import to make PF output less noisy
 os.environ["PF_LOGGING_LEVEL"] = "CRITICAL"
 
-from azure.ai.evaluation import evaluate
+from azure.ai.evaluation import GroundednessEvaluator, evaluate
 from azure.identity import AzurePipelinesCredential
 from tabulate import tabulate
 
@@ -22,10 +22,17 @@ DEFAULT_NUM_RUNS: int = 3
 
 def _review_apiview(query: str, language: str):
     """Internal global callable for evals framework."""
-    reviewer = ApiViewReview(target=query, language=language, base=None)
-    review = reviewer.run()
-    reviewer.close()
-    return {"actual": review.model_dump_json()}
+    # FIXME: Re-enable
+    # reviewer = ApiViewReview(target=query, language=language, base=None)
+    # review = reviewer.run()
+    # reviewer.close()
+    # return {"actual": review.model_dump_json()}
+
+    print("⚠️ WARNING: Loading cached APIReview!")
+    test_path = pathlib.Path(__file__).parent.parent / "scratch" / "eval" / "test.jsonl"
+    with open(test_path, "r", encoding="utf-8") as f:
+        test_data = json.load(f)
+        return {"actual": test_data["actual"]}
 
 
 class EvalRunner:
@@ -39,9 +46,6 @@ class EvalRunner:
 
         self._tests_directory = pathlib.Path(__file__).parent / "tests" / self.language
         self._test_file = pathlib.Path(test_path).name
-
-        # for best results, this should always be a different model from the one we are evaluating
-        self._judge_model = "gpt-5-mini"
         self._weights: dict[str, float] = {
             "exact_match_weight": 0.7,  # Exact match (rule id and line number)
             "groundedness_weight": 0.2,  # Staying grounded in guidelines
@@ -49,16 +53,11 @@ class EvalRunner:
             "false_positive_penalty": 0.3,  # Penalty for false positives
             "fuzzy_match_bonus": 0.2,  # Bonus for fuzzy match (right rule, wrong line)
         }
-        self._model_config: dict[str, str] = {
-            "azure_endpoint": self.settings.get("OPENAI_ENDPOINT"),
-            "api_key": self.settings.get("OPENAI_API_KEY"),
-            "azure_deployment": self._judge_model,
-            "api_version": "2025-03-01-preview",
-        }
 
     def run(self):
         """Run the evaluation."""
-        custom_eval = CustomAPIViewEvaluator(self)
+        # FIXME: Re-enable
+        # custom_eval = CustomAPIViewEvaluator()
         rule_ids = set()
 
         all_results = {}
@@ -93,7 +92,14 @@ class EvalRunner:
                 result = evaluate(
                     data=str(file),
                     evaluators={
-                        "metrics": custom_eval,
+                        "metrics": GroundednessEvaluator(
+                            model_config={
+                                "azure_endpoint": self.settings.get("OPENAI_ENDPOINT"),
+                                "api_key": self.settings.get("OPENAI_API_KEY"),
+                                "azure_deployment": "gpt-5-mini",
+                                "api_version": "2025-03-01-preview",
+                            }
+                        )
                     },
                     evaluator_config={
                         "metrics": {
@@ -131,11 +137,110 @@ class EvalRunner:
         self.calculate_coverage(rule_ids)
 
     def in_ci(self) -> bool:
-        return os.getenv("TF_BUILD") is not None
+        return bool(os.getenv("TF_BUILD"))
+
+    def record_run_result(self, result: dict[str, Any], rule_ids: Set[str]) -> list[dict[str, Any]]:
+        run_result = []
+        total_score = 0
+
+        for row in result["rows"]:
+            score = self.calculate_overall_score(row)
+            total_score += score
+            rules = [rule["rule_ids"] for rule in json.loads(row["inputs.response"])["comments"]]
+            rule_ids.update(*rules)
+
+            run_result.append(
+                {
+                    "testcase": row["inputs.testcase"],
+                    "expected": json.loads(row["inputs.response"]),
+                    "actual": json.loads(row["outputs.actual"]),
+                    # "expected_comments": row["outputs.metrics.expected_comments"],
+                    "comments_found": row["outputs.metrics.comments_found"],
+                    "valid_generic_comments": row["outputs.metrics.valid_generic_comments"],
+                    "invalid_generic_comments": row["outputs.metrics.invalid_generic_comments"],
+                    "true_positives": row["outputs.metrics.true_positives"],
+                    "false_positives": row["outputs.metrics.false_positives"],
+                    "false_negatives": row["outputs.metrics.false_negatives"],
+                    "percent_coverage": row["outputs.metrics.percent_coverage"],
+                    "rule_matches_wrong_line": row["outputs.metrics.rule_matches_wrong_line"],
+                    "wrong_line_details": row["outputs.metrics.wrong_line_details"],
+                    "fuzzy_matches": row["outputs.metrics.fuzzy_matches"],
+                    "similarity": row["outputs.metrics.similarity"],
+                    "groundedness": row["outputs.metrics.groundedness"],
+                    "groundedness_reason": row["outputs.metrics.groundedness_reason"],
+                    "overall_score": score,
+                }
+            )
+        average_score = total_score / len(result["rows"])
+        run_result.append({"average_score": average_score, "total_evals": len(result["rows"])})
+        return run_result
+
+    def show_results(self, all_results: dict[str, Any]) -> None:
+        """Display results in a table format."""
+        for name, test_results in all_results.items():
+            baseline_results = {}
+            baseline_path = pathlib.Path(__file__).parent / "results" / self.language / name[:-1]
+
+            if baseline_path.exists():
+                with open(baseline_path, "r", encoding="utf-8") as f:
+                    baseline_data = json.load(f)
+                    for result in baseline_data[:-1]:  # Skip summary
+                        baseline_results[result["testcase"]] = result
+                    baseline_results["average_score"] = baseline_data[-1]["average_score"]
+
+            self.output_table(baseline_results, test_results, name)
+
+    def establish_baseline(self, all_results: dict[str, Any]) -> None:
+        """Establish the current results as the new baseline."""
+
+        # only ask if we're not in CI
+        if self.in_ci() is False:
+            establish_baseline = input("\nDo you want to establish this as the new baseline? (y/n): ")
+            if establish_baseline.lower() == "y":
+                for name, result in all_results.items():
+                    output_path = pathlib.Path(__file__).parent / "results" / self.language / name[:-1]
+                    with open(str(output_path), "w", encoding="utf-8") as f:
+                        json.dump(result, indent=4, fp=f)
+
+        # whether or not we establish a baseline, we want to write results to a temp dir
+        log_path = pathlib.Path(__file__).parent / "results" / self.language / ".log"
+        if not log_path.exists():
+            log_path.mkdir(parents=True, exist_ok=True)
+
+        for name, result in all_results.items():
+            output_path = log_path / name[:-1]
+            with open(str(output_path), "w", encoding="utf-8") as f:
+                json.dump(result, indent=4, fp=f)
+
+    def calculate_coverage(self, rule_ids: set[str]) -> None:
+        """Calculate and output the coverage of tests based on the rule IDs."""
+
+        if self._test_file == "all":
+            # only update coverage if all tests are run
+            output_path = pathlib.Path(__file__).parent / "results" / self.language / "coverage.json"
+            guidelines_path = pathlib.Path(__file__).parent.parent / "guidelines" / self.language
+            guidelines = []
+            for file in guidelines_path.glob("*.json"):
+                with open(file, "r", encoding="utf-8") as f:
+                    guidelines.extend(json.loads(f.read()))
+            guideline_rule_ids = [rule["id"] for rule in guidelines]
+            difference = set(guideline_rule_ids).difference(rule_ids)
+            with open(str(output_path), "w+", encoding="utf-8") as f:
+                f.write(
+                    json.dumps(
+                        {
+                            "tested": list(rule_ids),
+                            "not_tested": list(difference),
+                            "coverage": len(rule_ids) / len(guideline_rule_ids) * 100,
+                        },
+                        indent=4,
+                    )
+                )
+            print(f"\nTest coverage for {self.language}: {len(rule_ids) / len(guideline_rule_ids) * 100:.2f}%")
 
     def calculate_overall_score(self, row: dict[str, Any]) -> float:
         """Calculate weighted score based on various metrics."""
-
+        return 1.0
         if row["outputs.metrics.expected_comments"] == 0:
             # tests with no violations are all or nothing
             # but still give credit if no violations found, but valid generic comments found
@@ -191,21 +296,6 @@ class EvalRunner:
             return f" (\033[31m{diff:{format_str}}\033[0m)"  # Red
         return f" ({diff:{format_str}})"
 
-    def show_results(self, all_results: dict[str, Any]) -> None:
-        """Display results in a table format."""
-        for name, test_results in all_results.items():
-            baseline_results = {}
-            baseline_path = pathlib.Path(__file__).parent / "results" / self.language / name[:-1]
-
-            if baseline_path.exists():
-                with open(baseline_path, "r", encoding="utf-8") as f:
-                    baseline_data = json.load(f)
-                    for result in baseline_data[:-1]:  # Skip summary
-                        baseline_results[result["testcase"]] = result
-                    baseline_results["average_score"] = baseline_data[-1]["average_score"]
-
-            self.output_table(baseline_results, test_results, name)
-
     def output_table(
         self, baseline_results: dict[str, Any], eval_results: list[dict[str, Any]], file_name: str
     ) -> None:
@@ -231,7 +321,8 @@ class EvalRunner:
             ground = result["groundedness"]
             sim = result["similarity"]
             valid_generic = result["valid_generic_comments"]
-            comments_found = f"{result['comments_found']} / {result['expected_comments']}"
+            # comments_found = f"{result['comments_found']} / {result['expected_comments']}"
+            comments_found = f"{result['comments_found']}"
 
             terminal_row = [testcase]
             if testcase in baseline_results:
@@ -270,88 +361,3 @@ class EvalRunner:
             print(
                 f"\n{file_name} average score: {eval_results[-1]['average_score']} {self.format_terminal_diff(eval_results[-1]['average_score'], baseline_results['average_score'])}\n\n"
             )
-
-    def calculate_coverage(self, rule_ids: set[str]) -> None:
-        """Calculate and output the coverage of tests based on the rule IDs."""
-
-        if self._test_file == "all":
-            # only update coverage if all tests are run
-            output_path = pathlib.Path(__file__).parent / "results" / self.language / "coverage.json"
-            guidelines_path = pathlib.Path(__file__).parent.parent / "guidelines" / self.language
-            guidelines = []
-            for file in guidelines_path.glob("*.json"):
-                with open(file, "r", encoding="utf-8") as f:
-                    guidelines.extend(json.loads(f.read()))
-            guideline_rule_ids = [rule["id"] for rule in guidelines]
-            difference = set(guideline_rule_ids).difference(rule_ids)
-            with open(str(output_path), "w+", encoding="utf-8") as f:
-                f.write(
-                    json.dumps(
-                        {
-                            "tested": list(rule_ids),
-                            "not_tested": list(difference),
-                            "coverage": len(rule_ids) / len(guideline_rule_ids) * 100,
-                        },
-                        indent=4,
-                    )
-                )
-
-            print(f"\nTest coverage for {self.language}: {len(rule_ids) / len(guideline_rule_ids) * 100:.2f}%")
-
-    def establish_baseline(self, all_results: dict[str, Any]) -> None:
-        """Establish the current results as the new baseline."""
-
-        # only ask if we're not in CI
-        if self.in_ci() is False:
-            establish_baseline = input("\nDo you want to establish this as the new baseline? (y/n): ")
-            if establish_baseline.lower() == "y":
-                for name, result in all_results.items():
-                    output_path = pathlib.Path(__file__).parent / "results" / self.language / name[:-1]
-                    with open(str(output_path), "w", encoding="utf-8") as f:
-                        json.dump(result, indent=4, fp=f)
-
-        # whether or not we establish a baseline, we want to write results to a temp dir
-        log_path = pathlib.Path(__file__).parent / "results" / self.language / ".log"
-        if not log_path.exists():
-            log_path.mkdir(parents=True, exist_ok=True)
-
-        for name, result in all_results.items():
-            output_path = log_path / name[:-1]
-            with open(str(output_path), "w", encoding="utf-8") as f:
-                json.dump(result, indent=4, fp=f)
-
-    def record_run_result(self, result: dict[str, Any], rule_ids: Set[str]) -> list[dict[str, Any]]:
-        run_result = []
-        total_score = 0
-
-        for row in result["rows"]:
-            score = self.calculate_overall_score(row)
-            total_score += score
-            rules = [rule["rule_ids"] for rule in json.loads(row["inputs.response"])["comments"]]
-            rule_ids.update(*rules)
-
-            run_result.append(
-                {
-                    "testcase": row["inputs.testcase"],
-                    "expected": json.loads(row["inputs.response"]),
-                    "actual": json.loads(row["outputs.actual"]),
-                    "expected_comments": row["outputs.metrics.expected_comments"],
-                    "comments_found": row["outputs.metrics.comments_found"],
-                    "valid_generic_comments": row["outputs.metrics.valid_generic_comments"],
-                    "invalid_generic_comments": row["outputs.metrics.invalid_generic_comments"],
-                    "true_positives": row["outputs.metrics.true_positives"],
-                    "false_positives": row["outputs.metrics.false_positives"],
-                    "false_negatives": row["outputs.metrics.false_negatives"],
-                    "percent_coverage": row["outputs.metrics.percent_coverage"],
-                    "rule_matches_wrong_line": row["outputs.metrics.rule_matches_wrong_line"],
-                    "wrong_line_details": row["outputs.metrics.wrong_line_details"],
-                    "fuzzy_matches": row["outputs.metrics.fuzzy_matches"],
-                    "similarity": row["outputs.metrics.similarity"],
-                    "groundedness": row["outputs.metrics.groundedness"],
-                    "groundedness_reason": row["outputs.metrics.groundedness_reason"],
-                    "overall_score": score,
-                }
-            )
-        average_score = total_score / len(result["rows"])
-        run_result.append({"average_score": average_score, "total_evals": len(result["rows"])})
-        return run_result

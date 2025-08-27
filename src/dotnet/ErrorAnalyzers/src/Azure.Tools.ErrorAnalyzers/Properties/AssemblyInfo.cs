58f3970a87285a0d3880e06c70790677bda7b3fb
Radhika Gupta <radhgupta@microsoft.com>
2025-08-27 12:55:51 -0700

    removed unnecessary project ref

--- src/dotnet/ErrorAnalyzers/src/Azure.Tools.ErrorAnalyzers/Properties/AssemblyInfo.cs
+++ src/dotnet/ErrorAnalyzers/src/Azure.Tools.ErrorAnalyzers/Properties/AssemblyInfo.cs
@@ -3,16 +3,10 @@
 
 using System.Runtime.CompilerServices;
 
-// Allow internal types to be visible to Client, General, and Management analyzer projects
-[assembly: InternalsVisibleTo("Azure.Tools.ErrorAnalyzers.ClientRuleAnalyzers")]
-[assembly: InternalsVisibleTo("Azure.Tools.ErrorAnalyzers.GeneralRuleAnalyzers")]
-[assembly: InternalsVisibleTo("Azure.Tools.ErrorAnalyzers.ManagementRuleAnalyzers")]
 
 // Allow internal types to be visible to test projects
 [assembly: InternalsVisibleTo("Azure.Tools.ErrorAnalyzers.Tests")]
-[assembly: InternalsVisibleTo("Azure.Tools.ErrorAnalyzers.ClientRuleAnalyzers.Tests")]
-[assembly: InternalsVisibleTo("Azure.Tools.ErrorAnalyzers.GeneralRuleAnalyzers.Tests")]
-[assembly: InternalsVisibleTo("Azure.Tools.ErrorAnalyzers.ManagementRuleAnalyzers.Tests")]
+
 
 // Allow internal types to be visible to dynamic proxy generation (for mocking frameworks)
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]

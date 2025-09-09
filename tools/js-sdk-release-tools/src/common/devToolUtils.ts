6365e064f839f6a1dc71c856a7a43ce4ff9bf808
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2025-09-09 12:39:58 +0800

    improve lint and format script (#11937)
    * improve lint and format script
    
    * Update tools/js-sdk-release-tools/src/common/devToolUtils.ts
    
    * Update npmUtils.ts
    
    * Update devToolUtils.ts

--- tools/js-sdk-release-tools/src/common/devToolUtils.ts
+++ tools/js-sdk-release-tools/src/common/devToolUtils.ts
@@ -6,9 +6,13 @@ import { runCommand, runCommandOptions } from './utils.js';
 
 export async function formatSdk(packageDirectory: string) {
     logger.info(`Start to format code in '${packageDirectory}'.`);
+    const hasSampleFolder = await exists(path.join(packageDirectory, "samples-dev"));
+    const samplesDev = hasSampleFolder ? ` "samples-dev/**/*.ts"` : '';
+    const hasTestFolder = await exists(path.join(packageDirectory, "test"));
+    const test = hasTestFolder ? ` "test/**/*.{ts,cts,mts}"` : '';
     const cwd = packageDirectory;
     const options = { ...runCommandOptions, cwd };
-    const formatCommand = 'run vendored prettier --write --config ../../../.prettierrc.json --ignore-path ../../../.prettierignore \"src/**/*.{ts,cts,mts}\" \"test/**/*.{ts,cts,mts}\" \"*.{js,cjs,mjs,json}\" \"samples-dev/*.ts\"';
+    const formatCommand = `run vendored prettier --write --config ../../../.prettierrc.json --ignore-path ../../../.prettierignore "src/**/*.{ts,cts,mts}"${test} "*.{js,cjs,mjs,json}"${samplesDev}`;
 
     try {
         await runCommand(`npm`, ['exec', '--', 'dev-tool', formatCommand], options, true, 300, true);
@@ -36,6 +40,8 @@ export async function updateSnippets(packageDirectory: string) {
 export async function lintFix(packageDirectory: string) {
     const hasSampleFolder = await exists(path.join(packageDirectory, "samples-dev"));
     const samplesDev = hasSampleFolder ? ' samples-dev' : '';
+    const hasTestFolder = await exists(path.join(packageDirectory, "test"));
+    const test = hasTestFolder ? ' test' : '';
     const cwd = packageDirectory;
     const options = { ...runCommandOptions, cwd };
 
@@ -43,7 +49,7 @@ export async function lintFix(packageDirectory: string) {
     await runCommand('pnpm', ['build', '--filter', `@azure/eslint-plugin-azure-sdk...`], runCommandOptions);
     logger.info("Build @azure/eslint-plugin-azure-sdk package successfully.");
     logger.info(`Start to fix lint errors in '${packageDirectory}'.`);
-    const lintFixCommand = `run vendored eslint package.json api-extractor.json src test${samplesDev} --fix --fix-type [problem,suggestion]`;
+    const lintFixCommand = `run vendored eslint package.json api-extractor.json src ${test}${samplesDev} --fix --fix-type [problem,suggestion]`;
 
     try {
         await runCommand(`npm`, ['exec', '--', 'dev-tool', lintFixCommand], options, true, 300, true);

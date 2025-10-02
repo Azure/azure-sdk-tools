0c3affc27203cf64c4cb58eb5aa1a41021c639b1
Juan Ospina <70209456+jeo02@users.noreply.github.com>
2025-10-02 15:17:18 -0700

    Fix run away conversation by limiting it to the number of tools that are expected

--- tools/ai-evals/azsdk-mcp/Helpers/ChatCompletion.cs
+++ tools/ai-evals/azsdk-mcp/Helpers/ChatCompletion.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.AI;
 using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol;
 
 namespace Azure.Sdk.Tools.McpEvals.Helpers
 {
@@ -12,6 +13,45 @@ namespace Azure.Sdk.Tools.McpEvals.Helpers
         {
             _chatClient = chatClient;
             _mcpClient = mcpClient;
+        }
+
+        public async Task<ChatResponse> GetChatResponseAsync(IEnumerable<ChatMessage> chat, int maxToolCalls)
+        {
+            var tools = await _mcpClient.ListToolsAsync();
+            var result = new List<ChatResponseUpdate>();
+            var toolsCalled = new HashSet<string>();
+
+            var chatOptions =
+                new ChatOptions
+                {
+                    Tools = [.. tools]
+                };
+
+            // GetResponseAsync allows the LLM to do too much. Limit it to the number of tools we expect
+            // Not including calling a tool again. 
+            await foreach(var message in _chatClient.GetStreamingResponseAsync(chat, chatOptions))
+            {
+                foreach(var content in message.Contents)
+                {
+                    if (content is FunctionCallContent func)
+                    {
+                        toolsCalled.Add(func.Name);
+                    }
+                }
+
+                if(message.Contents.Any())
+                {
+                    result.Add(message);
+                }
+                
+                
+                if(toolsCalled.Count >= maxToolCalls)
+                {
+                    break;
+                }
+            }
+
+            return result.ToChatResponse();
         }
 
         public async Task<ChatResponse> GetChatResponseAsync(IEnumerable<ChatMessage> chat)

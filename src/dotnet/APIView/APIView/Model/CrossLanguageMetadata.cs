3a6dce972b80a32e508424a0c98fc00a0e55f954
Alitzel Mendez <almend@microsoft.com>
2025-10-05 08:31:39 -0700

    [API View] Correctly deserialize cross language id from code file

---
+++ src/dotnet/APIView/APIView/Model/CrossLanguageMetadata.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace APIView.Model;
+
+public class CrossLanguageMetadata
+{
+    public string CrossLanguagePackageId { get; set; }
+    public Dictionary<string, string> CrossLanguageDefinitionId { get; set; }
+}

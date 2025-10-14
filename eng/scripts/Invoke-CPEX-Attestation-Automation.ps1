fb43da82a6fb6f5330e222ad61dc2cb0a9666bd9
Summer Warren <summerwarren@microsoft.com>
2025-10-14 15:23:39 -0700

    Change to Write-Host & Write-Warning

--- eng/scripts/Invoke-CPEX-Attestation-Automation.ps1
+++ eng/scripts/Invoke-CPEX-Attestation-Automation.ps1
@@ -71,7 +71,7 @@ function SendEmailNotification($emailTo, $emailCC, $emailSubject, $emailBody) {
         $body = @{ EmailTo = $emailTo; EmailCC = $emailCC; Subject = $emailSubject; Body = $emailBody} | ConvertTo-Json -Depth 3
         $response = Invoke-RestMethod -Uri $AzureSDKEmailUri -Method Post -Body $body -ContentType "application/json"
     } catch {
-        Write-Error "Failed to send email.`nTo: $emailTo`nCC: $emailCC`nSubject: $emailSubject`nBody: $emailBody`nException message: $($_.Exception.Message)"
+        Write-Warning "Failed to send email.`nTo: $emailTo`nCC: $emailCC`nSubject: $emailSubject`nBody: $emailBody`nException message: $($_.Exception.Message)"
     }
 }
 
@@ -202,7 +202,7 @@ foreach ($triage in $triages) {
             }
         }
     } catch {
-        Write-Error "Error processing triage item [$($triage.id)]`nException message: $($_.Exception.Message)"
+        Write-Warning "Error processing triage item [$($triage.id)]`nException message: $($_.Exception.Message)"
 
         $failedAttestations += @{
             productId = $productServiceTreeId
@@ -238,7 +238,7 @@ foreach ($releasePlan in $releasePlans) {
                     "*Private Preview*" { $KPI_ID_Mgmt_Private_Preview }
                     "*GA*" { $KPI_ID_Mgmt_GA }
                     default {
-                        Write-Output "Release Plan ID $($releasePlan.id): Management plane in scope, unknown lifecycle $($lifecycle)"
+                        Write-Host "Release Plan ID $($releasePlan.id): Management plane in scope, unknown lifecycle $($lifecycle)"
                         $null
                     }
                 }
@@ -251,13 +251,13 @@ foreach ($releasePlan in $releasePlans) {
                             "*Private Preview*" { $KPI_ID_Data_Private_Preview }
                             "*GA*" { $KPI_ID_Data_GA }
                             default {
-                                Write-Output "Release Plan ID $($releasePlan.id): Dataplane in scope, unknown lifecycle $($lifecycle)"
+                                Write-Host "Release Plan ID $($releasePlan.id): Dataplane in scope, unknown lifecycle $($lifecycle)"
                                 $null
                             }
                         }
                     }
                     default {
-                        Write-Output "Release Plan ID: Both Management Plane and DataPlane not in scope"
+                        Write-Host "Release Plan ID: Both Management Plane and DataPlane not in scope"
                         $null
                     }
                 }
@@ -270,7 +270,7 @@ foreach ($releasePlan in $releasePlans) {
 
         Update-AttestationStatusInWorkItem -workItemId $releasePlan.id -fieldName "Custom.AttestationStatus" -status "Completed"
     } catch {
-        Write-Error "Error processing release plan item [$($releasePlan.id)]`nException message: $($_.Exception.Message)"
+        Write-Warning "Error processing release plan item [$($releasePlan.id)]`nException message: $($_.Exception.Message)"
 
         $failedAttestations += @{
             productId = $productServiceTreeId

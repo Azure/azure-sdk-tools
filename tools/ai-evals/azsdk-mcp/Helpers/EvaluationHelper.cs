6b5c4effb0dc4f507e5a8a998982a6107d5431ad
Juan Ospina <70209456+jeo02@users.noreply.github.com>
2025-10-27 23:18:54 +0000

    eval helper

---
+++ tools/ai-evals/azsdk-mcp/Helpers/EvaluationHelper.cs
@@ -0,0 +1,18 @@
+using AwesomeAssertions;
+using Azure.Sdk.Tools.McpEvals.Evaluators;
+using Microsoft.Extensions.AI.Evaluation;
+
+namespace Azure.Sdk.Tools.McpEvals.Helpers
+{
+    public class EvaluationHelper
+    {
+        public static void ValidateToolInputsEvaluator(EvaluationResult? result)
+        {
+            EvaluationRating[] expectedRatings = [EvaluationRating.Good, EvaluationRating.Exceptional];
+            BooleanMetric expectedToolInput = result.Get<BooleanMetric>(ExpectedToolInputEvaluator.ExpectedToolInputMetricName);
+            expectedToolInput.Interpretation!.Failed.Should().BeFalse(because: expectedToolInput.Interpretation.Reason);
+            expectedToolInput.Interpretation.Rating.Should().BeOneOf(expectedRatings, because: expectedToolInput.Reason);
+            expectedToolInput.ContainsDiagnostics(d => d.Severity >= EvaluationDiagnosticSeverity.Warning).Should().BeFalse();
+        }
+    }
+}

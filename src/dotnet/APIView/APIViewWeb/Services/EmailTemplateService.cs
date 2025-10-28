74ee9ab582d9e3d8f30f523769684d52127f7ff4
Helen Gao <gaoh@microsoft.com>
2025-10-28 10:21:09 -0700

    fix truncated path (#12676)
    * fix truncated path
    
    * update

--- src/dotnet/APIView/APIViewWeb/Services/EmailTemplateService.cs
+++ src/dotnet/APIView/APIViewWeb/Services/EmailTemplateService.cs
@@ -31,7 +31,6 @@ namespace APIViewWeb.Services
     public class EmailTemplateService : IEmailTemplateService
     {
         private readonly string _apiviewEndpoint;
-        private const int MaxPackageNameLength = 60;
         // TODO: 3 days auto-approval feature is temporarily disabled
         // private const int BusinessDaysForDeadline = 3;
 
@@ -161,15 +160,10 @@ namespace APIViewWeb.Services
             var linksHtml = "";
             foreach (var review in languageReviews)
             {
-                var truncatedName = review.PackageName.Length > MaxPackageNameLength 
-                    ? review.PackageName[..MaxPackageNameLength] + "..." 
-                    : review.PackageName;
-
-                // Build HTML that matches the pending namespace approval tab format
                 linksHtml += $@"
                     <li class=""language-item"">
                         <span class=""language-name"">{review.Language}:</span>
-                        <a href=""{_apiviewEndpoint}/Assemblies/Review/{review.Id}"" class=""package-link"">{truncatedName}</a>
+                        <a href=""{_apiviewEndpoint}/Assemblies/Review/{review.Id}"" class=""package-link"">{review.PackageName}</a>
                     </li>";
             }
             return linksHtml;

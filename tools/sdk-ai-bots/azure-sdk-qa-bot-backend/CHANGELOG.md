d7ac1df179414b386bea027405bdd5f1b44460c9
ReleaseHelper <ReleaseHelper>
2025-10-28 15:11:01 +0800

    set up deploy pipeline

---
+++ tools/sdk-ai-bots/azure-sdk-qa-bot-backend/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Release History
+
+## 1.0.0 (2025-10-28)
+
+* Initial release
---
+++ tools/sdk-ai-bots/azure-sdk-qa-bot-backend/version.go
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package main
+
+const (
+	moduleName    = "github.com/azure-sdk-tools/tools/sdk-ai-bots/azure-sdk-qa-bot-backend"
+	moduleVersion = "v1.0.0"
+)

9473e8d67b78afb55e809eb160a7575d17fc65ac
Praveen Kuttappan <praveen.kuttappan@gmail.com>
2025-11-04 11:33:09 -0500

    Fix test error

--- tools/azsdk-cli/Azure.Sdk.Tools.Cli/Services/Languages/LanguageService.cs
+++ tools/azsdk-cli/Azure.Sdk.Tools.Cli/Services/Languages/LanguageService.cs
@@ -10,15 +10,19 @@ namespace Azure.Sdk.Tools.Cli.Services.Languages
         public static readonly ImmutableDictionary<string, SdkLanguage> RepoToLanguageMap = new Dictionary<string, SdkLanguage>()
         {
             { "azure-sdk-for-net", SdkLanguage.DotNet },
-            {  "azure-sdk-for-go", SdkLanguage.Go  },
+            { "azure-sdk-for-go", SdkLanguage.Go },
             { "azure-sdk-for-java", SdkLanguage.Java },
             { "azure-sdk-for-js", SdkLanguage.JavaScript },
             { "azure-sdk-for-python", SdkLanguage.Python },
-            { "azure-sdk-for-rust" , SdkLanguage.Rust}
+            { "azure-sdk-for-rust", SdkLanguage.Rust}
         }.ToImmutableDictionary();
 
         public static SdkLanguage GetLanguageForRepo(string repoName)
         {
+            if (string.IsNullOrEmpty(repoName))
+            {
+                return SdkLanguage.Unknown;
+            }
             if (RepoToLanguageMap.TryGetValue(repoName.ToLower(), out SdkLanguage language))
             {
                 return language;

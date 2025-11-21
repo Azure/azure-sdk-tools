34ee3bb6855aa913dcc13c5b3669fa4be893d2b0
KarishmaGhiya <kaghiya@microsoft.com>
2025-11-21 12:40:05 -0800

    update

--- tools/apiview/parsers/js-api-parser/src/tokenGenerators/interfaces.ts
+++ tools/apiview/parsers/js-api-parser/src/tokenGenerators/interfaces.ts
@@ -4,13 +4,13 @@ import { ReviewToken } from '../models';
 /**
  * Interface for token generators that create ReviewTokens from ApiItems.
  */
-export interface TokenGenerator<T = ApiItem> {
+export interface TokenGenerator<T extends ApiItem = ApiItem> {
     /**
      * Validates if the given ApiItem can be processed by this token generator.
      * @param item - The ApiItem to validate.
      * @returns True if the item is valid; otherwise, false.
      */
-    isValid(item: ApiItem): boolean;
+    isValid(item: ApiItem): item is T;
 
     /**
      * Generates ReviewTokens from the given ApiItem.
@@ -18,5 +18,5 @@ export interface TokenGenerator<T = ApiItem> {
      * @param deprecated - Indicates if the Api is deprecated.
      * @returns An array of ReviewTokens generated from the ApiItem.
      */
-    generate(item: ApiItem, deprecated: boolean): ReviewToken[];
+    generate(item: T, deprecated: boolean): ReviewToken[];
 }

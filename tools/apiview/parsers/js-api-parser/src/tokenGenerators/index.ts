7f12ed28f142b8b169e56d1b3ff4408ccef40c6f
KarishmaGhiya <kaghiya@microsoft.com>
2025-11-25 12:16:43 -0800

    removed fallback logic

--- tools/apiview/parsers/js-api-parser/src/tokenGenerators/index.ts
+++ tools/apiview/parsers/js-api-parser/src/tokenGenerators/index.ts
@@ -1,27 +1,26 @@
 import { enumTokenGenerator } from "./enum";
-import { ReviewToken } from '../models';
-import { ApiItem } from '@microsoft/api-extractor-model';
+import { ReviewToken } from "../models";
+import { ApiItem } from "@microsoft/api-extractor-model";
+import { functionTokenGenerator } from "./function";
 
 /**
  * Interface for token generators that create ReviewTokens from ApiItems.
  */
 export interface TokenGenerator<T extends ApiItem = ApiItem> {
-    /**
-     * Validates if the given ApiItem can be processed by this token generator.
-     * @param item - The ApiItem to validate.
-     * @returns True if the item is valid; otherwise, false.
-     */
-    isValid(item: ApiItem): item is T;
+  /**
+   * Validates if the given ApiItem can be processed by this token generator.
+   * @param item - The ApiItem to validate.
+   * @returns True if the item is valid; otherwise, false.
+   */
+  isValid(item: ApiItem): item is T;
 
-    /**
-     * Generates ReviewTokens from the given ApiItem.
-     * @param item - The ApiItem to process.
-     * @param deprecated - Indicates if the Api is deprecated.
-     * @returns An array of ReviewTokens generated from the ApiItem.
-     */
-    generate(item: T, deprecated?: boolean): ReviewToken[];
+  /**
+   * Generates ReviewTokens from the given ApiItem.
+   * @param item - The ApiItem to process.
+   * @param deprecated - Indicates if the Api is deprecated.
+   * @returns An array of ReviewTokens generated from the ApiItem.
+   */
+  generate(item: T, deprecated?: boolean): ReviewToken[];
 }
 
-export const generators: TokenGenerator[] = [
-    enumTokenGenerator
-];
+export const generators: TokenGenerator[] = [enumTokenGenerator, functionTokenGenerator];

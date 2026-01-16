d7260eb6ff016b5374f17ca565b4a0ccab6b4ceb
Scott Beddall <45376673+scbedd@users.noreply.github.com>
2026-01-16 13:52:29 -0800

    Enable pypi release from `archetype-tool-python` (#13601)
    * build up exactly what we need to test a doc warden release
    
    * ensure publishtopypi is enabled
    
    * fix issue
    
    * use a condition differently
    
    * remove feedname
    
    * updates
    
    * save
    
    * addressing some feedback
    
    * more feedback
    
    * more changes to the test steps
    
    * enable doc-warden release

--- packages/python-packages/doc-warden/setup.py
+++ packages/python-packages/doc-warden/setup.py
@@ -44,6 +44,11 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'License :: OSI Approved :: MIT License',
     ],
     packages=find_packages(),
@@ -54,7 +59,7 @@ setup(
         'pygments', # docutils uses pygments for parsing rst to html
         'beautifulsoup4', # parsing of generated html
         'jinja2', # used for generation from template for index_packages
-        'requests', # utilized to validate published repository URLs. 
+        'requests', # utilized to validate published repository URLs.
         'pathlib2'
     ],
     entry_points = {

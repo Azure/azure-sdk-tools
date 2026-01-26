fbdee681fbc796529dcc03ae8be67eb6fec0e94e
Alitzel Mendez <almend@microsoft.com>
2026-01-26 13:45:25 -0800

    feedback

--- src/dotnet/APIView/APIViewWeb/Models/AgentModels.cs
+++ src/dotnet/APIView/APIViewWeb/Models/AgentModels.cs
@@ -8,9 +8,15 @@ public class ApiViewAgentComment
 {
     [JsonPropertyName("lineNo")]
     public int LineNumber { get; set; }
-    [JsonPropertyName("lineId")]
+    /// <summary>
+    /// Preview feature: Line identifier. May change or be removed in future versions.
+    /// </summary>
+    [JsonPropertyName("_lineId")]
     public string LineId { get; set; }
-    [JsonPropertyName("lineText")]
+    /// <summary>
+    /// Preview feature: Line text content. May change or be removed in future versions.
+    /// </summary>
+    [JsonPropertyName("_lineText")]
     public string LineText { get; set; }
     [JsonPropertyName("createdOn")]
     public DateTimeOffset CreatedOn { get; set; }

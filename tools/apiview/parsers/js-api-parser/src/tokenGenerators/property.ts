2412d247e64ef558de15860ffce72d7187143071
KarishmaGhiya <kaghiya@microsoft.com>
2026-01-27 11:04:48 -0800

    fixed usage

--- tools/apiview/parsers/js-api-parser/src/tokenGenerators/property.ts
+++ tools/apiview/parsers/js-api-parser/src/tokenGenerators/property.ts
@@ -1,39 +1,42 @@
 import { ApiItem, ApiItemKind, ApiProperty, ApiPropertyItem } from "@microsoft/api-extractor-model";
 import { ReviewToken, TokenKind } from "../models";
 import { TokenGenerator } from "./index";
+import { createToken, processExcerptTokens } from "./helpers";
 
 function isValid(item: ApiItem): item is ApiPropertyItem {
-    return item.kind === ApiItemKind.Property || item.kind === ApiItemKind.PropertySignature;
+  return item.kind === ApiItemKind.Property || item.kind === ApiItemKind.PropertySignature;
 }
 
 function generate(item: ApiPropertyItem, deprecated?: boolean): ReviewToken[] {
-    const tokens: ReviewToken[] = [];
-    if (item.kind !== ApiItemKind.Property && item.kind !== ApiItemKind.PropertySignature) {
-        throw new Error(`Invalid item ${item.displayName} of kind ${item.kind} for Property token generator.`);
-    }
+  const tokens: ReviewToken[] = [];
+  if (item.kind !== ApiItemKind.Property && item.kind !== ApiItemKind.PropertySignature) {
+    throw new Error(
+      `Invalid item ${item.displayName} of kind ${item.kind} for Property token generator.`,
+    );
+  }
 
-    if (item instanceof ApiProperty && item.isStatic) {
-        tokens.push({ Kind: TokenKind.Keyword, Value: "static", HasSuffixSpace: true, IsDeprecated: deprecated });
-    }
+  if (item instanceof ApiProperty && item.isStatic) {
+    tokens.push(createToken(TokenKind.Keyword, "static", { hasSuffixSpace: true, deprecated }));
+  }
 
-    if (item.isReadonly) {
-        tokens.push({ Kind: TokenKind.Keyword, Value: "readonly", HasSuffixSpace: true, IsDeprecated: deprecated });
-    }
+  if (item.isReadonly) {
+    tokens.push(createToken(TokenKind.Keyword, "readonly", { hasSuffixSpace: true, deprecated }));
+  }
 
-    tokens.push({ Kind: TokenKind.MemberName, Value: item.displayName, IsDeprecated: deprecated, HasSuffixSpace: false });
+  tokens.push(createToken(TokenKind.MemberName, item.displayName, { deprecated }));
 
-    if (item.isOptional) {
-        tokens.push({ Kind: TokenKind.Punctuation, Value: "?", HasSuffixSpace: false, HasPrefixSpace: false, IsDeprecated: deprecated });
-    }
+  if (item.isOptional) {
+    tokens.push(createToken(TokenKind.Punctuation, "?", { deprecated }));
+  }
 
-    tokens.push({ Kind: TokenKind.Punctuation, Value: ":", HasSuffixSpace: true, HasPrefixSpace: false, IsDeprecated: deprecated });
+  tokens.push(createToken(TokenKind.Punctuation, ":", { hasSuffixSpace: true, deprecated }));
 
-    tokens.push({ Kind: TokenKind.TypeName, Value: item.propertyTypeExcerpt.text.trim(), IsDeprecated: deprecated });
+  processExcerptTokens(item.propertyTypeExcerpt.spannedTokens, tokens, deprecated);
 
-    return tokens;
+  return tokens;
 }
 
 export const propertyTokenGenerator: TokenGenerator<ApiPropertyItem> = {
-    isValid,
-    generate,
+  isValid,
+  generate,
 };

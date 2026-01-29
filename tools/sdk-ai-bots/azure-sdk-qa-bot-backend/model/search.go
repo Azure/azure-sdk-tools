113830e6e5a6bf4dabf61e254ff8912e4aa59ae9
Kyle Zhang <skywing918@hotmail.com>
2026-01-29 14:08:38 +0800

    Update tools/sdk-ai-bots/azure-sdk-qa-bot-backend/model/search.go
    Co-authored-by: JiaqiZhang-Dev <jiaqzhang@microsoft.com>

--- tools/sdk-ai-bots/azure-sdk-qa-bot-backend/model/search.go
+++ tools/sdk-ai-bots/azure-sdk-qa-bot-backend/model/search.go
@@ -313,10 +313,10 @@ func GetIndexLink(chunk Index) string {
 		return "https://dev.azure.com/azure-sdk/internal/_wiki/wikis/internal.wiki?wikiVersion=GBwikiMaster&pagePath=/" + wikiPath
 	case Source_AzureRestAPISpecDocs:
 		// Handle documents from azure-rest-api-specs documentation
-		return "https://github.com/Azure/azure-rest-api-specs/blob/main/documentation/" + path
+		return "https://github.com/Azure/azure-rest-api-specs/blob/main/" + path
 	case Source_AzureOpenapiDiffDocs:
 		// Handle documents from openapi-diff/docs
-		return "https://github.com/Azure/openapi-diff/blob/main/docs/" + path
+		return "https://github.com/Azure/openapi-diff/blob/main/" + path
 	default:
 		return ""
 	}

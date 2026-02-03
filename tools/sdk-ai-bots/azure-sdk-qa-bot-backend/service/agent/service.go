9cf7d375fffc9332a9bde28b6a37a5c65c4da489
FAREAST\chunyu <chunyu@microsoft.com>
2026-02-03 11:39:25 +0800

    remove unused code

--- tools/sdk-ai-bots/azure-sdk-qa-bot-backend/service/agent/service.go
+++ tools/sdk-ai-bots/azure-sdk-qa-bot-backend/service/agent/service.go
@@ -597,11 +597,7 @@ func (s *CompletionService) runParallelSearchAndMergeResults(ctx context.Context
 	var agenticChunks []model.Index
 	if withAgenticSearch {
 		agenticRes := <-agenticCh
-		// if agenticRes.err != nil && vectorRes.err != nil {
-		// 	return nil, fmt.Errorf("both agentic and knowledge searches failed: agentic error: %v, knowledge error: %v", agenticRes.err, vectorRes.err)
-		// }
 
-		// var agenticChunks []model.Index
 		if agenticRes.err != nil {
 			log.Printf("Agentic search failed: %v", agenticRes.err)
 			agenticChunks = []model.Index{}

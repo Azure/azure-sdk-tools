1880d0539ed5677ef310c10e198b56b038014b7b
Weidong Xu <weidxu@microsoft.com>
2026-03-03 15:22:39 +0800

    examples-automation, install pnpm for js (#14267)

--- tools/azure-rest-api-specs-examples-automation/js/tools.py
+++ tools/azure-rest-api-specs-examples-automation/js/tools.py
@@ -14,8 +14,16 @@ def check_call(cmd: List[str], work_dir: str):
 
 
 def publish_samples(sdk_path: str, module_relative_path: str):
+    samples_dev_path = os.path.join(sdk_path, module_relative_path, "samples-dev")
+    if not os.path.isdir(samples_dev_path):
+        raise RuntimeError(f"samples-dev folder not found at {samples_dev_path}")
+
+    npm_cmd = "npm" + (".cmd" if OS_WINDOWS else "")
     pnpm_cmd = "pnpm" + (".cmd" if OS_WINDOWS else "")
     npx_cmd = "npx" + (".cmd" if OS_WINDOWS else "")
+
+    cmd = [npm_cmd, "install", "-g", "pnpm"]
+    check_call(cmd, sdk_path)
 
     cmd = [pnpm_cmd, "install"]
     check_call(cmd, sdk_path)

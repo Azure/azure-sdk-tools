76fa2f3ac1d4e6fd6ea6fb953f8362dbc1ee8b78
Mike Harder <mharder@microsoft.com>
2026-03-07 08:37:09 +0000

    unit test calling "tsp-client -v"

--- eng/common-tests/tsp-client/test/tsp-client.test.js
+++ eng/common-tests/tsp-client/test/tsp-client.test.js
@@ -1,7 +1,20 @@
+import { dirname, resolve } from "path";
+import semver from "semver";
+import { fileURLToPath } from "url";
 import { describe, expect, it } from "vitest";
+import { execNpmExec } from "../src/exec.js";
+import { debugLogger } from "../src/logger.js";
 
-describe("hello world", () => {
-  it("should pass", () => {
-    expect("hello world").toBe("hello world");
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const engCommonTspClient = resolve(__dirname, "../../../common/tsp-client");
+
+describe("tsp-client", () => {
+  it("version parses as semver", async () => {
+    const { stdout } = await execNpmExec(["tsp-client", "-v"], {
+      logger: debugLogger,
+      prefix: engCommonTspClient,
+    });
+
+    expect(semver.parse(stdout.trim())).toBeTruthy();
   });
 });

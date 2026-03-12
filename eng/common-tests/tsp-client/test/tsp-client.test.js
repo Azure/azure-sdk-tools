5e71ec500872405b80dea77a6fe424b257c59136
Ray Chen <raychen@microsoft.com>
2026-03-12 13:08:55 -0700

    Added flag to init command test

--- eng/common-tests/tsp-client/test/tsp-client.test.js
+++ eng/common-tests/tsp-client/test/tsp-client.test.js
@@ -150,7 +150,7 @@ describe.concurrent.each([SdkName.Go, SdkName.Java, SdkName.Js, SdkName.Net, Sdk
           "tspconfig.yaml",
         );
 
-        await execTspClient(["init", "-c", localConfig], initLocalWorktree);
+        await execTspClient(["init", "--update-if-exists", "-c", localConfig], initLocalWorktree);
       });
 
       it("updates template", async (ctx) => {

be5ce0b3629203ed974e69a8ae9fff65eb8a0822
Travis Prescott <trpresco@microsoft.com>
2026-05-20 11:25:33 -0700

    Update pipelines.

--- eng/scripts/Apiview-Update-Generated-Review.ps1
+++ eng/scripts/Apiview-Update-Generated-Review.ps1
@@ -2,6 +2,8 @@
 param (
   [Parameter(Mandatory = $true)]
   [string]$BuildId,
+  [Parameter(Mandatory = $true)]
+  [string]$ApiviewResourceId,
   [string]$RepoName = "azure/azure-sdk-tools",
   [string]$ArtifactName = "apiview",
   [string]$MetadataFileName = "",
@@ -15,25 +17,47 @@ param (
 # uploads into APIView. This offline step abstracts and protects APIView server resources so caller doesn't have to
 # know about APIView server. Pipline will publish an artifact 'apiview' before this script is called
 
-# Script will send a request to APIView with build ID, artifact name and repo name as params.
-# Sample request is as follows:
-# https://apistaging.test.dev/review/UpdateApiReview?repoName=azure/azure-sdk-tools&buildId=1742433&artifact=apiview
+# Script will send an authenticated POST request to APIView with build ID, artifact name and repo name.
+# Requires AzurePowerShell@5 context for Get-AzAccessToken.
+# Sample request:
+# POST https://apiview.dev/review/UpdateApiReview
+# Body: { "repoName": "azure/azure-sdk-tools", "buildId": "1742433", "artifactPath": "apiview" }
 
 ####################################################################################################################
 
-$uri = $ApiviewUpdateUrl + "?artifact=" + $ArtifactName + "&buildId=" + $BuildId + "&repoName=" + $repoName
-if ($MetadataFileName) {
-    $encodedMetadataFileName = [Uri]::EscapeDataString($MetadataFileName)
-    $uri = $uri + "&metadataFile=" + $encodedMetadataFileName
+# Acquire Entra ID token for APIView app registration
+$tokenResponse = Get-AzAccessToken -ResourceUrl "api://$ApiviewResourceId"
+$token = $tokenResponse.Token
+if (-not $token) {
+    Write-Error "Failed to acquire access token for APIView (resource: api://$ApiviewResourceId)"
+    exit 1
 }
-Write-Host "Request URI: $($uri)"
+
+$body = @{
+    repoName     = $RepoName
+    artifactPath = $ArtifactName
+    buildId      = $BuildId
+    project      = "internal"
+}
+if ($MetadataFileName) {
+    $body.metadataFile = $MetadataFileName
+}
+
+$headers = @{
+    Authorization  = "Bearer $token"
+    "Content-Type" = "application/json"
+}
+
+$jsonBody = $body | ConvertTo-Json -Compress
+Write-Host "Request URL: $ApiviewUpdateUrl"
+Write-Host "Request body: $jsonBody"
 try
 {
-    $Response = Invoke-WebRequest -Method 'GET' $uri -MaximumRetryCount 3
+    $Response = Invoke-WebRequest -Method 'POST' -Uri $ApiviewUpdateUrl -Headers $headers -Body $jsonBody -MaximumRetryCount 3
     Write-Host "Response status : $($Response.StatusCode)"
 }
 catch
 {
-    Write-Host "Error $StatusCode - Exception details: $($_.Exception.Response)"
+    Write-Host "Error - Exception details: $($_.Exception.Response)"
     exit 1
 }

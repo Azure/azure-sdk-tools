7904f7db4bf2b0576370719032fec2a23dd7fed1
Daniel Jurek <djurek@microsoft.com>
2026-06-01 13:14:15 -0700

    WorkItemBase: Use "" instead of null for empty Date Time (#15798)
    * WorkItemBase: Use "" instead of null for empty Date Time
    
    * string.Empty, tests
    
    * Update changelog

---
+++ tools/azsdk-cli/Azure.Sdk.Tools.Cli.Tests/Models/AzureDevOps/WorkItemBaseTests.cs
@@ -0,0 +1,55 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Azure.Sdk.Tools.Cli.Attributes;
+using Azure.Sdk.Tools.Cli.Models.AzureDevOps;
+
+namespace Azure.Sdk.Tools.Cli.Tests.Models.AzureDevOps;
+
+[TestFixture]
+public class WorkItemBaseTests
+{
+    [Test]
+    public void GetPatchDocument_WithNullableDateTimeValue_RoundTripsDateFieldToJson()
+    {
+        var invalidSince = new DateTime(2026, 05, 29, 22, 55, 44, DateTimeKind.Utc);
+        var workItem = new NullableDateWorkItem
+        {
+            InvalidSince = invalidSince
+        };
+
+        var operation = GetFieldOperation(workItem, "Custom.InvalidSince");
+        var expectedValue = JsonSerializer.Deserialize<string>(JsonSerializer.Serialize(invalidSince))!;
+
+        Assert.That(operation["Value"]?.GetValue<string>(), Is.EqualTo(expectedValue));
+    }
+
+    [Test]
+    public void GetPatchDocument_WithNullNullableDateTime_RoundTripsEmptyStringToJson()
+    {
+        var workItem = new NullableDateWorkItem();
+
+        var operation = GetFieldOperation(workItem, "Custom.InvalidSince");
+
+        Assert.That(operation["Value"]?.GetValue<string>(), Is.EqualTo(string.Empty));
+    }
+
+    private static JsonObject GetFieldOperation(WorkItemBase workItem, string fieldName)
+    {
+        var patchDocument = JsonSerializer.SerializeToNode(workItem.GetPatchDocument()) as JsonArray;
+        Assert.That(patchDocument, Is.Not.Null);
+
+        var operation = patchDocument!
+            .OfType<JsonObject>()
+            .Single(op => op["Path"]?.GetValue<string>() == $"/fields/{fieldName}");
+
+        return operation!;
+    }
+
+    private sealed class NullableDateWorkItem : WorkItemBase
+    {
+        [FieldName("Custom.InvalidSince")]
+        public DateTime? InvalidSince { get; set; }
+    }
+}

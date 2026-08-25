2ddb241c73878b5a83842191c94de5c378597fb2
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2026-08-25 16:48:10 -0700

    Add product onboarding issue template and pipeline

---
+++ eng/scripts/Process-ProductOnboardingIssues.ps1
@@ -0,0 +1,119 @@
+$issues = `
+    gh issue list `
+      --label "product-onboarding" `
+      --state open `
+      --json number`
+  | ConvertFrom-Json `
+  | Select-Object -ExpandProperty number
+
+$issues_length = $issues.Length
+[Console]::Error.WriteLine("Found $issues_length issues to process.")
+
+foreach ($issue_number in $issues) {
+  $issue = gh issue view $issue_number
+  $issue = $issue -join "`n"
+
+  $product_id = ""
+  $product_name = ""
+  $product_type = ""
+  $product_lifecycle = ""
+  $service_id = ""
+  $service_name = ""
+  $needs_sdk_str = ""
+  $data_plane = "N/A"
+  $mgmt_plane = "N/A"
+  $submitter = ""
+
+  if ($issue -match "\#\#\#\sProduct\sID.*\n.*\n(?<ProductID>[^\n]+)") {
+    $product_id = $matches["ProductID"]
+    $product_id = $product_id.Trim()
+  }
+
+  if ($issue -match "\#\#\#\sProduct\sName.*\n.*\n(?<ProductName>[^\n]+)") {
+    $product_name = $matches["ProductName"]
+    $product_name = $product_name.Trim()
+  }
+
+  if ($issue -match "\#\#\#\sProduct\sType.*\n.*\n.*\{\s*`"label`"\s*=>\s*`"\s*(?<ProductType>[^`"]+)") {
+    $product_type = $matches["ProductType"]
+    $product_type = $product_type.Trim()
+  }
+
+  if ($issue -match "\#\#\#\sProduct\sLifecycle.*\n.*\n.*\{\s*`"label`"\s*=>\s*`"\s*(?<ProductLifecycle>[^`"]+)") {
+    $product_lifecycle = $matches["ProductLifecycle"]
+    $product_lifecycle = $product_lifecycle.Trim()
+  }
+
+  if ($issue -match "\#\#\#\sService\sID.*\n.*\n(?<ServiceID>[^\n]+)") {
+    $service_id = $matches["ServiceID"]
+    $service_id = $service_id.Trim()
+  }
+
+  if ($issue -match "\#\#\#\sService\sName.*\n.*\n(?<ServiceName>[^\n]+)") {
+    $service_name = $matches["ServiceName"]
+    $service_name = $service_name.Trim()
+  }
+
+  if ($issue -match "\#\#\#\s.*Azure\sSDK.*\n.*\n.*\{\s*`"label`"\s*=>\s*`"\s*(?<NeedsSDK>[^`"]+)") {
+    $needs_sdk_str = $matches["NeedsSDK"]
+    $needs_sdk_str = $needs_sdk_str.Trim()
+  }
+
+  if ($issue -match "\#\#\#\s.*[Dd]ata\s[Pp]lane.*\n.*\n.*\{\s*`"label`"\s*=>\s*`"\s*(?<DataPlane>[^`"]+)") {
+    $data_plane = $matches["DataPlane"]
+    $data_plane = $data_plane.Trim()
+  }
+
+  if ($issue -match "\#\#\#\s.*[Mm]anagement\s[Pp]lane.*\n.*\n.*\{\s*`"label`"\s*=>\s*`"\s*(?<MgmtPlane>[^`"]+)") {
+    $mgmt_plane = $matches["MgmtPlane"]
+    $mgmt_plane = $mgmt_plane.Trim()
+  }
+
+  if ($issue -match "\#\#\#\sSubmitter.*\n.*\n(?<Submitter>[^\n]+)") {
+    $submitter = $matches["Submitter"]
+    $submitter = $submitter.Trim()
+  }
+
+  if (
+         ($product_id        -ne "") `
+    -and ($product_name      -ne "") `
+    -and ($product_type      -ne "") `
+    -and ($product_lifecycle -ne "") `
+    -and ($service_id        -ne "") `
+    -and ($service_name      -ne "") `
+    -and ($needs_sdk_str     -ne "") `
+    -and ($data_plane        -ne "") `
+    -and ($mgmt_plane        -ne "") `
+    -and ($submitter         -ne "") `
+  ) {
+    $needs_sdk_bool = ($needs_sdk_str -ieq "Yes")
+    [Console]::Error.WriteLine("Processing issue #$issue_number.")
+
+    $sync_success = $false
+    try {
+      ./eng/scripts/Sync-ProductOnboardingStatus.ps1 `
+        -ProductID        "$product_id" `
+        -ProductName      "$product_name" `
+        -ProductType      "$product_type" `
+        -ProductLifecycle "$product_lifecycle" `
+        -ServiceID        "$service_id" `
+        -ServiceName      "$service_name" `
+        -NeedsSDK          $needs_sdk_bool `
+        -DataPlane        "$data_plane" `
+        -MgmtPlane        "$mgmt_plane" `
+        -Submitter        "$submitter"
+
+      $sync_success = $true
+    } catch {
+      [Console]::Error.WriteLine("Error syncing product onboarding status: $_")
+      [Console]::Error.WriteLine($_.Exception.StackTrace)
+    }
+
+    if ($sync_success) {
+      gh issue comment $issue_number --body "Product onboarding status synced successfully."
+      gh issue close $issue_number
+    }
+  } else {
+    [Console]::Error.WriteLine("Error parsing issue #$issue_number.")
+  }
+}
---
+++ eng/scripts/Sync-ProductOnboardingStatus.ps1
@@ -0,0 +1,65 @@
+[CmdletBinding()]
+param(
+   [Parameter(Mandatory=$true)]
+   [string] $ProductID,
+
+   [Parameter(Mandatory=$true)]
+   [string] $ProductName,
+
+   [Parameter(Mandatory=$true)]
+   [string] $ProductType,
+
+   [Parameter(Mandatory=$true)]
+   [string] $ProductLifecycle,
+
+   [Parameter(Mandatory=$true)]
+   [string] $ServiceID,
+
+   [Parameter(Mandatory=$true)]
+   [string] $ServiceName,
+
+   [Parameter(Mandatory=$true)]
+   [bool] $NeedsSDK,
+
+   [Parameter(Mandatory=$true)]
+   [string] $DataPlane,
+
+   [Parameter(Mandatory=$true)]
+   [string] $MgmtPlane,
+
+   [Parameter(Mandatory=$true)]
+   [string] $Submitter
+)
+
+[Console]::Error.WriteLine("Syncing product onboarding status:" `
+   + "`n`tProduct ID:        '$ProductID'" `
+   + "`n`tProduct Name:      '$ProductName'" `
+   + "`n`tProduct Type:      '$ProductType'" `
+   + "`n`tProduct Lifecycle: '$ProductLifecycle'" `
+   + "`n`n" `
+   + "`n`tService ID:        '$ServiceID'" `
+   + "`n`tService Name:      '$ServiceName'" `
+   + "`n`n" `
+   + "`n`tNeeds SDK:         '$NeedsSDK'" `
+   + "`n`tData Plane:        '$DataPlane'" `
+   + "`n`tManagement Plane:  '$MgmtPlane'" `
+   + "`n`n" `
+   + "`n`tSubmitter:         '$Submitter'" `
+   + "`n`n")
+
+$azsdk = "azsdk"
+if ($env:AZSDK) {
+  $azsdk = $env:AZSDK
+}
+
+& "$azsdk" product-onboarding sync --debug `
+  --product-id "$ProductID" `
+  --product-name "$ProductName" `
+  --product-type "$ProductType" `
+  --product-lifecycle "$ProductLifecycle" `
+  --service-id "$ServiceID" `
+  --service-name "$ServiceName" `
+  --needs-sdk $NeedsSDK `
+  --data-plane "$DataPlane" `
+  --management-plane "$MgmtPlane" `
+  --submitter "$Submitter"

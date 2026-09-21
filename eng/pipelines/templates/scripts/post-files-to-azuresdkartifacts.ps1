13cb682befa0878127abd1e8de04b05072bb96b1
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2026-09-21 13:17:01 -0700

    -Identity

--- eng/pipelines/templates/scripts/post-files-to-azuresdkartifacts.ps1
+++ eng/pipelines/templates/scripts/post-files-to-azuresdkartifacts.ps1
@@ -23,7 +23,7 @@ $filesForPublish = Get-ChildItem -Path $SearchPath -Include "$FileFilter"
 #$releaseId = GetReleaseId -ReleaseName $TargetRelease
 $releaseId = "azsdk_0.6.47"
 
-Connect-AzAccount -Subscription "Azure SDK Engineering System"
+Connect-AzAccount -Identity
 $context = New-AzStorageContext -StorageAccountName $StorageAccountName -UseConnectedAccount
 
 foreach ($artifact in $filesForPublish) {
